f2df5788d2f1124ac562f7ee3dd45fe9f4024ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:08 +0000

    Add package qdataclass/1.0.2/qdataclass-1.0.2.tar.gz

---
+++ packages/qdataclass/qdataclass-1.0.2.tar.gz/qdataclass-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+import pathlib
+__version__ = "1.0.2"
+
+setup(
+    name="qdataclass",
+    version=__version__,
+    author="林柏君",
+    author_email="1355767057@qq.com",
+    description="自动生成QProperty的getter setter notify",
+    packages=find_packages()
+)
