4e6c19f2ba091e62308a190b4ca3030d25613c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:03 +0000

    Add package napari-findaureus/0.0.3/napari_findaureus-0.0.3-py3-none-any.whl

---
+++ packages/napari-findaureus/napari_findaureus-0.0.3-py3-none-any.whl/findaureus/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.3"
+
+from ._reader import napari_get_reader
+from ._widget import Find_Bacteria
+from napari_plugin_engine import napari_hook_implementation
+
+def napari_experimental_provide_dock_widget():
+    return Find_Bacteria
+
+__all__ = (
+    "napari_get_reader",
+    "Find_Bacteria"
+)
