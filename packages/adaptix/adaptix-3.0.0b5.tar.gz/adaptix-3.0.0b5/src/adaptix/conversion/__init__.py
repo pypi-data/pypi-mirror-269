91b104956a6d50852520f6853aeb102d2c15cf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:02:58 +0000

    Add package adaptix/3.0.0b5/adaptix-3.0.0b5.tar.gz

---
+++ packages/adaptix/adaptix-3.0.0b5.tar.gz/adaptix-3.0.0b5/src/adaptix/conversion/__init__.py
@@ -0,0 +1,25 @@
+from adaptix._internal.conversion.facade.func import convert, get_converter, impl_converter
+from adaptix._internal.conversion.facade.provider import (
+    allow_unlinked_optional,
+    coercer,
+    forbid_unlinked_optional,
+    from_param,
+    link,
+    link_constant,
+)
+from adaptix._internal.conversion.facade.retort import AdornedConversionRetort, ConversionRetort, FilledConversionRetort
+
+__all__ = (
+    "convert",
+    "get_converter",
+    "impl_converter",
+    "link",
+    "link_constant",
+    "coercer",
+    "allow_unlinked_optional",
+    "forbid_unlinked_optional",
+    "from_param",
+    "AdornedConversionRetort",
+    "FilledConversionRetort",
+    "ConversionRetort",
+)
---
+++ packages/adaptix/adaptix-3.0.0b5.tar.gz/adaptix-3.0.0b5/tests/test_doc.py
@@ -0,0 +1,52 @@
+import importlib
+from pathlib import Path
+
+import pytest
+
+from adaptix._internal.feature_requirement import HAS_PY_311, HAS_SUPPORTED_PYDANTIC_PKG
+
+REPO_ROOT = Path(__file__).parent.parent
+DOCS_EXAMPLES_ROOT = REPO_ROOT / "docs" / "examples"
+EXCLUDE = ["__init__.py"]
+GLOB = "*.py"
+
+
+def pytest_generate_tests(metafunc):
+    if "import_path" not in metafunc.fixturenames:
+        return
+
+    paths_to_test = [
+        sub_path
+        for sub_path in DOCS_EXAMPLES_ROOT.rglob(GLOB)
+        if sub_path.name not in EXCLUDE and not sub_path.is_dir()
+    ]
+    metafunc.parametrize(
+        ["import_path", "case_id"],
+        [
+            pytest.param(
+                ".".join((path_to_test.parent / path_to_test.stem).relative_to(REPO_ROOT).parts),
+                str((path_to_test.parent / path_to_test.stem).relative_to(DOCS_EXAMPLES_ROOT).as_posix()),
+                id=str((path_to_test.parent / path_to_test.stem).relative_to(DOCS_EXAMPLES_ROOT).as_posix()),
+            )
+            for path_to_test in paths_to_test
+        ],
+    )
+
+
+CASES_REQUIREMENTS = {
+    "loading-and-dumping/tutorial/unexpected_error": HAS_PY_311,
+    "reference/integrations/native_pydantic": HAS_SUPPORTED_PYDANTIC_PKG,
+    "loading-and-dumping/extended_usage/private_fields_including_no_rename_pydantic": HAS_SUPPORTED_PYDANTIC_PKG,
+    "loading-and-dumping/extended_usage/private_fields_including_pydantic": HAS_SUPPORTED_PYDANTIC_PKG,
+    "loading-and-dumping/extended_usage/private_fields_skipping_pydantic": HAS_SUPPORTED_PYDANTIC_PKG,
+}
+
+
+def test_example(import_path: str, case_id: str):
+    if case_id in CASES_REQUIREMENTS:
+        requirement = CASES_REQUIREMENTS[case_id]
+        if not requirement:
+            pytest.skip(requirement.fail_reason)
+
+    pytest.register_assert_rewrite(import_path)
+    importlib.import_module(import_path)
