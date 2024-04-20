89618f272406235b07a83bdae4beef030a864cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:09 +0000

    Add package aryan-basic-math-operations/1.0.1/aryan_basic_math_operations-1.0.1.tar.gz

---
+++ packages/aryan-basic-math-operations/aryan_basic_math_operations-1.0.1.tar.gz/aryan_basic_math_operations-1.0.1/basic_math_operations/operations.py
@@ -0,0 +1,23 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def pow(x, y):
+    return x ** y
+
+def intDivide(x, y):
+    return x // y
+
+def leftShift(x, y):
+    return x << y
+
+def rightShift(x, y):
+    return x >> y
---
+++ packages/aryan-basic-math-operations/aryan_basic_math_operations-1.0.1.tar.gz/aryan_basic_math_operations-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aryan-basic_math_operations',
+    version='1.0.1',
+    packages=find_packages(),
+    description='A Python package for basic mathematical operations',
+    url='https://github.com/AryanAAB/Summer-School-on-Embedded-Systems-and-Robotics',
+    author='Aryan',
+    author_email='aryanbansal310@gmail.com',
+    license='MIT',
+)
