2fc62d625d73b45e119d5a4bd828a61dba13ce19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:02:56 +0000

    Add package ldjcourse/0.0.3/ldjcourse-0.0.3.tar.gz

---
+++ packages/ldjcourse/ldjcourse-0.0.3.tar.gz/ldjcourse-0.0.3/ldjcourse/pandas/v1_1/api.py
@@ -0,0 +1,12 @@
+from . import data as _data
+
+
+def get_data(data_name, *args, **kwargs):
+    obj = getattr(_data, data_name)
+    result = obj(*args, **kwargs)
+    return result
+
+
+def print_txt(txt1, txt2='*', num=30):
+    print('\n{}'.format(txt2) * num)
+    print('{}:'.format(txt1))
