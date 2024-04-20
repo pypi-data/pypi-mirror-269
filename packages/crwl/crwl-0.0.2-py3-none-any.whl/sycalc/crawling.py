e3b596c64d85415cd88d3507462bbb340f1e458a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:02:56 +0000

    Add package crwl/0.0.2/crwl-0.0.2-py3-none-any.whl

---
+++ packages/crwl/crwl-0.0.2-py3-none-any.whl/sycalc/crawling.py
@@ -0,0 +1,25 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+def search_news(keyword):
+    base_url = "https://search.naver.com/search.naver?ssc=tab.news.all&where=news&sm=tab_jum&query="
+    url = base_url + keyword
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 기사 제목, 링크 가져오기
+        news_titles = soup.find_all('a', {'class': 'news_tit'})
+
+        print("검색된 뉴스 url:")
+        for title in news_titles:
+            news_title = title.get_text()
+            news_link = title['href']
+            print(news_title, ":", news_link)
+
+    else:
+        print("웹페이지 접속 오류")
+        return None
