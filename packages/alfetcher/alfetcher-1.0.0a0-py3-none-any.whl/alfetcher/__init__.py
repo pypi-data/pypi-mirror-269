36fc46801c1bf881e004d45e89b42b3d4b8914ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:02:59 +0000

    Add package alfetcher/1.0.0a0/alfetcher-1.0.0a0-py3-none-any.whl

---
+++ packages/alfetcher/alfetcher-1.0.0a0-py3-none-any.whl/alfetcher/__init__.py
@@ -0,0 +1,3 @@
+from .al_fetcher import check_status_in_cache, get_userdata, reset_user_cache, clear_cache, get_latest_anime_entry_for_user, get_all_anime_for_user, get_anime_entry_for_user, get_anime_info, get_id
+from .utils import utils_read_json, utils_save_json
+from .al_config_utils import config_setup
