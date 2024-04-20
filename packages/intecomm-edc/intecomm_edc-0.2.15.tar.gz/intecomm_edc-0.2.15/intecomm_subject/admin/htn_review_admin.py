1346d2543df04fb3c019a73c0451dd7e98a2b14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:03:12 +0000

    Add package intecomm-edc/0.2.15/intecomm_edc-0.2.15.tar.gz

---
+++ packages/intecomm-edc/intecomm_edc-0.2.15.tar.gz/intecomm_edc-0.2.15/intecomm_subject/admin/htn_review_admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django_audit_fields.admin import audit_fieldset_tuple
+from edc_crf.admin import crf_status_fieldset_tuple
+
+from ..admin_site import intecomm_subject_admin
+from ..forms import HtnReviewForm
+from ..models import HtnReview
+from .modeladmin_mixins import CrfModelAdmin
+
+
+@admin.register(HtnReview, site=intecomm_subject_admin)
+class HtnReviewAdmin(CrfModelAdmin):
+    form = HtnReviewForm
+
+    fieldsets = (
+        (None, {"fields": ("subject_visit", "report_datetime")}),
+        ("Care", {"fields": ("managed_by", "care_delivery", "care_delivery_other")}),
+        crf_status_fieldset_tuple,
+        audit_fieldset_tuple,
+    )
+
+    radio_fields = {
+        "dx": admin.VERTICAL,
+        "care_delivery": admin.VERTICAL,
+        "crf_status": admin.VERTICAL,
+        "managed_by": admin.VERTICAL,
+    }
