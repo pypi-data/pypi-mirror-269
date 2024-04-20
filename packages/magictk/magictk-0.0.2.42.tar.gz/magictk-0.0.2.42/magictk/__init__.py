396fc491f83cb46dbbf5e1ae498213dd2fd4b0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:03 +0000

    Add package magictk/0.0.2.42/magictk-0.0.2.42.tar.gz

---
+++ packages/magictk/magictk-0.0.2.42.tar.gz/magictk-0.0.2.42/magictk/__init__.py
@@ -0,0 +1,9 @@
+from magictk.window import Window
+from magictk.basicwindow import Tk
+from magictk.button import Button, ButtonFill
+from magictk.progressbar import ProgressBar
+from magictk.checkbox import Checkbox, RadioGroup
+from magictk.submenu import Menu, MenuObjs
+from magictk.select import Select
+from magictk.frame import Frame
+from magictk.entry import Entry
