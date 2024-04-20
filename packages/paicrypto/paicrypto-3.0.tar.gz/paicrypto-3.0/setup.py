40a6cce0384863a0a8b8004defed69c4a7911700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:01 +0000

    Add package paicrypto/3.0/paicrypto-3.0.tar.gz

---
+++ packages/paicrypto/paicrypto-3.0.tar.gz/paicrypto-3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paicrypto',
+    version='3.0',
+    description='A collection of algorithms',
+    author='Adithya Pai B',
+    author_email='paiadithya26@gmail.com',
+    packages= find_packages(),
+    install_requires=[
+        'pycrypto',
+        'pycryptodome',
+        # Add any dependencies your project requires
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
