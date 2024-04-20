fc8e26fc0bdc7783e5fa32e9c59f658163d48fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:00 +0000

    Add package newdao-hello-tea/1.0.0/newdao-hello-tea-1.0.0.tar.gz

---
+++ packages/newdao-hello-tea/newdao-hello-tea-1.0.0.tar.gz/newdao-hello-tea-1.0.0/README.md
@@ -0,0 +1,4 @@
+# Hello Tea
+
+This is a Python package that prints "hello tea" using NumPy.
+
---
+++ packages/newdao-hello-tea/newdao-hello-tea-1.0.0.tar.gz/newdao-hello-tea-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newdao-hello-tea',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    entry_points={
+        'console_scripts': [
+            'newdao-hello-tea = newdao_hello_tea.main:print_hello_tea'
+        ]
+    },
+    author='Newdao',
+    author_email='l0dx16@gmail.com',
+    description='Prints "hello tea" using NumPy',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/newdao-hello-tea',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
