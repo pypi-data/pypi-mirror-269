58f2c423574008cfc91eb7f9438429d0d773d61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:02:41 +0000

    Add package numerical-pde-solver/0.1/numerical_pde_solver-0.1.tar.gz

---
+++ packages/numerical-pde-solver/numerical_pde_solver-0.1.tar.gz/numerical_pde_solver-0.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py example for setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name="numerical_pde_solver",
+    version="0.1",
+    author="Amit Kumar Jha",
+    author_email="jha.8@alumni.iitj.ac.in",
+    description="A package for solving PDEs using Euler and Milstein methods",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",  # ensure all dependencies are listed here
+    ],
+)
