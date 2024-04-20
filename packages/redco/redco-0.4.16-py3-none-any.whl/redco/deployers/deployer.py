ffae604ffb8c262306d0b3ad3c2c3d8589c535a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:03:25 +0000

    Add package redco/0.4.16/redco-0.4.16-py3-none-any.whl

---
+++ packages/redco/redco-0.4.16-py3-none-any.whl/redco/deployers/deployer.py
@@ -0,0 +1,314 @@
+#  Copyright 2021 Google LLC
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
+import jax
+import jax.numpy as jnp
+from flax.jax_utils import replicate, unreplicate
+from flax.training.common_utils import shard_prng_key
+
+from .data_utils import get_host_examples, get_data_batches
+from .opt_utils import get_lr_schedule_fn
+from .log_utils import get_logger, log_info, save_outputs
+from .model_parallel_utils.mesh_utils import (
+    get_mesh,
+    shard_params,
+    get_param_spec,
+    get_opt_state_spec,
+    get_sharding_rules)
+from .ckpt_utils import (
+    save_params,
+    save_opt_state,
+    load_params,
+    load_opt_state,
+    save_rng,
+    load_rng)
+
+
+class Deployer:
+    def __init__(self,
+                 jax_seed,
+                 n_model_shards=1,
+                 verbose=True,
+                 workdir=None,
+                 n_processes=None,
+                 host0_address=None,
+                 host0_port=None,
+                 process_id=None,
+                 n_local_devices=None,
+                 run_tensorboard=False,
+                 run_wandb=False):
+        if n_processes is None:
+            if 'SLURM_JOB_NUM_NODES' in os.environ:
+                n_processes = int(os.environ['SLURM_JOB_NUM_NODES'])
+                process_id = int(os.environ['SLURM_NODEID'])
+            else:
+                n_processes = 1
+
+        if n_processes > 1:
+            local_device_ids = None if n_local_devices is None \
+                else list(range(n_local_devices))
+
+            if host0_port is None:
+                host0_port = 11111
+
+            jax.distributed.initialize(
+                coordinator_address=f'{host0_address}:{host0_port}',
+                num_processes=n_processes,
+                process_id=process_id,
+                local_device_ids=local_device_ids)
+
+            print(f'process_id: {jax.process_index()} / {jax.process_count()}')
+            print(f'devices: {jax.local_device_count()} / {jax.device_count()}')
+
+        if workdir is not None:
+            os.makedirs(workdir, exist_ok=True)
+
+        self._verbose = verbose
+        self._workdir = workdir
+        self._logger = get_logger(verbose=verbose, workdir=workdir)
+
+        if run_wandb and jax.process_index() == 0:
+            import wandb
+            self._wandb_log_fn = wandb.log
+        else:
+            self._wandb_log_fn = None
+
+        if run_tensorboard and jax.process_index() == 0:
+            from flax.metrics import tensorboard
+            self._summary_writer = tensorboard.SummaryWriter(workdir)
+        else:
+            self._summary_writer = None
+
+        self._rng = jax.random.PRNGKey(seed=jax_seed)
+        self._mesh = get_mesh(n_model_shards=n_model_shards)
+
+    def process_batch_size(self, per_device_batch_size):
+        if self._mesh is None:
+            batch_size = per_device_batch_size * jax.local_device_count()
+            global_batch_size = batch_size * jax.process_count()
+        else:
+            batch_size = per_device_batch_size * self._mesh.shape['dp']
+            global_batch_size = batch_size
+
+        return batch_size, global_batch_size
+
+    def get_model_input_batches(self,
+                                examples,
+                                per_device_batch_size,
+                                collate_fn,
+                                shuffle,
+                                shuffle_rng,
+                                desc,
+                                is_train=False,
+                                accumulate_grad_batches=None):
+        batch_size, global_batch_size = self.process_batch_size(
+            per_device_batch_size=per_device_batch_size)
+
+        examples = get_host_examples(
+            examples=examples,
+            global_batch_size=global_batch_size,
+            shuffle=shuffle,
+            shuffle_rng=shuffle_rng,
+            mesh=self._mesh)
+
+        if not is_train:
+            desc = f'{desc} (global_batch_size = {global_batch_size})'
+        elif accumulate_grad_batches is None:
+            desc = f'{desc} (global_micro_batch_size = {global_batch_size})'
+        else:
+            desc = (f'{desc} ('
+                    f'global_micro_batch_size = {global_batch_size}, '
+                    f'accumulate_grad_batches = {accumulate_grad_batches})')
+
+        return get_data_batches(
+            examples=examples,
+            batch_size=batch_size,
+            collate_fn=collate_fn,
+            do_shard=(self.mesh is None),
+            desc=desc,
+            verbose=self._verbose)
+
+    def process_batch_preds(self, batch_preds_with_idxes):
+        if self._mesh is None:
+            batch_preds_with_idxes = jax.tree_util.tree_map(
+                lambda x: x[0], batch_preds_with_idxes)
+
+            batch_preds = batch_preds_with_idxes['raw_preds']
+            idxes = batch_preds_with_idxes['__idx__']
+
+            assert jax.tree_util.tree_all(jax.tree_util.tree_map(
+                lambda t: t.shape[0] * t.shape[1] == idxes.size, batch_preds))
+
+            preds = jax.tree_util.tree_map(
+                lambda t: t.reshape((t.shape[0] * t.shape[1],) + t.shape[2:]),
+                batch_preds)
+            idxes = idxes.reshape(-1)
+            idxes_argsort = jnp.argsort(idxes, axis=None)
+
+            return jax.tree_util.tree_map(lambda t: t[idxes_argsort], preds)
+        else:
+            return batch_preds_with_idxes['raw_preds']
+
+    def process_to_run_model(self, x, is_prng_key=False):
+        if self._mesh is None:
+            if is_prng_key:
+                return shard_prng_key(x)
+            else:
+                return replicate(x)
+        else:
+            return x
+
+    def process_to_deliver(self, x):
+        if self._mesh is None:
+            return unreplicate(x)
+        else:
+            return x
+
+    def get_lr_schedule_fn(self,
+                           train_size,
+                           per_device_batch_size,
+                           n_epochs,
+                           learning_rate,
+                           schedule_type='linear',
+                           warmup_rate=0.,
+                           warmup_steps=None,
+                           init_learning_rate=0.,
+                           end_learning_rate=0.):
+        _, global_batch_size = self.process_batch_size(
+            per_device_batch_size=per_device_batch_size)
+        total_train_steps = n_epochs * (train_size // global_batch_size)
+
+        if warmup_steps is None:
+            warmup_steps = int(total_train_steps * warmup_rate)
+
+        return get_lr_schedule_fn(
+            schedule_type=schedule_type,
+            total_train_steps=total_train_steps,
+            warmup_steps=warmup_steps,
+            init_learning_rate=init_learning_rate,
+            learning_rate=learning_rate,
+            end_learning_rate=end_learning_rate)
+
+    def get_sharding_rules(self, params):
+        if self._mesh is None:
+            return None
+        else:
+            sharding_rules = get_sharding_rules(
+                params=params, n_model_shards=self._mesh.shape['mp'])
+
+            self.log_info(
+                info='\n'.join([f'{t}' for t in sharding_rules]),
+                title='Sharding rules')
+
+            return sharding_rules
+
+    def get_params_spec(self, params, params_sharding_rules):
+        return get_param_spec(
+            params=params, params_sharding_rules=params_sharding_rules)
+
+    def get_opt_state_spec(self, params, params_spec, optimizer):
+        return get_opt_state_spec(
+            params=params, params_spec=params_spec, optimizer=optimizer)
+
+    def shard_params(self, params, params_spec):
+        return shard_params(
+            params=params, params_spec=params_spec, mesh=self._mesh)
+
+    def run_model_step(self, step_fn, input_args):
+        if self._mesh is None:
+            return step_fn(*input_args)
+        else:
+            with self._mesh:
+                return step_fn(*input_args)
+
+    def gen_rng(self):
+        self._rng, new_rng = jax.random.split(self._rng)
+        return new_rng
+
+    def log_info(self, info, title=None, step=None):
+        if jax.process_index() == 0:
+            log_info(
+                info=info,
+                title=title,
+                logger=self._logger,
+                summary_writer=self._summary_writer,
+                step=step)
+
+    def log_metrics(self, metrics, step):
+        if self._summary_writer is not None:
+            for metric_name, value in metrics.items():
+                self._summary_writer.scalar(metric_name, value, step=step)
+
+        if self._wandb_log_fn is not None:
+            self._wandb_log_fn(metrics, step)
+
+    def save_outputs(self, outputs, desc, step):
+        if self._workdir is not None and jax.process_index() == 0:
+            save_outputs(
+                workdir=self._workdir,
+                outputs=outputs,
+                desc=desc,
+                step=step,
+                logger=self._logger,
+                summary_writer=self._summary_writer)
+
+    def load_params(self, ckpt_dir):
+        self.log_info(f'loading params from {ckpt_dir} ...')
+        params = load_params(ckpt_dir=ckpt_dir)
+        self.log_info(f'params loaded from {ckpt_dir}')
+        return params
+
+    def load_opt_state(self, ckpt_dir, params, optimizer):
+        self.log_info(f'loading opt_state from {ckpt_dir} ...')
+        opt_state = load_opt_state(
+            ckpt_dir=ckpt_dir, params=params, optimizer=optimizer)
+        if opt_state is None:
+            self.log_info(f'opt_state not found in {ckpt_dir}. skipped.')
+        else:
+            self.log_info(f'opt_state loaded from {ckpt_dir}')
+        return opt_state
+
+    def save_params(self, params, ckpt_dir, max_shard_size):
+        self.log_info(f'saving params into {ckpt_dir} ...')
+        save_params(
+            mesh=self._mesh,
+            params=params,
+            ckpt_dir=ckpt_dir,
+            max_shard_size=max_shard_size)
+        self.log_info(f'params saved into {ckpt_dir}')
+
+    def save_opt_state(self, opt_state, ckpt_dir, max_shard_size):
+        self.log_info(f'saving opt_state into {ckpt_dir} ...')
+        save_opt_state(
+            mesh=self._mesh,
+            opt_state=opt_state,
+            ckpt_dir=ckpt_dir,
+            max_shard_size=max_shard_size)
+        self.log_info(f'opt_state saved into {ckpt_dir}')
+
+    def save_rng(self, ckpt_dir):
+        save_rng(rng=self._rng, ckpt_dir=ckpt_dir)
+        self.log_info(f'rng saved into {ckpt_dir}')
+
+    def load_rng(self, ckpt_dir):
+        self._rng = load_rng(ckpt_dir=ckpt_dir)
+        self.log_info(f'rng updated by {ckpt_dir}.')
+
+    @property
+    def mesh(self):
+        return self._mesh
+
+    @property
+    def workdir(self):
+        return self._workdir
---
+++ packages/redco/redco-0.4.16-py3-none-any.whl/redco/deployers/model_parallel_utils/mesh_utils.py
@@ -0,0 +1,149 @@
+#  Copyright 2021 Google LLC
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import numpy as np
+import jax
+from jax.experimental.pjit import pjit
+from jax.sharding import Mesh
+from jax.sharding import PartitionSpec as P
+from flax.core.frozen_dict import FrozenDict, unfreeze
+from flax.traverse_util import flatten_dict
+import optax
+
+from .partition_utils import set_partitions
+
+
+def get_mesh(n_model_shards):
+    if n_model_shards == 1:
+        return None
+
+    assert jax.device_count() % n_model_shards == 0
+
+    mesh_devices = np.array(jax.devices()).reshape(
+        jax.device_count() // n_model_shards, n_model_shards)
+    mesh = Mesh(mesh_devices, ('dp', 'mp'))
+
+    return mesh
+
+
+def get_param_spec(params, params_sharding_rules):
+    return set_partitions(unfreeze(params), params_sharding_rules)
+
+
+def shard_params(params, params_spec, mesh):
+    shard_fn = pjit(
+        lambda x: x, in_shardings=(params_spec,), out_shardings=params_spec)
+
+    with mesh:
+        return shard_fn(params)
+
+
+def get_opt_state_spec(params, params_spec, optimizer):
+    def init_fn(params_):
+        return optimizer.init(params_)
+
+    def get_opt_spec(x):
+        if isinstance(x, (dict, FrozenDict,)):
+            return params_spec
+        return None
+
+    params_shapes = jax.tree_util.tree_map(
+        lambda x: jax.ShapeDtypeStruct(x.shape, x.dtype), params)
+
+    return jax.tree_util.tree_map(
+        get_opt_spec, jax.eval_shape(init_fn, params_shapes),
+        is_leaf=lambda x: isinstance(x, (dict, FrozenDict, optax.EmptyState,)))
+
+
+def get_sharding_rules(params, n_model_shards):
+    def get_valid_mp_dims(param):
+        result = []
+        for i, dim in enumerate(param.shape):
+            if dim % n_model_shards == 0:
+                result.append(i)
+        result.reverse()
+        return result
+
+    def inside_attention(flat_key):
+        for k in flat_key:
+            if 'attention' in k.lower() or 'attn' in k.lower():
+                return True
+        return False
+
+    sharding_rules = {}
+    flat_params = flatten_dict(params)
+    last_shard_dim = None
+    for flat_key in sorted(flat_params.keys(), key=lambda t: (len(t), t[-2:])):
+        rule_key, param = flat_key[-2:], flat_params[flat_key]
+
+        if rule_key[-1] not in ['kernel', 'embedding']:
+            sharding_rules[flat_key[-1:]] = None
+            continue
+
+        if rule_key[-1] == 'embedding':
+            rule_key = rule_key[-1:]
+
+        if rule_key in sharding_rules:
+            if sharding_rules[rule_key] is None:
+                continue
+            else:
+                for dim_size, rule_str in zip(
+                        param.shape, sharding_rules[rule_key]):
+                    assert rule_str != 'mp' or dim_size % n_model_shards == 0
+        elif len(param.shape) != 2:
+            sharding_rules[rule_key] = None
+        else:
+            valid_mp_dims = get_valid_mp_dims(param)
+            is_special = False
+
+            if rule_key[-1] == 'embedding':
+                is_special = True
+                valid_mp_dims.reverse()
+            elif inside_attention(flat_key) and (
+                    rule_key[0][0] in ['q', 'k', 'v'] or \
+                    rule_key[0][-1] in ['q', 'k', 'v']):
+                is_special = True
+            elif inside_attention(flat_key) and (
+                    rule_key[0][0] == 'o' or rule_key[0][-1] == 'o'):
+                is_special = True
+                valid_mp_dims.reverse()
+            elif flat_key[-2].startswith('up') or \
+                    flat_key[-2].startswith('gate') or \
+                    flat_key[-2].startswith('wi'):
+                is_special = True
+            elif flat_key[-2].startswith('down') or \
+                    flat_key[-2].startswith('wo'):
+                is_special = True
+                valid_mp_dims.reverse()
+            elif flat_key[-2].startswith('head') or \
+                    flat_key[-2].endswith('head'):
+                is_special = True
+
+            if len(valid_mp_dims) > 0:
+                shard_dim = valid_mp_dims[0]
+                if is_special == False and \
+                        shard_dim == last_shard_dim and len(valid_mp_dims) > 1:
+                    shard_dim = valid_mp_dims[1]
+            else:
+                shard_dim = None
+
+            rule_tuple = [None] * len(param.shape)
+            if shard_dim is not None:
+                rule_tuple[shard_dim] = 'mp'
+            sharding_rules[rule_key] = P(*rule_tuple)
+
+            if not is_special:
+                last_shard_dim = shard_dim
+
+    return list(sharding_rules.items())
