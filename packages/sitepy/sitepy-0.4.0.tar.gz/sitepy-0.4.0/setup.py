bbea11a64bda67089ac8302cb531be495bc16432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:03:10 +0000

    Add package sitepy/0.4.0/sitepy-0.4.0.tar.gz

---
+++ packages/sitepy/sitepy-0.4.0.tar.gz/sitepy-0.4.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sitepy',
+    version='0.4.0',
+    description='A simple web framework.',
+    author='WolfTheDev',
+    author_email='wolfthedev@gmail.com',
+    url='https://github.com/WolfTheDeveloper/sitepy',
+    license='Apache 2.0',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # Add this line
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/sitepy/sitepy-0.4.0.tar.gz/sitepy-0.4.0/sitepy/__init__.py
@@ -0,0 +1,6 @@
+from .core import *
+from . import gpt
+from . import recaptcha
+from . import profanity
+from . import db
+from . import log
