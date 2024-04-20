35ee44f4e3b59bfb5adc7c26ecaeb060acc73295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:03:27 +0000

    Add package mitmproxy-rs/0.5.2/mitmproxy_rs-0.5.2.tar.gz

---
+++ packages/mitmproxy-rs/mitmproxy_rs-0.5.2.tar.gz/mitmproxy_rs-0.5.2/src/lib.rs
@@ -0,0 +1,14 @@
+pub use network::MAX_PACKET_SIZE;
+
+pub mod intercept_conf;
+pub mod ipc;
+#[cfg(target_os = "macos")]
+pub mod macos;
+pub mod messages;
+pub mod network;
+pub mod packet_sources;
+pub mod processes;
+pub mod shutdown;
+pub mod util;
+#[cfg(windows)]
+pub mod windows;
---
+++ packages/mitmproxy-rs/mitmproxy_rs-0.5.2.tar.gz/mitmproxy_rs-0.5.2/src/packet_sources/mod.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use tokio::sync::{broadcast, mpsc};
+
+use crate::messages::{TransportCommand, TransportEvent};
+
+#[cfg(target_os = "macos")]
+pub mod macos;
+pub mod udp;
+#[cfg(windows)]
+pub mod windows;
+pub mod wireguard;
+
+#[async_trait]
+pub trait PacketSourceConf {
+    type Task: PacketSourceTask + Send + 'static;
+    type Data: Send + 'static;
+
+    fn name(&self) -> &'static str;
+
+    async fn build(
+        self,
+        transport_events_tx: mpsc::Sender<TransportEvent>,
+        transport_commands_rx: mpsc::UnboundedReceiver<TransportCommand>,
+        shutdown: broadcast::Receiver<()>,
+    ) -> Result<(Self::Task, Self::Data)>;
+}
+
+#[async_trait]
+pub trait PacketSourceTask {
+    async fn run(mut self) -> Result<()>;
+}
