03ecc8ce2b19d3d4a709c13737214780770c48c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:02:51 +0000

    Add package flechemano/0.3/flechemano-0.3.tar.gz

---
+++ packages/flechemano/flechemano-0.3.tar.gz/flechemano-0.3/README.md
@@ -0,0 +1,36 @@
+
+```
+# flechemano
+
+flechemano is a Python package developed for blacktea024 to integrate it with PyPI.
+
+## Installation
+
+You can install flechemano using pip:
+
+```
+pip install flechemano
+```
+
+## Usage
+
+```python
+import oktay1024
+
+# Example usage here
+```
+
+## Dependencies
+
+flechemano depends on the following:
+- blacktea024
+
+## Author
+
+flechemano
+
+## License
+
+This project is licensed under the MIT License.
+```
+
---
+++ packages/flechemano/flechemano-0.3.tar.gz/flechemano-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flechemano',
+    version='0.3',
+    description='A package developed for blacktea024 to integrate it with PyPI.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='flechemano',
+    url='https://github.com/flechemano/flechemano',
+    packages=find_packages(),
+    install_requires=[
+        'blacktea024',
+    ],
+    python_requires='>=3.6',
+)
+
