80f7fbdf7989cdab4c599cb19d9ed000896f28b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:08 +0000

    Add package sssm/0.0.1/sssm-0.0.1.tar.gz

---
+++ packages/sssm/sssm-0.0.1.tar.gz/sssm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sssm',
+    version='0.0.1',
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'importlib-metadata',
+    ],
+)
