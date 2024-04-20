cb88dd021c590f91d8ad05912661b47a98b105bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:03:01 +0000

    Add package PBasicMaths/0.2/PBasicMaths-0.2.tar.gz

---
+++ packages/PBasicMaths/PBasicMaths-0.2.tar.gz/PBasicMaths-0.2/PrakBasicMaths/operations.py
@@ -0,0 +1,14 @@
+# PrakBasicMaths/operations.py
+
+def addition(x, y):
+    """Addition"""
+    return x + y
+
+def subtraction(x, y):
+    """Subtraction"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication"""
+    return x * y
+
---
+++ packages/PBasicMaths/PBasicMaths-0.2.tar.gz/PBasicMaths-0.2/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PBasicMaths',
+    version='0.2',
+    packages=find_packages(),
+    description='A Python package for basic mathematical operations',
+    author='Prakrititz Borah',
+    author_email='Prakrititz.Borah@iiitb.ac.in',
+    url='https://github.com/SweetBunny123/PrakBasicMaths',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
