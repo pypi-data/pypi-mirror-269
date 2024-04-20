71b3ac79f6bf7d2b53177950a6f07de00753da17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:02:34 +0000

    Add package ypricemagic/3.4.11/ypricemagic-3.4.11.tar.gz

---
+++ packages/ypricemagic/ypricemagic-3.4.11.tar.gz/ypricemagic-3.4.11/y/prices/tokenized_fund/basketdao.py
@@ -0,0 +1,35 @@
+
+import asyncio
+from decimal import Decimal
+from typing import Optional
+
+import a_sync
+from brownie.convert.datatypes import EthAddress
+from multicall import Call
+
+from y.classes.common import ERC20, WeiBalance
+from y.datatypes import Block, UsdPrice
+
+
+@a_sync.a_sync(default='sync')
+async def is_basketdao_index(address: EthAddress) -> bool:
+    try:
+        return await Call(address, 'getAssetsAndBalances()(address[],uint[])')
+    except ValueError:
+        return False
+
+@a_sync.a_sync(default='sync')
+async def get_price(address: EthAddress, block: Optional[Block] = None) -> UsdPrice:
+    balances, total_supply = await asyncio.gather(
+        Call(address, 'getAssetsAndBalances()(address[],uint[])',block_id=block),
+        ERC20(address, asynchronous=True).total_supply_readable(block=block),
+    )
+
+    balances = [
+        WeiBalance(balance, token, block)
+        for token, balance
+        in zip(balances[0],balances[1])
+    ]
+
+    tvl = sum(await asyncio.gather(*[bal.__value_usd__ for bal in balances]))
+    return UsdPrice(tvl / Decimal(total_supply))
