0ef3968d25a19a90dfda14f498482a16d1e0ad89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:03:07 +0000

    Add package gtsystem/0.1.5/gtsystem-0.1.5.tar.gz

---
+++ packages/gtsystem/gtsystem-0.1.5.tar.gz/gtsystem-0.1.5/gtsystem/benchmark.py
@@ -0,0 +1,17 @@
+from .bedrock import sonnet_text
+
+def accuracy(prompt, system='', result=''):
+    system = """You are evaluating the accuracy of an LLM response. 
+    You will be given a system prompt and/or a user prompt, and response from an LLM and 
+    you will evaluate the response accuracy as a percentage to second decimal relative to 
+    your own response to the same system and/or user prompt. You will only respond with 
+    the response accuracy number. If you are not able to evaluate 
+    the accuracy, you will respond with 'Cannot Evaluate Accuracy'."""
+    prompt = f"""Evaluate the accuracy of the following LLM response based on the 
+    given System and/or User prompt:
+    System: {system} 
+    User: {prompt}
+    Response: {result}"""
+    
+    accuracy = sonnet_text(prompt=prompt, system=system)
+    return accuracy
