863b05b11041dfe3e5bff454b0b71c354d66e1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:02:32 +0000

    Add package pytest-ignore-flaky/2.2.1/pytest_ignore_flaky-2.2.1.tar.gz

---
+++ packages/pytest-ignore-flaky/pytest_ignore_flaky-2.2.1.tar.gz/pytest_ignore_flaky-2.2.1/NEWS.rst
@@ -0,0 +1,42 @@
+v2.2.1
+======
+
+Bugfixes
+--------
+
+- Fix typo in plugin name. (#13)
+
+
+2.2.0
+=====
+
+ - Require Python 3.8.
+
+2.1.0
+=====
+
+ - Updated to use ``pytest.hookimpl`` fixing DeprecationWarning (#4).
+
+2.0.0
+=====
+
+ - register "flaky" mark
+ - Drop support for python 2.7, 3.4, 3.5.
+ - Add support for python 3.8, 3.9.
+ - Support pytest > 6.0
+
+
+1.0.0
+=====
+
+- replace function removed by pytest version 4.1.0
+
+0.1.1
+=====
+
+- fix installation using `setup.py`
+
+0.1.0
+=====
+
+- initial release
---
+++ packages/pytest-ignore-flaky/pytest_ignore_flaky-2.2.1.tar.gz/pytest_ignore_flaky-2.2.1/tests/test_functional.py
@@ -0,0 +1,60 @@
+import sys
+
+import pytest
+
+
+if sys.version_info < (3, 12):
+    from importlib_resources import files
+else:
+    from importlib.resources import files
+
+
+pytest_plugins = ('pytester',)
+
+
+@pytest.fixture(autouse=True)
+def testdir(testdir):
+    # disable enabler so tests run without --ignore-flaky
+    testdir.makepyprojecttoml('[tool.pytest-enabler.ignore_flaky]')
+    return testdir
+
+
+def get_results(recorder):
+    '''filter records to get only call results'''
+    results = {}
+    for result in recorder.getreports():
+        when = getattr(result, 'when', None)
+        if when is None:
+            continue
+        test_name = result.nodeid.split('::')[-1]
+        results[test_name, when] = result.outcome
+    return results
+
+
+TEST_SAMPLE = files().joinpath('sample.py').read_text(encoding='utf-8')
+
+
+def test_ignore_flaky(testdir, capsys):
+    test = testdir.makepyfile(TEST_SAMPLE)
+    rec = testdir.inline_run('--ignore-flaky', test)
+    results = get_results(rec)
+    assert results['test_ok', 'call'] == 'passed'
+    assert results['test_mf', 'call'] == 'skipped'
+
+
+def test_fail_flaky(testdir, capsys):
+    test = testdir.makepyfile(TEST_SAMPLE)
+    rec = testdir.inline_run(test)
+    results = get_results(rec)
+    assert results['test_ok', 'call'] == 'passed'
+    assert results['test_mf', 'call'] == 'failed'
+
+
+TEST_SAMPLE_SUCCEED = files().joinpath('sample succeed.py').read_text(encoding='utf-8')
+
+
+def test_success_flaky(testdir, capsys):
+    test = testdir.makepyfile(TEST_SAMPLE_SUCCEED)
+    rec = testdir.inline_run('--ignore-flaky', test)
+    results = get_results(rec)
+    assert results['test_flaky_ok', 'call'] == 'passed'
