d553861cd7b8029d079bdb48ff2cbc55ff2ba266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 15:02:48 +0000

    Add package dogma-data/0.2.8/dogma_data-0.2.8.tar.gz

---
+++ packages/dogma-data/dogma_data-0.2.8.tar.gz/dogma_data-0.2.8/dogma_data/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['write_awkward', 'read_awkward', 'fast_permute_and_pack', 'awkward_from_list_of_arrays']
+from ._dogma_data import write_awkward, read_awkward, fast_permute_and_pack, awkward_from_list_of_arrays
+from .fasta import parse_fasta
