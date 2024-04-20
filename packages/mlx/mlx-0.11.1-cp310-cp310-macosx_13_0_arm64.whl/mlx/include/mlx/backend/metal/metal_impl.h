e0c2e36fe5d7103cfdf7dbc9e0924f20750ebe08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:02:52 +0000

    Add package mlx/0.11.1/mlx-0.11.1-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/mlx/mlx-0.11.1-cp310-cp310-macosx_13_0_arm64.whl/mlx/include/mlx/backend/metal/metal_impl.h
@@ -0,0 +1,18 @@
+// Copyright © 2023-2024 Apple Inc.
+
+#pragma once
+
+#include <memory>
+
+#include "mlx/array.h"
+#include "mlx/stream.h"
+
+namespace mlx::core::metal {
+
+void new_stream(Stream stream);
+
+std::unique_ptr<void, std::function<void(void*)>> new_scoped_memory_pool();
+
+std::function<void()> make_task(array arr, bool signal);
+
+} // namespace mlx::core::metal
