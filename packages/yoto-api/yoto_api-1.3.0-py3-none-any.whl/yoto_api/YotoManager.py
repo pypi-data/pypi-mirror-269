cdf1234484e47e34312c14a7e1beb1d5a497d5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:02:33 +0000

    Add package yoto-api/1.3.0/yoto_api-1.3.0-py3-none-any.whl

---
+++ packages/yoto-api/yoto_api-1.3.0-py3-none-any.whl/yoto_api/YotoManager.py
@@ -0,0 +1,47 @@
+"""YotoManager.py"""
+
+import datetime
+import logging
+import pytz
+
+from .YotoAPI import YotoAPI
+from .Token import Token
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class YotoManager:
+    def __init__(self, username: str, password: str) -> None:
+        self.username: str = username
+        self.password: str = password
+        self.api: YotoAPI = YotoAPI()
+        self.players: dict = {}
+        self.token: Token = None
+        self.players: list = None
+        self.library: list = None
+
+    def initialize(self) -> None:
+        self.token: Token = self.api.login(self.username, self.password)
+        self.players = self.api.get_devices(self.token)
+        self.library = self.api.get_library(self.token)
+
+    def update_player_status(self) -> None:
+        # Updates the data with current player data.
+        self.api.update_devices(self.token, self.players)
+
+    def update_cards(self) -> None:
+        # Updates library and all card data.  Typically only required on startup.
+        # TODO: Should update the self.library object with a current dict of players. Should it do details for all cards too or separate?
+        self.library = self.api.update_library(self.token)
+
+    def check_and_refresh_token(self) -> bool:
+        if self.token is None:
+            self.initialize()
+            return True
+        # Check if valid and correct if not
+        if self.token.valid_until <= datetime.datetime.now(pytz.utc):
+            _LOGGER.debug(f"{DOMAIN} - Refresh token expired")
+            self.token: Token = self.api.refresh_token(self.token)
+            return True
+        return False
---
+++ packages/yoto-api/yoto_api-1.3.0-py3-none-any.whl/yoto_api/YotoPlayer.py
@@ -0,0 +1,16 @@
+"""YotoPlayers class"""
+
+from dataclasses import dataclass
+import datetime
+
+
+@dataclass
+class YotoPlayer:
+    id: str = None
+    name: str = None
+    deviceType: str = None
+    online: bool = None
+    last_updated_at: datetime.datetime = None
+
+
+# {'devices': [{'deviceId': 'y23IBS76kCaOSrGlz29XhIFO', 'name': 'Yoto Player', 'description': 'nameless.limit', 'online': False, 'releaseChannel': 'general', 'deviceType': 'v3', 'deviceFamily': 'v3', 'deviceGroup': '', 'hasUserGivenName': False}]}
