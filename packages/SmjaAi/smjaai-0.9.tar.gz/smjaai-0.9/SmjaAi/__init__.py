05044bd9a0ed155499ac673073cbbe5d78efb87e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:04 +0000

    Add package SmjaAi/0.9/smjaai-0.9.tar.gz

---
+++ packages/SmjaAi/smjaai-0.9.tar.gz/smjaai-0.9/SmjaAi/__init__.py
@@ -0,0 +1,21 @@
+import requests , random
+class Ai:
+    def __init__(self, text):
+        self.tex = text
+
+    def GPT(self):
+        while True:
+            rq = '123'
+            d = random.choice(rq)
+            e = random.choice(rq)
+            c = random.choice(rq)
+            b = random.choice(rq)
+            j = d + e
+            url = 'https://backend.aichattings.com/api/v2/chatgpt/talk'
+            data = {'ep_user_id': j+'62','locale':' en','model':' gpt3','msg':self.tex}
+            req = requests.post(url,data=data).text
+            da = {'text':req,
+            'status':'successfully'}
+            return da
+       
+
