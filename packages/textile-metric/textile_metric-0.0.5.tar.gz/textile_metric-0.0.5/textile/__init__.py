5480a16afda22052f29200936e4d47b1e747a3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:18 +0000

    Add package textile-metric/0.0.5/textile_metric-0.0.5.tar.gz

---
+++ packages/textile-metric/textile_metric-0.0.5.tar.gz/textile_metric-0.0.5/textile/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from textile.textile import Textile
+from textile.utils.image_utils import read_and_process_image
