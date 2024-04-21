de717ba24ec7068ec8591a1ef4ec24b9c26901be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:31 +0000

    Add package chainzpy/0.7/chainzpy-0.7.tar.gz

---
+++ packages/chainzpy/chainzpy-0.7.tar.gz/chainzpy-0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chainzpy",
+    version="0.7",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "bs4"
+    ],
+    description="API wrapper for https://chainz.cryptoid.info/",
+    long_description="This API wrapper allows you to gather information about peoples crypto wallets and their transactions. It is important to remember that this only works for cryptos that chainz supports"
+)
+
