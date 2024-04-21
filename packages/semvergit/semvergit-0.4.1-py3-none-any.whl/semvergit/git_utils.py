af850e5bddca269631c6e6922cc2df8c43007933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:08 +0000

    Add package semvergit/0.4.1/semvergit-0.4.1-py3-none-any.whl

---
+++ packages/semvergit/semvergit-0.4.1-py3-none-any.whl/semvergit/git_utils.py
@@ -0,0 +1,87 @@
+"""Git utilities."""
+
+from functools import wraps
+from typing import Any, Callable, List, Optional
+
+from git import Head, Repo
+from loguru import logger
+from semver import VersionInfo
+
+
+def drywrap(func: Callable) -> Callable:
+    """Dry run wrapper."""
+
+    @wraps(func)
+    def dryfunc(*args: Any, **kwargs: Any) -> Optional[Callable]:
+        """Dry run function."""
+        dry_run = kwargs.get("dry_run")
+        if dry_run is not None and dry_run:
+            logger.debug(f"Dry run: {func.__name__}(*{args}, **{kwargs})")
+            return None
+
+        if dry_run is not None:
+            del kwargs["dry_run"]
+        return func(*args, **kwargs)
+
+    return dryfunc
+
+
+def get_repo(search_parent_directories: bool = True) -> Repo:
+    """Get repo."""
+    repo = Repo(search_parent_directories=search_parent_directories)
+    logger.debug(f"Working on Repository: {repo.working_tree_dir}")
+    return repo
+
+
+def get_active_branch(repo: Repo) -> Head:
+    """Get active branch."""
+    branch = repo.active_branch
+    return branch
+
+
+def pull_remote(repo: Repo) -> None:
+    """Pull remote."""
+    remote = repo.remotes.origin
+    remote.pull()
+    logger.debug(f"Pulled remote {remote.name}")
+
+
+def get_tags_with_prefix(repo: Repo, prefix: str = "v") -> List[str]:
+    """Get tags as list of strings."""
+    tags = repo.tags
+    results = [tag.name for tag in tags if tag.name.startswith(prefix)]
+    logger_detail = f"with prefix -{prefix}-" if prefix else "no prefix"
+    logger.debug(f"Fetched tags: {results} ({logger_detail})")
+    return results
+
+
+@drywrap
+def new_commit(repo: Repo, message: str) -> str:
+    """New commit."""
+    new_commit_id = repo.index.commit(message=message)
+    short_commit_id = new_commit_id.hexsha[:7]
+    logger.debug(f"Created commit [{short_commit_id}] {message}")
+    return short_commit_id
+
+
+@drywrap
+def add_file(repo: Repo, file_path: str) -> None:
+    """Add file."""
+    repo.index.add([file_path])
+    logger.debug(f"Added file {file_path}")
+
+
+@drywrap
+def set_tag(repo: Repo, tag: str) -> VersionInfo:
+    """Set tag."""
+    new_tag = repo.create_tag(tag)
+    logger.debug(f"Created {str(new_tag)}")
+    return new_tag
+
+
+@drywrap
+def push_remote(repo: Repo, tag_str: str) -> None:
+    """Push remote."""
+    remote = repo.remotes.origin
+    remote.push(tag_str)
+    logger.debug(f"Pushed remote {remote.name}")
---
+++ packages/semvergit/semvergit-0.4.1-py3-none-any.whl/semvergit/log_utils.py
@@ -0,0 +1,36 @@
+"""Log utils."""
+
+import sys
+from enum import Enum
+
+from loguru import logger
+
+
+class LogLevel(Enum):
+    """LogLevel."""
+
+    ERROR = 0
+    INFO = 1
+    DEBUG = 2
+
+
+LOGGER_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{message}</level>"
+
+LOGGER_FORMAT_DEBUG = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>\n"
+    "{message}"
+)
+
+
+def set_logger(log_level: LogLevel = LogLevel.INFO) -> None:
+    """Set logger."""
+    if log_level == LogLevel.DEBUG:
+        logger.add(sys.stderr, format=LOGGER_FORMAT_DEBUG, level="DEBUG")
+    elif log_level == LogLevel.INFO:
+        logger.add(sys.stderr, format=LOGGER_FORMAT, level="INFO")
+    elif log_level == LogLevel.ERROR:
+        logger.add(sys.stderr, format=LOGGER_FORMAT, level="ERROR")
+    else:
+        raise ValueError(f"Invalid debug mode: {log_level}")
