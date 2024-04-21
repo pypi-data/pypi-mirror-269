ca923b4825e0c8d263b9fae2c0aa966ca7bf9c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:28 +0000

    Add package mteb/1.6.37/mteb-1.6.37.tar.gz

---
+++ packages/mteb/mteb-1.6.37.tar.gz/mteb-1.6.37/mteb/abstasks/AbsTask.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+import random
+from abc import ABC, abstractmethod
+
+import datasets
+import numpy as np
+import torch
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+
+class AbsTask(ABC):
+    metadata: TaskMetadata
+
+    def __init__(self, seed=42, **kwargs):
+        self.dataset = None
+        self.data_loaded = False
+        self.is_multilingual = False
+        self.is_crosslingual = False
+        self.save_suffix = kwargs.get("save_suffix", "")
+
+        self.seed = seed
+        random.seed(self.seed)
+        np.random.seed(self.seed)
+        torch.manual_seed(self.seed)
+        torch.cuda.manual_seed_all(self.seed)
+
+    def dataset_transform(self):
+        """
+        Transform operations applied to the dataset after loading.
+        Override this method if your dataset requires any transformation.
+        """
+        pass
+
+    def load_data(self, **kwargs):
+        """
+        Load dataset from HuggingFace hub
+        """
+        if self.data_loaded:
+            return
+        self.dataset = datasets.load_dataset(**self.metadata_dict["dataset"])
+        self.dataset_transform()
+        self.data_loaded = True
+
+    @property
+    def metadata_dict(self) -> dict[str, str]:
+        metadata_dict = dict(self.metadata)
+        return metadata_dict
+
+    @abstractmethod
+    def evaluate(self, model, split="test"):
+        """
+        Evaluates a Sentence Embedding Model on the task.
+        Returns a dict (that can be serialized to json).
+        :param model: Sentence embedding method. Implements a encode(sentences) method, that encodes sentences
+        and returns a numpy matrix with the sentence embeddings
+        :param split: Which datasplit to be used.
+        """
+        raise NotImplementedError
+
+    @property
+    def languages(self) -> set[str]:
+        """
+        Returns the languages of the task
+        """
+        return self.metadata.languages
+
+    def __repr__(self) -> str:
+        """
+        Format the representation of the task such that it appears as:
+
+        TaskObjectName(name='{name}', languages={lang1, lang2, ...})
+        """
+        langs = self.languages
+        if len(langs) > 3:
+            langs = list(langs)[:3]
+            langs.append("...")
+        return (
+            f"{self.__class__.__name__}(name='{self.metadata.name}', languages={langs})"
+        )
