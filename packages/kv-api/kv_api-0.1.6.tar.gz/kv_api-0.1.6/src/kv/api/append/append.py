30e0f412a821034f20568fc3039f17fb945a0521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:02:58 +0000

    Add package kv-api/0.1.6/kv_api-0.1.6.tar.gz

---
+++ packages/kv-api/kv_api-0.1.6.tar.gz/kv_api-0.1.6/src/kv/api/append/append.py
@@ -0,0 +1,22 @@
+from typing import Generic, TypeVar, overload, Literal
+from abc import ABC, abstractmethod
+from haskellian.either import Either
+from haskellian.asyn.promises import Promise
+from ..errors import DBError, InexistentItem
+from ..api import KV
+
+T = TypeVar('T')
+
+class Appendable(ABC, Generic[T]):
+  @overload
+  @abstractmethod
+  def append(self, id: str, values: list[T], *, create: Literal[False]) -> Promise[Either[DBError|InexistentItem, None]]:
+    """Appends `values` if it already existed. Otherwise doesn't append, and returns `Left[ExistentItem]`"""
+  @overload
+  @abstractmethod
+  def append(self, id: str, values: list[T], *, create: bool = True) -> Promise[Either[DBError, None]]:
+    """Appends `values` to `id`, creating the item if needed"""
+
+class AppendableKV(KV[list[T]], Appendable, Generic[T]):
+  ...
+  
---
+++ packages/kv-api/kv_api-0.1.6.tar.gz/kv_api-0.1.6/src/kv/api/errors/errors.py
@@ -0,0 +1,30 @@
+from typing import Literal, Any
+from dataclasses import dataclass
+
+class StrMixin:
+  def __str__(self) -> str:
+    return self.__repr__()
+
+@dataclass(eq=False)
+class InexistentItem(StrMixin, BaseException):
+  key: str | None = None
+  detail: Any | None = None
+  reason: Literal['inexistent-item'] = 'inexistent-item'
+
+@dataclass(eq=False)
+class ExistentItem(StrMixin, BaseException):
+  key: str
+  detail: Any | None = None
+  reason: Literal['existent-item'] = 'existent-item'
+
+@dataclass(eq=False)
+class DBError(StrMixin, BaseException):
+  detail: Any = None
+  reason: Literal['db-error'] = 'db-error'
+
+@dataclass(eq=False)
+class InvalidData(StrMixin, BaseException):
+  detail: Any = None
+  reason: Literal['invalid-data'] = 'invalid-data'
+
+ReadError = DBError | InvalidData | InexistentItem
