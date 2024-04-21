eb4157fce9a5a67a35260b815ae9eecda339b33f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:02 +0000

    Add package counts/0.0.7/counts-0.0.7.tar.gz

---
+++ packages/counts/counts-0.0.7.tar.gz/counts-0.0.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name='counts',
+    version='0.0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A Python library for managing rent expiration countdowns',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Erick Adikah',
+    author_email='your.email@example.com',
+    license='MIT',
+    url='https://github.com/Erickadikah/count',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[],
+)
