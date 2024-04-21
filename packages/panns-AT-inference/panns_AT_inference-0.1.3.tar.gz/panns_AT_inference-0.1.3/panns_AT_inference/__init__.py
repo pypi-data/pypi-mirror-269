e488da63606b36d176ece05ba2759f39ec9377b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:20 +0000

    Add package panns-AT-inference/0.1.3/panns_AT_inference-0.1.3.tar.gz

---
+++ packages/panns-AT-inference/panns_AT_inference-0.1.3.tar.gz/panns_AT_inference-0.1.3/panns_AT_inference/__init__.py
@@ -0,0 +1,4 @@
+from .inference import AudioTagging
+from .config import labels
+
+__version__ = "0.1.0"
---
+++ packages/panns-AT-inference/panns_AT_inference-0.1.3.tar.gz/panns_AT_inference-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="panns_AT_inference",
+    version="0.1.3",
+    author="Qiuqiang Kong, Stefano Giacomelli (Ph.D. student UnivAQ)",
+    author_email="qiuqiangkong@gmail.com, stefano.giacomelli@graduate.univaq.it",
+    description="panns_AT_inference: audio tagging inference toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StefanoGiacomelli/panns_AT_inference",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'torch', 'matplotlib', 'librosa', 'torchlibrosa'],
+    python_requires='>=3.6',
+)
