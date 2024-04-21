a46bd20db7ac9b40f23b721a4926bc216d6b7459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:12 +0000

    Add package hspylib/1.12.36/hspylib-1.12.36.tar.gz

---
+++ packages/hspylib/hspylib-1.12.36.tar.gz/hspylib-1.12.36/hspylib/core/config/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v1.12.36
+#
+# Package: main.hspylib.core.config
+"""Package initialization."""
+
+__all__ = [
+    'app_config', 
+    'parser_factory', 
+    'properties'
+]
+__version__ = '1.12.36'
