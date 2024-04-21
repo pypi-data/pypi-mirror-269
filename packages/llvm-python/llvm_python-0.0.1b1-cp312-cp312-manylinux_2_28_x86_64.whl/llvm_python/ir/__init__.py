6a5ed147edadb04ca4b262697c33c1ef9ea9553f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:03:11 +0000

    Add package llvm-python/0.0.1b1/llvm_python-0.0.1b1-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/llvm-python/llvm_python-0.0.1b1-cp312-cp312-manylinux_2_28_x86_64.whl/llvm_python/ir/__init__.py
@@ -0,0 +1,10 @@
+from .module import Module
+from .function import Function
+from .block import Block
+from .argument import Argument
+from .type import Type
+from .value import Value
+from .instruction import Instruction
+
+
+__version__ = "0.0.1b1"
