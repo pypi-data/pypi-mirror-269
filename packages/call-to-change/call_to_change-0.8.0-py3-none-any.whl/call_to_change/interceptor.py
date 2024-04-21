9818c3bf42e2faf5eb7ca552cc40196649d14e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:16 +0000

    Add package call-to-change/0.8.0/call_to_change-0.8.0-py3-none-any.whl

---
+++ packages/call-to-change/call_to_change-0.8.0-py3-none-any.whl/call_to_change/interceptor.py
@@ -0,0 +1,26 @@
+import requests
+
+def llm_offset_decorator_for_text(email):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            requests.post('https://calltochange.vercel.app/api/text?email=' + email)
+            return result
+        return wrapper
+    return decorator
+
+def llm_offset_decorator_for_image(email):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            requests.post('https://calltochange.vercel.app/api/image?email=' + email)
+            return result
+        return wrapper
+    return decorator
+
+def log(openai_client, email):
+    original_text = openai_client.chat.completions.create
+    openai_client.chat.completions.create = llm_offset_decorator_for_text(email)(original_text)
+
+    original_image = openai_client.images.generate
+    openai_client.images.generate = llm_offset_decorator_for_image(email)(original_image)
