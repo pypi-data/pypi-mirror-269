d4acc16eafa74b0d34e9f9fe6d9e36cf50017821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:23 +0000

    Add package gemini-output-wrapper/0.2.0/gemini-output-wrapper-0.2.0.tar.gz

---
+++ packages/gemini-output-wrapper/gemini-output-wrapper-0.2.0.tar.gz/gemini-output-wrapper-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gemini-output-wrapper",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "importlib",
+        "typing",
+        "json",
+        "google-generativeai",
+        "inspect",
+    ],
+    description="Library for wrapping Gemini API output with Json and Python objects",
+    author="Min Htet Naing",
+    author_email="minhtetnaing25mhn@gmail.com",
+    url="https://github.com/Osbertt-19/gemini-output-wrapper.git",
+)
