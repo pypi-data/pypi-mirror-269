10359d6f88adb228a7832dce9ccfefb4d343dd59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:38 +0000

    Add package CI-CD-SergiCastillo/0.0.0/CI-CD-SergiCastillo-0.0.0.tar.gz

---
+++ packages/CI-CD-SergiCastillo/CI-CD-SergiCastillo-0.0.0.tar.gz/CI-CD-SergiCastillo-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CI-CD-SergiCastillo',
+    packages=find_packages(),
+    install_requires=[
+        'coverage',
+        'pylint',
+        'pytest',
+        'ggshield',
+        'psycopg2',
+        'flask',
+        'json-log-formatter',
+        'twine'
+              
+    ],
+    author='Sergi Castillo',
+    description='KeepCoding',
+    url='https://github.com/sergicastin/CI-CD-SergiCastillo',
+)
