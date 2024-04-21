6e8db16c8a8f63d2a61caea2c5cc10e6c18e7b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:34 +0000

    Add package movoid-robotframework-selenium/1.2.2/movoid_robotframework_selenium-1.2.2-py3-none-any.whl

---
+++ packages/movoid-robotframework-selenium/movoid_robotframework_selenium-1.2.2-py3-none-any.whl/test/__init__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/4/20 23:11
+# Description   : 
+"""
+from movoid_package import Stub
+
+Stub(r'E:\000develop\movoid_robotframework_selenium\RobotFrameworkSelenium\common.py')
+Stub(r'E:\000develop\movoid_robotframework_selenium\RobotFrameworkSelenium\action\action.py')
