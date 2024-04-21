2a2518018cf20b998b5c6d89c076b6074310e38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:44 +0000

    Add package pcre2/0.4.0/pcre2-0.4.0-cp38-cp38-win_amd64.whl

---
+++ packages/pcre2/pcre2-0.4.0-cp38-cp38-win_amd64.whl/pcre2/__init__.py
@@ -0,0 +1,7 @@
+from .methods import compile, findall, match, scan, split, substitute
+from .consts import (
+    __libpcre2_version__,
+    CompileOption,
+    A, I, M, U, S, X
+)
+__version__ = "0.4.0"
