c5239df961061b013b607ec75bf484115b185495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:27 +0000

    Add package nokey/0.2.0/nokey-0.2.0-py3-none-any.whl

---
+++ packages/nokey/nokey-0.2.0-py3-none-any.whl/nokey/helperFuncs/nokey_apis.py
@@ -0,0 +1,20 @@
+nokey_apis = {
+    "country_info": ["RestCountries"],
+    "finance": ["Wallstreet Bets"],
+    "geolocation": ["IP API", "Zippopotomus"],
+    "spaceflight": ["Spaceflight News"],
+    "education": ["University Domains and Names"],
+    "food": ["Fruityvice"],
+    "random": ["RandomUserGenerator"],
+    "jokes": ["JokeAPI"],
+    "animals": ["DogAPI"],
+    "science": ["Nobel Prize API"],
+    "weather": ["National Weather Service API"],
+    "developer_tools": ["URL Shortener", "URLHaus", "APIsGuru", "Microlink"],
+    "inspiration": ["Dictum"],
+    "language": ["Free Dictionary"],
+    "games": ["Free To Game", "Open Trivia Database"],
+    "activities": ["Bored API"],
+    "calendar": ["Nager.Date"],
+    "government": ["Federal Register"]
+}
