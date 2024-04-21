e2e9a849183fa5ff6625bfd7068146767f96b3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:38 +0000

    Add package math-ops-131/1.0/math_ops_131-1.0.tar.gz

---
+++ packages/math-ops-131/math_ops_131-1.0.tar.gz/math_ops_131-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_ops_131',
+    version='1.0',
+    packages=find_packages(),
+    description='A Python package for basic mathematical operations.',
+    author='Unnath Chittimalla',
+    author_email='unnath.chittimalla@iiitb.ac.in',
+    url='https://github.com/AspiringPianist/math_operations_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
