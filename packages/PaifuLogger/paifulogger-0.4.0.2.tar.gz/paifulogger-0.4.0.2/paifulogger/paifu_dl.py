94de05fad6c46958bca95fa4656b6cb5771e2061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:09 +0000

    Add package PaifuLogger/0.4.0.2/paifulogger-0.4.0.2.tar.gz

---
+++ packages/PaifuLogger/paifulogger-0.4.0.2.tar.gz/paifulogger-0.4.0.2/paifulogger/paifu_dl.py
@@ -0,0 +1,143 @@
+import argparse
+import json
+import os
+import re
+import sys
+import urllib.error
+
+from .log import _get_lang
+from .src.config import config_path
+from .src.i18n import LocalStr
+from .src.get_paifu import get_paifu
+
+
+url_reg = r"https?://tenhou\.net/\d/\?log=\d{10}gm-\w{4}-\w{4}-\w{8}&tw=\d"
+
+
+def _get_urls(urls, local_lang: LocalStr) -> list[str]:
+    """
+    Get urls from input or args.url.
+
+    If remake, get urls from url_log.h5
+    Else if not given args.url, get urls from input.
+
+    Note:
+        `re.findall(url_reg, url)` will return a list of urls that match the regular expression.
+    """
+
+    check_urls = []
+
+    if not urls:
+        for url in re.findall(url_reg, input(local_lang.hint_input)):
+            check_urls.append(url)
+    else:
+        for url in urls:
+            check_urls.extend(re.findall(url_reg, url))
+    return check_urls
+
+
+def paifu_dl(
+    urls: str | list[str] | None = None,
+    *,
+    local_lang: LocalStr = LocalStr("en", os.path.dirname(os.path.abspath(__file__))),
+    output: str = "./",
+    mjai: bool = False,
+) -> int:
+    """
+    Download paifu from tenhou.net.
+
+    Args:
+        urls: str | list[str]
+            The url of the game log.
+        local_lang: LocalStr
+            The localized string.
+        output: str
+            The output directory.
+        mjai: bool
+            If True, download MJAI format.
+    """
+
+    if isinstance(urls, str):
+        urls = [urls]
+
+    check_urls = _get_urls(urls, local_lang)
+
+    retCode = 0
+    for url in check_urls:
+        try:
+            get_paifu(url, local_lang, output, mjai)
+            print(f"paifu_dl: {url} has been downloaded.")
+        except urllib.error.URLError:
+            print(local_lang.hint_url, url)
+            retCode = 1
+        except OSError:
+            print(local_lang.hint_url, url)
+            retCode = 1
+    return retCode
+
+
+def pdl(args: argparse.Namespace) -> int:
+    if args.version:
+        from .version import __version__
+
+        print("Tenhou-Paifu-Logger", __version__)
+        return 0
+
+    local_lang = _get_lang(args.lang)
+
+    return paifu_dl(
+        args.url,
+        local_lang=local_lang,
+        output=args.output,
+        mjai=args.mjai,
+    )
+
+
+def pdl_parser(
+    config_path: str | None = None, parser: argparse.ArgumentParser | None = None
+) -> argparse.Namespace:
+    config = {}
+    if config_path and os.path.exists(f"{config_path}/config.json"):
+        with open(f"{config_path}/config.json", "r") as f:
+            config = json.load(f)
+
+    if parser is None:
+        parser = argparse.ArgumentParser(description="Paifu Downloader")
+    parser.add_argument("url", nargs="*", help="URL of the match.")
+    parser.add_argument(
+        "-l",
+        "--lang",
+        type=str,
+        help="""Language of the program and output files. Default is English. 
+        Available languages: English(en), 繁體中文(zh_tw), 简体中文(zh), 日本語(ja).""",
+        default=config.get("lang", None),
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Output directory. Default is './'.",
+        default=config.get("output", None),
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        help="Show version of the program. If this is used, all other arguments will be ignored and the program will be closed.",
+    )
+    parser.add_argument(
+        "--mjai",
+        action="store_true",
+        help="Output MJAI format paifu.",
+        default=config.get("mjai", False),
+    )
+
+    args = parser.parse_args()
+    if "pdl" in args.url:
+        args.url.remove("pdl")
+    return args
+
+
+def main(plog_parser: argparse.ArgumentParser | None = None):
+    args = pdl_parser(config_path(), plog_parser)
+    sys.exit(pdl(args))
---
+++ packages/PaifuLogger/paifulogger-0.4.0.2.tar.gz/paifulogger-0.4.0.2/paifulogger/src/get_paifu.py
@@ -0,0 +1,65 @@
+import gzip
+import os
+import urllib.request
+import xml.etree.ElementTree as ET
+
+from .i18n import LocalStr, localized_str
+from .Paifu import Paifu
+
+
+HEADER = {
+    "Host": "e.mjv.jp",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "Connection": "keep-alive",
+}
+
+
+def url_request_handler(url: str):
+    url = url.split("=")[1]
+    url = "https://tenhou.net/0/log/?" + url[:-3]
+    req = urllib.request.Request(url=url, headers=HEADER)
+    opener = urllib.request.build_opener()
+    response = opener.open(req)
+    response = gzip.decompress(response.read()).decode("utf-8")
+    return response
+
+
+def get_paifu(
+    url: str,
+    local_lang: LocalStr = localized_str(
+        "en", os.path.dirname(os.path.abspath(__file__)).split("src")[0]
+    ),
+    output: str = "./",
+    mjai: bool = False,
+) -> Paifu:
+    response = url_request_handler(url)
+    root = ET.fromstring(response)
+    paifu = Paifu(url, root)
+    path = f"{output}/{local_lang.paifu}/{paifu.go_str}/"
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    url = url.split("=")[1] + "=" + url.split("=")[2]
+    with open(path + url + ".xml", "w") as t:
+        t.write(response)
+
+    # mjai format output
+    if mjai:
+        if paifu.player_num == 3:
+            print(local_lang.sanma_mjai_error)
+            return paifu
+
+        try:
+            from .mjlog2mjai.parse import load_mjlog, parse_mjlog_to_mjai
+        except ImportError:
+            print(local_lang.log2mjai_import_error)
+            return paifu
+        if not os.path.isdir(path + "/mjai/"):
+            os.makedirs(path + "/mjai/")
+        with open(path + "/mjai/" + url + ".mjson", "w", encoding="UTF-8") as f:
+            f.write(parse_mjlog_to_mjai(load_mjlog(path + url + ".xml")))
+
+    return paifu
