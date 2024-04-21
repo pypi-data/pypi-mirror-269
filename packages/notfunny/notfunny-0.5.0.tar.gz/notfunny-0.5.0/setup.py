4adcfb67d2a1aa445c05fa3dc5ac47599329a624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:46 +0000

    Add package notfunny/0.5.0/notfunny-0.5.0.tar.gz

---
+++ packages/notfunny/notfunny-0.5.0.tar.gz/notfunny-0.5.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='notfunny',
+      version='0.5.0',
+      description='Only the worst',
+      url='http://github.com/ohthepain/notfunny',
+      author='Paul Wilkinson',
+      author_email='paul@thisco.co',
+      license='MIT',
+      packages=['notfunny'],
+      install_requires=[
+            'markdown'
+      ],
+      zip_safe=False)
