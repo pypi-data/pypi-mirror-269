5c5e142b94ebb460650ed722a1921ce12f772d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:23 +0000

    Add package Montreal-Forced-Aligner/3.0.6/montreal_forced_aligner-3.0.6.tar.gz

---
+++ packages/Montreal-Forced-Aligner/montreal_forced_aligner-3.0.6.tar.gz/montreal_forced_aligner-3.0.6/montreal_forced_aligner/command_line/anchor.py
@@ -0,0 +1,27 @@
+"""Command line functions for launching anchor annotation"""
+from __future__ import annotations
+
+import logging
+import sys
+
+import rich_click as click
+
+__all__ = ["anchor_cli"]
+
+logger = logging.getLogger("mfa")
+
+
+@click.command(name="anchor", short_help="Launch Anchor")
+@click.help_option("-h", "--help")
+def anchor_cli(*args, **kwargs) -> None:  # pragma: no cover
+    """
+    Launch Anchor Annotator (if installed)
+    """
+    try:
+        from anchor.command_line import main
+    except ImportError:
+        logger.error(
+            "Anchor annotator utility is not installed, please install it via `conda install -c conda-forge anchor-annotator`."
+        )
+        sys.exit(1)
+    main()
