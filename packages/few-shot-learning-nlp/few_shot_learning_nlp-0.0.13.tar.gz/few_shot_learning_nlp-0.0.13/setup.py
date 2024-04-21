fe929fef109a217a0d7c0965886555ee66106f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:28 +0000

    Add package few-shot-learning-nlp/0.0.13/few_shot_learning_nlp-0.0.13.tar.gz

---
+++ packages/few-shot-learning-nlp/few_shot_learning_nlp-0.0.13.tar.gz/few_shot_learning_nlp-0.0.13/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from requirements import REQUIRED_PACKAGES
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="few-shot-learning-nlp",
+    version="0.0.13",
+    author="Pedro Silva",
+    author_email="pedrolmssilva@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/peulsilva/few-shot-learning-nlp",
+    packages=setuptools.find_packages(),
+    install_requires = REQUIRED_PACKAGES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
