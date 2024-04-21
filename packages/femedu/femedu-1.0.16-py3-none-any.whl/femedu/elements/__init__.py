836fab065a1b5ba68eacddf71e861cfb4d6633d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:23 +0000

    Add package femedu/1.0.16/femedu-1.0.16-py3-none-any.whl

---
+++ packages/femedu/femedu-1.0.16-py3-none-any.whl/femedu/elements/__init__.py
@@ -0,0 +1,25 @@
+"""
+=====================================================
+Finite elements based on a variational formulation.
+=====================================================
+
+Elements in this group share the following features:
+* small deformation kinematics
+* based on variational principles
+* numeric integration of stiffness and internal force, thus, allowing for the use of inelastic material
+"""
+
+__all__ = (
+    "Element",
+    "DrawElement",
+    "Faces",
+    "Face2D",
+    "Face3D",
+)
+
+from .Element import *
+from .DrawElement import *
+from .Faces import *
+from .Face2D import *
+from .Face3D import *
+
---
+++ packages/femedu/femedu-1.0.16-py3-none-any.whl/femedu/utilities/__init__.py
@@ -0,0 +1,22 @@
+__all__ = (
+    "Integration",
+    "LineIntegration",
+    "TriangleIntegration",
+    "QuadIntegration",
+    "TetraIntegration",
+    "BrickIntegration",
+    "ShapeFunctions",
+    "LineShapes",
+    "QuadShapes",
+    "TriangleShapes",
+)
+
+from .LineIntegration import *
+from .TriangleIntegration import *
+from .QuadIntegration import *
+from .TetraIntegration import *
+from .BrickIntegration import *
+
+from .LineShapes import *
+from .QuadShapes import *
+from .TriangleShapes import *
