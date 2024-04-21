c4adce8b79568882cb59de45fa06f9947e13df9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:10 +0000

    Add package TPE-Bot/6.8.8/TPE-Bot-6.8.8.tar.gz

---
+++ packages/TPE-Bot/TPE-Bot-6.8.8.tar.gz/TPE-Bot-6.8.8/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TPE-Bot", # Replace with your own username
+    version="6.8.8",
+    author="Ester Goh",
+    description="A TPEdu All-in-one Chatbot Package",
+    packages=setuptools.find_packages(),
+    package_data={'': ['.env', 'Bot/*.env','Bot/*.py','Bot/*/*.py', 'Bot/*/*/*.py', 'Data/*/*.js', 'Data/*/*.html', 'Data/*/*.json', 'QA/*.py']},
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'torch', 
+        'openpyxl', 
+        'transformers', 
+        'sentencepiece', 
+        'python-Levenshtein', 
+        'sentence-transformers', 
+        'fuzzywuzzy', 
+        'protobuf==3.20.0',
+        'python-dotenv',
+        'openai',
+        'nltk'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
