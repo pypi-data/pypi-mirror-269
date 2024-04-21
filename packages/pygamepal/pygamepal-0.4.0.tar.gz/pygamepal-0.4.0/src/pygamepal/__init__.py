da48aafb1673d2717602ef4329e9294e5216375b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:46 +0000

    Add package pygamepal/0.4.0/pygamepal-0.4.0.tar.gz

---
+++ packages/pygamepal/pygamepal-0.4.0.tar.gz/pygamepal-0.4.0/src/pygamepal/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.4.0"
+__author__ = 'Rik Cross'
+__license__ = 'MIT'
+
+import pygame
+
+from .game import *
+from .input import *
+from .transition import *
+from .camera import *
+from .particles import *
+from .spriteImage import *
+from .trigger import *
+
+from .textureList import *
+from .splitTexture import *
+from .flatten import *
+
+from .drawText import *
