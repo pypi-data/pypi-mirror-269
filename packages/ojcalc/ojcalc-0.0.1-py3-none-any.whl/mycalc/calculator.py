e85532e107efb742323ea5f8d754292fe5498c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:02 +0000

    Add package ojcalc/0.0.1/ojcalc-0.0.1-py3-none-any.whl

---
+++ packages/ojcalc/ojcalc-0.0.1-py3-none-any.whl/mycalc/calculator.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+def add(a, b):
+    return a+b
+
+def subtract(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a/b
+
+def square(a, b):
+    return a**b
+
+def share(a, b):
+    return a//b
