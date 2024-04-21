04031731ff119d32b4c1ce595cd1110c1ef02ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:05 +0000

    Add package rio-ui/0.5.12/rio_ui-0.5.12-py3-none-any.whl

---
+++ packages/rio-ui/rio_ui-0.5.12-py3-none-any.whl/rio/components/html.py
@@ -0,0 +1,33 @@
+from typing import final
+
+from .fundamental_component import FundamentalComponent
+
+__all__ = ["Html"]
+
+
+@final
+class Html(FundamentalComponent):
+    """
+    # Html
+
+    Renders HTML.
+
+    `Html` allows you to embed HTML in your app. It takes HTML code as input and
+    renders it. If you want to embed an entire website, use the `Website`
+    component instead.
+
+
+    ## Attributes
+
+    `html`: The HTML to render.
+
+    ## Example
+
+    This minimal example will render a simple HTML code:
+    TODO: Add example
+    """
+
+    html: str
+
+
+Html._unique_id = "Html-builtin"
