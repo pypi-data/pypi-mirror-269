95159831d16061632a3f0e65e2933bf91c8c7ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:27 +0000

    Add package metaffi-api/0.0.21/metaffi_api-0.0.21.tar.gz

---
+++ packages/metaffi-api/metaffi_api-0.0.21.tar.gz/metaffi_api-0.0.21/metaffi/__init__.py
@@ -0,0 +1,17 @@
+"""Python MetaFFI API"""
+
+__version__ = "0.0.21"
+
+__all__ = ['metaffi', 'metaffi_types', 'metaffi_runtime', 'metaffi_module', 'metaffi_handle', 'metaffi_types', 'xllr_wrapper', 'pycdts_converter']
+
+import metaffi.metaffi_types
+import metaffi.metaffi_runtime
+import metaffi.metaffi_module
+import metaffi.metaffi_handle
+import metaffi.metaffi_types
+import metaffi.xllr_wrapper
+import metaffi.pycdts_converter
+
+
+
+
