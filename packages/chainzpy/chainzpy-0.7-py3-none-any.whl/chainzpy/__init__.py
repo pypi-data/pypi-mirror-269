4ea8b3881c911aaa6124224ac90fba6d094359ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:31 +0000

    Add package chainzpy/0.7/chainzpy-0.7-py3-none-any.whl

---
+++ packages/chainzpy/chainzpy-0.7-py3-none-any.whl/chainzpy/__init__.py
@@ -0,0 +1,10 @@
+from .main import set_data_to_json
+from .main import set_api_key
+from .main import getAddressBalance
+from .main import getAddressBalanceUSD
+from .main import getAddressInfo
+from .main import getRecentTransactions
+from .main import getRichestRank
+from .main import getTotalRecievedAmount
+from .main import getTransactionSummary
+
