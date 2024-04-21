27d9c6b05b48bc44cd27034a7083e96b09169a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:25 +0000

    Add package dndlprof/0.9.0/dndlprof-0.9.0-py3-none-any.whl

---
+++ packages/dndlprof/dndlprof-0.9.0-py3-none-any.whl/dnd/common.py
@@ -0,0 +1,199 @@
+import os
+import sys
+from dataclasses import dataclass, field
+from distutils.spawn import find_executable
+import functools
+import json
+import yaml
+import subprocess
+import tempfile
+from contextlib import contextmanager
+
+def get_optional_env(name : str, default : str = None):
+    return os.environ.get(name, default)
+
+def get_required_env(name : str):
+    if name in os.environ: return os.environ[name]
+    raise RuntimeError(f'Environment variable {name} is required')
+
+def shortstr(s : str, maxlen : int = 20):
+    if len(s) <= maxlen: return s
+    return s[:maxlen - 3] + '...'
+
+def command_exists(name):
+    return find_executable(name) is not None
+
+def parse_int_tuple(s : str):
+    return tuple(map(int, s.replace('(', '').replace(')', '').split(',')))
+
+@contextmanager
+def check_subprocess():
+    try:
+        yield
+    except subprocess.CalledProcessError as e:
+        print('Error: subprocess returned with an error!')
+        print(e)
+        if e.stdout is not None:
+            print('==== STDOUT ====')
+            print(e.stdout.decode())
+        if e.stderr is not None:
+            print('==== STDERR ====')
+            print(e.stderr.decode())
+        print('================')
+        raise
+
+@contextmanager
+def temp_file(suffix : str, delete : bool = True):
+    fname = tempfile.mktemp(suffix=suffix)
+    yield fname
+    if delete: os.remove(fname)
+
+dnd_config_file = get_optional_env('DND_CONFIG', '~/.dnd-config.yaml')
+
+@dataclass
+class GlobalConfig:
+    ncu_replay_mode : str = 'application'
+    ncu_extra_cli : list[str] = field(default_factory=list)
+    nsys_num_samples : int = 10
+
+    @staticmethod
+    def from_file(fname : str):
+        with open(fname, 'r') as f:
+            yd = yaml.safe_load(f)
+
+        defconfig = GlobalConfig()
+
+        return GlobalConfig(
+            ncu_replay_mode=yd.get('ncu_replay_mode', defconfig.ncu_replay_mode),
+            ncu_extra_cli=yd.get('ncu_extra_cli', defconfig.ncu_extra_cli),
+            nsys_num_samples=yd.get('nsys_num_samples', defconfig.nsys_num_samples)
+        )
+
+dnd_config_file = os.path.expanduser(dnd_config_file)
+if os.path.exists(dnd_config_file):
+    dnd_config = GlobalConfig.from_file(dnd_config_file)
+else:
+    dnd_config = GlobalConfig()
+
+@dataclass
+class Kernel:
+    kid : int
+    name : str
+    nsys_avg_lat_ns : float
+    nvtx_range : str
+    grid : tuple
+    block : tuple
+    reg_per_thread : int
+    static_smem : int
+    dynamic_smem : int
+    metrics : dict
+
+    @functools.cached_property
+    def sanitized_name(self):
+        sn = self.name \
+            .replace('void ', '') \
+            .replace('at::native::', '') \
+            .replace('<unnamed>::', '') \
+            .replace('cutlass::', '')
+
+        if '<' in sn: sn = sn[:sn.index('<')]
+        if '(' in sn: sn = sn[:sn.index('(')]
+        return sn
+
+    @property
+    def threads_per_block(self): return self.block[0] * self.block[1] * self.block[2]
+
+    @property
+    def reg_per_block(self): return self.reg_per_thread * self.threads_per_block
+
+    @property
+    def tot_smem(self): return self.static_smem + self.dynamic_smem
+
+    @property
+    def ncu_lat_ns(self): return self.metrics['gpu__time_duration.sum']
+
+    @property
+    def dram_util(self): return self.metrics['gpu__dram_throughput.avg.pct_of_peak_sustained_elapsed'] / 100
+
+    @property
+    def sm_util(self): return self.metrics['sm__throughput.avg.pct_of_peak_sustained_elapsed'] / 100
+
+    @property
+    def tensor_util(self): return self.metrics['sm__pipe_tensor_cycles_active.avg.pct_of_peak_sustained_elapsed'] / 100
+
+    @property
+    def yaml_repr(self):
+        return '{' + ', '.join([
+            f'kid: {self.kid}',
+            f'name: "{self.name}"',
+            f'nsys_avg_lat_ns: {self.nsys_avg_lat_ns}',
+            f'nvtx_range: "{self.nvtx_range}"',
+            f'grid: "{self.grid}"',
+            f'block: "{self.block}"',
+            f'reg_per_thread: {self.reg_per_thread}',
+            f'static_smem: {self.static_smem}',
+            f'dynamic_smem: {self.dynamic_smem}',
+            f'metrics: {json.dumps(self.metrics)}'
+        ]) + '}'
+
+    @staticmethod
+    def from_yaml(yd):
+        return Kernel(
+            kid=yd['kid'],
+            name=yd['name'],
+            nsys_avg_lat_ns=yd['nsys_avg_lat_ns'],
+            nvtx_range=yd['nvtx_range'],
+            grid=parse_int_tuple(yd['grid']),
+            block=parse_int_tuple(yd['block']),
+            reg_per_thread=yd['reg_per_thread'],
+            static_smem=yd['static_smem'],
+            dynamic_smem=yd['dynamic_smem'],
+            metrics=yd['metrics']
+        )
+
+@dataclass
+class Operator:
+    uid : str
+    kerns : 'list[Kernel]'
+
+    def print_yaml(self, file=sys.stdout, indent=0):
+        print(f'{"  " * indent}- uid: {self.uid}', file=file)
+        print(f'{"  " * indent}  kerns:', file=file)
+        for k in self.kerns:
+            print(f'{"  " * indent}    - {k.yaml_repr}', file=file)
+
+    @staticmethod
+    def from_yaml(yd):
+        return Operator(
+            uid=yd['uid'],
+            kerns=[Kernel.from_yaml(kd) for kd in yd['kerns']] \
+                if yd['kerns'] is not None else []
+        )
+
+def read_kerns_yaml(filename : str) -> 'dict[str, list[Operator]]':
+    with open(filename) as f:
+        yd = yaml.safe_load(f)
+
+    return {k: [Operator.from_yaml(opd) for opd in yd[k]] for k in yd}
+
+
+@dataclass
+class Profile:
+    params : dict
+    bench : dict
+    trace : dict
+
+    @staticmethod
+    def from_file(filename : str) -> 'Profile':
+        with open(filename) as f:
+            yd = yaml.safe_load(f)
+
+        return Profile(
+            params=yd['params'],
+            bench=yd['bench'],
+            trace={
+                k: [Operator.from_yaml(opd) for opd in v] \
+                    if v is not None else []
+                for k, v in yd['trace'].items()
+            }
+        )
---
+++ packages/dndlprof/dndlprof-0.9.0-py3-none-any.whl/dnd/nvtools.py
@@ -0,0 +1,251 @@
+import os
+import sys
+import functools
+import numpy as np
+import tempfile
+import pandas as pd
+from dataclasses import dataclass, field
+import subprocess
+import json
+
+from .common import *
+
+default_metrics = [
+    'gpu__time_duration.sum',
+    'gpu__dram_throughput.avg.pct_of_peak_sustained_elapsed',
+    # 'sm__inst_executed_pipe_fp16.avg.pct_of_peak_sustained_elapsed',
+    'sm__pipe_tensor_cycles_active.avg.pct_of_peak_sustained_elapsed',
+    'sm__throughput.avg.pct_of_peak_sustained_elapsed'
+]
+
+NCU_PATH = get_optional_env('NCU_PATH', 'ncu')
+NSYS_PATH = get_optional_env('NSYS_PATH', 'nsys')
+
+if not command_exists(NCU_PATH):
+    print(f'NCU not found (NCU_PATH={NCU_PATH}). Please set NCU_PATH to valid ncu binary.', file=sys.stderr)
+    sys.exit(1)
+
+if not command_exists(NSYS_PATH):
+    print(f'NSYS not found (NSYS_PATH={NSYS_PATH}). Please set NSYS_PATH to valid nsys binary.', file=sys.stderr)
+    sys.exit(1)
+
+@functools.lru_cache
+def get_ncu_version(path : str):
+    return subprocess.check_output([path, '--version']) \
+        .decode().strip().split('\n')[-1].split(' ')[1]
+
+
+@functools.lru_cache
+def get_nsys_version(path : str):
+    return subprocess.check_output([path, '--version']) \
+        .decode().strip().split(' ')[-1].split('-')[0]
+
+class Reader(object):
+    def __init__(self, g):
+        self.g = g
+    def read(self, n=0):
+        try: return next(self.g)
+        except StopIteration: return ''
+
+def read_ncu_output(output):
+    it = iter(output.split('\n'))
+    line = ''
+    while not line.startswith('"ID","Process ID","Process Name",'):
+        line = next(it)
+
+    yield line + '\n'
+
+    for line in it: yield line + '\n'
+
+def read_nsys_output(output):
+    it = iter(output.split('\n'))
+    line = ''
+    while not line.startswith('Start (ns)'):
+        line = next(it)
+
+    yield line + '\n'
+
+    for line in it: yield line + '\n'
+
+@dataclass(frozen=True)
+class NcuConfig:
+    replay_mode : str = 'application'
+    metrics : 'list[str]' = field(default_factory=lambda: default_metrics)
+    use_nvtx : bool = False
+    env : 'dict[str, str]' = field(default_factory=lambda: os.environ.copy())
+
+
+@dataclass(frozen=True)
+class NsysConfig:
+    num_samples : int = 1
+    env : 'dict[str, str]' = field(default_factory=lambda: os.environ.copy())
+
+def run_ncu(
+    prog_args : 'list[str]',
+    use_cuda_profiler_api : bool = False,
+    ncu_config : NcuConfig = NcuConfig(),
+    quiet : bool = False
+):
+    if not quiet: print('>>> Running NCU...')
+
+    ncu_env = ncu_config.env.copy()
+    ncu_env['CUDA_LAUNCH_BLOCKING'] = '1'
+
+    if ncu_config.use_nvtx:
+        nvtx_args = [
+            '--nvtx',
+            '--print-nvtx-rename', 'kernel'
+        ]
+    else:
+        nvtx_args = []
+
+    cmdline = [
+        NCU_PATH,
+        '--csv',
+        *nvtx_args,
+        '--profile-from-start', 'no' if use_cuda_profiler_api else 'yes',
+        '--replay-mode', ncu_config.replay_mode,
+        '--metrics', ','.join(ncu_config.metrics)
+    ] + dnd_config.ncu_extra_cli + prog_args
+
+    with check_subprocess():
+        ncu_output = subprocess.check_output(cmdline, env=ncu_env).decode()
+
+    if not quiet:print('>>> Done!')
+
+    return pd.read_csv(
+        Reader(read_ncu_output(ncu_output)),
+        low_memory=False,
+        thousands=r',')
+
+def run_nsys(
+    prog_args : 'list[str]',
+    use_cuda_profiler_api : bool = False,
+    nsys_config : NsysConfig = NsysConfig(),
+    quiet : bool = False
+):
+
+    with temp_file(suffix='.nsys-rep') as temp_nsys_rep:
+        if not quiet: print('>>> Running NSYS...')
+        nsys_env = nsys_config.env.copy()
+
+        cmdline = [
+            NSYS_PATH,
+            'profile',
+            '-t', 'cuda,cudnn,cublas',
+            '-o', temp_nsys_rep
+        ] + prog_args
+
+        if use_cuda_profiler_api:
+            cmdline += [
+                '--capture-range=cudaProfilerApi',
+                '--capture-range-end=stop'
+            ]
+
+        with check_subprocess():
+            subprocess.check_output(cmdline, env=nsys_env).decode()
+
+        if not quiet: print('>>> Done!')
+
+        report_name = {
+            '2022.1.3.3': 'gputrace',
+        }.get(get_nsys_version(NSYS_PATH), 'cuda_gpu_trace')
+
+        stats_cmdline = [
+            NSYS_PATH,
+            'stats',
+            '-r', report_name,
+            '-f', 'csv',
+            temp_nsys_rep
+        ]
+
+        with check_subprocess():
+            stats_output = subprocess.check_output(stats_cmdline).decode()
+
+    return pd.read_csv(
+        Reader(read_nsys_output(stats_output)),
+        low_memory=False,
+        thousands=r',')
+
+def run_ncu_nsys(
+    prog_args : 'list[str]',
+    use_cuda_profiler_api : bool = False,
+    ncu_config : NcuConfig = NcuConfig(),
+    nsys_config : NsysConfig = NsysConfig(),
+    quiet : bool = False
+) -> 'list[Kernel]':
+
+    ncu_df = run_ncu(
+        prog_args,
+        use_cuda_profiler_api,
+        ncu_config,
+        quiet
+    )
+
+    ncu_names = dict()
+    ncu_metrics = dict()
+
+    for row in ncu_df.iterrows():
+        row = row[1]
+        ncu_names[row['ID']] = row['Kernel Name']
+        if row['ID'] not in ncu_metrics: ncu_metrics[row['ID']] = dict()
+        ncu_metrics[row['ID']][row['Metric Name']] = row['Metric Value']
+
+    ncu_ordered_ids = sorted(ncu_names.keys())
+    ncu_ordered_names = [ncu_names[i] for i in ncu_ordered_ids]
+
+    nsys_dfs = [
+        run_nsys(
+            prog_args,
+            use_cuda_profiler_api,
+            nsys_config,
+            quiet
+        )
+        for _ in range(nsys_config.num_samples)
+    ]
+
+    if ncu_config.use_nvtx:
+        nvtx_ranges = []
+        new_names = []
+
+        for n in ncu_ordered_names:
+            if '/' in n:
+                nvtx_range, name = n.split('/')
+                nvtx_ranges.append(nvtx_range)
+                new_names.append(name)
+            else:
+                nvtx_ranges.append('')
+                new_names.append(n)
+
+        ncu_ordered_names = new_names
+
+    else: nvtx_ranges = [''] * len(ncu_ordered_names)
+
+    kerns = []
+
+    kid = 0
+    for rows in zip(*[df.iterrows() for df in nsys_dfs]):
+        rows = [r[1] for r in rows]
+        if kid >= len(ncu_ordered_ids): break
+        if rows[0]['Name'] != ncu_ordered_names[kid]: continue
+
+        assert all(r['Name'] == rows[0]['Name'] for r in rows)
+
+        kerns.append(Kernel(
+            kid=kid,
+            name=rows[0]['Name'],
+            nsys_avg_lat_ns=np.mean([r['Duration (ns)'] for r in rows]),
+            nvtx_range=nvtx_ranges[kid],
+            grid=(int(rows[0]['GrdX']), int(rows[0]['GrdY']), int(rows[0]['GrdZ'])),
+            block=(int(rows[0]['BlkX']), int(rows[0]['BlkY']), int(rows[0]['BlkZ'])),
+            reg_per_thread=int(rows[0]['Reg/Trd']),
+            static_smem=int(float(rows[0]['StcSMem (MB)']) * 2**20),
+            dynamic_smem=int(float(rows[0]['DymSMem (MB)']) * 2**20),
+            metrics=ncu_metrics[ncu_ordered_ids[kid]]
+        ))
+
+        kid += 1
+
+    assert kid == len(ncu_ordered_ids)
+    return kerns
+
