70eed80ab9c331fda455fe1307040bd9a9cd35b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:27 +0000

    Add package fisspy/1.0.2/fisspy-1.0.2-py3-none-any.whl

---
+++ packages/fisspy/fisspy-1.0.2-py3-none-any.whl/fisspy/__init__.py
@@ -0,0 +1,18 @@
+"""
+FISSPy
+======
+
+An free and open-source Python package for `GST/FISS <http://fiss.snu.ac.kr>`_ instrument.
+
+Links
+-----
+Homepage : http://fiss.snu.ac.kr \n
+Documentation : http://fiss.snu.ac.kr/fisspy
+"""
+
+from __future__ import absolute_import
+__author__="SNU Solar Group"
+__version__="1.0.2"
+
+
+from . import cm, makevideo, analysis, align, read, correction, preprocess, image, data
