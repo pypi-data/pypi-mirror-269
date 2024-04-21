af4cb650f3a041a810329d3f6c01e378ce7c79bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:03:14 +0000

    Add package jenkins-pysdk/1.2/jenkins_pysdk-1.2.tar.gz

---
+++ packages/jenkins-pysdk/jenkins_pysdk-1.2.tar.gz/jenkins_pysdk-1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jenkins_pysdk',
+    version='1.2',  # Don't forget to update version in version.py :)
+    packages=find_packages(),
+    install_requires=[
+        'httpx',
+        'orjson',
+        'pydantic',
+        'urllib3',
+    ],
+    author='KnownZero',
+    author_email="gihjeefs@protonmail.com",
+    description='2024 Python SDK for Jenkins',
+    url='https://github.com/KnownZero/JenkinsPythonSDK',
+)
