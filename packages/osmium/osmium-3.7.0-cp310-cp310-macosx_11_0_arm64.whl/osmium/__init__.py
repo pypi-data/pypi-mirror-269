2aa4d7b1b788cfb5556c5f2b892ca4a999119104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:48 +0000

    Add package osmium/3.7.0/osmium-3.7.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/osmium/osmium-3.7.0-cp310-cp310-macosx_11_0_arm64.whl/osmium/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# This file is part of pyosmium. (https://osmcode.org/pyosmium/)
+#
+# Copyright (C) 2024 Sarah Hoffmann <lonvia@denofr.de> and others.
+# For a full list of authors see the git log.
+
+from osmium._osmium import *
+from osmium.helper import *
+from osmium.simple_handler import SimpleHandler
+from osmium.file_processor import FileProcessor
+import osmium.io
+import osmium.osm
+import osmium.index
+import osmium.geom
+import osmium.area
+import osmium.filter
