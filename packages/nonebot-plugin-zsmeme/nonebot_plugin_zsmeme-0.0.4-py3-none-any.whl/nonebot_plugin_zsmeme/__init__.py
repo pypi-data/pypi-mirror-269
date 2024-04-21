9373474c9cadf40da27ae6df5783b9157f554020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:00 +0000

    Add package nonebot-plugin-zsmeme/0.0.4/nonebot_plugin_zsmeme-0.0.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-zsmeme/nonebot_plugin_zsmeme-0.0.4-py3-none-any.whl/nonebot_plugin_zsmeme/__init__.py
@@ -0,0 +1,23 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.plugin import PluginMetadata
+from .utils import the_meme_url
+
+
+__plugin_meta__ = PluginMetadata(
+    name='战双表情',
+    description='发送bwiki的战双表情',
+    usage='使用命令：战双表情/zsmeme',
+    type="application",
+    homepage="https://github.com/shi-yingyingjiang/nonebot-plugin-zsmeme",
+    supported_adapters = {"nonebot.adapters.onebot.v11"},
+)
+
+
+pgr_meme = on_command("战双表情", aliases={'zsmeme','pgrmeme'})
+
+
+@pgr_meme.handle()
+async def get_img():
+    the_url = await the_meme_url()
+    await pgr_meme.send(MessageSegment.image(the_url))
