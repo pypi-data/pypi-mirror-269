5b77a98a5d1f53048114e7df4144592f7b5aff03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:28 +0000

    Add package BJSCP/0.0.3/BJSCP-0.0.3-py3-none-any.whl

---
+++ packages/BJSCP/BJSCP-0.0.3-py3-none-any.whl/BJSCP/CP.py
@@ -0,0 +1,170 @@
+import subprocess
+import datetime
+import re
+import os
+
+__in_ext__ = "in"
+__out_ext__ = "out"
+__ans_ext__ = "ans"
+
+# utility
+def dropExtension(filename: str, extension: str):
+    return filename.rstrip(extension)
+
+def getFileName(filename, default):
+    if filename is None:
+        return default
+    return filename
+
+# === Run Each ===
+def execute(sourceName: str,
+            ioFileName: str = None,
+            timeout: float = None,
+            verbose: bool = True,
+            ) -> (bool, int):
+    rawName = dropExtension(sourceName, ".py")  # Drop extension
+
+    # Direct file
+    input_file = getFileName(ioFileName, rawName) + "." + __in_ext__
+    output_file = getFileName(ioFileName, rawName) + "." + __out_ext__
+
+    (fin, fout) = (None, None)
+    try:
+        fin = open(input_file, "r")
+        fout = open(output_file, "w")
+
+        start = datetime.datetime.now()     # execute target source code
+        subprocess.call(
+            f"python {rawName}.py",
+            stdin=fin,
+            stdout=fout,
+            timeout=timeout
+        )
+        end = datetime.datetime.now()
+        elapse = (end - start).microseconds // 1000     # check elapsed time
+
+        if verbose:
+            print(f"Executed '{sourceName}' for '{ioFileName}': {elapse}ms")
+        return True, elapse
+    except FileNotFoundError as e:          # There is not a file
+        print(f"There is not file '{e.filename}'")
+    except subprocess.TimeoutExpired:       # Limited time out
+        if verbose:
+            print(f"Timeout '{sourceName}' for '{ioFileName}'")
+        return False, timeout
+    finally:
+        if fin is not None:     # close files
+            fin.close()
+        if fout is not None:
+            fout.close()
+    return False, 0
+
+def test(sourceName: str,
+            ioFileName: str = None,
+            timeout: float = None,
+            verbose: bool = True
+            ):
+    rawName = dropExtension(sourceName, ".py")  # Drop extension
+
+    # execute
+    (state, elapse) = execute(sourceName, ioFileName=ioFileName, timeout=timeout, verbose=False)
+    if(not state):  # Run Fail
+        if elapse == timeout and verbose:   # Timeout
+            print(f"Timeout '{sourceName}' for '{ioFileName}'")
+        return state, elapse
+
+    # Direct file
+    submit_file = getFileName(ioFileName, rawName) + "." + __out_ext__
+    answer_file = getFileName(ioFileName, rawName) + "." + __ans_ext__
+
+    (fsub, fans) = (None, None)
+    try:
+        fsub = open(submit_file, "r")
+        fans = open(answer_file, "r")
+
+        while True:
+            # readline and normalization
+            while subLine := fsub.readline():
+                subLine = re.sub("\\s+", " ", subLine)
+                subLine = subLine.strip()
+                if len(subLine) > 0:
+                    break
+            while ansLine := fans.readline():
+                ansLine = re.sub("\\s+", " ", ansLine)
+                ansLine = ansLine.strip()
+                if len(ansLine) > 0:
+                    break
+
+            if not subLine and not ansLine:     # Pass
+                print(f"Passed {sourceName} for '{ioFileName}': {elapse}ms")
+                return (True, elapse)
+            elif subLine or ansLine:    # Continuous
+                if subLine == ansLine:
+                    pass
+                else:   # Fail: unmatched
+                    print(f"Failed {sourceName} for '{ioFileName}': {elapse}ms")
+                    break
+            else:   # Fail: unmatched
+                print(f"Failed {sourceName} for '{ioFileName}': {elapse}ms")
+                break
+
+        return (False, elapse)
+    except FileNotFoundError as e:
+        print(f"There is not file '{e.filename}'")
+    finally:
+        if fsub is not None:
+            fsub.close()
+        if fans is not None:
+            fans.close()
+
+
+# === Run All ===
+def executeAll(filename: str, ioPath: str = None, timeout: float = None, verbose: bool = True):
+    if ioPath is None:
+        ioPath = os.path.dirname(filename)
+
+    for file in os.listdir(ioPath):
+        if re.match("\\w+\\."+__in_ext__, file):
+            ioFile = ioPath + '/' + dropExtension(file, "." + __in_ext__)
+            execute(filename, ioFileName=ioFile, timeout=timeout, verbose=verbose)
+
+
+def testAll(filename: str, ioPath: str = None, timeout: float = None, verbose: bool = True):
+    if ioPath is None:
+        ioPath = os.path.dirname(filename)
+
+    for file in os.listdir(ioPath):
+        if re.match("\\w+\\."+__in_ext__, file):
+            ioFile = ioPath + '/' + dropExtension(file, "." + __in_ext__)
+            test(filename, ioFileName=ioFile, timeout=timeout, verbose=verbose)
+
+
+def makeSampleFile(filename: str, text: str):
+    fout = open(filename, "w")
+    fout.write(text)
+    fout.close()
+
+
+def loadSample(verbose: bool = True):
+    dirName = "SampleData"
+    os.makedirs(dirName, exist_ok=True)
+
+    # source file
+    makeSampleFile(dirName + "/test.py", """
+a = int(input())
+b = int(input())
+
+print(f"a + b = {a + b}")
+    """)
+    # test file 0: expected pass
+    makeSampleFile(dirName + "/test0.in", "3\n4")
+    makeSampleFile(dirName + "/test0.ans", "a + b = 7")
+    # test file 1: expected fail
+    makeSampleFile(dirName + "/test1.in", "1\n1")
+    makeSampleFile(dirName + "/test1.ans", "a + b = 5")
+    # test file 2: expected pass
+    makeSampleFile(dirName + "/test2.in", "2\n1")
+    makeSampleFile(dirName + "/test2.ans", "a + b = 3")
+
+    if(verbose):
+        print("SampleData가 성공적으로 생성되었습니다.")
---
+++ packages/BJSCP/BJSCP-0.0.3-py3-none-any.whl/BJSCP/__init__.py
@@ -0,0 +1,5 @@
+from BJSCP.CP import test, testAll, execute, executeAll
+from BJSCP.CP import __in_ext__, __out_ext__, __ans_ext__
+from BJSCP.CP import loadSample
+
+__version__ = "0.0.3"
