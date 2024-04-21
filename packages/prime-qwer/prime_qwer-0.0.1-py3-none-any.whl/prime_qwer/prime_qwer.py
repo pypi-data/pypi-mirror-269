f3868e0d209736b647562b1bd845af5b32e4ecc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:07 +0000

    Add package prime-qwer/0.0.1/prime_qwer-0.0.1-py3-none-any.whl

---
+++ packages/prime-qwer/prime_qwer-0.0.1-py3-none-any.whl/prime_qwer/prime_qwer.py
@@ -0,0 +1,26 @@
+def prime_num(number):
+    if number <= 1:
+        return False
+    elif number <= 3:
+        return True
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def prime_factors(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    for i in range(3, int(n**0.5) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 2:
+        factors.append(n)
+    return factors
