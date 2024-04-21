05092ec5b3c735ab6aca24d8ff70c114818bcdae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:26 +0000

    Add package moss-cappa/0.1.1/moss_cappa-0.1.1.tar.gz

---
+++ packages/moss-cappa/moss_cappa-0.1.1.tar.gz/moss_cappa-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert_file('README.md', 'rst')
+   long_description = long_description.replace("\r","")  
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='moss_cappa',
+    version='0.1.1',
+    description='Python-клиент Moss, предназначенный для использования в комплексе автоматической проверки программ CAPPA',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='shroud007',
+    url='https://github.com/Shroud007/moss_cappa',
+    download_url='https://github.com/Shroud007/moss_cappa/releases',
+    keywords=['cappa', 'moss', 'plagiarism'],
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'lxml'
+    ],
+)
