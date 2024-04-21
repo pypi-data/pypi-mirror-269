7311f752247260fc0d087e19ce26a92a17473b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:43 +0000

    Add package gliner-spacy/0.0.5/gliner-spacy-0.0.5.tar.gz

---
+++ packages/gliner-spacy/gliner-spacy-0.0.5.tar.gz/gliner-spacy-0.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='gliner-spacy',
+    version='0.0.5', 
+    author='William J. B. Mattingly',
+    description='A SpaCy wrapper for the GLiNER model for enhanced Named Entity Recognition capabilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/theirstory/gliner-spacy',
+    packages=find_packages(),
+    install_requires=[
+        'spacy',
+        'gliner',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',  
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7,<=3.10',
+)
