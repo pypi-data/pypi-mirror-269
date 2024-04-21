5d4eea00aebcbc5c498fd77fb6054817c6451c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:33 +0000

    Add package cerbogen/0.0.20/cerbogen-0.0.20.tar.gz

---
+++ packages/cerbogen/cerbogen-0.0.20.tar.gz/cerbogen-0.0.20/setup.py
@@ -0,0 +1,50 @@
+
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import os
+
+# Custom installation class to run setup.py after installation
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        subprocess.call([sys.executable, os.path.join(self.install_lib, 'cerbogen', 'setup.py')])
+
+setup(
+    name='cerbogen',
+    version = '0.0.20',
+    description='A Python package for CerboTech',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Clarence Parmar',
+    author_email='git.clarence@gmail.com',
+    url='https://github.com/clarenceparmar/cerbogen',
+    packages=find_packages(),
+    
+    package_data={
+
+        '': ['ffmpeg/mac/*', 'data/img/*' ]
+    },
+
+    install_requires=[
+
+        "matplotlib", "pydub", "numpy" , "librosa" 
+
+    ],
+    
+    python_requires='>=3.6',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+    }
+
+    
+)
