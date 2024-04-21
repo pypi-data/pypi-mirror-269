c62ac15382947c8272e440abaec02617798d5636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:30 +0000

    Add package nonebot-plugin-yinying-chat/1.1/nonebot_plugin_yinying_chat-1.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-yinying-chat/nonebot_plugin_yinying_chat-1.1-py3-none-any.whl/nonebot_plugin_yinying_chat/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+import nonebot
+
+
+#定义配置文件
+class Config(BaseModel):
+    yinying_api_key: Optional[str] = "" # 在这里输入银影APIKEY
+    yinying_model_name: Optional[str] = "" #在这里输入模型名称
+    yinying_app_id: Optional[str] = ""    #在这里输入AppID
+    enable_private_chat: bool = True    #是否开启私聊
+    yinying_chat_public: bool = False  # 群聊是否开启公共会话
+
+
+class ConfigError(Exception):
+    pass
+
+global_config = nonebot.get_driver().config
