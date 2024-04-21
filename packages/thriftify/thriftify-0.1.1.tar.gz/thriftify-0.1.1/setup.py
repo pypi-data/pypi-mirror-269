af2d3d7c7a0caf1be8a6299d21af3cec85ef752f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:00 +0000

    Add package thriftify/0.1.1/thriftify-0.1.1.tar.gz

---
+++ packages/thriftify/thriftify-0.1.1.tar.gz/thriftify-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(name='thriftify',
+      version='0.1.1',
+      description='Converts xlsx files into thrift binary or json blobs',
+      long_description='Uses thrift-generated python reflection classes to match spreadsheet data with your thrift file. You can then load the resulting blob in any language supported by thrift.',
+      long_description_content_type='text/x-rst',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+      ],
+      keywords='thrift config',
+      url='http://github.com/ohthepain/thriftify',
+      author='Paul Wilkinson',
+      author_email='paul@thisco.co',
+      license='MIT',
+      packages=['thriftify'],
+      install_requires=[
+            'openpyxl',
+            'datetime',
+            'thrift',
+            'argparse',
+            'datetime',
+      ],
+      scripts=['bin/thriftify'],
+      entry_points = {
+        'console_scripts': ['thriftify=thriftify.command_line:main'],
+      },
+      zip_safe=False)
+
