75186f542c629fa46313c70b9a6fef067bb326c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:26 +0000

    Add package wagtail-fedit/1.5.3rc4/wagtail_fedit-1.5.3rc4.tar.gz

---
+++ packages/wagtail-fedit/wagtail_fedit-1.5.3rc4.tar.gz/wagtail_fedit-1.5.3rc4/wagtail_fedit/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+
+app_name = "wagtail_fedit"
+
+urlpatterns = [
+    # Frontend Editing
+    path(
+        "edit/<str:adapter_id>/<str:app_label>/<str:model_name>/<str:model_id>/<str:field_name>/", 
+        views.EditAdapterView.as_view(), name="edit"
+    ),
+]
+
+model_based_views = (
+    ("editable", views.FEditableView),
+    ("publish", views.PublishView),
+    ("submit", views.SubmitView),
+    ("unpublish", views.UnpublishView),
+    ("cancel", views.CancelView),
+)
+
+for name, view in model_based_views:
+    view.url_name = f"wagtail_fedit:{name}"
+    view.url_pattern = f"{name}/<str:object_id>/<str:app_label>/<str:model_name>/"
+    urlpatterns.append(
+        path(view.url_pattern, view.as_view(), name=name)
+    )
