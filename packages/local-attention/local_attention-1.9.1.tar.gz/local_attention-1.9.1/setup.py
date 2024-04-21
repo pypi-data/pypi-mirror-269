3d9b7b93b72512b14f9356422969b502a0d1ce53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:02:57 +0000

    Add package local-attention/1.9.1/local_attention-1.9.1.tar.gz

---
+++ packages/local-attention/local_attention-1.9.1.tar.gz/local_attention-1.9.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'local-attention',
+  packages = find_packages(),
+  version = '1.9.1',
+  license='MIT',
+  description = 'Local attention, window with lookback, for language modeling',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/local-attention',
+  keywords = [
+    'transformers',
+    'attention',
+    'artificial intelligence'
+  ],
+  install_requires=[
+    'einops>=0.6.0',
+    'torch'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
