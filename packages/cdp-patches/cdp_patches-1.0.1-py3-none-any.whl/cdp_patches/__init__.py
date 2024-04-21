a443159f2698ec0328897784101dba9609b5a434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:03:13 +0000

    Add package cdp-patches/1.0.1/cdp_patches-1.0.1-py3-none-any.whl

---
+++ packages/cdp-patches/cdp_patches-1.0.1-py3-none-any.whl/cdp_patches/__init__.py
@@ -0,0 +1,13 @@
+import platform
+
+VERSION = "1.0.1"
+
+system_name = platform.system()
+if system_name == "Windows":
+    is_windows = True
+elif system_name == "Linux":
+    is_windows = False
+else:
+    raise SystemError("Unknown system (You´re probably using MacOS, which is currently not supported).")
+
+__all__ = ["VERSION", "is_windows"]
