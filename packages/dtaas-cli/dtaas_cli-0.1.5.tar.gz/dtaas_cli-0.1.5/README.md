8b480884849cd72ca81c7e1ee5d7d9fbbe240dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 15:03:22 +0000

    Add package dtaas-cli/0.1.5/dtaas_cli-0.1.5.tar.gz

---
+++ packages/dtaas-cli/dtaas_cli-0.1.5.tar.gz/dtaas_cli-0.1.5/README.md
@@ -0,0 +1,84 @@
+# DTaaS Command Line Interface
+
+This is a command line tool for the INTO-CPS-Association Digital Twins as a Service. 
+
+## Prerequisite
+
+Please visit and download the [DTaaS Software](https://github.com/INTO-CPS-Association/DTaaS). 
+
+The DTaaS service with base users and essential containers should be up and running before using the CLI.
+
+
+## Installation
+
+Simply install using:
+
+We recommend installing this in a virutal environment.
+
+Steps to install:
+
+- Change the working folder:
+
+```bash
+cd <DTaaS-directory>/cli
+```
+
+- Recommended (for windows) to install this in a virtual environment
+
+```bash
+python -m venv env
+env\Scripts\activate
+```
+
+- To install, simply:
+
+```bash
+pip install dtaas-cli
+```
+
+## Usage
+
+### Setup
+
+Setup the _dtaas.toml_ file in the _cli_ directory,
+edit the fields appropriately.
+
+### Add users
+
+To add new users using the CLI, fill in the _users.add_ list in
+_dtaas.toml_ with the Gitlab instance usernames of the users to be added
+
+Then simply:
+
+```bash
+dtaas admin user add
+```
+
+### Delete users
+
+To delete existing users, fill in the _users.delete_ list in
+_dtaas_.toml_ with the Gitlab instance usernames of the users to be deleted.
+
+Then simply:
+
+```bash
+dtaas admin user delete
+```
+
+### Additional Points to Remember
+
+- The base DTaaS system should be up and running before adding/deleting users with the CLI
+
+- The _user add_ CLI will add and start a container for a new user.
+  It can also start a container for an existing user if that container was somehow stopped.
+  It shows a _Running_ status for existing user containers that are already up and running,
+  it doesn't restart them.
+
+- Configure the _server-dns_ in the _dtaas.toml_ file with the domain name of your server.
+  If you want to bring up the server locally, please set this to _"localhost"_.
+
+- _user add_ and _user delete_ CLIs return an error if the _add_ and _delete_ lists in
+  _dtaas.toml_ are empty, respectively.
+
+- Currently the _email_ fields for each user in dtaas.toml are not in use, and are not necessary
+  to fill in. These emails must be configured manually for each user in the docker/
---
+++ packages/dtaas-cli/dtaas_cli-0.1.5.tar.gz/dtaas_cli-0.1.5/src/pkg/utils.py
@@ -0,0 +1,73 @@
+"This file has generic helper functions and variables for dtaas cli"
+
+import yaml
+import tomlkit
+
+LOCALHOST_SERVER = "localhost"
+
+def importYaml(filename):
+    """This function is used to import a yaml file safely"""
+    config = {}
+    try:
+        with open(filename,'r') as file:
+            config = yaml.safe_load(file)
+    except FileNotFoundError:
+        return {}, None
+    except Exception as err:
+        return None, Exception(f"Error while getting yaml file: {filename}, " + str(err))
+    return config if config is not None else {}, None
+
+def exportYaml(data, filename):
+    """This function is used to export to a yaml file safely"""
+    try:
+        with open(filename, 'w') as file:
+            yaml.safe_dump(data, file, sort_keys=False)
+    except Exception as err:
+        return Exception(f"Error while writing yaml to file: {filename}, " + str(err))
+    return None
+
+def importToml(filename):
+    """This function is used to import a toml file safely"""
+    try:
+        with open(filename, 'r') as file:
+            config = tomlkit.load(file)
+    except Exception as err:
+        return None, Exception(f"Error while getting toml file: {filename}, " + str(err))
+    return config, None
+
+def replaceAll(obj, mapping):
+    """This function is used to replace all placeholders with values in a nested object"""
+    if isinstance(obj,str):
+        obj, err = replaceString(obj,mapping)
+        return obj, err
+
+    if isinstance(obj,list):
+
+        for ind, val in enumerate(obj):
+            obj[ind], err = replaceAll(val, mapping)
+            if err is not None:
+                return None, err
+        return obj, None
+
+    if isinstance(obj,dict):
+
+        for key in obj:
+            if not isinstance(key,str):
+                return None, Exception("Config substitution failed: Key is not a string")
+            obj[key], err = replaceAll(obj[key], mapping)
+            if err is not None:
+                return None, err
+        return obj, None
+
+    return None, Exception("Config substition failed: Object format not valid")
+
+def replaceString(obj, mapping):
+    for key in mapping:
+        obj = obj.replace(key, mapping[key])
+    return obj, None
+
+
+
+def checkError(err):
+    if err is not None:
+        raise err
