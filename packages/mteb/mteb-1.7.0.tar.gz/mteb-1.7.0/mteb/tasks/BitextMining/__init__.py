ea231f9ae4c5758149e61aaca235240dd151388c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:03:00 +0000

    Add package mteb/1.7.0/mteb-1.7.0.tar.gz

---
+++ packages/mteb/mteb-1.7.0.tar.gz/mteb-1.7.0/mteb/tasks/BitextMining/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from .dan.BornholmskBitextMining import *
+from .multilingual.BUCCBitextMining import *
+from .multilingual.DiaBLaBitextMining import *
+from .multilingual.FloresBitextMining import *
+from .multilingual.norwegian_courts_bitext_mining import *
+from .multilingual.NorwegianCourtsBitextMining import *
+from .multilingual.RomaTalesBitextMining import *
+from .multilingual.TatoebaBitextMining import *
+from .vie.VieMedEVBitextMining import *
