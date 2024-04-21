784b46a8c2157d38f93fef329668a95b53612da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:18 +0000

    Add package gtsystem/0.1.7/gtsystem-0.1.7.tar.gz

---
+++ packages/gtsystem/gtsystem-0.1.7.tar.gz/gtsystem-0.1.7/README.md
@@ -0,0 +1,216 @@
+# GenAI Techne System (gtsystem)
+*A low code Python package for crafting GenAI applications quickly*
+
+GenAI Techne is on a mission to help enterprise and professionals excel in the craft of Generative AI. Check out the [GenAI Techne Substack](https://genaitechne.substack.com/) where you can read more about our mission, read gtsystem documentation, learn from step-by-step tutorials, and influence the roadmap of gtsystem for your use cases.
+
+[![](https://raw.githubusercontent.com/GenaiTechne/gtsystem/main/gtsystem.jpeg)](https://genaitechne.substack.com/)
+
+## Getting Started
+
+The get started using `gtsystem` package follow these steps.
+
+**Step 1.** Install gtsystem package using `pip install gtsystem`
+
+**Step 2.** Open a Jupyter notebook and try this sample.
+
+```python
+from gtsystem import openai, bedrock, anthropic, ollama, instrument
+prompt = 'How many faces does a tetrahedron have?'
+openai.text(prompt)
+bedrock.text(prompt)
+anthropic.text(prompt)
+ollama.text(prompt)
+instrument.metrics.stats()
+```
+
+Note: To install the dependencies and setup each of the vendor APIs you can continue reading [here](https://github.com/GenaiTechne/gtsystem?tab=readme-ov-file#installing-dependencies).
+
+## Features and Notebook Samples
+
+The `gtsystem` package source is available in this [repository on GitHub](https://github.com/GenaiTechne/gtsystem).
+
+You can read more about the vision behind gtsystem on the [GenAI Techne substack post](https://genaitechne.substack.com/p/excelling-in-the-craft-of-generative).
+
+[![](https://raw.githubusercontent.com/GenaiTechne/gtsystem/main/gtsystem-features.jpg)](https://genaitechne.substack.com/p/excelling-in-the-craft-of-generative)
+
+You can learn `gtsystem` API by following along the [notebook samples](https://github.com/GenaiTechne/gtsystem/tree/main/notebooks) included in the `gtsystem` repo. This samples are documented on the [GenAI Techne Substack](https://genaitechne.substack.com/).
+
+0. **Review model leaderboard:** Use `00-leaderboard.ipynb` for reviewing model leaderboard, filter by ranking, vendors, models to decide which one to explore.
+
+1. **Evaluate models using one line of code:** Refer `01-evaluate.ipynb` for single statement prompt evaluations across multiple models including OpenAI GPT, Bedrock hosted Claude or Llama.
+
+2. **Render LLM responses:** Use `02-render.ipynb` for well formatted rendering of the model responses including markdown tables.
+
+3. **Load evaluation tasks from Excel:** Try `03-tasks.ipynb` for automating evaluation tasks - find, list, load prompts by task, including optimal parameter values for temperature and TopP.
+
+4. **Instrument speed and size (cost) of response:** Reuse `04-instrument.ipynb` for instrumenting and comparing multiple models across latency and size of response.
+
+5. **Benchmark quality of response:** Use `05-benchmark.ipynb` for automated benchmarking quality of responses from models like Llama and Claude using GPT-4 as an LLM evaluator.
+
+6. **Run models on your laptop:** Get `06-ollama.ipynb` to run models like Mistral, Llama, and CodeLlama locally on your laptop and compare models hosted on Cloud or proprietary model APIs.
+
+7. **Low code sample:** Check out simple `07-low-code-sample.ipynb` to appreciate how much can be done with just simple GTSystem APIs.
+
+8. **Go from prototype to production:** Start with `08-prototype-to-production.ipynb` to go from prototyping using best models, then exploring local models on laptop, finally comparing fastest vendors like Groq in one seamless workflow.
+
+9. **Visual chat on Bedrock:** Explore `09-chat-bedrock.ipynb` for visual chat using Bedrock hosted models.
+
+10. **Visual chat on Anthropic:** Explore `10-chat-anthropic.ipynb` for visual chat using Anthropic hosted models.
+
+11. **Visual chat on OpenAI:** Explore `11-chat-openai.ipynb` for visual chat using OpenAI GPT4-Turbo.
+
+## What's New
+
+### 2024-04-20 (Release 0.1.7)
+
+- Render from cached chat history to save time and cost
+- Added chat.search feature to search saved openai and claude chats
+- Fixed markdown table rendering for render.chat
+- Optimized GptChat and ClaudeChat classes to inherit from BaseChat
+
+### 2024-04-20 (Release 0.1.6)
+
+- Improved benchmark.quality to provide step-by-step weighted criteria based quality scoring
+- Parametrized benchmark quality criteria using data/config.yaml
+- Minor fixes to groq API
+
+### 2024-04-19 (Release 0.1.5)
+
+- Groq with Llama3
+- Ollama with Llama3
+
+### 2024-04-19 (Release 0.1.4)
+
+- Stream chat and text for OpenAI
+- openai.chat() simplifier API
+- GenAI Advisor - Trends feature
+
+### 2024-04-17 (Release 0.1.3)
+
+- Save, list, load, and render chat for Bedrock/Claude3
+- Save, list, load, and render chat for Anthropic
+- Save, list, load, and render chat for OpenAI
+
+### 2024-04-15 (Release 0.1.2)
+
+- Multimodal Chat using OpenAI.
+
+### 2024-04-14 (Release 0.1.1)
+
+- Multimodal Chat using Anthropic/Claude.
+- Multimodal Chat using Bedrock/Claude.
+- LMSYS Leaderboard lite integration.
+
+## Installing Dependencies
+
+You can install following dependencies to work with `gtsystem` based on your needs. Start with our `requirements.txt` or create your own. Then run `pip install -r requirements.txt` within your environment.
+
+```
+# Python capabilities
+pandas
+markdown
+openpyxl
+
+# Jupyter notebook
+jupyterlab
+ipywidgets
+
+# AWS for Bedrock managed models
+boto3
+awscli
+botocore
+
+# OpenAI for GPT models
+openai
+
+# Anthropic models
+anthropic
+
+# Ollama for LLMs running on your laptop
+ollama
+
+# Groq for open models on fast Groq LPUs
+groq
+```
+
+## Amazon Bedrock Setup
+
+To use [Amazon Bedrock](https://aws.amazon.com/bedrock/) hosted models like Llama and Claude follow these steps.
+
+**Step 1.** Login to AWS Console > Launch Identity and Access Management (IAM) > Create a user for Command-Line Interface (CLI) access. 
+*Read [Bedrock documentation](https://docs.aws.amazon.com/bedrock/latest/userguide/security-iam.html) for more details.*
+
+**Step 2.** Install [AWS CLI](https://aws.amazon.com/cli/) > Run `aws configure` in Terminal > Add credentials from Step 1.
+
+## Ollama Setup
+
+To use Ollama provided LLMs locally on your laptop follow these steps.
+
+**Step 1.** Download [Ollama](https://ollama.com/)
+*Note the memory requirements for each model. 7b models generally require at least 8GB of RAM. 13b models generally require at least 16GB of RAM. 70b models generally require at least 64GB of RAM*
+
+**Step 2.** Find model Ollama [library](https://ollama.com/library) > Run command `ollama pull <model>` in terminal to download the model.
+*Currently gtsystem supports popular models like llama2, mistral, and llava.*
+
+## OpenAI Setup
+
+To use OpenAI models follow these steps.
+
+**Step 1.** Signup for [OpenAI API](https://openai.com/blog/openai-api) access and get the API key.
+
+**Step 2.** Add OpenAI API Key to your `~/.zshrc` or `~/.bashrc` using `export OPENAI_API_KEY="your-key-here"`
+
+## Groq Setup
+
+To use open models on fast Groq LPUs follow these steps.
+
+**Step 1.** Signup for [Groq API](https://console.groq.com/) access and get the API key.
+
+**Step 2.** Add Groq API Key to your `~/.zshrc` or `~/.bashrc` using `export GROQ_API_KEY="your-key-here"`
+
+
+## Basic Python Environment
+
+If you are new to Python then here is how you can get started from scratch.
+
+First, you should be running the latest Python on your system with Python package manager upgraded to the latest.
+
+```bash
+python --version
+# should return Python 3.10.x or higher as on Jan'23
+pip --version
+# should return pip 22.3.x or higher as on Jan'23
+```
+
+Follow this guide for [Mac OS X](https://docs.python-guide.org/starting/install3/osx/) if you do not have the latest Python. If installing specific version of Python for managing dependencies then follow [this thread](https://apple.stackexchange.com/questions/237430/how-to-install-specific-version-of-python-on-os-x) to install using `pyenv` Python version manager. If required upgrade pip to the latest using the following command.
+
+```bash
+pip install --user --upgrade pip
+```
+
+We will now create a virtual environment for our MLOps setup so that our dependencies are isolated and do not conflict with the system installed packages. We will follow [this guide](https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/#creating-a-virtual-environment) for creating and managing the virtual environment. First change to the directory where we will develop our application.
+
+```bash
+python -m venv env
+```
+
+If you run ls env you will see following folders and files created.
+
+```bash
+bin        include    lib        pyvenv.cfg
+```
+
+Now we can activate our virtual environment like so. You will notice that development directory prefixed with the (env) to indicate you are now running in the virtual environment.
+
+```bash
+. env/bin/activate
+```
+
+You can confirm that you are not running inside the virtual environment with its own Python.
+
+```bash
+which python
+## should return /Users/.../env/bin/python
+```
+
+To leave the virtual environment using deactivate command. Re-enter using same command as earlier.
---
+++ packages/gtsystem/gtsystem-0.1.7.tar.gz/gtsystem-0.1.7/gtsystem/bedrock.py
@@ -0,0 +1,295 @@
+import json
+from venv import logger
+import os
+
+import boto3
+from botocore.config import Config
+from botocore.exceptions import ClientError
+
+from .instrument import metrics
+from .chat import ClaudeChat
+
+BEDROCK_RUNTIME = None
+BEDROCK = None
+
+def _get_client(assumed_role=None, region=None, runtime=True):
+    if region is None:
+        target_region = os.environ.get("AWS_REGION", os.environ.get("AWS_DEFAULT_REGION"))
+    else:
+        target_region = region
+
+    session_kwargs = {"region_name": target_region}
+    client_kwargs = {**session_kwargs}
+
+    profile_name = os.environ.get("AWS_PROFILE")
+    if profile_name:
+        session_kwargs["profile_name"] = profile_name
+
+    retry_config = Config(
+        region_name=target_region,
+        retries={
+            "max_attempts": 10,
+            "mode": "standard",
+        },
+    )
+    session = boto3.Session(**session_kwargs)
+
+    if assumed_role:
+        sts = session.client("sts")
+        response = sts.assume_role(
+            RoleArn=str(assumed_role),
+            RoleSessionName="langchain-llm-1"
+        )
+        client_kwargs["aws_access_key_id"] = response["Credentials"]["AccessKeyId"]
+        client_kwargs["aws_secret_access_key"] = response["Credentials"]["SecretAccessKey"]
+        client_kwargs["aws_session_token"] = response["Credentials"]["SessionToken"]
+
+    if runtime:
+        service_name='bedrock-runtime'
+    else:
+        service_name='bedrock'
+
+    bedrock_client = session.client(
+        service_name=service_name,
+        config=retry_config,
+        **client_kwargs
+    )
+
+    return bedrock_client
+
+def _init_runtime():
+    global BEDROCK_RUNTIME
+    BEDROCK_RUNTIME = _get_client(
+        assumed_role=os.environ.get("BEDROCK_ASSUME_ROLE", None),
+        region=os.environ.get("AWS_DEFAULT_REGION", None)
+    )
+
+def _init():
+    global BEDROCK
+    BEDROCK = _get_client(
+        assumed_role=os.environ.get("BEDROCK_ASSUME_ROLE", None),
+        region=os.environ.get("AWS_DEFAULT_REGION", None),
+        runtime=False
+    )
+
+def list_models(vendor):
+    if BEDROCK is None:
+        _init()
+    listModels = BEDROCK.list_foundation_models(byProvider=vendor)
+    print("\n".join(list(map(lambda x: f"{x['modelName']} : { x['modelId'] }", listModels['modelSummaries']))))
+
+CHAT = ClaudeChat()
+
+def _claude3_chat(prompt, system='', temperature=0.0, topP=1, tokens=4096, model="", image_url="", 
+                  reset=False, cache=False):
+    if cache:
+        cache_match = CHAT.match(prompt)
+        if cache_match:
+            CHAT.load(cache_match)
+            return next(msg['content'] for msg in CHAT.messages if msg['role'] == 'assistant')
+
+    if reset:
+        CHAT.reset_context()
+    
+    if system != "":
+        CHAT.set_system(system)
+    
+    if image_url != "":
+        CHAT.add_image_message(image_url=image_url, prompt=prompt)
+    else:
+        CHAT.add_message("user", prompt)
+
+    try:
+        if BEDROCK_RUNTIME is None:
+            _init_runtime()
+
+        response = BEDROCK_RUNTIME.invoke_model(
+            modelId=model,
+            body=json.dumps(
+                {
+                    "anthropic_version": "bedrock-2023-05-31",
+                    "system": CHAT.get_system(),
+                    "max_tokens": tokens,
+                    "temperature": temperature,
+                    "top_p": topP,
+                    "max_tokens": tokens,
+                    "messages": CHAT.get_messages(),
+                }
+            ),
+        )
+
+        result = json.loads(response.get('body').read())
+        CHAT.add_message("assistant", result['content'][0]['text'])
+
+        return result['content'][0]['text']
+
+    except ClientError as err:
+        logger.error(
+            "Couldn't invoke Claude 3. Here's why: %s: %s",
+            err.response["Error"]["Code"],
+            err.response["Error"]["Message"],
+        )
+        raise
+
+
+@metrics.track
+def sonnet_chat(prompt, system='', temperature=0.0, topP=1.0, tokens=512, image_url="", 
+                reset=False, cache=False):
+    return _claude3_chat(prompt, system=system, 
+                         temperature=temperature, topP=topP, tokens=tokens, 
+                         model='anthropic.claude-3-sonnet-20240229-v1:0', 
+                         image_url=image_url, reset=reset, cache=cache)
+
+@metrics.track
+def haiku_chat(prompt, system='', temperature=0.0, topP=1.0, tokens=512, image_url="", 
+               reset=False, cache=False):
+    return _claude3_chat(prompt, system=system, 
+                         temperature=temperature, topP=topP, tokens=tokens, 
+                         model='anthropic.claude-3-haiku-20240307-v1:0', 
+                         image_url=image_url, reset=reset, cache=cache)
+
+def _claude3_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512, model=''):
+    try:
+        if BEDROCK_RUNTIME is None:
+            _init_runtime()
+
+        response = BEDROCK_RUNTIME.invoke_model(
+            modelId=model,
+            body=json.dumps(
+                {
+                    "anthropic_version": "bedrock-2023-05-31",
+                    "system": system,
+                    "temperature": temperature,
+                    "top_p": topP,
+                    "max_tokens": tokens,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": [{"type": "text", "text": prompt}],
+                        }
+                    ],
+                }
+            ),
+        )
+
+        result = json.loads(response.get('body').read())
+        return result['content'][0]['text']
+
+    except ClientError as err:
+        logger.error(
+            "Couldn't invoke Claude 3. Here's why: %s: %s",
+            err.response["Error"]["Code"],
+            err.response["Error"]["Message"],
+        )
+        raise
+
+@metrics.track
+def sonnet_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512):
+    return _claude3_text(prompt, system=system, 
+                         temperature=temperature, topP=topP, tokens=tokens, 
+                         model='anthropic.claude-3-sonnet-20240229-v1:0')
+
+@metrics.track
+def haiku_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512):
+    return _claude3_text(prompt, system=system, 
+                         temperature=temperature, topP=topP, tokens=tokens, 
+                         model='anthropic.claude-3-haiku-20240307-v1:0')
+
+def _claude2_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512, model=''):
+
+    decorated_prompt = f'Human: {(system + " " + prompt).strip()}\n\nAssistant:\n'
+    body = json.dumps({"prompt": decorated_prompt, 
+                       "temperature": temperature,
+                       "top_p": topP,
+                       "max_tokens_to_sample": tokens})
+    modelId = model
+    accept = "application/json"
+    contentType = "application/json"
+    
+    try:
+        if BEDROCK_RUNTIME is None:
+            _init_runtime()
+
+        response = BEDROCK_RUNTIME.invoke_model(
+            body=body, modelId=modelId, accept=accept, contentType=contentType
+        )
+        response_body = json.loads(response.get("body").read())
+        response_text = response_body.get("completion").strip()
+    
+        return response_text
+    
+    except ClientError as error:
+    
+        if error.response['Error']['Code'] == 'AccessDeniedException':
+               print(f"\x1b[41m{error.response['Error']['Message']}\
+                    \nTo troubeshoot this issue please refer to the following resources.\
+                     \nhttps://docs.aws.amazon.com/IAM/latest/UserGuide/troubleshoot_access-denied.html\
+                     \nhttps://docs.aws.amazon.com/bedrock/latest/userguide/security-iam.html\x1b[0m\n")
+    
+        else:
+            raise error
+
+@metrics.track        
+def claude2_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512):
+    return _claude2_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512, model='anthropic.claude-v2:1')
+
+@metrics.track        
+def instant_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512):
+    return _claude2_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512, model='anthropic.claude-instant-v1')
+
+@metrics.track        
+def llama2_text(prompt, system='', temperature=0.0, topP=1.0, tokens=512, model='meta.llama2-70b-chat-v1'):
+
+    if system != '':
+        decorated_prompt = f'<<SYS>>{system}<</SYS>>\n[INST]User:{prompt}[/INST]\nAssistant:'
+    else:
+        decorated_prompt = prompt
+
+    body = json.dumps({ 
+        'prompt': decorated_prompt,
+        'max_gen_len': tokens,
+        'top_p': topP,
+        'temperature': temperature
+    })
+
+    modelId = model
+    accept = "application/json"
+    contentType = "application/json"
+    
+    try:
+        if BEDROCK_RUNTIME is None:
+            _init_runtime()
+
+        response = BEDROCK_RUNTIME.invoke_model(
+            body=body, modelId=modelId, accept=accept, contentType=contentType
+        )
+        response_body = json.loads(response.get('body').read().decode('utf-8'))
+        response_text = response_body['generation'].strip()
+    
+        return response_text
+    
+    except ClientError as error:
+    
+        if error.response['Error']['Code'] == 'AccessDeniedException':
+               print(f"\x1b[41m{error.response['Error']['Message']}\
+                    \nTo troubeshoot this issue please refer to the following resources.\
+                     \nhttps://docs.aws.amazon.com/IAM/latest/UserGuide/troubleshoot_access-denied.html\
+                     \nhttps://docs.aws.amazon.com/bedrock/latest/userguide/security-iam.html\x1b[0m\n")
+    
+        else:
+            raise error
+
+def text(prompt, system='', temperature=0.0, topP=1.0, tokens=512, model='sonnet'):
+    match model:
+        case 'sonnet':
+            return sonnet_text(prompt, system, temperature, topP, tokens)
+        case 'haiku':
+            return haiku_text(prompt, system, temperature, topP, tokens)
+        case 'claude2':
+            return claude2_text(prompt, system, temperature, topP, tokens)
+        case 'instant':
+            return instant_text(prompt, system, temperature, topP, tokens)
+        case 'llama2':
+            return llama2_text(prompt, system, temperature, topP, tokens)
+        case _:
+            return 'Please specify a valid model name'
