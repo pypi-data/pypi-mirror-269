b4a3d89a7f21e97ff676f234d2f691a9a73fb8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:02:52 +0000

    Add package cs2-battle-bot-api-client/0.0.16/cs2_battle_bot_api_client-0.0.16-py3-none-any.whl

---
+++ packages/cs2-battle-bot-api-client/cs2_battle_bot_api_client-0.0.16-py3-none-any.whl/cs2_battle_bot_api_client/models/create_guild_member.py
@@ -0,0 +1,66 @@
+from typing import Any, Dict, List, Type, TypeVar
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+T = TypeVar("T", bound="CreateGuildMember")
+
+
+@_attrs_define
+class CreateGuildMember:
+    """
+    Attributes:
+        user_id (str):
+        username (str):
+    """
+
+    user_id: str
+    username: str
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        user_id = self.user_id
+
+        username = self.username
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "user_id": user_id,
+                "username": username,
+            }
+        )
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        user_id = d.pop("user_id")
+
+        username = d.pop("username")
+
+        create_guild_member = cls(
+            user_id=user_id,
+            username=username,
+        )
+
+        create_guild_member.additional_properties = d
+        return create_guild_member
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
