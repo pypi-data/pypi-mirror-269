0ad3196d7d910d2f4664c6adc04fae25b1ba9ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 03:03:11 +0000

    Add package few-shot-learning-nlp-pedrolsilva/0.0.2/few_shot_learning_nlp_pedrolsilva-0.0.2.tar.gz

---
+++ packages/few-shot-learning-nlp-pedrolsilva/few_shot_learning_nlp_pedrolsilva-0.0.2.tar.gz/few_shot_learning_nlp_pedrolsilva-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="few-shot-learning-nlp-pedrolsilva",
+    version="0.0.2",
+    author="Pedro Silva",
+    author_email="pedrolmssilva@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/peulsilva/few-shot-learning-nlp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
