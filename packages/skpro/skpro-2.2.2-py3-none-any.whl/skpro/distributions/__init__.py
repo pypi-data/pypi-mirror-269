c9ec4e5211ccea857d9bf613618e4299091f3960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:02:58 +0000

    Add package skpro/2.2.2/skpro-2.2.2-py3-none-any.whl

---
+++ packages/skpro/skpro-2.2.2-py3-none-any.whl/skpro/distributions/__init__.py
@@ -0,0 +1,33 @@
+"""Probability distribution objects."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+# adapted from sktime
+
+__all__ = [
+    "Empirical",
+    "Fisk",
+    "Laplace",
+    "Logistic",
+    "LogNormal",
+    "Mixture",
+    "Normal",
+    "Poisson",
+    "QPD_Empirical",
+    "QPD_S",
+    "QPD_B",
+    "QPD_U",
+    "TDistribution",
+    "Weibull",
+]
+
+from skpro.distributions.empirical import Empirical
+from skpro.distributions.fisk import Fisk
+from skpro.distributions.laplace import Laplace
+from skpro.distributions.logistic import Logistic
+from skpro.distributions.lognormal import LogNormal
+from skpro.distributions.mixture import Mixture
+from skpro.distributions.normal import Normal
+from skpro.distributions.poisson import Poisson
+from skpro.distributions.qpd import QPD_B, QPD_S, QPD_U
+from skpro.distributions.qpd_empirical import QPD_Empirical
+from skpro.distributions.t import TDistribution
+from skpro.distributions.weibull import Weibull
