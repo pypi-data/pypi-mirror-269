39e87a17c75b361bd0cf806fafe93fa8d25f50cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 04:03:03 +0000

    Add package notfunny/0.6.11/notfunny-0.6.11.tar.gz

---
+++ packages/notfunny/notfunny-0.6.11.tar.gz/notfunny-0.6.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(name='notfunny',
+      version='0.6.11',
+      description='Only the worst',
+      long_description='Really, the worst jokes around. Actually just the one worst. This is really just a test project for packaging python projects. But as an added bonus it really isnt funny.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='joke',
+      url='http://github.com/ohthepain/notfunny',
+      author='Paul Wilkinson',
+      author_email='paul@thisco.co',
+      license='MIT',
+      packages=['notfunny'],
+      scripts=['bin/notfunny-joke'],
+      entry_points = {
+        'console_scripts': ['funniest-joke=funniest.command_line:main'],
+      },
+      install_requires=[
+            # 'unittest'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
