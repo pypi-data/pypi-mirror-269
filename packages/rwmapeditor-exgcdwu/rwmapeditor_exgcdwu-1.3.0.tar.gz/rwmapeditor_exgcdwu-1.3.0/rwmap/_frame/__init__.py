a5e232563714d57fdfb503935455ecaa8d5955db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-21 14:03:22 +0000

    Add package rwmapeditor-exgcdwu/1.3.0/rwmapeditor_exgcdwu-1.3.0.tar.gz

---
+++ packages/rwmapeditor-exgcdwu/rwmapeditor_exgcdwu-1.3.0.tar.gz/rwmapeditor_exgcdwu-1.3.0/rwmap/_frame/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding=utf8 -*-
+from rwmap._frame._element_ori import ElementOri
+from rwmap._frame._coordinate import Coordinate
+from rwmap._frame._coordinate import Rectangle
+from rwmap._frame._element_property import ElementProperties
+from rwmap._frame._coordinate import TagCoordinate
