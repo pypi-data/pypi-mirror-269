af36a50bd527a5a5cbbcedab521de80f2656de96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:34 +0000

    Add package html-email-sender/0.1/html_email_sender-0.1.tar.gz

---
+++ packages/html-email-sender/html_email_sender-0.1.tar.gz/html_email_sender-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='html_email_sender',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Jinja2',
+    ],
+)
