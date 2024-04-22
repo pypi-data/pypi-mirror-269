7d15d838189c5cb89aa7957022b1f80d1191f0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:19 +0000

    Add package pygifconvt-test-jw/1.0.2/pygifconvt_test_jw-1.0.2.tar.gz

---
+++ packages/pygifconvt-test-jw/pygifconvt_test_jw-1.0.2.tar.gz/pygifconvt_test_jw-1.0.2/pygifconvert_test_jw/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Pygifconvt: a very serious Python library for image to gif converter
+
+"""
+__title__ = "Pygifconvt"
+__author__ = "swat018"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2024 swat018"
