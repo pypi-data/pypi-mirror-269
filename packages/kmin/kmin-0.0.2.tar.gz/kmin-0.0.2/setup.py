68ecb77cd443906dbfb6da9bb0cc86ba8a566758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:59 +0000

    Add package kmin/0.0.2/kmin-0.0.2.tar.gz

---
+++ packages/kmin/kmin-0.0.2.tar.gz/kmin-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name="kmin",
+    version="0.0.2",
+    author="kyengminkim",
+    url="https://github.com/kyengmin/",
+    packages=['makingpassword'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+
+)
