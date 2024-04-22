ba88baabbd7880db7d41ebc102925e338750306d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:15 +0000

    Add package x23186925-lib/0.0.2/x23186925_lib-0.0.2.tar.gz

---
+++ packages/x23186925-lib/x23186925_lib-0.0.2.tar.gz/x23186925_lib-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="x23186925_lib",
+    # Replace with your own username above
+    version="0.0.2",
+    author="Mohammad Amaan",
+    author_email="mdamaanshaikh03@gmail.com",
+    description="A small email package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    # if you have libraries that your module/package/library
+    #you would include them in the install_requires argument
+    install_requires=[''],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
