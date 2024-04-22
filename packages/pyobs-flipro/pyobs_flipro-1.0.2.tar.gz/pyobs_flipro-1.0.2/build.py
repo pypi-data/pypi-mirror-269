62e3e15e1a4a02ef8c9a62cb316609bf8646d60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:08 +0000

    Add package pyobs-flipro/1.0.2/pyobs_flipro-1.0.2.tar.gz

---
+++ packages/pyobs-flipro/pyobs_flipro-1.0.2.tar.gz/pyobs_flipro-1.0.2/build.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import Extension, Distribution
+import numpy
+from Cython.Build import cythonize
+from Cython.Distutils.build_ext import new_build_ext as cython_build_ext
+
+
+def build(**kwargs) -> None:
+    # if running in RTD, skip compilation
+    if os.environ.get("READTHEDOCS") == "True":
+        return
+
+    # compile FLI library
+    os.system("cd lib && make && cd ..")
+
+    extensions = [
+        Extension(
+            "pyobs_flipro.fliprodriver",
+            ["pyobs_flipro/fliprodriver.pyx"],
+            library_dirs=["lib/"],
+            libraries=["libflipro", "libflialgo", "cfitsio", "usb-1.0"],
+            include_dirs=[numpy.get_include()],
+            extra_compile_args=["-fPIC"],
+        )
+    ]
+    ext_modules = cythonize(extensions)
+
+    distribution = Distribution(
+        {
+            "name": "extended",
+            "ext_modules": ext_modules,
+            "cmdclass": {
+                "build_ext": cython_build_ext,
+            },
+        }
+    )
+
+    distribution.run_command("build_ext")
+    build_ext_cmd = distribution.get_command_obj("build_ext")
+    build_ext_cmd.copy_extensions_to_source()
+
+
+if __name__ == "__main__":
+    build()
