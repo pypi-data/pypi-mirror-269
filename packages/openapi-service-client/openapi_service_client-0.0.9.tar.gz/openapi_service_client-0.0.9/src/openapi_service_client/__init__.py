297927a3cc8893316ff6ae4bda44f8120cd4e1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:08 +0000

    Add package openapi-service-client/0.0.9/openapi_service_client-0.0.9.tar.gz

---
+++ packages/openapi-service-client/openapi_service_client-0.0.9.tar.gz/openapi_service_client-0.0.9/src/openapi_service_client/__init__.py
@@ -0,0 +1,8 @@
+from openapi_service_client.client import OpenAPIServiceClient
+from openapi_service_client.client_configuration import ClientConfiguration, ClientConfigurationBuilder
+
+__all__ = [
+    "OpenAPIServiceClient",
+    "ClientConfiguration",
+    "ClientConfigurationBuilder",
+]
