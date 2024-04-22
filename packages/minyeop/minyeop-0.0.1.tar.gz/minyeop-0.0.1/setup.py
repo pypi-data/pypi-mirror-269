ab758dcb29dcd3c869c33bacae13ded73a37a9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:25 +0000

    Add package minyeop/0.0.1/minyeop-0.0.1.tar.gz

---
+++ packages/minyeop/minyeop-0.0.1.tar.gz/minyeop-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+  name = 'minyeop',
+  version = '0.0.1',
+  description = 'nice caluclator',
+  author = 'myheo',
+  url = 'https://github.com/hmy7788/calc0001.git',
+  download_url= 'https://github.com/hmy7788/calc0001.git',
+  pakage = [ 'mycalc' ],
+  classifiers= [
+    "Programming Language :: Python :: 3",
+  ]
+)
