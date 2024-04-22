c9e44b77cefa4d1cc2c34c9effe3445936238c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:17 +0000

    Add package OrganisorAndLoginRequiredMixin/1.0.0/organisorandloginrequiredmixin-1.0.0.tar.gz

---
+++ packages/OrganisorAndLoginRequiredMixin/organisorandloginrequiredmixin-1.0.0.tar.gz/organisorandloginrequiredmixin-1.0.0/README.md
@@ -0,0 +1,5 @@
+OrganisorAndLoginRequiredMixin is a module that can be implemented in Django framework. This module customizes the login functionality of the Django framework and enables admin to setup permissions based on the login.
+
+#Installation
+
+pip install OrganisorAndLoginRequiredMixin
---
+++ packages/OrganisorAndLoginRequiredMixin/organisorandloginrequiredmixin-1.0.0.tar.gz/organisorandloginrequiredmixin-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OrganisorAndLoginRequiredMixin',
+    version='1.0.0',
+    author='Shayam Shah',
+    author_email='shayamshah1999@gmail.com',
+    description='This package is to implement customized login operations on Django Framework.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+)
