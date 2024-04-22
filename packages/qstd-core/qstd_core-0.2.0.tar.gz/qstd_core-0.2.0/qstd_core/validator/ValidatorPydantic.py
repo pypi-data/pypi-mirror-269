32e63fabd9aaf3a7f2e9108be1f913de1d50573b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:07 +0000

    Add package qstd-core/0.2.0/qstd_core-0.2.0.tar.gz

---
+++ packages/qstd-core/qstd_core-0.2.0.tar.gz/qstd_core-0.2.0/qstd_core/validator/ValidatorPydantic.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, ValidationError
+
+from .ValidatorABS import ValidatorABS
+from .exceptions import SchemaValidationException
+
+
+class ValidatorPydantic(ValidatorABS):
+    schema: BaseModel
+
+    def validate(self, payload: dict):
+        try:
+            return self.schema.parse_obj(payload)
+        except ValidationError as ex:
+            raise self.errors_mapper(ex)
+
+    def errors_mapper(self, ex: ValidationError):
+        errors_list = []
+        for error in ex.errors():
+            loc = error['loc']
+            errors_list.append(
+                SchemaValidationException.SchemaValidationItemException(
+                    self.format_error_message(error['msg'], loc[len(loc) - 1]),
+                    location=loc
+                )
+            )
+        return SchemaValidationException(
+            self.target_name,
+            errors_list
+        )
+
+    @classmethod
+    def format_error_message(cls, message: str, field_name: str) -> str:
+        return message.format(field_name=field_name)
