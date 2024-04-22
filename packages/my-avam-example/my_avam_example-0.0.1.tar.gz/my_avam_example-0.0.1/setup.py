ebb928292dcd13c4f8b9e8d497b1ba0a03a97702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:04:26 +0000

    Add package my-avam-example/0.0.1/my_avam_example-0.0.1.tar.gz

---
+++ packages/my-avam-example/my_avam_example-0.0.1.tar.gz/my_avam_example-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='audio-visual',
+    version='0.1',
+    description='add project description',
+    author='َAmal Abdellah',
+    author_email='feetseg@gmail.com',
+    packages=find_packages(),
+)
