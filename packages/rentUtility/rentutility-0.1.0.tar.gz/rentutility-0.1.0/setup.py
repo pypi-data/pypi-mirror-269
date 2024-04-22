24c864d723c4addf30a313f51df5fb1e0abcabab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:04:05 +0000

    Add package rentUtility/0.1.0/rentutility-0.1.0.tar.gz

---
+++ packages/rentUtility/rentutility-0.1.0.tar.gz/rentutility-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rentUtility',
+    version='0.1.0',
+    description='A Python library for rental based system and perform important calculations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ayush Jadhav',
+    author_email='jadhavayush2020@email.com',
+    url='https://github.com/Ayush-NCI/rentaltility',
+    packages=find_packages(),
+    license='MIT',
+    
+)
