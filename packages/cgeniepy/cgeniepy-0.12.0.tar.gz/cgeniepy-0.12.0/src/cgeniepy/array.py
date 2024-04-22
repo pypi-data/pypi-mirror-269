f56e33f24ff692b9b40f42da18fa02512fe78569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:50 +0000

    Add package cgeniepy/0.12.0/cgeniepy-0.12.0.tar.gz

---
+++ packages/cgeniepy/cgeniepy-0.12.0.tar.gz/cgeniepy-0.12.0/src/cgeniepy/array.py
@@ -0,0 +1,381 @@
+import numpy as np
+import xarray as xr
+from scipy.stats import sem
+import regionmask
+
+from .grid import Interporaltor, GridOperation
+from .plot import GriddedDataVis
+from .chem import Chemistry
+import cgeniepy.table as ct
+from functools import cache
+
+
+class GriddedData:
+    """
+    GriddedData is a class to store and compute GENIE netcdf data.
+
+    It stores data in xarray.DataArray format, and provides optimalised methods for GENIE model output to compute statistics.    
+    """
+    
+    def __init__(self, array=np.nan, mutable=False, attrs={}):
+        """
+        Initialise an instance of GriddedData
+
+        Parameters
+        ----------
+        array : xarray.DataArray
+        """
+        
+        # set data
+        self.data = array
+        self.mutable = mutable
+        self.attrs = attrs
+
+        ## formatting the unit
+        if 'units' in self.attrs:
+            self.attrs['units'] = Chemistry().format_unit(self.attrs['units'])
+
+    def __getitem__(self, item):
+        "make GriddedData subscriptable like xarray.DataArray"
+        return self.data[item]
+
+    def interpolate(self, *args, **kwargs):
+        """
+        Interpolate the GriddedData to a finer grid, mostly useful for GENIE plotting
+        """
+        
+        coords = tuple([self.data[dim].values for dim in self.data.dims])
+        values = self.data.values
+        dims = self.data.dims
+        
+        if self.mutable:
+            self.data = Interporaltor(dims, coords,values, *args, **kwargs).to_xarray()
+            return self            
+        else:
+            return GriddedData(Interporaltor(dims, coords,values, *args, **kwargs).to_xarray())
+        
+
+    def sel(self, *args, **kwargs):
+        """select grid points based on the given coordinates
+        a wrapper to xarray `sel` method
+
+        -----------
+        Examples:
+        -----------
+        >>> model = GenieModel(path)
+        >>> var = model.get_var(target_var)
+        >>> var.search_grid(lon=XX, lat=XX, zt=XX, method='nearest')
+        
+        ## for loop over a data frame (df) with longitude, latitude, depth columns
+        >>> modelvar = []
+        >>> for i in range(len(df)):
+        >>>     lat = df.latitude.iloc[i][0]
+        >>>     lon = df.longitude[i].iloc[0]
+        >>>     zt = df.depth[i].iloc[0]
+        >>>     kwargs = {'lat': lat, 'lon':lon, 'zt': zt, 'method': 'nearest'}
+        >>>     var = var.sel(**kwargs).values
+        >>>     modelvar.append(var)        
+        """
+
+        if self.mutable:
+            self.data = self.data.sel(*args, **kwargs)
+            return self
+        else:
+            return GriddedData(self.data.sel(*args, **kwargs))
+
+
+    def isel(self, *args, **kwargs):
+        """select grid points based on the given index
+        a wrapper to xarray `isel` method
+        """
+        if self.mutable:
+            self.data = self.data.isel(*args, **kwargs)
+            return self
+        else:
+            return GriddedData(self.data.isel(*args, **kwargs), mutable=False, attrs=self.attrs)
+    
+    def normalise_longitude(self, method='g2n', *args, **kwargs):
+        """Normalise GENIE's longitude (eastern degree) to normal longitude (-180, 180)
+
+        -----------
+        Example
+        -----------
+
+        >>> Model = GenieModel("a path")
+        >>> Model.get_var('abc').normalise_longitude()
+        """
+        gp = GridOperation()
+
+        match method:
+            case 'g2n':
+                output = gp.apply_g2n(self.data, *args, **kwargs)
+            case 'n2g':
+                output = gp.apply_n2g(self.data, *args, **kwargs)
+            case 'e2n':
+                output = gp.apply_e2n(self.data, *args, **kwargs)
+            case 'n2e':
+                output = gp.apply_n2e(self.data, *args, **kwargs)
+            case _:
+                raise ValueError("Invalid method")
+
+        if self.mutable:
+            self.data = output
+            return self        
+        else:
+             return GriddedData(output, mutable=False, attrs=self.attrs)
+                    
+
+    def __add__(self, other):
+        """Allow GriddedData to be added by a number or another GriddedData
+        """
+        sum = GriddedData()
+        if hasattr(other, "array"):
+            sum.data = self.data + other.array
+        else:
+            ## a scalar
+            sum.data = self.data + other
+        return sum
+
+    def __sub__(self, other):
+        """
+        Allow GriddedData to be subtracted by a number or another GriddedData
+        """
+        diff = GriddedData()
+        if hasattr(other, "data"):
+            diff.data = self.data - other.data
+        else:
+            ## a scalar
+            diff.data = self.data - other
+        return diff
+
+    def __truediv__(self, other):
+        """
+        Allow GriddedData to be divided by a number or another GriddedData
+
+        NA/NA -> NA
+        """
+        quotient_array = np.zeros_like(self.data)
+        
+        if isinstance(other, GriddedData):
+            quotient_array = np.divide(self.data, other.data)
+        else:
+            # Handle division by a scalar
+            quotient_array = np.divide(self.data, other)
+
+        quotient = GriddedData()
+        quotient.data = xr.DataArray(quotient_array)
+        return quotient
+
+    def __mul__(self, other):
+        """
+        Allow GriddedData to be multiplied by a number or another GriddedData
+        """
+        product = GriddedData()
+        if hasattr(other, "data"):
+            product.data = self.data * other.data
+        else:
+            try:
+                product.data = self.data * other
+            except ValueError:
+                print("Sorry, only number and GriddedData are accepted")
+
+        return product
+
+
+    def max(self, *args, **kwargs):
+        self.data = self.data.max(*args, **kwargs)
+        return self
+
+
+    def min(self, *args, **kwargs):
+        if self.mutable:
+            self.data = self.data.min(*args, **kwargs)
+            return self
+        else:
+            return GriddedData(self.data.min(*args, **kwargs), mutable=False, attrs=self.attrs)
+
+
+    def sum(self, *args, **kwargs):
+        if self.mutable:
+            self.data = self.data.sum(*args, **kwargs)
+            return self
+        else:
+            return GriddedData(self.data.sum(*args, **kwargs), mutable=False, attrs=self.attrs)
+
+
+    def mean(self, *args, **kwargs):
+        if self.mutable:
+            self.data = self.data.mean(*args, **kwargs)
+            return self
+        else:
+            return GriddedData(self.data.mean(*args, **kwargs), mutable=False, attrs=self.attrs)
+    
+
+    def median(self, *args, **kwargs):
+        if self.mutable:
+            self.data = self.data.median(*args, **kwargs)
+            return self
+        else:
+            return GriddedData(self.data.median(*args, **kwargs), mutable=False, attrs=self.attrs)
+
+    def sd(self, *args, **kwargs):
+        "standard deviation of the mean"
+        if self.mutable:
+            self.data = np.std(self.data, *args, **kwargs)
+            return self
+        else:
+            return np.std(self.data, *args, **kwargs)
+
+
+    def variance(self, *args, **kwargs):
+        if self.mutable:
+            self.data = np.var(self.data, *args, **kwargs)
+            return self
+        else:
+            return np.var(self.data, *args, **kwargs)
+
+    def se(self, *args, **kwargs):
+        "standard error of the mean"
+        if self.mutable:
+            self.data = sem(self.data, nan_policy="omit", axis=None, *args, **kwargs)
+            return self
+        else:
+            return sem(self.data, nan_policy="omit", axis=None, *args, **kwargs)
+
+    def weighted_mean(self, weights, *args, **kwargs):
+        """
+        compute the weighted average of the array (e.g., ocean volume)
+        """
+        array_ma = self.data.to_masked_array()
+        return np.ma.average(array_ma, weights=weights, *args, **kwargs)        
+    
+    def sel_modern_basin(self, basin):
+        """
+        select modern basin from regionmask.defined_regions.ar6.ocean
+
+        The 58 defines marine regions from the sixth IPCC assessment report (AR6), Iturbide et al., (2020) ESSD
+        
+        https://regionmask.readthedocs.io/en/stable/_images/plotting_ar6_all.png
+        
+        --------------------------------
+        47: North Pacific
+        48: Equatorial Pacific
+        49: South Pacific
+        
+        50: North Atlantic Ocean
+        51: Equatorial Atlantic Ocean
+        52: Southern Atlantic Ocean
+
+        57: Southern Ocean
+        --------------------------------        
+        """
+        ocean = regionmask.defined_regions.ar6.ocean
+        index = ocean.map_keys(basin)
+        mask = ocean.mask(self.normalise_longitude())        
+
+        if self.mutable:
+            self.data = self.normalise_longitude().data.where(mask == index)                    
+            return self
+        else:
+            return GriddedData(self.normalise_longitude().data.where(mask == index), mutable=False, attrs=self.attrs)
+
+
+    def mask_basin(self, base, basin, subbasin):
+
+        """
+        use pre-defined grid mask to select_basin, mostly used for cGENIE model
+        """
+        gp = GridOperation()
+        data = self.data
+        mask = gp.GENIE_grid_mask(base=base, basin=basin, subbasin=subbasin, invert=True)
+
+        if self.data.ndim > 2:
+            mask = np.broadcast_to(mask, (16, 36, 36))
+
+        mask_data = np.ma.array(data, mask=mask)
+        mask_data = np.ma.masked_invalid(mask_data)
+
+
+        if self.mutable:
+            self.data = xr.DataArray(mask_data, dims=data.dims, coords=data.coords)            
+            return self
+        else:
+            return xr.DataArray(mask_data, dims=data.dims, coords=data.coords)
+
+
+    @cache
+    def ocn_only_data(self, index=False):
+        """
+        remove the NA grid (i.e., land definition)
+
+        :param index: return GeoIndex if True or real value if false
+        """
+
+        stacked_data = self.data.stack(x=self.data.dims)            
+        stacked_ocn_mask =~np.isnan(stacked_data)
+        ocn_only_data = stacked_data[stacked_ocn_mask]
+        
+        if index:
+            return np.stack(ocn_only_data.x.values)
+        else:
+            return ocn_only_data
+        
+
+    def search_point(self, point, ignore_na=False, to_genielon=False, **kwargs):
+        """
+        search the nearest grid point to the given coordinates
+        
+        :param point: a list/tuple of coordinate values in the same order to the data dimension (self.data.dims)
+        :param ignore_na: whether only check ocean data (which ignore the NA grids)
+        :param to_genielon: whether convert the input longitude to genie longitude, input point must be list if True
+        """
+        ## ignore the first dimension (which is time)        
+        ndim = self.data.ndim
+        
+        if len(point) != ndim:
+            raise ValueError("Input point has incompatiable coordinate")
+        
+        if ignore_na:
+            index_pool = self.ocn_only_data(index=True)
+            match ndim:
+                case 3:
+                    ## point: (depth, lat, lon); Index: (depth, lat, lon)
+                    if to_genielon: point[2] = GridOperation().lon_g2n(point[2])
+                    distances = GridOperation().geo_dis3d(point, index_pool[:, 0:4])
+                    idx_min = np.argmin(distances)
+                case 2:
+                    ## point: (lat, lon); Index: (lat, lon)
+                    if to_genielon: point[1] = GridOperation().lon_g2n(point[1])
+                    distances = GridOperation().geo_dis2d(point, index_pool[:, 0:3])
+                    idx_min = np.argmin(distances)
+            
+            nearest_value = self.ocn_only_data(index=False).values[idx_min]
+            return nearest_value
+        else:
+            ## construct a dictionary based on self.data.dims and input point, and method ("nearest")
+            kwargs = dict(zip(self.data.dims, point), method='nearest')
+            return self.data.sel(**kwargs).values.item()
+
+    def to_GriddedDataVis(self):
+        """
+        convert GriddedData to GriddedDataVis
+        """
+        return GriddedDataVis(self)
+
+    def plot(self, *args, **kwargs):
+        """
+        plot the data using GriddedDataVis
+        """
+        return self.to_GriddedDataVis().plot(*args, **kwargs)
+
+    def to_dataframe(self):
+        """
+        convert GriddedData to pandas.DataFrame
+        """
+        return self.data.to_dataframe()
+
+    def to_ScatterData(self):
+        """
+        convert GriddedData to ScatterData
+        """
+        return ct.ScatterData(self.to_dataframe())
---
+++ packages/cgeniepy/cgeniepy-0.12.0.tar.gz/cgeniepy-0.12.0/src/cgeniepy/skill.py
@@ -0,0 +1,342 @@
+import numpy as np
+from scipy.spatial import distance
+import matplotlib.pyplot as plt
+
+class ArrComparison:
+    
+    "quantitatively compare similarity metrics between two 2D arrays (model and observation)"
+
+    def __init__(self, model, observation):
+        """
+        
+        :param: model: array-like data
+        :param: observation: array-like data
+
+        --------
+        Example:
+        --------
+        
+        from cgeniepy.skill import ModelSkill
+
+        ## generate 2d data
+        import numpy as np
+        x = np.random.rand(36, 36)
+        y = np.random.rand(36, 36)
+
+        ## calculate skill score
+        ms = ModelSkill(x, y)
+        ms.mscore()
+        """
+        self.model = model
+        self.data = observation
+
+        ## check data type, if not float, convert to float
+        if self.model.dtype != np.float64:
+            self.model = self.model.astype(np.float64)
+        if self.data.dtype != np.float64:
+            self.data = self.data.astype(np.float64)
+        
+
+    def safe_unveil(self, array):
+        "get pure array from a numpy masked array object"
+        if array.__class__ != np.ma.core.MaskedArray:
+            return array
+        else:
+            return array.filled(np.nan)
+
+    def intersect_index(self, verbose=False):
+        """ Return the index where corresponding values are not
+        nan in both input arrays. One then can filter the array
+        by the output boolean array.
+        """
+
+        # If both are not NaN, return it
+        array1 = self.safe_unveil(self.model)
+        array2 = self.safe_unveil(self.data)
+
+        indx_array = np.logical_and(~np.isnan(array1), ~np.isnan(array2))
+
+        if verbose is True:
+            num = indx_array.flatten()[indx_array.flatten() == True].shape[0]
+            print("Summary: {} elements simultaneously exist.".format(num))
+
+        return indx_array
+
+    def mscore(self):
+        """
+        Calculate skill metric M-score. See more in the paper Watterson, I. G. (1996)
+        """
+
+        # get common set
+        indx = self.intersect_index()
+        sub_data1 = self.model[indx]
+        sub_data2 = self.data[indx]
+
+        # calculate M-score
+        mse = np.square(np.subtract(sub_data1, sub_data2)).mean()
+        v1 = sub_data1.var()
+        v2 = sub_data2.var()
+        g1 = sub_data1.mean()
+        g2 = sub_data2.mean()
+
+        mscore = (2 / np.pi) * np.arcsin(1 - mse / (v1 + v2 + np.square(g1 - g2)))
+
+        return mscore
+
+    def pearson_r(self):
+        """
+        calculate pearson correlation coefficient for 2D array
+        """
+        # get common set
+        indx = self.intersect_index()
+        sub_data1 = self.model[indx]
+        sub_data2 = self.data[indx]
+
+        # correlation matrix
+        corr_mat = np.corrcoef(sub_data1, sub_data2)
+        corr = corr_mat[0, 1]
+
+        return corr
+
+    def cos_similarity(self):
+        """
+        Cosine similarity that ignores the magnitude of data, but focuses on the direction
+        """
+        # get common set
+        indx = self.intersect_index()
+        sub_data1 = self.model[indx].ravel()
+        sub_data2 = self.data[indx].ravel()
+
+        if sub_data1.mean() != 0 and sub_data2.mean() != 0:
+            cos_sim = 1 - distance.cosine(sub_data1.flatten(), sub_data2.flatten())
+        else:
+            cos_sim = np.nan
+
+        return cos_sim
+
+    def rmse(self):
+        """
+        Root Mean Sqaure Error (rmse, or rmsd)
+        """
+        data1 = self.model
+        data2 = self.data
+
+        error_2d = data1 - data2
+        error_1d = error_2d.ravel()[~np.isnan(error_2d.ravel())]
+        rmse = np.sqrt(np.square(error_1d).mean())
+
+        return rmse
+
+    def nrmse(self):
+        "Normalised RMSE facilitating the comparison between datasets or models with different scales"
+
+        norm_rmse = self.rmse()/np.nanmean(self.model)
+        return norm_rmse
+
+    def crmse(self):
+        """
+        centred Root Mean Sqaure Error (rmse, or rmsd). See Talor, K. E. (2001) JGR
+        """
+        # select data
+        indx = self.intersect_index()
+        sub_data1 = self.model[indx].ravel()
+        sub_data2 = self.data[indx].ravel()
+
+        # calculate std
+        sigma1 = np.std(sub_data1)
+        sigma2 = np.std(sub_data2)
+
+        # pearson correlation
+        corr_mat = np.corrcoef(sub_data1, sub_data2)
+        corr = corr_mat[0, 1]
+
+        # central rmse
+        crmse = sigma1**2 + sigma2**2 - 2 * sigma1 * sigma2 * corr
+
+        return crmse
+
+    def plot(self):
+        ## a x-y plot
+        fig, ax = plt.subplots()
+        ax.scatter(self.model, self.data)
+        ax.set_xlabel("Model")
+        ax.set_ylabel("Observation")
+        ax.set_title("Model vs Observation")
+
+    
+class DFComparison(ArrComparison):
+
+    def __init__(self, df, model_col, observation_col):
+        self.model = df[model_col].to_numpy()
+        self.data = df[observation_col].to_numpy()
+        super().__init__(self.model, self.data)
+
+ 
+# class TaylorDiagram(object):
+#     """
+#     Taylor diagram.
+#     Plot model standard deviation and correlation to reference data in a single-quadrant polar plot,
+#     with r=stddev and theta=arccos(correlation).
+
+#     modified from Yannick Copin, https://gist.github.com/ycopin/3342888
+#     reference: https://matplotlib.org/stable/gallery/axisartist/demo_floating_axes.html
+#     """
+
+#     def __init__(
+#             self,
+#             fig=None,
+#             figscale=1,
+#             subplot=111,
+#             xmax=None,
+#             tmax=np.pi / 2,
+#             ylabel="Standard Deviation",
+#             rotation=None,
+#     ):
+
+#         """
+#         Set up Taylor diagram axes, i.e. single quadrant polar
+#         plot, using `mpl_toolkits.axisartist.floating_axes`.
+
+#         Parameters:
+
+#         * fig: input Figure or None
+#         * subplot: subplot definition
+#         * xmax: the length of radius, xmax can be 1.5* reference std
+#         """
+
+#         # --------------- tickers --------------------------
+#         # Correlation labels (if half round)
+#         cor_label = np.array([0, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.99, 1])
+
+#         # add the negative ticks if more than half round
+#         excess_theta = tmax - np.pi / 2
+#         if excess_theta > 0:
+#             cor_label = np.concatenate((-cor_label[:0:-1], cor_label))
+
+#         # convert to radian
+#         rad = np.arccos(cor_label)
+#         # tick location
+#         gl = gf.FixedLocator(rad)
+#         # tick formatting: bind radian and correlation coefficient
+#         tf = gf.DictFormatter(dict(zip(rad, map(str, cor_label))))
+
+#         # --------------- coordinate -----------------------
+#         # Standard deviation axis extent (in units of reference stddev)
+#         # xmin must be 0, which is the centre of round
+
+#         self.xmin = 0
+#         self.xmax = xmax
+#         self.tmax = tmax
+
+#         # ------- curvilinear coordinate definition -------
+#         # use built-in polar transformation (i.e., from theta and r to x and y)
+#         tr = PolarAxes.PolarTransform()
+#         ghelper = fa.GridHelperCurveLinear(
+#             tr,
+#             extremes=(0, self.tmax, self.xmin, self.xmax),
+#             grid_locator1=gl,
+#             tick_formatter1=tf,
+#         )
+
+#         # ------- create floating axis -------
+#         if fig is None:
+#             fig_height = 4.5 * figscale
+#             fig_width = fig_height * (1 + np.sin(excess_theta))
+#             fig = plt.figure(figsize=(fig_width, fig_height), dpi=100)
+
+#         ax = fa.FloatingSubplot(fig, subplot, grid_helper=ghelper)
+#         fig.add_subplot(ax)
+
+#         # Adjust axes
+#         # Angle axis
+#         ax.axis["top"].label.set_text("Correlation")
+#         ax.axis["top"].toggle(ticklabels=True, label=True)
+#         # inverse the direction
+#         ax.axis["top"].set_axis_direction("bottom")
+#         ax.axis["top"].major_ticklabels.set_axis_direction("top")
+#         ax.axis["top"].label.set_axis_direction("top")
+
+#         # X axis
+#         ax.axis["left"].set_axis_direction("bottom")
+
+#         # Y axis direction & label
+#         ax.axis["right"].toggle(all=True)
+#         ax.axis["right"].label.set_text(ylabel)
+#         ax.axis["right"].set_axis_direction("top")
+#         # ticklabel direction
+#         ax.axis["right"].major_ticklabels.set_axis_direction("left")
+
+#         ax.axis["bottom"].set_visible(False)
+
+#         # ------- Set instance attribute ----------
+#         self.fig = fig
+#         # Graphical axes
+#         self._ax = ax
+#         # grid line
+#         self._ax.grid(True, zorder=0, linestyle="--")
+#         # aspect ratio
+#         self._ax.set_aspect(1)
+#         # A parasite axes for further plotting data
+#         self.ax = ax.get_aux_axes(tr)
+#         # Collect sample points for latter use (e.g. legend)
+#         self.samplePoints = []
+
+#     def add_ref(self, refstd, reflabel="Observation", linestyle="-", color="k"):
+#         """add a reference point"""
+#         self.refstd = refstd
+#         # Add reference point
+#         # slightly higher than 0 so star can be fully seen
+#         l = self.ax.plot(0.01, self.refstd, "k*", ls="", ms=10)
+#         # xy for the point, xytext for the text (the coordinates are
+#         # defined in xycoords and textcoords, respectively)
+#         self.ax.annotate(
+#             reflabel,
+#             xy=(0.01, self.refstd),
+#             xycoords="data",
+#             xytext=(-25, -30),
+#             textcoords="offset points",
+#         )
+#         # add stddev contour
+#         t = np.linspace(0, self.tmax)
+#         r = np.zeros_like(t) + self.refstd
+#         self.ax.plot(t, r, linestyle=linestyle, color=color)
+#         self.samplePoints.append(l)
+
+#     def add_scatter(self, stddev, corrcoef, *args, **kwargs):
+#         """
+#         Add sample (*stddev*, *corrcoeff*) to the Taylor
+#         diagram. *args* and *kwargs* are directly propagated to the
+#         `Figure.plot` command.
+#         """
+
+#         l = self.ax.scatter(
+#             np.arccos(corrcoef), stddev, *args, **kwargs
+#         )  # (theta, radius)
+#         self.samplePoints.append(l)
+
+#         return l
+
+#     def add_contours(self, levels=5, **kwargs):
+#         """
+#         Add constant centered RMS difference contours, defined by *levels*.
+#         """
+
+#         rs, ts = np.meshgrid(
+#             np.linspace(self.xmin, self.xmax), np.linspace(0, self.tmax)
+#         )
+#         # Compute centered RMS difference
+#         crmse = np.sqrt(self.refstd**2 + rs**2 - 2 * self.refstd * rs * np.cos(ts))
+#         contours = self.ax.contour(ts, rs, crmse, levels, linestyles="--", **kwargs)
+#         self.ax.clabel(contours, contours.levels[::1], inline=False)
+
+#         return contours
+
+#     def add_legend(self, *args, **kwargs):
+#         return self.ax.legend(*args, **kwargs)
+
+#     def add_annotation(self, *args, **kwargs):
+#         return self.ax.annotation(*args, **kwargs)
+
+#     def savefig(self, *args, **kwargs):
+#         self.fig.savefig(*args, **kwargs)
+    
