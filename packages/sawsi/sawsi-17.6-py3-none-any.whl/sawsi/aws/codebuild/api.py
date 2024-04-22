6a7f5e61b8bdc2aa50e5a70a564e933694641200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:03:45 +0000

    Add package sawsi/17.6/sawsi-17.6-py3-none-any.whl

---
+++ packages/sawsi/sawsi-17.6-py3-none-any.whl/sawsi/aws/codebuild/api.py
@@ -0,0 +1,29 @@
+import boto3
+from sawsi.aws import shared
+from typing import List
+
+
+class CodeBuildAPI:
+    def __init__(self, credentials=None, region=shared.DEFAULT_REGION):
+        """
+        :param credentials: {
+            "aws_access_key_id": "str",
+            "aws_secret_access_key": "str",
+            "region_name": "str",
+            "profile_name": "str",
+        }
+        """
+        self.boto3_session = shared.get_boto_session(credentials)
+        self.cache = {}
+        self.client = boto3.client('codebuild', region_name=region)
+
+    def batch_get_builds(self, build_ids: List[str]):
+        # 커밋 기반으로 파일 가쟈오기
+        response = self.client.batch_get_builds(
+            ids=build_ids  # 여기에 조회하고자 하는 빌드 ID를 리스트 형태로 입력
+        )
+        return response
+
+
+if __name__ == '__main__':
+    pass
