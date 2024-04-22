3095ddfbe1bdd37f80319fb0e8c9f625b5fb96c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:42 +0000

    Add package sampa/1.0.0.43/sampa-1.0.0.43.tar.gz

---
+++ packages/sampa/sampa-1.0.0.43.tar.gz/sampa-1.0.0.43/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+import json
+from pathlib import Path
+from typing import Optional
+
+setup(
+    name = "sampa",
+    version = "1.0.0.43",
+    entry_points={'console_scripts': ['sampa = sampa:main']},
+    description = "...",
+    author = "Augusto de Lelis Araujo", 
+    author_email = "augusto-lelis@outlook.com",
+    license = "Closed source",
+    install_requires=['numpy',
+                      'requests',
+                      'pyfiglet',
+                      'vasprocar'],
+    package_data={"": ['*.dat', '*.png', '*.jpg', '*']},
+)
+
+
+# python3 -m pip install --upgrade twine
+# python setup.py sdist
+# python -m twine upload dist/*
