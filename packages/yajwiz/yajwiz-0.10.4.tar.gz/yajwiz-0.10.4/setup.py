5f3b7f3c47584f13e63b22bed399ca14ad81046a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:14 +0000

    Add package yajwiz/0.10.4/yajwiz-0.10.4.tar.gz

---
+++ packages/yajwiz/yajwiz-0.10.4.tar.gz/yajwiz-0.10.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="yajwiz",
+    version="0.10.4",
+    author="Iikka Hauhio",
+    author_email="fergusq@kaivos.org",
+    packages=["yajwiz"],
+    description="Klingon NLP toolkit",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.8',
+    install_requires=["appdirs", "requests"]
+)
