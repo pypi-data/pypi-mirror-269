8338026e396c9d082443b0ee5d2c662b6d6cb946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:03:31 +0000

    Add package newsly-mail/0.2/newsly_mail-0.2-py3-none-any.whl

---
+++ packages/newsly-mail/newsly_mail-0.2-py3-none-any.whl/newsly_mail/newsletter_mail.py
@@ -0,0 +1,15 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(sender_email, receiver_email, subject, body, smtp_server, smtp_port, smtp_username, smtp_password):
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
