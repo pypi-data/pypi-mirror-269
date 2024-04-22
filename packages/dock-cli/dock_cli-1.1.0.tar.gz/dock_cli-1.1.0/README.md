5d180df5d851205754acb274a7b7373478772449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:03:38 +0000

    Add package dock-cli/1.1.0/dock_cli-1.1.0.tar.gz

---
+++ packages/dock-cli/dock_cli-1.1.0.tar.gz/dock_cli-1.1.0/README.md
@@ -0,0 +1,146 @@
+# Dock CLI
+
+CLI tool for managing containerized applications in a Git repository.
+
+It allows you to implement automatic version control for Docker images and Helm charts in a quick and easy way.
+
+## Installation
+
+Install and update using [pip](https://pip.pypa.io/en/stable/getting-started/).
+
+```bash
+$ pip install -U dock-cli
+```
+
+## Getting Started
+
+Assuming you have a repository with multiple Docker images and Helm charts that require automatic version control.
+
+For example, the folder structure of your repository is as follows:
+
+```
+<repository_root>/
+ | - .git/
+ | - charts/
+ | | - myFirstChart/
+ | | | - Chart.yaml
+ | | - mySecondChart/
+ | | | - Chart.yaml
+ | - images/
+ | | - myFirstImage/
+ | | | - Dockerfile
+ | | - mySecondImage/
+ | | | - Dockerfile
+ | - dock.ini
+```
+
+Where `dock.ini` can be empty or nonexistent, you can use the following command to update `dock.ini`:
+
+```bash
+# Set default registry for all images
+$ dock image config set-registry posen
+Set [DEFAULT] registry = posen
+
+# Set default registry for all charts
+$ dock chart config set-registry oci://registry-1.docker.io/posen
+Set [DEFAULT] oci-registry = oci://registry-1.docker.io/posen
+
+# Add images/myFirstImage/ to the configuration
+$ dock image config set images/myFirstImage/
+Del [images/myFirstImage] registry
+Set [images/myFirstImage] image-file = Dockerfile
+Del [images/myFirstImage] image-name
+Del [images/myFirstImage] depends-on
+Set [images/myFirstImage] type = image
+
+images/myFirstImage:
+- registry: posen
+- image-file: Dockerfile
+- image-name:
+- depends-on:
+- type: image
+
+# Add images/mySecondImage/ to the configuration
+$ dock image config set images/mySecondImage/ --depends-on=images/myFirstImage/
+Del [images/mySecondImage] registry
+Set [images/mySecondImage] image-file = Dockerfile
+Del [images/mySecondImage] image-name
+Set [images/mySecondImage] depends-on = images/myFirstImage
+Set [images/mySecondImage] type = image
+
+images/mySecondImage:
+- registry: posen
+- image-file: Dockerfile
+- image-name:
+- depends-on: images/myFirstImage
+- type: image
+
+# Add charts/myFirstChart/ to the configuration
+$ dock chart config set charts/myFirstChart/
+Del [charts/myFirstChart] oci-registry
+Set [charts/myFirstChart] type = chart
+
+charts/myFirstChart:
+- oci-registry: oci://registry-1.docker.io/posen
+- type: chart
+
+# Add charts/mySecondChart/ to the configuration
+$ dock chart config set charts/mySecondChart/
+Del [charts/mySecondChart] oci-registry
+Set [charts/mySecondChart] type = chart
+
+charts/mySecondChart:
+- oci-registry: oci://registry-1.docker.io/posen
+- type: chart
+```
+
+And the content of `dock.ini` is as follows:
+
+```bash
+$ cat dock.ini
+[DEFAULT]
+registry = posen
+oci-registry = oci://registry-1.docker.io/posen
+
+[images/myFirstImage]
+image-file = Dockerfile
+type = image
+
+[images/mySecondImage]
+image-file = Dockerfile
+depends-on = images/myFirstImage
+type = image
+
+[charts/myFirstChart]
+type = chart
+
+[charts/mySecondChart]
+type = chart
+```
+
+Then you can use the following command to push to the registry:
+
+- List all images and charts
+    ```bash
+    $ dock image list
+    images/myFirstImage
+    images/mySecondImage
+    $ dock chart list
+    charts/myFirstChart
+    charts/mySecondChart
+    ```
+- Build all images and Package all charts
+    ```bash
+    $ dock image list | xargs -r dock image build
+    $ dock chart list | xargs -r dock chart package
+    ```
+- Push all images and charts
+    ```bash
+    $ dock image list | xargs -r dock image push
+    $ dock chart list | xargs -r dock chart push
+    ```
+
+## Links
+
+- Source: https://github.com/Posen2101024/dock
+- PyPI: https://pypi.org/project/dock-cli/
---
+++ packages/dock-cli/dock_cli-1.1.0.tar.gz/dock_cli-1.1.0/dock_cli/cli/image.py
@@ -0,0 +1,132 @@
+import configparser
+import itertools
+import logging
+import click
+from dock_cli.utils import callback as cb
+from dock_cli.utils import commands as cmd
+from dock_cli.utils import helpers as hlp
+from dock_cli.utils import utils
+from dock_cli.utils.schema import ImageConfigOptions as Image, SectionType
+
+@click.group(name='image', cls=hlp.OrderedGroup)
+@click.pass_obj
+def cli(obj):
+    """Manage images
+
+    This is a command line interface for manage images
+    """
+    obj.helper = hlp.ImageHelper(obj.config, obj.config_dir, obj.command)
+
+@cli.command(name='list',
+             help='List all images')
+@click.pass_obj
+def image_list(obj):
+    for section in obj.helper.get_images():
+        click.echo(section)
+
+@cli.command(name='diff',
+             help='List all images that have been changed between commits')
+@click.pass_obj
+@click.argument('commit1', required=False, type=str, default='HEAD')
+@click.argument('commit2', required=False, type=str)
+def image_diff(obj, commit1, commit2):
+    for section in obj.helper.get_updated_images(commit1, commit2):
+        click.echo(section)
+
+@cli.command(name='show',
+             help='Show detailed information about a specific image')
+@click.pass_obj
+@click.argument('section', required=True, type=str, callback=cb.validate_section)
+@click.argument('tag', required=False, type=str, default='latest')
+def image_show(obj, section, tag):
+    click.echo(obj.helper.get_image(section, tag))
+
+@cli.command(name='build',
+             help='Build images')
+@click.pass_obj
+@click.argument('sections', nargs=-1, required=True, type=str, callback=cb.validate_section)
+@click.option('--tag', 'tags', multiple=True, type=str, default=['latest'], show_default=True,
+              help='Specify one or multiple tags for the image')
+def image_build(obj, sections, tags):
+    for section in sections:
+        cmd.run([obj.command.docker, 'build', obj.helper.get_section_path(section),
+                 '--file', obj.helper.get_section_file(section),
+                 *itertools.chain(*[('--tag', obj.helper.get_image(section, tag)) for tag in tags])])
+
+@cli.command(name='push',
+             help='Push images')
+@click.pass_obj
+@click.argument('sections', nargs=-1, required=True, type=str, callback=cb.validate_section)
+@click.option('--tag', 'tags', multiple=True, type=str, default=['latest'], show_default=True,
+              help='Specify one or multiple tags for the image')
+def image_push(obj, sections, tags):
+    for section in sections:
+        for tag in tags:
+            cmd.run([obj.command.docker, 'push', obj.helper.get_image(section, tag)])
+
+@cli.command(name='clean',
+             help='Clean images')
+@click.pass_obj
+@click.argument('sections', nargs=-1, required=True, type=str, callback=cb.validate_section)
+@click.option('--tag', 'tags', multiple=True, type=str, default=['latest'], show_default=True,
+              help='Specify one or multiple tags for the image')
+def image_clean(obj, sections, tags):
+    for section in sections:
+        for tag in tags:
+            cmd.run([obj.command.docker, 'rmi', '--force', obj.helper.get_image(section, tag)])
+
+@cli.group(name='config', cls=hlp.OrderedGroup)
+def config_cli():
+    """Manage images' configuration
+
+    This is a command line interface for manage images' configuration
+    """
+
+@config_cli.command(name='view',
+                    help="View current images' configuration")
+@click.pass_obj
+def config_view(obj):
+    for section in obj.helper.get_images():
+        utils.print_image_config(obj.config, section)
+
+@config_cli.command(name='set',
+                    help='Add or update an image section in the configuration')
+@click.pass_obj
+@click.argument('section', required=True, type=click.Path(exists=True, file_okay=False),
+                callback=cb.transform_to_section)
+@click.option('--registry', required=False, type=str,
+              help='Name of the registry for this section.')
+@click.option('--image-file', required=False, type=str, default='Dockerfile', show_default=True,
+              help='Name of the Dockerfile for this section.')
+@click.option('--image-name', required=False, type=str,
+              help='Name of the image for this section.')
+@click.option('--depends-on', required=False, multiple=True,
+              type=click.Path(exists=True),
+              callback=cb.multiline_sections,
+              help='List of sections or paths that this section depends on.')
+def config_set(obj, section, registry, image_file, image_name, depends_on):
+    # pylint: disable=too-many-arguments
+    if not obj.config.has_option(configparser.DEFAULTSECT, Image.REGISTRY):
+        logging.getLogger(__name__).warning(
+            "Recommended to set the default registry with 'dock image config set-registry' first")
+    if obj.config.has_section(section) is False:
+        obj.config.add_section(section)
+    utils.set_config_option(obj.config, section, Image.REGISTRY, registry)
+    utils.set_config_option(obj.config, section, Image.FILE, image_file)
+    utils.set_config_option(obj.config, section, Image.NAME, image_name)
+    utils.set_config_option(obj.config, section, Image.DEPENDS_ON, depends_on)
+    utils.set_config_option(obj.config, section, Image.TYPE, SectionType.IMAGE)
+    click.echo()
+    utils.print_image_config(obj.config, section)
+    click.echo()
+    obj.helper.validate_section(section)
+    utils.update_config(obj.config, obj.config_file)
+
+@config_cli.command(name='set-registry',
+                    help='Set default registry for all images in the configuration')
+@click.pass_obj
+@click.argument('registry', required=False, type=str, default='namespace')
+def config_set_registry(obj, registry):
+    utils.set_config_option(obj.config, configparser.DEFAULTSECT, Image.REGISTRY, registry)
+    click.echo()
+    utils.update_config(obj.config, obj.config_file)
