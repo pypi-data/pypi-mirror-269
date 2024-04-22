97402c1c8b69f8b68fefbb75ff5df8ba3e54908f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:03:39 +0000

    Add package monit-agd/1.0/monit-agd-1.0.tar.gz

---
+++ packages/monit-agd/monit-agd-1.0.tar.gz/monit-agd-1.0/README.md
@@ -0,0 +1,56 @@
+### Monit
+
+**Instalação:**
+```bash
+pip install git+https://github.com/Agencia-de-Dados-bsb/monit.git
+```
+**Exemplo arquivo `.env`:**
+```bash
+# Project info
+PROJECT=sample_project
+COMPANY=acme
+LOCATION=ec2
+DEV=coder
+
+# Database info
+DB_USER=user
+DB_PASSWORD=p@ssw0rd
+DB_HOST=localhost
+DB_DATABASE=teste
+```
+**Exemplo de Uso:**
+```Python
+import time
+
+from monit.core import Monitor
+from monit.error import SetupError
+
+def main():
+    # Initialize the Monitor
+    monit = Monitor()
+
+    try:
+        # Your code that might raise exceptions
+        # For demonstration purposes, let's raise an exception
+        time.sleep(5)
+        raise ValueError("This is a sample error.")
+
+    except Exception as e:
+        # Notify the Monitor about the error
+        monit.notify(SetupError, e)
+
+    monit.end()
+
+
+if __name__ == "__main__":
+    main()
+```
+**Tipos de erros:**
+```bash
+SetupError
+DatabaseError
+HTTPError
+FileError
+FolderError
+TooManyRequests
+```
---
+++ packages/monit-agd/monit-agd-1.0.tar.gz/monit-agd-1.0/monit/core.py
@@ -0,0 +1,50 @@
+import time
+import sys
+
+from monit import config
+from monit import func
+from monit.database import Database, DataManager, MonitInit, MonitErr, MonitEnd
+
+DB_URL = f'mysql+pymysql://{config.user}:{config.password}@{config.host}/{config.database}'
+
+class DatabaseManager:
+    def __init__(self, db_url):
+        self.db = Database(db_url)
+        self.data_manager = DataManager(self.db)
+
+    def insert_data(self, data):
+        id = self.data_manager.insert(data)
+        return id
+
+    def close(self):
+        self.data_manager.close()
+
+
+class Monitor:
+    def __init__(self):
+        self.init_time = time.perf_counter()
+        self.db_manager = DatabaseManager(DB_URL)
+        self.initial_register()
+
+    def initial_register(self):
+        table = func.build_table(None, None, MonitInit(), None)
+        self.id_init = self.db_manager.insert_data(table)
+
+    def register(self, table, type=None, err=None):
+        table.id_init = self.id_init  # put the row id of the initial register
+        table = func.build_table(type, err, table, self.init_time)
+        self.db_manager.insert_data(table)
+
+    def end(self):
+        table = MonitEnd()
+        self.register(table)
+        self.db_manager.close()
+
+    def notify(self, type=None, error=None):
+        table = MonitErr()
+        self.register(table, type, error)
+
+    def notify_and_exit(self, type=None, error=None):
+        self.notify(type, error)
+        self.db_manager.close()
+        sys.exit(1)
