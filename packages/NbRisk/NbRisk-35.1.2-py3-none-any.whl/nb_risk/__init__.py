df9b2fcc515b62fa120a68f3928394b7fb983ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:04:17 +0000

    Add package NbRisk/35.1.2/NbRisk-35.1.2-py3-none-any.whl

---
+++ packages/NbRisk/NbRisk-35.1.2-py3-none-any.whl/nb_risk/__init__.py
@@ -0,0 +1,27 @@
+from extras.plugins import PluginConfig
+from .version import __version__
+
+
+class NbriskConfig(PluginConfig):
+    name = "nb_risk"
+    base_url = "nb_risk"
+    verbose_name = "Risk Management"
+    description = "NIST 800-30 Risk Management for Netbox"
+    version = __version__
+    author = "Renato Almdida Oliveira"
+    author_email = "renato.almeida.oliveira@gmail.com"
+    min_version = "3.5.0"
+    max_version = "3.7.99"
+    required_settings = []
+    default_settings = {
+        "supported_assets": [
+            "dcim.device",
+            "virtualization.virtualmachine",
+            "tenancy.tenant",
+            "dcim.site",
+        ],
+        "additional_assets": [],
+    }
+
+
+config = NbriskConfig  # noqa
---
+++ packages/NbRisk/NbRisk-35.1.2-py3-none-any.whl/nb_risk/forms.py
@@ -0,0 +1,240 @@
+from django import forms
+
+from netbox.forms import (
+    NetBoxModelForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelBulkEditForm,
+    NetBoxModelImportForm,
+)
+
+from dcim.models import Device, DeviceType
+from utilities.forms import BootstrapMixin
+from utilities.forms.fields import (
+    DynamicModelMultipleChoiceField,
+    SlugField,
+    DynamicModelChoiceField,
+)
+
+from . import models, choices
+
+# ThreatSource Forms
+
+
+class ThreatSourceForm(NetBoxModelForm):
+    class Meta:
+        model = models.ThreatSource
+        fields = [
+            "name",
+            "threat_type",
+            "capability",
+            "intent",
+            "targeting",
+            "description",
+            "notes",
+        ]
+
+
+class ThreatSourceFilterForm(NetBoxModelFilterSetForm):
+    model = models.ThreatSource
+
+    class Meta:
+        fields = ["name", "threat_type", "capability", "intent", "targeting"]
+
+
+class ThreatSourceImportForm(NetBoxModelImportForm):
+    class Meta:
+        model = models.ThreatSource
+        fields = [
+            "name",
+            "threat_type",
+            "capability",
+            "intent",
+            "targeting",
+            "description",
+            "notes",
+        ]
+
+class ThreatSourceBulkEditForm(NetBoxModelBulkEditForm):
+    model = models.ThreatSource
+
+    comments = forms.Textarea(
+        attrs={'class': 'font-monospace'}
+    )
+
+    class Meta:
+        nullable_fields = ("intent", "targeting", "description", "notes")
+# ThreatEvent Forms
+
+
+class ThreatEventForm(NetBoxModelForm):
+
+    vulnerability = DynamicModelMultipleChoiceField(
+        queryset=models.VulnerabilityAssignment.objects.all(),
+        required=True,
+    )
+
+    class Meta:
+        model = models.ThreatEvent
+        fields = [
+            "name",
+            "threat_source",
+            "description",
+            "notes",
+            "relevance",
+            "likelihood",
+            "impact",
+            "vulnerability",
+        ]
+
+
+class ThreatEventFilterForm(NetBoxModelFilterSetForm):
+    model = models.ThreatEvent
+
+    class Meta:
+        fields = [
+            "name",
+            "threat_source",
+            "relevance",
+            "likelihood",
+            "impact",
+            "vulnerability",
+        ]
+
+
+# Vulnerability Forms
+
+
+class VulnerabilityForm(NetBoxModelForm):
+
+    fieldsets = (
+        ("Vulnerability", ("name", "cve", "description", "notes"),),
+        ("CVSSv2 Score", ("cvssaccessVector", "cvssaccessComplexity", "cvssauthentication", "cvssconfidentialityImpact", "cvssintegrityImpact", "cvssavailabilityImpact", "cvssbaseScore")),
+    )
+    class Meta:
+        model = models.Vulnerability
+        fields = [
+            "name",
+            "cve",
+            "description",
+            "notes",
+            "cvssaccessVector",
+            "cvssaccessComplexity",
+            "cvssauthentication",
+            "cvssconfidentialityImpact",
+            "cvssintegrityImpact",
+            "cvssavailabilityImpact",
+            "cvssbaseScore",
+        ]
+
+
+class VulnerabilityFilterForm(NetBoxModelFilterSetForm):
+    model = models.Vulnerability
+
+    class Meta:
+        fields = ["name", "cve"]
+
+
+class VulnerabilitySearchFilterForm(NetBoxModelFilterSetForm):
+    model = models.Vulnerability
+    q = forms.CharField(label="cpeName", required=False)
+
+    device_type = DynamicModelChoiceField(
+        queryset=DeviceType.objects.all(),
+        required=False,
+    )
+    version = forms.CharField(required=False)
+
+    part = forms.ChoiceField(
+        choices=choices.CVE_PART_CHOICES,
+        required=False,
+    )
+
+class VulnerabilityImportForm(NetBoxModelImportForm):
+    class Meta:
+        model = models.Vulnerability
+        fields = [
+            "name",
+            "cve",
+            "description",
+            "notes",
+            "cvssaccessVector",
+            "cvssaccessComplexity",
+            "cvssauthentication",
+            "cvssconfidentialityImpact",
+            "cvssintegrityImpact",
+            "cvssavailabilityImpact",
+            "cvssbaseScore",
+        ]
+
+# VulnerabilityAssignment Forms
+
+
+class VulnerabilityAssignmentForm(BootstrapMixin, forms.ModelForm):
+
+    vulnerability = DynamicModelChoiceField(
+        queryset=models.Vulnerability.objects.all(),
+        required=True,
+    )
+
+    class Meta:
+        model = models.VulnerabilityAssignment
+        fields = ["asset_object_type", "asset_id", "vulnerability"]
+        widgets = {
+            "asset_object_type": forms.HiddenInput(),
+            "asset_id": forms.HiddenInput(),
+        }
+
+
+class VulnerabilityAssignmentFilterForm(NetBoxModelFilterSetForm):
+    model = models.VulnerabilityAssignment
+
+    class Meta:
+        fields = ["vulnerability"]
+
+
+# Risk Forms
+
+
+class RiskForm(NetBoxModelForm):
+    class Meta:
+        model = models.Risk
+        fields = [
+            "name",
+            "threat_event",
+            "description",
+            "notes",
+            "likelihood",
+            "impact",
+        ]
+
+
+class RiskFilterForm(NetBoxModelFilterSetForm):
+    model = models.Risk
+
+    class Meta:
+        fields = ["name", "threat_event", "description", "impact", "likelihood"]
+
+# Control Forms
+
+class ControlForm(NetBoxModelForm):
+    class Meta:
+        model = models.Control
+        fields = [
+            "name",
+            "description",
+            "notes",
+            "category",
+            "risk",
+        ]
+
+class ControlFilterForm(NetBoxModelFilterSetForm):
+    model = models.Control
+
+    class Meta:
+        fields = [
+            "name",
+            "description",
+            "notes",
+            "category",
+            "risk",
+        ]
