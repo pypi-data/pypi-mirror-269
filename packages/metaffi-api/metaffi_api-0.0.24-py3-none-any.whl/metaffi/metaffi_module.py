fdcfe014b51469101105c10a060f1d88de994736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:34 +0000

    Add package metaffi-api/0.0.24/metaffi_api-0.0.24-py3-none-any.whl

---
+++ packages/metaffi-api/metaffi_api-0.0.24-py3-none-any.whl/metaffi/metaffi_module.py
@@ -0,0 +1,99 @@
+import ctypes.util
+import metaffi.xllr_wrapper
+from metaffi.metaffi_types import *
+import metaffi.metaffi_runtime
+
+XCallParamsRetType = ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint64))
+XCallNoParamsRetType = ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint64))
+XCallParamsNoRetType = ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint64))
+XCallNoParamsNoRetType = ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p, ctypes.POINTER(ctypes.c_uint64))
+
+create_lambda_python_code = ctypes.c_char_p.in_dll(metaffi.xllr_wrapper.xllr_python3, 'create_lambda_python_code').value.decode('utf-8')
+exec(create_lambda_python_code)
+
+
+def make_metaffi_callable(f: Callable) -> Callable:
+	params_metaffi_types, retval_metaffi_types = metaffi.metaffi_types.get_callable_types(f)
+	
+	params_type = ctypes.c_uint64 * len(params_metaffi_types)
+	params_array = params_type(*params_metaffi_types)
+	
+	retvals_type = ctypes.c_uint64 * len(retval_metaffi_types)
+	retvals_array = retvals_type(*retval_metaffi_types)
+	
+	err = ctypes.c_char_p()
+	err_len = ctypes.c_uint32()
+	
+	xllr_python3_bytes = 'xllr.python311'.encode('utf-8')
+	
+	pxcall_and_context_array = metaffi.xllr_wrapper.xllr.make_callable(xllr_python3_bytes, len(xllr_python3_bytes), f, params_array, retvals_array, len(params_metaffi_types), len(retval_metaffi_types), ctypes.byref(err), ctypes.byref(err_len))
+	
+	pxcall_and_context_array = ctypes.cast(pxcall_and_context_array, ctypes.POINTER(ctypes.c_void_p * 2))
+	
+	if len(params_metaffi_types) > 0 and len(retval_metaffi_types) > 0:
+		pxcall = XCallParamsRetType(pxcall_and_context_array.contents[0])
+	elif len(params_metaffi_types) > 0 and len(retval_metaffi_types) == 0:
+		pxcall = XCallParamsNoRetType(pxcall_and_context_array.contents[0])
+	elif len(params_metaffi_types) == 0 and len(retval_metaffi_types) > 0:
+		pxcall = XCallNoParamsRetType(pxcall_and_context_array.contents[0])
+	else:
+		pxcall = XCallNoParamsNoRetType(pxcall_and_context_array.contents[0])
+	
+	context = pxcall_and_context_array.contents[1]
+	
+	res = create_lambda(pxcall, context, params_metaffi_types, retval_metaffi_types)
+	setattr(res, 'pxcall_and_context', ctypes.addressof(pxcall_and_context_array.contents))
+	setattr(res, 'params_metaffi_types', params_metaffi_types)
+	setattr(res, 'retval_metaffi_types', retval_metaffi_types)
+	return res
+
+
+class MetaFFIModule:
+	def __init__(self, runtime: metaffi.metaffi_runtime.MetaFFIRuntime, xllr: metaffi.xllr_wrapper._XllrWrapper, module_path: str):
+		self.runtime = runtime
+		self.xllr = xllr
+		self.module_path = module_path
+	
+	def load(self, function_path: str, params_metaffi_types: Tuple[metaffi.metaffi_types.metaffi_type_info] | None,
+			retval_metaffi_types: Tuple[metaffi.metaffi_types.metaffi_type_info] | None) -> Callable[..., Tuple[Any, ...]]:
+		
+		if params_metaffi_types is None:
+			params_metaffi_types = tuple()
+		
+		if retval_metaffi_types is None:
+			retval_metaffi_types = tuple()
+		
+		# Create ctypes arrays for params_metaffi_types and retval_metaffi_types
+		ParamsArray = metaffi.metaffi_types.metaffi_type_info * len(params_metaffi_types)
+		params_array = ParamsArray(*params_metaffi_types)
+		
+		RetvalArray = metaffi.metaffi_types.metaffi_type_info * len(retval_metaffi_types)
+		retval_array = RetvalArray(*retval_metaffi_types)
+		
+		# capsule the metaffi_type_info objects to access them in call_xcall
+		if not isinstance(params_metaffi_types, tuple):
+			params_metaffi_types = tuple(params_metaffi_types)
+		
+		if not isinstance(retval_metaffi_types, tuple):
+			retval_metaffi_types = tuple(retval_metaffi_types)
+		
+		# Call xllr.load_function
+		pxcall_and_context = self.xllr.load_function('xllr.' + self.runtime.runtime_plugin, self.module_path, function_path, params_array, retval_array, len(params_metaffi_types), len(retval_metaffi_types))
+		
+		pxcall_and_context_array = ctypes.cast(pxcall_and_context, ctypes.POINTER(ctypes.c_void_p * 2))
+		
+		pxcall = None
+		if len(params_metaffi_types) > 0 and len(retval_metaffi_types) > 0:
+			pxcall = XCallParamsRetType(pxcall_and_context_array.contents[0])
+		elif len(params_metaffi_types) > 0 and len(retval_metaffi_types) == 0:
+			pxcall = XCallParamsNoRetType(pxcall_and_context_array.contents[0])
+		elif len(params_metaffi_types) == 0 and len(retval_metaffi_types) > 0:
+			pxcall = XCallNoParamsRetType(pxcall_and_context_array.contents[0])
+		else:
+			pxcall = XCallNoParamsNoRetType(pxcall_and_context_array.contents[0])
+		
+		context = pxcall_and_context_array.contents[1]
+		
+		func_lambda: Callable[..., ...] = lambda *args: metaffi.xllr_wrapper.xllr_python3.call_xcall(pxcall, context, params_metaffi_types, retval_metaffi_types, None if not args else args)
+		
+		return func_lambda
---
+++ packages/metaffi-api/metaffi_api-0.0.24-py3-none-any.whl/metaffi/xllr_wrapper.py
@@ -0,0 +1,97 @@
+import ctypes
+from ctypes import *
+from metaffi.metaffi_types import *
+import platform
+import os
+
+
+def get_dynamic_lib_path_from_metaffi_home(fname: str):
+	osname = platform.system()
+	if os.getenv('METAFFI_HOME') is None:
+		raise RuntimeError('No METAFFI_HOME environment variable')
+	elif fname is None:
+		raise RuntimeError('fname is None')
+	
+	if osname == 'Windows':
+		return os.getenv('METAFFI_HOME') + '\\' + fname + '.dll'
+	elif osname == 'Darwin':
+		return os.getenv('METAFFI_HOME') + '/' + fname + '.dylib'
+	else:
+		return os.getenv('METAFFI_HOME') + '/' + fname + '.so'  # for everything that is not windows or mac, return .so
+
+
+if platform.system() == 'Windows':
+	os.add_dll_directory(os.getenv('METAFFI_HOME')+'\\bin\\')
+
+xllr_python3 = ctypes.cdll.LoadLibrary(get_dynamic_lib_path_from_metaffi_home('xllr.python311'))
+xllr_python3.call_xcall.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.py_object, ctypes.py_object, ctypes.py_object]
+xllr_python3.call_xcall.restype = ctypes.py_object
+
+xllr = cdll.LoadLibrary(get_dynamic_lib_path_from_metaffi_home('xllr'))
+
+# Set argtypes and restype
+xllr.load_runtime_plugin.argtypes = [ctypes.c_char_p, ctypes.c_uint32, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint32)]
+xllr.load_runtime_plugin.restype = None
+
+xllr.free_runtime_plugin.argtypes = [ctypes.c_char_p, ctypes.c_uint32, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint32)]
+xllr.free_runtime_plugin.restype = None
+
+xllr.load_function.argtypes = [ctypes.c_char_p, ctypes.c_uint32, ctypes.c_char_p, ctypes.c_uint32, ctypes.c_char_p, ctypes.c_uint32, ctypes.POINTER(metaffi_type_info), ctypes.POINTER(metaffi_type_info), ctypes.c_uint8, ctypes.c_uint8, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint32)]
+xllr.load_function.restype = ctypes.POINTER(ctypes.c_void_p)
+
+xllr.free_function.argtypes = [ctypes.c_char_p, ctypes.c_uint32, ctypes.POINTER(ctypes.c_void_p), ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint32)]
+xllr.free_function.restype = None
+
+xllr.alloc_cdts_buffer.argtypes = [ctypes.c_uint64, ctypes.c_uint64]
+xllr.alloc_cdts_buffer.restype = ctypes.c_void_p
+
+xllr.make_callable.argtypes = [ctypes.c_char_p, ctypes.c_uint32, ctypes.py_object, ctypes.POINTER(ctypes.c_uint64), ctypes.POINTER(ctypes.c_uint64), ctypes.c_uint8, ctypes.c_uint8, ctypes.POINTER(ctypes.c_char_p), ctypes.POINTER(ctypes.c_uint32)]
+xllr.make_callable.restype = ctypes.POINTER(ctypes.c_void_p)
+
+class _XllrWrapper:
+	def __init__(self):
+		global xllr
+		
+		if platform.system() == 'Windows':
+			os.add_dll_directory(os.environ['METAFFI_HOME'])
+			os.add_dll_directory(os.environ['METAFFI_HOME'] + '\\bin')
+	
+	def load_runtime_plugin(self, runtime_plugin: str) -> None:
+		err = ctypes.c_char_p()
+		err_len = ctypes.c_uint32()
+		xllr.load_runtime_plugin(runtime_plugin.encode('utf-8'), len(runtime_plugin), ctypes.byref(err), ctypes.byref(err_len))
+		if err_len.value > 0:
+			raise RuntimeError(err.value[:err_len.value].decode('utf-8'))
+	
+	def free_runtime_plugin(self, runtime_plugin: str) -> None:
+		err = ctypes.c_char_p()
+		err_len = ctypes.c_uint32()
+		xllr.free_runtime_plugin(runtime_plugin.encode('utf-8'), len(runtime_plugin), ctypes.byref(err), ctypes.byref(err_len))
+		if err_len.value > 0:
+			raise RuntimeError(err.value[:err_len.value].decode('utf-8'))
+	
+	def load_function(self, runtime_plugin_name: str, module_path: str, function_path: str,
+						params_types: ctypes.POINTER(metaffi_type_info),
+						retvals_types: ctypes.POINTER(metaffi_type_info), params_count: int,
+						retval_count: int) -> ctypes.c_void_p:
+		err = ctypes.c_char_p()
+		err_len = ctypes.c_uint32()
+		result = xllr.load_function(runtime_plugin_name.encode('utf-8'), len(runtime_plugin_name), module_path.encode('utf-8'), len(module_path),
+										function_path.encode('utf-8'), len(function_path), params_types, retvals_types, params_count,
+										retval_count, ctypes.byref(err), ctypes.byref(err_len))
+		if err_len.value > 0:
+			raise RuntimeError(err.value[:err_len.value].decode('utf-8'))
+		return result
+	
+	def free_function(self, runtime_plugin_name: str, pff: ctypes.POINTER(ctypes.c_void_p)) -> None:
+		err = ctypes.create_string_buffer(1000)
+		err_len = ctypes.c_uint32()
+		xllr.free_function(runtime_plugin_name, len(runtime_plugin_name), pff, ctypes.byref(err), ctypes.byref(err_len))
+		if err_len.value > 0:
+			raise RuntimeError(err.value[:err_len.value].decode('utf-8'))
+	
+	def alloc_cdts_buffer(self, params_count: int, ret_count: int) -> ctypes.c_void_p:
+		return xllr.alloc_cdts_buffer(params_count, ret_count)
+
+
+xllr_wrapper: _XllrWrapper = _XllrWrapper()
