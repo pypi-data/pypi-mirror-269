0684e6de6303896b063022e6141734387e305389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:04:12 +0000

    Add package multivoro/0.0.1/multivoro-0.0.1.tar.gz

---
+++ packages/multivoro/multivoro-0.0.1.tar.gz/multivoro-0.0.1/tests/test_compute_voronoi.py
@@ -0,0 +1,66 @@
+import pytest
+import numpy as np
+from multivoro import compute_voronoi
+
+from numpy import testing as npt
+
+
+@pytest.mark.parametrize(
+    "cfg",
+    [
+        {
+            "points": np.array([[-1.0, 0.0, 0.0], [1.0, 0.0, 0.0]]),
+            "radii": np.array([1.0, 1.0]),
+            "limits": np.array([[-2.0, -1.0, -1.0], [2.0, 1.0, 1.0]]),
+            "expected": [
+                {
+                    "vertices": [
+                        [-2.0, -1.0, -1.0],
+                        [0.0, -1.0, -1.0],
+                        [-2.0, 1.0, -1.0],
+                        [0.0, 1.0, -1.0],
+                        [-2.0, -1.0, 1.0],
+                        [0.0, -1.0, 1.0],
+                        [-2.0, 1.0, 1.0],
+                        [0.0, 1.0, 1.0],
+                    ],
+                },
+                {
+                    "vertices": [
+                        [0.0, -1.0, -1.0],
+                        [2.0, -1.0, -1.0],
+                        [0.0, 1.0, -1.0],
+                        [2.0, 1.0, -1.0],
+                        [0.0, -1.0, 1.0],
+                        [2.0, -1.0, 1.0],
+                        [0.0, 1.0, 1.0],
+                        [2.0, 1.0, 1.0],
+                    ],
+                },
+            ]
+        },
+    ],
+)
+def test_compute_voronoi(cfg):
+
+    cells = compute_voronoi(
+        points=cfg["points"],
+        radii=cfg["radii"],
+        limits=cfg["limits"],
+    )
+    assert len(cells) == len(cfg["expected"])
+    for i, cell in enumerate(cells):
+        npt.assert_allclose(cell.get_vertices(), cfg["expected"][i]["vertices"])
+
+
+
+def test_compute_voronoi__raises():
+
+    points = np.array([[-2.0, 0.0, 0.0], [1.0, 0.0, 0.0]])
+
+    radii = np.array([1.0, 1.0])
+
+    limits = np.array([[-1., -1., -1.], [1., 1., 1.]])
+
+    with pytest.raises(ValueError):
+        compute_voronoi(points, radii=radii, limits=limits)
