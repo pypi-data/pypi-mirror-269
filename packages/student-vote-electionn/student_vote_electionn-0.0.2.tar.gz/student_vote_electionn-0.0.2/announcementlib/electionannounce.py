28f8e2b1d7f3c12f92a53d78bff6ee9d7362d061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:12 +0000

    Add package student-vote-electionn/0.0.2/student_vote_electionn-0.0.2.tar.gz

---
+++ packages/student-vote-electionn/student_vote_electionn-0.0.2.tar.gz/student_vote_electionn-0.0.2/announcementlib/electionannounce.py
@@ -0,0 +1,14 @@
+import time
+
+announcements = [
+    "Announcement 1: Don't forget to cast your vote in the student election!",
+    "Announcement 2: Meet the candidates at the upcoming debate on Friday.",
+    "Announcement 3: The voting deadline has been extended to accommodate more students.",
+    "Announcement 4: Check your email for important election updates.",
+    "Announcement 5: Voting booths are open in the student center from 9 AM to 5 PM.",
+    "Announcement 6: Get involved in shaping the future of our campus - vote now!",
+]
+
+def rotate_announcements():
+    return announcements
+
