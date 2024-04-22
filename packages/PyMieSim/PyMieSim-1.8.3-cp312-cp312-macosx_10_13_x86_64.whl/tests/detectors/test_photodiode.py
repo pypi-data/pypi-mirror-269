e6911fc788fe63b421d37f9b8499fd4cf1aa5e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:06 +0000

    Add package PyMieSim/1.8.3/PyMieSim-1.8.3-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/PyMieSim/PyMieSim-1.8.3-cp312-cp312-macosx_10_13_x86_64.whl/tests/detectors/test_photodiode.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from PyMieSim.single.scatterer import Sphere
+from PyMieSim.single.source import Gaussian
+from PyMieSim.single.detector import Photodiode
+
+samplings = [100, 200, 300, 400]
+
+
+@pytest.mark.parametrize('sampling', samplings)
+def test_photodiode(sampling: int):
+    source = Gaussian(
+        wavelength=750e-9,
+        polarization_value=0,
+        polarization_type='linear',
+        optical_power=1,
+        NA=0.3
+    )
+    scatterer = Sphere(
+        diameter=100e-9,
+        source=source,
+        index=1.4,
+        medium_index=1.0
+    )
+
+    detector = Photodiode(
+        NA=0.2,
+        sampling=sampling,
+        gamma_offset=0,
+        phi_offset=0
+    )
+
+    detector.get_footprint(scatterer=scatterer)
+
+    detector.plot()
+
+# -
