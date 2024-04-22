dd8da7803e48a7526e9574ca1d562915d0f7be5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:07 +0000

    Add package kestra-ion/0.0.2/kestra_ion-0.0.2.tar.gz

---
+++ packages/kestra-ion/kestra_ion-0.0.2.tar.gz/kestra_ion-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="kestra-ion",
+    version=os.getenv('PACKAGE_VERSION', '0.0.0'),
+    author="Anna Geller",
+    author_email="hello@kestra.io",
+    packages=find_packages(),
+    url="https://github.com/anna-geller/kestra-ion",
+    license="LICENSE.txt",
+    description="A simple package to read Ion data into a list of dictionaries suitable for use in dataframes.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "amazon.ion",
+        "python-dateutil",
+    ],
+    tests_require=["pytest"],
+    extras_require={"test": ["pytest"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
