8e505436e928cf390ab96a90c00c3d6675875c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 05:03:12 +0000

    Add package kungfupanda/0.14/kungfupanda-0.14.tar.gz

---
+++ packages/kungfupanda/kungfupanda-0.14.tar.gz/kungfupanda-0.14/setup.py
@@ -0,0 +1,68 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        print(print(r"""
+        
+ __          __  _                            _          _   _          
+ \ \        / / | |                          | |        | | | |         
+  \ \  /\  / /__| | ___ ___  _ __ ___   ___  | |_ ___   | |_| |__   ___ 
+   \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | __| '_ \ / _ \
+    \  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) | | |_| | | |  __/
+     \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__|_| |_|\___|
+                                                                         
+                                                                         
+  ____  _         ______    _     _____                _       
+ |  _ \(_)       |  ____|  | |   |  __ \              | |      
+ | |_) |_  __ _  | |__ __ _| |_  | |__) |_ _ _ __   __| | __ _ 
+ |  _ <| |/ _` | |  __/ _` | __| |  ___/ _` | '_ \ / _` |/ _` |
+ | |_) | | (_| | | | | (_| | |_  | |  | (_| | | | | (_| | (_| |
+ |____/|_|\__, | |_|  \__,_|\__| |_|   \__,_|_| |_|\__,_|\__,_|
+           __/ |                                               
+          |___/                                                
+  _____                                      _ _         
+ / ____|                                    (_) |        
+| |     ___  _ __ ___  _ __ ___  _   _ _ __  _| |_ _   _ 
+| |    / _ \| '_ ` _ \| '_ ` _ \| | | | '_ \| | __| | | |
+| |___| (_) | | | | | | | | | | | |_| | | | | | |_| |_| |
+ \_____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|_|\__|\__, |
+                                                    __/ |
+                                                   |___/ 
+
+
+""")
+
+
+)
+
+
+setup(
+    name='kungfupanda',
+    version='0.14',
+    author='The big fat panda',
+    author_email='po_the_big_fat_panda@gmail.com',
+    description="A simple package to make everything easy for programmers for doing everything the cool way",
+    long_description="""A simple package for the people who are not bound by society rules 
+    made by an cute panda who trained to be a dragon warrior
+    For all the story watch the original triology
+    Kung Fu Panda 1
+    Kung Fu Panda 2
+    Kung Fu Panda 3
+    """,
+    url='',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',
+        'numpy>=1.21.0',
+        'pandas>=2.2.2',
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+
+
+)
+
