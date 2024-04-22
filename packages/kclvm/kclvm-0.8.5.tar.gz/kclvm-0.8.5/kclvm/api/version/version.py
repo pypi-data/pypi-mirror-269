5e5aee205c65868ef210349079121909084f690a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:04:21 +0000

    Add package kclvm/0.8.5/kclvm-0.8.5.tar.gz

---
+++ packages/kclvm/kclvm-0.8.5.tar.gz/kclvm-0.8.5/kclvm/api/version/version.py
@@ -0,0 +1,7 @@
+# Copyright 2021 The KCL Authors. All rights reserved.
+
+import os
+from pathlib import Path
+
+VERSION = "0.8.5"
+CHECKSUM = Path(os.path.dirname(__file__)).joinpath("checksum.txt").read_text().strip()
