9211e384cf5d2d0babef9288fa925d15e886a4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:57 +0000

    Add package bookio-fetchfox/2.4.0/bookio_fetchfox-2.4.0-py3-none-any.whl

---
+++ packages/bookio-fetchfox/bookio_fetchfox-2.4.0-py3-none-any.whl/fetchfox/tokens/book_token.py
@@ -0,0 +1,23 @@
+from fetchfox.constants.book import (
+    BOOK_TOKEN_ASSET_ID,
+    BOOK_TOKEN_ASSET_NAME,
+    BOOK_TOKEN_COINGECKO_ID,
+    BOOK_TOKEN_POLICY_ID,
+    BOOK_TOKEN_FINGERPRINT,
+)
+from fetchfox.constants.currencies import BOOK
+
+from .base import CardanoToken
+
+
+class BookToken(CardanoToken):
+    def __init__(self, dexhunter_partner_code: str):
+        super().__init__(
+            asset_id=BOOK_TOKEN_ASSET_ID,
+            asset_name=BOOK_TOKEN_ASSET_NAME,
+            fingerprint=BOOK_TOKEN_FINGERPRINT,
+            policy_id=BOOK_TOKEN_POLICY_ID,
+            symbol=BOOK,
+            coingecko_id=BOOK_TOKEN_COINGECKO_ID,
+            dexhunter_partner_code=dexhunter_partner_code,
+        )
