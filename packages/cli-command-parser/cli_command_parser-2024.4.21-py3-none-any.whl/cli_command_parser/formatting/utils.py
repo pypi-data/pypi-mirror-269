5a41b45d5f1d5dda0178d40acb9d14e729712eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:06 +0000

    Add package cli-command-parser/2024.4.21/cli_command_parser-2024.4.21-py3-none-any.whl

---
+++ packages/cli-command-parser/cli_command_parser-2024.4.21-py3-none-any.whl/cli_command_parser/formatting/utils.py
@@ -0,0 +1,158 @@
+"""
+Utils for usage / help text formatters
+
+:author: Doug Skrypa
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, Any, Collection, Sequence, Iterator, Iterable, List
+
+from ..compat import WCTextWrapper
+from ..config import ShowDefaults
+from ..context import ctx
+from ..utils import _NotSet, wcswidth
+
+if TYPE_CHECKING:
+    from ..typing import Bool, Strs, OptStrs
+
+__all__ = ['format_help_entry', 'line_iter']
+
+
+def format_help_entry(
+    usage_parts: Iterable[str],
+    description: OptStrs,
+    prefix: str = '',
+    tw_offset: int = 0,
+    *,
+    lpad: int = 2,
+    cont_indent: int = 2,
+    usage_delim: str = ', ',
+) -> str:
+    if prefix:
+        line_prefix = prefix + ' ' * (lpad - len(prefix))
+    else:
+        line_prefix = ' ' * lpad
+
+    config = ctx.config
+    term_width = ctx.terminal_width - tw_offset
+    usage_width = max(config.min_usage_column_width, config.usage_column_width - tw_offset - 2)
+    if not description:
+        return '\n'.join(
+            line_prefix + line for line in combine_and_wrap(usage_parts, term_width, cont_indent, usage_delim)
+        )
+
+    after_pad_width = usage_width - lpad
+    usage_lines = tuple(combine_and_wrap(usage_parts, term_width, cont_indent, usage_delim))
+    description_lines = [''] * _description_start_line(usage_lines, after_pad_width)
+    description_lines.extend(_normalize_column_width(_single_line_strs(description), term_width - usage_width - 2))
+    format_row = f'{line_prefix}{{:<{after_pad_width}s}}  {{}}'.format
+    return '\n'.join(format_row(*row).rstrip() for row in line_iter(usage_lines, description_lines))
+
+
+def combine_and_wrap(parts: Iterable[str], max_width: int, cont_indent: int = 0, delim: str = ', ') -> Iterator[str]:
+    """Combine the given strings using the given delimiter, wrapping to a new line at max_width."""
+    delim_end = delim.rstrip()
+    delim_len = len(delim)
+    line_len = delim_end_len = len(delim_end)
+    line_parts = []
+    last = None
+    for part in parts:
+        part_len = wcswidth(part)
+        line_len += part_len + delim_len
+        if line_len >= max_width:
+            if last:
+                yield last + delim_end
+                prefix = ' ' * cont_indent
+            else:
+                prefix = ''
+
+            if line_parts and line_len > max_width:
+                last = prefix + delim.join(line_parts)
+                line_parts = [part]
+                line_len = delim_end_len + cont_indent + part_len
+            else:
+                line_parts.append(part)
+                last = prefix + delim.join(line_parts)
+                line_parts = []
+                line_len = delim_end_len + cont_indent - delim_len
+        else:
+            line_parts.append(part)
+
+    if line_parts:
+        if last:
+            yield last + delim_end
+            prefix = ' ' * cont_indent
+        else:
+            prefix = ''
+        yield prefix + delim.join(line_parts)
+    elif last:
+        yield last
+
+
+def _description_start_line(usage: Iterable[str], max_usage_width: int) -> int:
+    widths = [wcswidth(line) for line in usage]
+    if max(widths, default=0) <= max_usage_width:
+        return 0
+
+    widths.reverse()
+    line = len(widths)
+    for width in widths:
+        if width > max_usage_width:
+            break
+        line -= 1
+    return line
+
+
+def _single_line_strs(lines: Strs) -> List[str]:
+    if isinstance(lines, str):
+        lines = (lines,)
+    return [line for full_line in lines for line in full_line.splitlines()]
+
+
+def _normalize_column_width(lines: Sequence[str], column_width: int, cont_indent: int = 0) -> Iterator[str]:
+    if max(map(wcswidth, lines)) + cont_indent <= column_width:
+        yield from lines
+    else:
+        tw = WCTextWrapper(column_width, break_long_words=True, break_on_hyphens=True)
+        for line in lines:
+            if wcswidth(line) + cont_indent >= column_width:
+                yield from tw.wrap(line)
+            else:
+                yield line
+
+
+def _should_add_default(default: Any, help_text: Optional[str], param_show_default: Optional[Bool]) -> bool:
+    if default is _NotSet:
+        return False
+    elif param_show_default is not None:
+        return param_show_default
+    sd = ctx.config.show_defaults
+    if sd._value_ < 2 or (sd & ShowDefaults.MISSING and help_text and 'default:' in help_text):  # noqa
+        return False
+    elif sd & ShowDefaults.ANY:
+        return True
+    elif sd & ShowDefaults.NON_EMPTY:
+        return bool(default) or not (default is None or isinstance(default, Collection))
+    else:
+        return bool(default)
+
+
+def line_iter(*columns: Strs) -> Iterator[List[str, ...]]:
+    """More complicated than what would be necessary for just 2 columns, but this will scale to handle 3+"""
+    exhausted = 0
+    column_count = len(columns)
+
+    def _iter(column: Strs) -> Iterator[str]:
+        nonlocal exhausted
+        yield from column.splitlines() if isinstance(column, str) else column
+        exhausted += 1
+        while True:
+            yield ''
+
+    column_iters = [_iter(c) for c in columns]
+    while True:
+        row = [next(ci) for ci in column_iters]  # pylint: disable=R1708
+        if exhausted == column_count:  # `while exhausted < column_count:` always results in 1 extra row
+            break
+        yield row
---
+++ packages/cli-command-parser/cli_command_parser-2024.4.21-py3-none-any.whl/cli_command_parser/typing.py
@@ -0,0 +1,64 @@
+"""
+Type checking aliases.
+
+:author: Doug Skrypa
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Callable, TypeVar, Type, Union, Optional, Collection, Sequence, TextIO, BinaryIO
+from typing import Pattern, Tuple, List, Dict
+
+if TYPE_CHECKING:
+    from datetime import datetime, date, time, timedelta
+    from enum import Enum
+    from pathlib import Path
+    from .commands import Command
+    from .config import CommandConfig, AllowLeadingDash
+    from .core import CommandMeta
+    from .inputs import InputType
+    from .parameters import Parameter, ParamGroup
+
+T = TypeVar('T')
+T_co = TypeVar('T_co', covariant=True)
+TypeFunc = Callable[[str], T_co]
+
+NT = TypeVar('NT', bound=float, covariant=True)
+Number = Union[NT, None]
+NumType = Callable[[Union[str, float, int]], NT]
+RngType = Union[range, int, Sequence[int]]
+
+InputTypeFunc = Union[None, TypeFunc, 'InputType', range, Type['Enum'], Pattern]
+ChoicesType = Optional[Collection[Any]]
+
+Bool = Union[bool, Any]
+StrSeq = Sequence[str]
+Strs = Union[str, StrSeq]
+OptStr = Optional[str]
+OptStrs = Optional[Strs]
+Strings = Collection[str]
+PathLike = Union[str, 'Path']
+
+Locale = Union[str, Tuple[Optional[str], Optional[str]]]
+TimeBound = Union['datetime', 'date', 'time', 'timedelta', None]
+
+FP = Union[TextIO, BinaryIO]
+Deserializer = Callable[[Union[str, bytes, FP]], Any]
+Serializer = Callable[..., Union[str, bytes, None]]
+Converter = Union[Deserializer, Serializer]
+
+Config = Optional['CommandConfig']
+AnyConfig = Union[Config, Dict[str, Any]]
+LeadingDash = Union['AllowLeadingDash', str, bool]
+
+Param = TypeVar('Param', bound='Parameter')
+ParamList = List[Param]
+ParamOrGroup = Union[Param, 'ParamGroup']
+
+CommandObj = TypeVar('CommandObj', bound='Command')
+CommandType = TypeVar('CommandType', bound='CommandMeta')
+CommandCls = Union[CommandType, Type[CommandObj]]
+CommandAny = Union[CommandCls, CommandObj]
+
+CommandMethod = Callable[[CommandObj], T_co]
+DefaultFunc = Union[Callable[[], T_co], CommandMethod]
