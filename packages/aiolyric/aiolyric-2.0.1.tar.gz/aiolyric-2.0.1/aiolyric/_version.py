62a4a119948898cd680785682d7117294417ab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:05 +0000

    Add package aiolyric/2.0.1/aiolyric-2.0.1.tar.gz

---
+++ packages/aiolyric/aiolyric-2.0.1.tar.gz/aiolyric-2.0.1/aiolyric/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides aiolyric version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update aiolyric` to change this file.
+
+from incremental import Version
+
+__version__ = Version("aiolyric", 2, 0, 1)
+__all__ = ["__version__"]
