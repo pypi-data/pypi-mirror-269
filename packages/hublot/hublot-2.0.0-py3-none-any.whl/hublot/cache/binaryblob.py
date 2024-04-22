276f51ef2c0bb378a6fc6dcc54946d3f1e870996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:04:01 +0000

    Add package hublot/2.0.0/hublot-2.0.0-py3-none-any.whl

---
+++ packages/hublot/hublot-2.0.0-py3-none-any.whl/hublot/cache/binaryblob.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+"""
+Functions for serialising request and response objects to binary blobs, which can then be stored in any storage.
+
+The format used is designed to mimic the HTTP exchange that travels on the wire, i.e. the blob starts with an loose rendering of
+the HTTP request head, then request body if present, then the response head, then body. This makes it convenient to inspect the
+blobs for manual debugging.
+"""
+
+# standards
+from codecs import getwriter
+from io import BytesIO
+import re
+from typing import Callable, Optional, Tuple
+
+# hublot
+from ..datastructures import CompiledRequest, Headers, Response
+
+
+EOL = "\r\n"
+EOL_BYTES = EOL.encode("UTF-8")
+EOL_LEN = len(EOL)
+
+
+Writer = getwriter("UTF-8")  # pylint: disable=invalid-name
+
+
+def compose_binary_blob(res: Response) -> bytes:
+    output = BytesIO()
+    write = Writer(output).write
+    _compose_request_blob(res.request, output, write)
+    _compose_response_blob(res, output, write)
+    return output.getvalue()
+
+
+def _compose_request_blob(
+    creq: CompiledRequest,
+    output: BytesIO,
+    write: Callable[[str], None],
+) -> None:
+    write(f"{creq.method} {creq.url}{EOL}")
+    for key, value in sorted(creq.headers.items()):
+        write(f"{key}: {value}{EOL}")
+    write(EOL)
+    if "Content-Length" in creq.headers:
+        body = b"" if creq.data is None else creq.data
+        content_length = int(creq.headers["Content-Length"])
+        if content_length != len(body):
+            # Don't write it out because we won't be able to read it back
+            raise Exception(f"body has {len(body)} bytes but Content-Length is {content_length}")
+        output.write(body)
+        write(EOL)
+    else:
+        assert creq.data is None, "compile_request should've set the Content-Length"
+    write(EOL)
+
+
+def _compose_response_blob(
+    res: Response,
+    output: BytesIO,
+    write: Callable[[str], None],
+) -> None:
+    write(f"HTTP {res.status_code} {res.reason}{EOL}")
+    for key, value in sorted(res.headers.items()):
+        write(f"{key}: {value}{EOL}")
+    write(EOL)
+    if res.content is not None:
+        output.write(res.content)
+
+
+def parse_binary_blob(data: bytes) -> Response:
+    pos = 0
+    method, url, pos = _parse_line(data, pos, r"^(\w+) (.+)$")
+    req_headers, req_body, pos = _parse_message(data, pos)
+    creq = CompiledRequest(
+        url=url,
+        method=method,
+        headers=req_headers,
+        data=req_body,
+        num_retries=0,
+    )
+    status_code, reason, pos = _parse_line(data, pos, r"^HTTP (\d+) (.*)$")
+    res_headers, res_body, pos = _parse_message(data, pos, read_to_end=True)
+    assert res_body is not None  # since we passed `read_to_end=True`
+    return Response(
+        request=creq,
+        from_cache=True,
+        history=[],  # set elsewhere
+        status_code=int(status_code),
+        reason=reason,
+        headers=res_headers,
+        content=res_body,
+    )
+
+
+def _parse_message(
+    data: bytes,
+    pos: int,
+    read_to_end: bool = False,
+) -> Tuple[Headers, Optional[bytes], int]:
+    headers = Headers()
+    while data[pos : pos + EOL_LEN] != EOL_BYTES:
+        key, value, pos = _parse_line(data, pos, r"^([^:]+): (.*)$")
+        headers[key] = value
+    pos += EOL_LEN
+    body: Optional[bytes] = None
+    if read_to_end:
+        body = data[pos:]
+        pos = len(data)
+    elif headers.get("Content-Length"):
+        length = int(headers["Content-Length"])
+        body = data[pos : pos + length]
+        pos += length + (2 * EOL_LEN)
+    else:
+        pos += EOL_LEN
+    return headers, body, pos
+
+
+def _parse_line(data: bytes, pos: int, regex: str):
+    eol_pos = data.find(EOL_BYTES, pos)
+    line = data[pos:eol_pos].decode("UTF-8")
+    match = re.search(regex, line)
+    if not match:  # pragma: no cover
+        raise ValueError(repr(line))
+    return (*match.groups(), eol_pos + EOL_LEN)
---
+++ packages/hublot/hublot-2.0.0-py3-none-any.whl/hublot/logs.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# standards
+from dataclasses import dataclass
+import logging
+from typing import Iterator, List, Optional, Union
+
+# hublot
+from .datastructures import CompiledRequest
+
+LOGGER = logging.getLogger("hublot")
+
+
+@dataclass(frozen=False)
+class LogEntry:
+    creq: CompiledRequest
+    is_redirect: bool = False
+    cache_key_str: Optional[str] = None
+    cached: Optional[bool] = None
+    courtesy_seconds: Optional[float] = None
+    engine_short_code: Optional[str] = None
+
+    def _compose_line(self) -> Iterator[str]:
+        if self.cache_key_str:
+            yield f"[{self.cache_key_str}] "
+        if self.cached:
+            yield "[cached] "
+        else:
+            engine_and_sleep = self._compose_engine_and_sleep()
+            if engine_and_sleep:
+                yield f"{engine_and_sleep:8s} "
+            else:
+                yield "         "
+        if self.is_redirect:
+            yield "-> "
+        yield self.creq.url
+        if self.creq.data is not None:
+            yield f" [{self.creq.method} {len(self.creq.data)} bytes]"
+
+    def _compose_engine_and_sleep(self) -> Optional[str]:
+        if self.cached:
+            return "[cached]"
+        parts: List[str] = []
+        if self.engine_short_code:
+            parts.append(self.engine_short_code)
+        if self.courtesy_seconds and self.courtesy_seconds > 0.5:
+            parts.append(f"{round(self.courtesy_seconds)}s")
+        if not parts:
+            return None
+        return "[%s]" % "+".join(parts)
+
+    def __str__(self) -> str:
+        return "".join(self._compose_line())
+
+
+def basic_logging_config(level: Union[int, str] = "INFO", propagate: bool = False) -> None:
+    """
+    Sets up logging for the common use case. Calls `logging.basicConfig`, lowers verbosity for the `urllib3` logger. If `propagate`
+    is False (the default), a new handler will be attached to `hublot.LOGGER` that logs in a simple format to stderr, and does not
+    propagate log events to the root logger.
+    """
+    if not isinstance(level, int):
+        level = getattr(logging, level)
+    logging.basicConfig(level=level)
+    logging.getLogger("urllib3").setLevel(max(logging.WARNING, level))
+    if not propagate and not LOGGER.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(message)s", None, "%")
+        handler.setFormatter(formatter)
+        LOGGER.addHandler(handler)
+        LOGGER.propagate = False
