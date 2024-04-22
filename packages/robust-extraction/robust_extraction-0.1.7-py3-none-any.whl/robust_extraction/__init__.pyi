f72327c5e96a9e7ead938fb57ba6698dd15f3fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:13 +0000

    Add package robust-extraction/0.1.7/robust_extraction-0.1.7-py3-none-any.whl

---
+++ packages/robust-extraction/robust_extraction-0.1.7-py3-none-any.whl/robust_extraction/__init__.pyi
@@ -0,0 +1,8 @@
+from .pipeline import descaled_extract, Result, Error
+from .contours import extract_contours, Contours
+from .perspective import Corners
+from .manual import correct_perspective, extract_grid
+
+__all__ = [
+  'descaled_extract', 'Result', 'Error', 'extract_contours', 'Contours', 'Corners', 'correct_perspective', 'extract_grid'
+]
