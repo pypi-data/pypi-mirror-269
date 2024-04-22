5389a3abb162015b254224dfe75bd6b711e5e1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 05:03:18 +0000

    Add package botl/111/botl-111.tar.gz

---
+++ packages/botl/botl-111.tar.gz/botl-111/botl/modules/__init__.py
@@ -0,0 +1,35 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C,R
+# ruff: noqa: F401
+
+
+"modules"
+
+
+from . import cmd, err, flt, fnd, log, mbx, mdl, mod, req, rst, tdo
+from . import irc, rss, thr, tmr, udp, wsd
+
+
+def __dir__():
+    return (
+       'err',
+       'flt',
+       'fnd',
+       'irc',
+       'log',
+       'man',
+       'mbx',
+       'mdl',
+       'mod',
+       'req',
+       'rss',
+       'rst',
+       'tdo',
+       'thr',
+       'udp',
+       'wsd'
+    )
+
+
+__all__ = __dir__()
