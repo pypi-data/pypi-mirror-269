001aebeba07a3286445839398d595ffb253b611b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:18 +0000

    Add package chainhashing/0.0.2/chainhashing-0.0.2-py3-none-any.whl

---
+++ packages/chainhashing/chainhashing-0.0.2-py3-none-any.whl/chainhashing/example.py
@@ -0,0 +1,8 @@
+class Maxim:
+    def __init__(self):
+        self.name = "Maxim"
+    def say_hello(self):
+        return f"Hello, my name is {self.name}!"
+
+def test():
+    print("Hello, World!")
