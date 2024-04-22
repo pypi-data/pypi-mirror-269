66c7a977a3207d7298b8a66000cbfff8fdc4b11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:03:53 +0000

    Add package spacy/4.0.0.dev3/spacy-4.0.0.dev3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/spacy/spacy-4.0.0.dev3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/spacy/tokens/__init__.py
@@ -0,0 +1,8 @@
+from .doc import Doc
+from .doc_bin import DocBin
+from .morphanalysis import MorphAnalysis
+from .span import Span
+from .span_group import SpanGroup
+from .token import Token
+
+__all__ = ["Doc", "DocBin", "MorphAnalysis", "Span", "SpanGroup", "Token"]
