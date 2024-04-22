d4a337bd517d2c0396f4dbb5b02f403c192feeb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 05:03:09 +0000

    Add package pyrinter/0.0.1a6/pyrinter-0.0.1a6-py3-none-any.whl

---
+++ packages/pyrinter/pyrinter-0.0.1a6-py3-none-any.whl/pyrinter/__init__.py
@@ -0,0 +1,7 @@
+from .document import Document
+from .document import Font
+from .document import Align
+from .document import PaperSize
+from .utils import printer_utils
+from .printers import Printer
+
