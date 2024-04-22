8ebbb3e40b2fa0f01ecf03a22d8830201b8c6fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:03:43 +0000

    Add package nonebot-plugin-gshisbanner/1.3.1/nonebot_plugin_gshisbanner-1.3.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-gshisbanner/nonebot_plugin_gshisbanner-1.3.1-py3-none-any.whl/nonebot_plugin_gshisbanner/__init__.py
@@ -0,0 +1,44 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+
+require("nonebot_plugin_saa")
+from nonebot_plugin_saa import enable_auto_select_bot
+
+from .config import Config
+from .constant import DRIVER
+from .adapters import *  # noqa: F401, F403
+from .start import init_group_card  # noqa: F401
+
+enable_auto_select_bot()
+DRIVER.on_startup(init_group_card)
+
+__version__ = "1.3.1"
+__plugin_meta__ = PluginMetadata(
+    name="gshisbanner",
+    description="这是一个在机器人上获取原神历史卡池的插件",
+    usage="""
+    (
+        usage1: [角色/武器名]历史卡池/历史up[长度],
+        explain1: 获取该角色/武器的历史卡池信息,
+        example1: [莫娜历史卡池，刻晴历史up6]
+    ),
+    (
+        usage2: [版本号]卡池/up,
+        explain2: 获取该版本的卡池信息,
+        example2: [1.3卡池,1.4up,4.0.1up]
+    ),
+    (
+        usage3: [刷新/更新]历史卡池/别名,
+        explain3: 刷新历史卡池信息或者刷新别名,
+        example3: [刷新历史卡池,更新别名]
+    )
+    """,
+    type="application",
+    homepage="https://github.com/forchannot/nonebot-plugin-gshisbanner",
+    config=Config,
+    supported_adapters={"~onebot.v11", "~red"},
+    extra={
+        "author": "forchannot",
+        "version": __version__,
+    },
+)
---
+++ packages/nonebot-plugin-gshisbanner/nonebot_plugin_gshisbanner-1.3.1-py3-none-any.whl/nonebot_plugin_gshisbanner/send.py
@@ -0,0 +1,71 @@
+from typing import Union
+from datetime import datetime
+
+from nonebot import require
+
+require("nonebot_plugin_saa")
+from nonebot_plugin_saa import (
+    Text,
+    Image,
+    MessageFactory,
+    PlatformTarget,
+    AggregatedMessageFactory,
+)
+
+Sendable = Union[MessageFactory, AggregatedMessageFactory]
+
+
+# 角色历史卡池文字合并转发预处理
+async def word_send_from_name(
+    send_target: PlatformTarget, real_name: str, info, length
+):
+    end_time = datetime.strptime(info[0]["end"], "%Y-%m-%d %H:%M:%S").date()
+    start_time = datetime.strptime(info[0]["start"], "%Y-%m-%d %H:%M:%S").date()
+    end_t = (datetime.now().date() - end_time).days
+    start_t = (datetime.now().date() - start_time).days
+    if end_t > 0:
+        delta_time = f"最近一次up距离现在已有{end_t}天"
+    elif start_t < 0:
+        delta_time = f"还未开始up,距离开始还有约{-start_t}天"
+    else:
+        delta_time = (
+            "当前还剩最后一天up了"
+            if end_t == 0
+            else f"当前正在up中,距离结束还有约{-end_t}天"
+        )
+    msg_content = f"{real_name}{delta_time}"
+    for i in range(0, len(info), length):
+        msg = msg_content if i == 0 else []
+        await send_banner_info(send_target, msg, info[i : i + length])
+
+
+# 版本卡池文字合并转发预处理
+async def word_send_from_version(send_target: PlatformTarget, version, info):
+    msg_content = f"{version}版本卡池"
+    await send_banner_info(send_target, msg_content, info)
+
+
+# 合并转发
+async def send_forward_msg(send_target: PlatformTarget, info: Sendable):
+    await info.send_to(send_target)
+
+
+# 合并转发最终处理
+async def send_banner_info(send_target: PlatformTarget, msg_content, banner_info):
+    msgs = MessageFactory([Text(msg_content)])
+    for info in banner_info:
+        start = datetime.strptime(info["start"], "%Y-%m-%d %H:%M:%S").date()
+        end = datetime.strptime(info["end"], "%Y-%m-%d %H:%M:%S").date()
+        version = "{}.{}  卡池{}".format(*info["version"].split("."))
+        banner_five = " ".join(info.get("five_character", info.get("five_weapon", [])))
+        banner_four = " ".join(info.get("four_character", info.get("four_weapon", [])))
+        msgs.append(
+            f"版本：{version}\n五星：{banner_five}"
+            f"\n四星：{banner_four}\nup时间：\n{start}~~{end}"
+        )
+    forward_msg = AggregatedMessageFactory(list(msgs))
+    await send_forward_msg(send_target, forward_msg)
+
+
+async def send_pic(send_target: PlatformTarget, pic: Image):
+    await pic.send_to(send_target)
