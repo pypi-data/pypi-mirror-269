e09117767cbeb16793972a6ef9ff36a4893e72ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:20 +0000

    Add package rtmf6/0.0.2/rtmf6-0.0.2-py3-none-any.whl

---
+++ packages/rtmf6/rtmf6-0.0.2-py3-none-any.whl/rtmf6/main.py
@@ -0,0 +1,10 @@
+"""Reactive transport modeling with MODFLOW 6."""
+
+
+def main():
+    """Show meta infos."""
+    print('Reactive transport modeling with MODFLOW 6.')
+
+
+if __name__ == '__main__':
+    main()
