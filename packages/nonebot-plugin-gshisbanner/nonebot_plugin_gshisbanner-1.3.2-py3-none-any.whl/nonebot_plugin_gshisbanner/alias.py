a8ece07cc05e8d344d6839d577a2664b924d1c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:03:18 +0000

    Add package nonebot-plugin-gshisbanner/1.3.2/nonebot_plugin_gshisbanner-1.3.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-gshisbanner/nonebot_plugin_gshisbanner-1.3.2-py3-none-any.whl/nonebot_plugin_gshisbanner/alias.py
@@ -0,0 +1,32 @@
+from typing import Dict, List, Tuple, Optional, cast
+
+from .constant import alias_path
+from .deal_json import load_json
+
+
+def get_name_by_alias(target: str, target_type: str) -> Optional[str]:
+    """
+    :param target: 别名
+    :param target_type: 类型
+    :return: 真正的名字
+    """
+    data: Dict[str, List[str]] = cast(Dict, load_json(alias_path))[target_type]
+    return next((k for k, v in data.items() if target in v), None)
+
+
+def find_name(target: str) -> Tuple[Optional[str], str]:
+    """
+    :param target: 从用户输入获取到的名称/别名
+    :return: 真正的名字, 类型[角色/武器] or None
+    """
+    types = ["角色", "武器"]
+    result = None
+    found_type = "None"
+    for _ in range(2):
+        for target_type in types:
+            if (result := get_name_by_alias(target, target_type)) is not None:
+                found_type = target_type
+                break
+        if result is not None:
+            break
+    return result, found_type
---
+++ packages/nonebot-plugin-gshisbanner/nonebot_plugin_gshisbanner-1.3.2-py3-none-any.whl/nonebot_plugin_gshisbanner/config.py
@@ -0,0 +1,20 @@
+# 插件的配置文件
+from pathlib import Path
+from typing import Literal
+
+from nonebot import get_driver
+from pydantic import Extra, BaseModel
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    gshisbanner_forward_length: int = 10
+    """单次合并转发条数"""
+    gshisbanner_json_url: str = "banners.52v6.com/data"
+    """卡池信息来源网站"""
+    send_type: Literal["forward", "pic"] = "forward"
+    """发送方式：[“合并转发”，“图片”]"""
+    pic_font_path: str = (Path(__file__).parent / "fonts" / "misans.ttf").as_posix()
+    """图片字体路径"""
+
+
+plugin_config = Config.parse_obj(get_driver().config.dict())
