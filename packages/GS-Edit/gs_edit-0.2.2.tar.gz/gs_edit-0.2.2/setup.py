0eab391b051819bc032b00f1047964387ee37e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:04:05 +0000

    Add package GS-Edit/0.2.2/gs_edit-0.2.2.tar.gz

---
+++ packages/GS-Edit/gs_edit-0.2.2.tar.gz/gs_edit-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GS-Edit', 
+    version='0.2.2',
+    description='The Best Code Editor - GS',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  
+    author='James Kaddissi',
+    author_email='jameskaddissi@gmail.com',
+    url='https://github.com/james-kaddissi/gs_edit',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={
+        '': ['*.qss'], 
+        'gsedit': ['css/*.qss'],
+        '': ['*.png', '*.svg'],
+        'gsedit': ['images/*.png', 'images/*.svg'],
+    },
+    install_requires=[
+        'PyQt5',  
+        'QScintilla',
+        'jedi'
+    ],
+    entry_points={
+        'console_scripts': [
+            'gs=gsedit.cli:run', 
+        ],
+    },
+    python_requires='>=3.6',
+)
