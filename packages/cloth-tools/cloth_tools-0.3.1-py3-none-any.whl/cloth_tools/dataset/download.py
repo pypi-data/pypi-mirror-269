0b1cffff7ea321f510fd266ce7177e63f2a91bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:03:54 +0000

    Add package cloth-tools/0.3.1/cloth_tools-0.3.1-py3-none-any.whl

---
+++ packages/cloth-tools/cloth_tools-0.3.1-py3-none-any.whl/cloth_tools/dataset/download.py
@@ -0,0 +1,80 @@
+import os
+import urllib
+import zipfile
+from pathlib import Path
+
+import requests
+from cloth_tools.dataset.format import COMPETITION_OBSERVATION_FILENAMES
+from tqdm import tqdm
+
+
+def download_and_extract_dataset(data_dir: str, dataset_zip_url: str) -> str:
+    """Download and extract a Cloth Competition Dataset zip file from a URL.
+
+    Args:
+        data_dir: The parent directory to save the extracted dataset to.
+        dataset_zip_url: The URL of the dataset zip file.
+
+    Returns:
+        The path to the extracted dataset directory.
+    """
+    os.makedirs(data_dir, exist_ok=True)
+    with requests.get(dataset_zip_url, stream=True) as response:
+        response.raise_for_status()
+
+        total_size = int(response.headers.get("content-length", 0))
+        block_size = 1024 * 1024  # 1 MB at a time
+
+        zip_name = os.path.basename(urllib.parse.urlparse(dataset_zip_url).path)
+        zip_path = os.path.join(data_dir, zip_name)
+        dataset_dir = os.path.join(data_dir, zip_name.split(".")[0])
+
+        progress_bar = tqdm(total=total_size, unit="B", unit_scale=True)
+
+        with open(zip_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=block_size):
+                if chunk:  # filter out keep-alive chunks
+                    f.write(chunk)
+                    progress_bar.update(len(chunk))
+
+    with zipfile.ZipFile(zip_path) as z:
+        z.extractall(data_dir)
+
+    return dataset_dir
+
+
+def get_latest_observation_start_url(server_url: str) -> str:
+    url = server_url + "/latest_observation_start_dir"  # simple API to get the latest observation directory
+    response = requests.get(url)
+    return server_url + "/" + response.text
+
+
+def download_latest_observation(dataset_dir: str, server_url: str) -> tuple[str, str]:
+    """Download the latest start observation's files from the Cloth Competition server.
+    Also returns the sample ID, which must be used when uploading a grasp.json file.
+
+    Args:
+        dataset_dir: The directory to save observation files to.
+        server_url: URL of the server to download from.
+
+    Returns:
+        A tuple containing the path to the observation directory and the sample ID.
+    """
+    files_to_retrieve = COMPETITION_OBSERVATION_FILENAMES
+    observation_dir_url = get_latest_observation_start_url(server_url)
+
+    sample_dirname = observation_dir_url.split("/")[-2]
+    sample_datetime = sample_dirname.split("sample_")[-1]
+
+    sample_dir = Path(dataset_dir) / sample_dirname
+    observation_start_dir = sample_dir / "observation_start"
+    os.makedirs(observation_start_dir, exist_ok=True)
+
+    for _, filename in files_to_retrieve.items():
+        url = observation_dir_url + "/" + filename
+        response = requests.get(url)
+        filepath = os.path.join(observation_start_dir, filename)
+        with open(filepath, "wb") as f:
+            f.write(response.content)
+
+    return str(observation_start_dir), sample_datetime
---
+++ packages/cloth-tools/cloth_tools-0.3.1-py3-none-any.whl/cloth_tools/drake/scenes.py
@@ -0,0 +1,110 @@
+import tempfile
+
+import airo_models
+import numpy as np
+import open3d as o3d
+from airo_camera_toolkit.point_clouds.conversions import point_cloud_to_open3d
+from airo_drake import X_URBASE_ROSBASE, add_floor, add_manipulator, add_wall
+from airo_models import mesh_urdf_path
+from airo_typing import HomogeneousMatrixType, PointCloud
+from pydrake.math import RigidTransform, RollPitchYaw
+from pydrake.multibody.tree import ModelInstanceIndex
+from pydrake.planning import RobotDiagramBuilder
+
+# Some fixed transforms and default poses (e.g. for when you don't have access to the real robot)
+ARM_Y_DEFAULT = 0.45
+
+X_CB_B = X_URBASE_ROSBASE
+
+# The default pose of the left robot base when using the ROS URDFs
+X_W_L_DEFAULT = RigidTransform(rpy=RollPitchYaw([0, 0, np.pi / 2]), p=[0, ARM_Y_DEFAULT, 0])
+X_W_LCB_DEFAULT = X_W_L_DEFAULT.GetAsMatrix4() @ np.linalg.inv(X_CB_B.GetAsMatrix4())
+
+# The default pose of the right robot base when using the ROS URDFs
+X_W_R_DEFAULT = RigidTransform(rpy=RollPitchYaw([0, 0, np.pi / 2]), p=[0, -ARM_Y_DEFAULT, 0])
+X_W_RCB_DEFAULT = X_W_R_DEFAULT.GetAsMatrix4() @ np.linalg.inv(X_CB_B.GetAsMatrix4())
+
+
+def add_cloth_competition_dual_ur5e_scene(
+    robot_diagram_builder: RobotDiagramBuilder,
+    X_W_LCB: HomogeneousMatrixType | None = None,
+    X_W_RCB: HomogeneousMatrixType | None = None,
+) -> tuple[tuple[ModelInstanceIndex, ModelInstanceIndex], tuple[ModelInstanceIndex, ModelInstanceIndex]]:
+    parser = robot_diagram_builder.parser()
+    parser.SetAutoRenaming(True)
+
+    X_W_L = X_W_L_DEFAULT if X_W_LCB is None else RigidTransform(X_W_LCB @ X_CB_B.GetAsMatrix4())
+    X_W_R = X_W_R_DEFAULT if X_W_RCB is None else RigidTransform(X_W_RCB @ X_CB_B.GetAsMatrix4())
+
+    add_floor(robot_diagram_builder, y_size=2.4)
+
+    # Add three safety walls
+    arm_y = X_W_L.translation()[1]
+    wall_thickness = 0.2
+    wall_left_position = np.array([0, arm_y + 0.7 + wall_thickness / 2, 0])
+    wall_right_position = np.array([0, -arm_y - 0.7 - wall_thickness / 2, 0])
+    wall_back_position = np.array([0.9 + wall_thickness / 2, 0, 0])
+    add_wall(robot_diagram_builder, "XZ", 2.0, 2.0, wall_thickness, wall_left_position, "wall_left")
+    add_wall(robot_diagram_builder, "XZ", 2.0, 2.0, wall_thickness, wall_right_position, "wall_right")
+    add_wall(robot_diagram_builder, "YZ", 2.0, 2.7, wall_thickness, wall_back_position, "wall_back")
+
+    # The robot arms
+    arm_left_index, gripper_left_index = add_manipulator(
+        robot_diagram_builder, "ur5e", "robotiq_2f_85", X_W_L, static_gripper=True
+    )
+    arm_right_index, gripper_right_index = add_manipulator(
+        robot_diagram_builder, "ur5e", "robotiq_2f_85", X_W_R, static_gripper=True
+    )
+
+    return (arm_left_index, arm_right_index), (gripper_left_index, gripper_right_index)
+
+
+def add_cloth_obstacle_to_builder(
+    robot_diagram_builder: RobotDiagramBuilder, point_cloud_cloth: PointCloud
+) -> tuple[ModelInstanceIndex, o3d.t.geometry.TriangleMesh]:
+
+    # Create mesh URDF of the convex hull of the cloth point cloud
+    pcd = point_cloud_to_open3d(point_cloud_cloth)
+    hull = pcd.compute_convex_hull()
+
+    # Scaling disable for now because this might make it collide with that gripper that's holding it in the air
+    # hull.scale(1.1, hull.get_center())  # make 10% larger
+
+    hull_file = tempfile.NamedTemporaryFile(prefix="cloth_hull_", suffix=".obj", delete=False)
+    hull_filename = hull_file.name
+    o3d.t.io.write_triangle_mesh(hull_filename, hull)  # equiv? hull.write_obj(hull_filename)
+
+    hull_urdf_path = mesh_urdf_path(hull_filename, "cloth_hull")
+
+    # Add the cloth hull to the scene
+    plant = robot_diagram_builder.plant()
+    parser = robot_diagram_builder.parser()
+    hull_index = parser.AddModels(hull_urdf_path)[0]
+
+    world_frame = plant.world_frame()
+    hull_frame = plant.GetFrameByName("base_link", hull_index)
+    plant.WeldFrames(world_frame, hull_frame)
+
+    return hull_index, hull
+
+
+def add_safety_wall_to_builder(
+    robot_diagram_builder: RobotDiagramBuilder, X_W_TCP: HomogeneousMatrixType
+) -> ModelInstanceIndex:
+    plant = robot_diagram_builder.plant()
+    parser = robot_diagram_builder.parser()
+    safety_wall_thickness = 0.05
+    safety_wall_urdf_path = airo_models.box_urdf_path((0.2, safety_wall_thickness, 0.3), "safety_wall")
+
+    # Position the safety wall in front of the left TCP
+    shift = safety_wall_thickness / 2 + 0.01
+    p_W_S = X_W_TCP[:3, 3] + X_W_TCP[:3, 2] * shift
+    X_W_S = RigidTransform(p=p_W_S)
+
+    # Add the cloth hull to the scene
+    safety_wall_index = parser.AddModels(safety_wall_urdf_path)[0]
+    world_frame = plant.world_frame()
+    safety_wall_frame = plant.GetFrameByName("base_link", safety_wall_index)
+    plant.WeldFrames(world_frame, safety_wall_frame, X_W_S)
+
+    return safety_wall_index
