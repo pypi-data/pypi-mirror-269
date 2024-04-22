e8668a5f2b179396f757e5002c60d590125ed327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:18 +0000

    Add package essential-packages/0.2/essential_packages-0.2.tar.gz

---
+++ packages/essential-packages/essential_packages-0.2.tar.gz/essential_packages-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='essential_packages',
+    version='0.2',
+    packages= find_packages(),
+    install_requires =[
+
+    ],
+
+    entry_points ={
+        "console_scripts":[
+            
+            "codex = essential_packages:hello",
+        ],
+    },
+)
