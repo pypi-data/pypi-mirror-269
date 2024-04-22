4b2d907482e902ca3bc53e092aec1dbd52f2a4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:07 +0000

    Add package netbox-contract/2.0.13/netbox_contract-2.0.13-py3-none-any.whl

---
+++ packages/netbox-contract/netbox_contract-2.0.13-py3-none-any.whl/netbox_contract/search.py
@@ -0,0 +1,30 @@
+from netbox.search import SearchIndex, register_search
+
+from .models import Contract, Invoice, ServiceProvider
+
+
+@register_search
+class ServiceProviderIndex(SearchIndex):
+    model = ServiceProvider
+    fields = (
+        ('name', 100),
+        ('comments', 5000),
+    )
+
+
+@register_search
+class ContractIndex(SearchIndex):
+    model = Contract
+    fields = (
+        ('name', 100),
+        ('comments', 5000),
+    )
+
+
+@register_search
+class InvoiceIndex(SearchIndex):
+    model = Invoice
+    fields = (
+        ('number', 100),
+        ('comments', 5000),
+    )
