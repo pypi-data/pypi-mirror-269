8aa56d7a665430387a0454aecb8c0efdc639df79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:03:52 +0000

    Add package ngstks/1.0.0/ngstks-1.0.0.tar.gz

---
+++ packages/ngstks/ngstks-1.0.0.tar.gz/ngstks-1.0.0/setup.py
@@ -0,0 +1,26 @@
+"""
+    Setup file for ngstools.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 4.5.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    try:
+        setup(
+            name='ngstks',
+            version='1.0.0',
+            # Other metadata about your package
+        )
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
