e2be3062716924710859d1a4af353910a29220af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:53 +0000

    Add package ds18b20-datalogger/0.0.4/ds18b20_datalogger-0.0.4-py3-none-any.whl

---
+++ packages/ds18b20-datalogger/ds18b20_datalogger-0.0.4-py3-none-any.whl/ds18b20_datalogger/cli.py
@@ -0,0 +1,48 @@
+import json
+import sys
+from pathlib import Path
+
+from ds18b20_datalogger.core import read_ds18b20_sensor_matrix, send_measurement_mqtt
+from ds18b20_datalogger.model import Settings
+from ds18b20_datalogger.util import msg
+
+if sys.version_info < (3, 9):
+    from importlib_resources import files  # pragma: nocover
+else:
+    from importlib.resources import files
+
+
+def main():
+
+    # Sanity checks.
+    if not sys.argv[1:]:
+        raise ValueError("Program needs a subcommand")
+    subcommand = sys.argv[1]
+
+    # Evaluate subcommand.
+    if subcommand in ["run", "read"]:
+        if not sys.argv[2:]:
+            raise ValueError("Program needs a configuration file")
+        configfile = Path(sys.argv[2])
+        if not configfile.exists():
+            raise ValueError(f"Configuration file does not exist: {configfile}")
+        settings = Settings.from_file(configfile)
+        reading = read_ds18b20_sensor_matrix(settings.devicemap)
+        if subcommand == "read":
+            print(json.dumps(reading.to_dict(), indent=2))  # noqa: T201
+        elif subcommand == "run":
+            send_measurement_mqtt(settings.mqtt, reading)
+
+    elif subcommand == "make-config":
+        print(  # noqa: T201
+            msg("Please make sure to edit the configuration file before starting the program."), file=sys.stderr
+        )
+        config_template = files("ds18b20_datalogger") / "datalogger.yaml"
+        print(config_template.read_text(), file=sys.stdout)  # noqa: T201
+
+    elif subcommand == "make-dashboard":
+        dashboard = files("ds18b20_datalogger") / "grafana-dashboard.json"
+        print(dashboard.read_text(), file=sys.stdout)  # noqa: T201
+
+    else:
+        raise ValueError(f"Subcommand unknown: {subcommand}")
---
+++ packages/ds18b20-datalogger/ds18b20_datalogger-0.0.4-py3-none-any.whl/ds18b20_datalogger/model.py
@@ -0,0 +1,114 @@
+import dataclasses
+import typing as t
+from collections import OrderedDict
+from pathlib import Path
+
+import yaml
+
+from ds18b20_datalogger.util import partition
+
+
+@dataclasses.dataclass
+class Device:
+    """
+    Manage a one-wire device, mapping a sysfs path to a user-assigned name/label.
+    """
+
+    name: str
+    path: str
+    offset: t.Union[float, None] = None
+
+
+class DeviceMap:
+    """
+    Manage a map of one-wire devices to names/labels.
+    """
+
+    def __init__(self):
+        self.devices: t.List[Device] = []
+
+    def by_name(self, name: str) -> Device:
+        for device in self.devices:
+            if device.name == name:
+                return device
+        raise KeyError(f"Device not found: name={name}")
+
+    def by_fullpath(self, path: Path) -> Device:
+        for device in self.devices:
+            if str(device.path) == str(path):
+                return device
+        raise KeyError(f"Device not found: path={path}")
+
+    def by_pathname(self, path: Path) -> Device:
+        needle = Path(path).name
+        for device in self.devices:
+            if Path(device.path).name == needle:
+                return device
+        raise KeyError(f"Device not found: pathname={needle}")
+
+
+@dataclasses.dataclass
+class MqttSettings:
+    """
+    Container for MQTT settings.
+    """
+
+    host: str
+    topic: str
+    port: t.Optional[int] = None
+    username: t.Optional[str] = None
+    password: t.Optional[str] = None
+    client_id: t.Optional[str] = None
+
+
+@dataclasses.dataclass
+class Settings:
+    """
+    General settings container.
+    """
+
+    mqtt: t.Union[MqttSettings, None]
+    devicemap: t.Union[DeviceMap, None]
+
+    @classmethod
+    def from_file(cls, configfile: Path):
+        data = yaml.safe_load(configfile.read_text())
+        devicemap = DeviceMap()
+        for item in data["one-wire"]:
+            devicemap.devices.append(Device(**item))
+        mqtt = None
+        if "mqtt" in data:
+            mqtt = MqttSettings(**data["mqtt"])
+        return cls(mqtt=mqtt, devicemap=devicemap)
+
+
+@dataclasses.dataclass
+class Measurement:
+    """
+    Manage a measurement, coming from a device.
+    """
+
+    name: str
+    value: float
+
+
+@dataclasses.dataclass
+class Reading:
+    """
+    Manage a reading, made of multiple measurements.
+    """
+
+    measurements: t.List[Measurement] = dataclasses.field(default_factory=list)
+
+    def add_measurement(self, name: str, value: float):
+        self.measurements.append(Measurement(name=name, value=value))
+
+    def to_dict(self) -> t.Dict[str, float]:
+        data = OrderedDict()
+        for measurement in self.measurements:
+            data[measurement.name] = measurement.value
+        return data
+
+    def to_matrix(self, rowlength: int):
+        values = [m.value for m in self.measurements]
+        return list(partition(values, rowlength))
