e182db85427dce11ec7894c762088798c087f2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:03:52 +0000

    Add package marcuslion/0.2.45/marcuslion-0.2.45.tar.gz

---
+++ packages/marcuslion/marcuslion-0.2.45.tar.gz/marcuslion-0.2.45/marcuslion/config.py
@@ -0,0 +1,34 @@
+import os
+"""
+MarcusLion Global variable
+"""
+__env__ = os.getenv('MARCUSLION_ENV', "uat")
+__api_key__ = os.getenv('MARCUSLION_API_KEY', "b7f88c9c-94ba-11ee-b9d1-0242ac120002")
+__base_url__ = os.getenv('MARCUSLION_API_HOST', "https://uat.marcuslion.com")
+__api_version__ = "core/api/v2"  # no starting slash
+project_id = os.getenv('MARCUSLION_API_PROJECT_ID', "7ea7393b-017d-3ba9-a363-5b7a1043e0fes")
+
+
+def set_config(env):
+    global base_url
+    global __env__
+    if env == "qa":
+        base_url = "https://qa1.marcuslion.com"
+        __env__ = env
+    elif env == "uat":
+        base_url = "https://uat.marcuslion.com"
+        __env__ = env
+    else:
+        raise RuntimeError("Invalid environment")
+
+    pass
+
+
+def get_config():
+    return {
+        "env": __env__,
+        "api_key": __api_key__,
+        "base_url": __base_url__,
+        "project_id": project_id,
+        "api_version": __api_version__
+    }
