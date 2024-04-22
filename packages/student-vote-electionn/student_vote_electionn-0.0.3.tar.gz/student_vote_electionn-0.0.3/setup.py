d35f3792e00927c5af0fbddb95f1c07ba92549a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:13 +0000

    Add package student-vote-electionn/0.0.3/student_vote_electionn-0.0.3.tar.gz

---
+++ packages/student-vote-electionn/student_vote_electionn-0.0.3.tar.gz/student_vote_electionn-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='student_vote_electionn',
+  version='0.0.3',
+  description='This is simple announcement that will show up on the screen.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Yamini x23166401@student.ncirl.ie',
+  author_email='x23166401@student.ncirl.ie',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='studentvoteelectionn', 
+  packages=find_packages(),
+  install_requires=[
+    'django',
+    ] 
+)
