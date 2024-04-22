96cff27985c08fbfee1313af355bb13ffeacf086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:34 +0000

    Add package paicrypto/5.0/paicrypto-5.0.tar.gz

---
+++ packages/paicrypto/paicrypto-5.0.tar.gz/paicrypto-5.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paicrypto',
+    version='5.0',
+    description='A collection of algorithms',
+    author='Adithya Pai B',
+    author_email='paiadithya26@gmail.com',
+    packages= find_packages(),
+    install_requires=[
+        'pycryptodome'
+        # Add any dependencies your project requires
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
