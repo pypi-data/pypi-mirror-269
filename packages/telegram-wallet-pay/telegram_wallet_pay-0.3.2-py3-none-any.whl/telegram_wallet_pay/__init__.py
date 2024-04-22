acba1746fbc2c0c7736458b45f42624518ba53ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:04:00 +0000

    Add package telegram-wallet-pay/0.3.2/telegram_wallet_pay-0.3.2-py3-none-any.whl

---
+++ packages/telegram-wallet-pay/telegram_wallet_pay-0.3.2-py3-none-any.whl/telegram_wallet_pay/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.3.2"
+__api_version__ = "1.2.0"
+__all__ = ["TelegramWalletPay", "schemas", "errors", "tools"]
+
+from . import errors, schemas, tools
+from .client import TelegramWalletPay
