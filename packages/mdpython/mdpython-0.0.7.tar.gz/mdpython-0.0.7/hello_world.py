4c8743573f805ed2720800aca50b9f21834c9fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:04:01 +0000

    Add package mdpython/0.0.7/mdpython-0.0.7.tar.gz

---
+++ packages/mdpython/mdpython-0.0.7.tar.gz/mdpython-0.0.7/hello_world.py
@@ -0,0 +1,5 @@
+from mdpython.debugutils import version
+
+version.save("app_dev", r"D:\data\version")
+version.timeline("app_dev", r"D:\data\version")
+version.compare("app_dev", r"D:\data\version")
