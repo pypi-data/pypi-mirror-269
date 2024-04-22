1217bd870a2dd415a59570911aee14340a9ff0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:04:02 +0000

    Add package Jvav/1.9.0/Jvav-1.9.0-py3-none-any.whl

---
+++ packages/Jvav/Jvav-1.9.0-py3-none-any.whl/jvav/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+from jvav.utils import (
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+    SjsUtil,
+    SgpUtil,
+    RankUtil,
+)
+
+__version__ = "1.9.0"
+
+VERSION = __version__
+
+__all__ = [
+    VERSION,
+    BaseUtil,
+    JavLibUtil,
+    DmmUtil,
+    JavBusUtil,
+    AvgleUtil,
+    MagnetUtil,
+    SukebeiUtil,
+    WikiUtil,
+    TransUtil,
+    JavDbUtil,
+    SjsUtil,
+    SgpUtil,
+    RankUtil,
+]
