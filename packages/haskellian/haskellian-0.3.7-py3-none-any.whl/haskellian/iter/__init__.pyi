45fdeb9046ea8eab80c2fd2f5db4b9e4573a8565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 15:04:04 +0000

    Add package haskellian/0.3.7/haskellian-0.3.7-py3-none-any.whl

---
+++ packages/haskellian/haskellian-0.3.7-py3-none-any.whl/haskellian/iter/__init__.pyi
@@ -0,0 +1,20 @@
+from .lifting import lift
+from .basics import isiterable, flatmap, flatten
+from .zipping import unzip, uncons, pairwise
+from .slicing import fst, snd, head, tail, last, skip, take, take_while
+from .indexing import at, pick
+from .batching import batch, split
+from .nested import ndmap, ndrange, ndenumerate, ndflat
+from .transposing import transpose, transpose_ragged
+from .iter import Iter
+
+__all__ = [
+  'isiterable', 'flatmap', 'flatten',
+  'unzip', 'uncons', 'pairwise',
+  'fst', 'snd', 'head', 'tail', 'last', 'skip', 'take', 'take_while',
+  'at', 'pick',
+  'batch', 'split',
+  'ndmap', 'ndrange', 'ndenumerate', 'ndflat',
+  'transpose', 'transpose_ragged',
+  'Iter', 'lift'
+]
