9e2a0971ea1378d298aa49c055da6382933fb805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:33 +0000

    Add package aa-forum/2.1.0/aa_forum-2.1.0-py3-none-any.whl

---
+++ packages/aa-forum/aa_forum-2.1.0-py3-none-any.whl/aa_forum/static/aa_forum/javascript/aa-forum-desktop-widgets.js
@@ -0,0 +1,44 @@
+/* global aaForumDashboardWidgetsSettings, bootstrap */
+
+'use strict';
+
+const aaForumDashboardWidgets = document.getElementById('aa-forum-dashboard-widgets');
+const aaForumUnreadTopicsWidget = document.getElementById('aa-forum-dashboard-widget-unread-topics');
+
+if (aaForumUnreadTopicsWidget) {
+    const aaForumUnreadTopicsWidgetContent = aaForumUnreadTopicsWidget.querySelector('.aa-forum-dashboard-widget-unread-topics-content');
+
+    fetch(aaForumDashboardWidgetsSettings.unreadTopics.ajaxUrl)
+        .then((response) => {
+            if (response.ok) {
+                return response.text();
+            }
+            throw new Error('Something went wrong');
+        })
+        .then((responseText) => {
+            // console.log('Unread Forum Topics Widget: ', responseText);
+
+            if (responseText !== '') {
+                aaForumUnreadTopicsWidgetContent.innerHTML = responseText;
+
+                // Show the widget area
+                const showWidgetArea = new bootstrap.Collapse(aaForumDashboardWidgets, { // eslint-disable-line no-unused-vars
+                    show: true
+                });
+
+                // Show the widget
+                const showWidget = new bootstrap.Collapse(aaForumUnreadTopicsWidget, { // eslint-disable-line no-unused-vars
+                    show: true
+                });
+
+                // Initialize Bootstrap tooltips
+                [].slice.call(document.querySelectorAll('[data-bs-tooltip="aa-forum"]'))
+                    .map((tooltipTriggerEl) => {
+                        return new bootstrap.Tooltip(tooltipTriggerEl);
+                    });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
