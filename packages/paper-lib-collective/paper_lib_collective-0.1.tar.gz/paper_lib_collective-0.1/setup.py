ff9b290aa06cfe337bb911512e4e5e67c2696c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:05 +0000

    Add package paper-lib-collective/0.1/paper_lib_collective-0.1.tar.gz

---
+++ packages/paper-lib-collective/paper_lib_collective-0.1.tar.gz/paper_lib_collective-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import pathlib
+import setuptools
+
+setuptools.setup(
+    name='paper_lib_collective',
+    version='0.1',
+    description='Collective python packages from papers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://google.com',
+    author='Author Name', 
+    author_email='author_email@mail.com',
+    license='The Unlicense',
+    projects_urls={
+        "Documentation": "x",
+        "Source": "https://github.com"
+    },
+    python_requires=">3.9,<3.12",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["paper = paper.cli:main"]},
+
+)
