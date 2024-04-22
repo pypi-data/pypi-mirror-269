0f4e3bede3902ef01aee880634e87e027b7fcd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 03:03:36 +0000

    Add package openshift-cli-installer/2.0.62/openshift_cli_installer-2.0.62.tar.gz

---
+++ packages/openshift-cli-installer/openshift_cli_installer-2.0.62.tar.gz/openshift_cli_installer-2.0.62/openshift_cli_installer/cli.py
@@ -0,0 +1,278 @@
+import datetime
+import os
+import shutil
+import time
+
+import click
+from simple_logger.logger import get_logger
+
+from openshift_cli_installer.libs.clusters.ocp_clusters import OCPClusters
+from openshift_cli_installer.libs.user_input import UserInput
+from openshift_cli_installer.utils.click_dict_type import DictParamType
+from openshift_cli_installer.utils.clusters import (
+    destroy_clusters_from_s3_bucket_or_local_directory,
+)
+from openshift_cli_installer.utils.const import (
+    CREATE_STR,
+    DESTROY_CLUSTERS_FROM_S3_BASE_DATA_DIRECTORY,
+    DESTROY_STR,
+)
+from openshift_cli_installer.utils.gcp_utils import set_gcp_configuration, restore_gcp_configuration
+
+
+@click.command("installer")
+@click.option(
+    "-a",
+    "--action",
+    type=click.Choice([CREATE_STR, DESTROY_STR]),
+    help="Action to perform Openshift cluster/s",
+)
+@click.option(
+    "-p",
+    "--parallel",
+    help="Run clusters install/uninstall in parallel",
+    is_flag=True,
+    show_default=True,
+)
+@click.option(
+    "--ssh-key-file",
+    help="id_rsa.pub file path for AWS IPI or ACM clusters",
+    default="/openshift-cli-installer/ssh-key/id_rsa.pub",
+    type=click.Path(),
+    show_default=True,
+)
+@click.option(
+    "--clusters-install-data-directory",
+    help="""
+\b
+Path to clusters install data.
+    For install this will be used to store the install data.
+    For uninstall this will be used to uninstall the clusters.
+    Also used to store clusters kubeconfig.
+    Default: "/openshift-cli-installer/clusters-install-data"
+""",
+    default=os.environ.get("CLUSTER_INSTALL_DATA_DIRECTORY"),
+    type=click.Path(),
+    show_default=True,
+)
+@click.option(
+    "--registry-config-file",
+    help="""
+    \b
+registry-config file, can be obtained from https://console.redhat.com/openshift/create/local.
+(Needed only for AWS IPI clusters)
+    """,
+    default=os.environ.get("PULL_SECRET"),
+    type=click.Path(),
+    show_default=True,
+)
+@click.option(
+    "--docker-config-file",
+    type=click.Path(),
+    default=os.path.expanduser("~/.docker/config.json"),
+    help="""
+    \b
+Path to Docker config.json file.
+File must include token for `registry.ci.openshift.org`
+(Needed only for AWS IPI clusters)
+    """,
+)
+@click.option(
+    "--s3-bucket-name",
+    help="S3 bucket name to store install folder backups",
+    show_default=True,
+)
+@click.option("--s3-bucket-path", help="S3 bucket path to store the backups", show_default=True, default="")
+@click.option(
+    "--s3-bucket-path-uuid",
+    help="S3 bucket path UUID to append to the S3 zip file name",
+    show_default=True,
+)
+@click.option(
+    "--s3-bucket-object-name",
+    help="S3 bucket object name; Will be saved as a zip file",
+    show_default=True,
+)
+@click.option(
+    "--ocm-token",
+    help="OCM token.",
+    default=os.environ.get("OCM_TOKEN"),
+)
+@click.option(
+    "--aws-access-key-id",
+    help="AWS access-key-id, needed for OSD AWS clusters.",
+    default=os.environ.get("AWS_ACCESS_KEY_ID"),
+)
+@click.option(
+    "--aws-secret-access-key",
+    help="AWS secret-access-key, needed for OSD AWS clusters.",
+    default=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+)
+@click.option(
+    "--aws-account-id",
+    help="AWS account-id, needed for OSD AWS and Hypershift clusters.",
+    default=os.environ.get("AWS_ACCOUNT_ID"),
+)
+@click.option(
+    "-c",
+    "--cluster",
+    type=DictParamType(),
+    help="""
+\b
+Cluster/s to install.
+Format to pass is:
+    'name=cluster1;base-domain=aws.domain.com;platform=aws;region=us-east-2;version=4.14.0-ec.2'
+Required parameters:
+    name: Cluster name.
+    base-domain: Base domain for the cluster.
+    platform: Cloud platform to install the cluster on, supported platforms are: aws, rosa and hypershift.
+    region: Region to use for the cloud platform.
+    version: Openshift cluster version to install
+\b
+Check <aws/gcp>-install-config-template.j2 for variables that can be overwritten by the user.
+For example:
+    fips=true
+    worker-flavor=m5.xlarge
+    worker-replicas=6
+    """,
+    multiple=True,
+)
+@click.option(
+    "--destroy-clusters-from-s3-bucket",
+    help="""
+\b
+Destroy clusters from S3 bucket, --s3-bucket-name is required and optional --s3-bucket-path.
+    """,
+    show_default=True,
+    is_flag=True,
+)
+@click.option(
+    "--destroy-clusters-from-s3-bucket-query",
+    help="""
+\b
+Destroy cluster(s) from S3 bucket which match only files that have it.
+    """,
+    show_default=True,
+)
+@click.option(
+    "--destroy-clusters-from-install-data-directory",
+    help="""
+\b
+Destroy clusters from cluster data files located at --clusters-install-data-directory
+    """,
+    show_default=True,
+    is_flag=True,
+)
+@click.option(
+    "--destroy-clusters-from-install-data-directory-using-s3-bucket",
+    help="""
+\b
+Destroy clusters from cluster data files located at --clusters-install-data-directory
+Get the S3 object from cluster_data.yaml, download, extract it and call destroy cluster
+    """,
+    show_default=True,
+    is_flag=True,
+)
+@click.option(
+    "--clusters-yaml-config-file",
+    help="""
+    \b
+    YAML file with configuration to create clusters, any option in YAML file will override the CLI option.
+    See manifests/clusters.example.yaml for example.
+    """,
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--gcp-service-account-file",
+    help="""
+\b
+Path to GCP service account json file.
+""",
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--must-gather-output-dir",
+    help="""
+\b
+Path to must-gather output directory.
+must-gather will try to collect data when cluster installation fails and cluster can be accessed.
+""",
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--dry-run",
+    help="For testing, only verify user input",
+    is_flag=True,
+    show_default=True,
+)
+@click.option(
+    "--pdb",
+    help="Drop to `ipdb` shell on exception",
+    is_flag=True,
+    show_default=True,
+)
+def main(**kwargs):
+    """
+    Create/Destroy Openshift cluster/s
+    """
+    kwargs.pop("pdb", None)
+    user_input = UserInput(**kwargs)
+
+    if user_input.dry_run:
+        return
+
+    gcp_params = set_gcp_configuration(user_input=user_input)
+
+    try:
+        if (
+            user_input.destroy_clusters_from_s3_bucket
+            or user_input.destroy_clusters_from_install_data_directory
+            or user_input.destroy_clusters_from_install_data_directory_using_s3_bucket
+            or user_input.destroy_clusters_from_s3_bucket_query
+        ):
+            user_input.destroy_from_s3_bucket_or_local_directory = True
+            user_input = destroy_clusters_from_s3_bucket_or_local_directory(user_input=user_input)
+
+            try:
+                clusters = OCPClusters(user_input=user_input)
+                clusters.run_create_or_destroy_clusters()
+            finally:
+                shutil.rmtree(DESTROY_CLUSTERS_FROM_S3_BASE_DATA_DIRECTORY, ignore_errors=True)
+
+        else:
+            user_input.destroy_from_s3_bucket_or_local_directory = False
+            clusters = OCPClusters(user_input=user_input)
+            clusters.run_create_or_destroy_clusters()
+
+            if user_input.action == CREATE_STR:
+                clusters.install_acm_on_clusters()
+                clusters.enable_observability_on_acm_clusters()
+                clusters.attach_clusters_to_acm_cluster_hub()
+
+    finally:
+        restore_gcp_configuration(gcp_params=gcp_params)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    should_raise = False
+    _logger = get_logger(name="openshift-cli-installer")
+    try:
+        main()
+    except Exception as ex:
+        import sys
+        import traceback
+
+        ipdb = __import__("ipdb")  # Bypass debug-statements pre-commit hook
+
+        if "--pdb" in sys.argv:
+            extype, value, tb = sys.exc_info()
+            traceback.print_exc()
+            ipdb.post_mortem(tb)
+        else:
+            _logger.error(ex)
+            should_raise = True
+    finally:
+        _logger.info(f"Total execution time: {datetime.timedelta(seconds=time.time() - start_time)}")
+        if should_raise:
+            sys.exit(1)
---
+++ packages/openshift-cli-installer/openshift_cli_installer-2.0.62.tar.gz/openshift_cli_installer-2.0.62/openshift_cli_installer/tests/test_user_input.py
@@ -0,0 +1,119 @@
+import shlex
+import pytest
+from ocp_utilities.utils import run_command
+from openshift_cli_installer.utils.const import AWS_STR, AWS_OSD_STR, HYPERSHIFT_STR, GCP_STR, S3_STR
+import subprocess
+
+
+@pytest.mark.parametrize(
+    "command, expected",
+    [
+        ("--ocm-token=123", "'action' must be provided, supported actions: `('destroy', 'create')`"),
+        (
+            f"--action=create --ocm-token='' --cluster='name=test-cl;platform={AWS_STR}'",
+            "--ocm-token is required for clusters",
+        ),
+        ("--action=create --ocm-token=123", "At least one '--cluster' option must be provided"),
+        (
+            f"--action=create --ocm-token=123 --cluster='platform={AWS_STR}'",
+            "Cluster name or name_prefix must be provided",
+        ),
+        (
+            f"--action=create --ocm-token=123 --cluster='name=test-cl;platform={AWS_STR}' --cluster='name=test-cl;platform={AWS_STR}'",
+            "Cluster names must be unique:",
+        ),
+        (
+            f"--action=create --ocm-token=123 --registry-config-file=reg.json --cluster='name=test-cl;platform={AWS_STR};acm=true'",
+            "The following keys must be booleans: ['acm']",
+        ),
+        (
+            f"--action=create --ocm-token=123 --cluster='name=test-cl;platform={AWS_STR};log_level=unsupported'",
+            "log levels are not supported for openshift-installer cli",
+        ),
+        (
+            f"--action=create --ocm-token=123 --cluster='name=test-cl;platform={AWS_STR}'",
+            "Registry config file is required for IPI cluster installations",
+        ),
+        (
+            f"--action=create --ocm-token=123 --docker-config-file='' --registry-config-file=reg.json --cluster='name=test-cl;platform={AWS_STR}'",
+            "Docker config file is required for IPI installations",
+        ),
+        (
+            f"--action=create --ocm-token=123 --docker-config-file='dok.json' --registry-config-file=reg.json --ssh-key-file='' --cluster='name=test-cl;platform={AWS_STR}'",
+            "SSH file is required for IPI cluster installations",
+        ),
+        (
+            f"--action=create --ocm-token=123 --aws-secret-access-key='' --aws-access-key-id='' --cluster='name=test-cl;platform={AWS_OSD_STR}'",
+            "--aws-secret-access-key and --aws-access-key-id required for AWS OSD OR ACM cluster installations",
+        ),
+        (
+            f"--action=create --ocm-token=123 --aws-secret-access-key=123 --aws-access-key-id=123 --aws-account-id='' --cluster='name=test-cl;platform={AWS_OSD_STR}'",
+            "--aws-account-id required for AWS OSD or Hypershift installations",
+        ),
+        (
+            f"--action=create --ocm-token=123 --aws-secret-access-key=123 --aws-access-key-id=123 --aws-account-id=123 --cluster='name=test-cl;platform={HYPERSHIFT_STR};acm=True'",
+            f"ACM not supported for {HYPERSHIFT_STR} clusters",
+        ),
+        (
+            f"--action=create --ocm-token=123 --cluster='name=test-cl;platform={AWS_STR};acm-clusters=mycluser1'",
+            "Managed ACM clusters: Cluster not found",
+        ),
+        (
+            f"--action=create --ocm-token=123 --registry-config-file=reg.json --cluster='name=test-cl;platform={GCP_STR}'",
+            "`--gcp-service-account-file` option must be provided for gcp-osd and gcp clusters",
+        ),
+        (
+            f"--action=create --ocm-token=123 --registry-config-file=reg.json --cluster='name=test-cl;platform={AWS_STR};acm-observability=True;acm-observability-storage-type=bad'",
+            "The following storage types are not supported for observability",
+        ),
+        (
+            f"--action=create --ocm-token=123 --registry-config-file=reg.json --aws-secret-access-key='' --aws-access-key-id='' --cluster='name=test-cl;platform={AWS_STR};acm-observability=True;acm-observability-storage-type={S3_STR}'",
+            "The following clusters are missing storage data for observability:",
+        ),
+        ('--action=create --ocm-token=123 --cluster="name=test-cl"', "is missing platform"),
+        (
+            "--action=create --ocm-token=123 --cluster='name=test-cl;platform=unsupported'",
+            "platform 'unsupported' is not supported",
+        ),
+        (
+            "--action=create --ocm-token=123 --destroy-clusters-from-s3-bucket --cluster='name=test-cl;platform=unsupported'",
+            "`--s3-bucket-name` must be provided when running with",
+        ),
+        (
+            "--action=create --ocm-token=123 --destroy-clusters-from-s3-bucket-query --cluster='name=test-cl;platform=unsupported'",
+            "`--s3-bucket-name` must be provided when running with",
+        ),
+        (
+            "--action=create --ocm-token=123 --destroy-clusters-from-install-data-directory --destroy-clusters-from-install-data-directory-using-s3-bucket --cluster='name=test-cl;platform=unsupported'",
+            "`--destroy-clusters-from-install-data-directory-using-s3-bucket` is not supported when running with `--destroy-clusters-from-install-data-directory`",
+        ),
+        (
+            " --action=create --ocm-token=123 --registry-config-file=reg.json --cluster='name=test-cl;platform=aws;stream=stream;region=reg1'",
+            "Clusters data directory: /openshift-cli-installer/clusters-install-data is not writable",
+        ),
+        (
+            "--action=create --ocm-token=123 --registry-config-file=reg.json --clusters-install-data-directory=/tmp/clinstall --cluster='name=test-cl;platform=aws;stream=bad-stream;region=reg1'",
+            "aws platform does not support stream bad-stream, supported streams are ('stable', 'nightly', 'ec', 'ci', 'rc')",
+        ),
+        (
+            "--action=create --ocm-token=123 --registry-config-file=reg.json --clusters-install-data-directory=/tmp/clinstall --cluster='name=test-cl;platform=rosa;channel-group=bad-stream;region=reg1'",
+            "rosa platform does not support channel-group bad-stream, supported channels are ('stable', 'candidate', 'nightly')",
+        ),
+    ],
+)
+def test_user_input(command, expected):
+    base_command = "poetry run python openshift_cli_installer/cli.py --dry-run"
+
+    if command:
+        base_command += f" {command}"
+
+    rc, _, err = run_command(
+        command=shlex.split(base_command),
+        verify_stderr=False,
+        check=False,
+        capture_output=False,
+        stderr=subprocess.PIPE,
+    )
+
+    assert not rc
+    assert expected in err
