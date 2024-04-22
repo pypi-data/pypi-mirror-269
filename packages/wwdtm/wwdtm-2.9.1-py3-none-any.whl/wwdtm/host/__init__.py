f4378642792f8626d06ff60a544c6d9bb718c512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:03:30 +0000

    Add package wwdtm/2.9.1/wwdtm-2.9.1-py3-none-any.whl

---
+++ packages/wwdtm/wwdtm-2.9.1-py3-none-any.whl/wwdtm/host/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2018-2024 Linh Pham
+# wwdtm is released under the terms of the Apache License 2.0
+# SPDX-License-Identifier: Apache-2.0
+#
+# vim: set noai syntax=python ts=4 sw=4:
+"""Wait Wait Stats: Host module."""
+from .appearances import HostAppearances
+from .host import Host
+from .utility import HostUtility
