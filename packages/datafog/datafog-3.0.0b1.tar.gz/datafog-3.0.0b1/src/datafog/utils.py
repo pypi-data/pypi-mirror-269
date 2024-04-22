b9c84d1434deb8f1e125273ab2bc233dc4f899ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 14:03:38 +0000

    Add package datafog/3.0.0b1/datafog-3.0.0b1.tar.gz

---
+++ packages/datafog/datafog-3.0.0b1.tar.gz/datafog-3.0.0b1/src/datafog/utils.py
@@ -0,0 +1,16 @@
+
+import en_spacy_pii_fast
+from pydantic import HttpUrl
+import requests
+
+
+def get_json_shape(json_data: dict) -> str:
+    if isinstance(json_data, dict):
+        return f"Root is a dictionary with {len(json_data)} keys."
+    elif isinstance(json_data, list):
+        return f"Root is a list with {len(json_data)} elements."
+    else:
+        return "Root is neither a dictionary nor a list."
+    
+    
+
