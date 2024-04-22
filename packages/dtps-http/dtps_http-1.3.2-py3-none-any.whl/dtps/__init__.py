377f032e7e2a707357445ba35f1dbc8a2e99807d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 04:03:14 +0000

    Add package dtps-http/1.3.2/dtps_http-1.3.2-py3-none-any.whl

---
+++ packages/dtps-http/dtps_http-1.3.2-py3-none-any.whl/dtps/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "1.3.2"
+
+from logging import DEBUG, getLogger
+
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+
+from .config import *
+from .ergo_ui import *
+
+from dtps_http import RawData, TransformError
+
+_ = RawData, TransformError
