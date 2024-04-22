da8671009a66b9b5a6d8c23ab79142949e7581e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-22 05:03:07 +0000

    Add package hostfinder/1.0.1/hostfinder-1.0.1-py3-none-any.whl

---
+++ packages/hostfinder/hostfinder-1.0.1-py3-none-any.whl/hostfinder/main/main.py
@@ -0,0 +1,14 @@
+from ..models import Options
+from .hostfinder import HostFinder
+
+
+def find_hosts(options: Options | None = None) -> list[str]:
+    host_finder = HostFinder() if options is None else HostFinder(options)
+    return list(host_finder.find_hosts())
+
+
+def find_host(options: Options | None = None) -> str | None:
+    host_finder = HostFinder() if options is None else HostFinder(options)
+    host_finder.options.show_progress = False
+    hosts = host_finder.find_hosts()
+    return next(hosts, None)
