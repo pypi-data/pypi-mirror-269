dae37e9728bc309685b84528b4893b3c205a97de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:43 +0000

    Add package Canaille/0.0.53/canaille-0.0.53-cp311-cp311-manylinux_2_39_x86_64.whl

---
+++ packages/Canaille/canaille-0.0.53-cp311-cp311-manylinux_2_39_x86_64.whl/canaille/backends/ldap/models.py
@@ -0,0 +1,219 @@
+import ldap.filter
+
+import canaille.core.models
+import canaille.oidc.models
+
+from .backend import Backend
+from .ldapobject import LDAPObject
+
+
+class User(canaille.core.models.User, LDAPObject):
+    attribute_map = {
+        "id": "entryUUID",
+        "created": "createTimestamp",
+        "last_modified": "modifyTimestamp",
+        "user_name": "uid",
+        "password": "userPassword",
+        "preferred_language": "preferredLanguage",
+        "family_name": "sn",
+        "given_name": "givenName",
+        "formatted_name": "cn",
+        "display_name": "displayName",
+        "emails": "mail",
+        "phone_numbers": "telephoneNumber",
+        "formatted_address": "postalAddress",
+        "street": "street",
+        "postal_code": "postalCode",
+        "locality": "l",
+        "region": "st",
+        "photo": "jpegPhoto",
+        "profile_url": "labeledURI",
+        "employee_number": "employeeNumber",
+        "department": "departmentNumber",
+        "title": "title",
+        "organization": "o",
+        "groups": "memberOf",
+        "lock_date": "pwdEndTime",
+    }
+
+    def match_filter(self, filter):
+        if isinstance(filter, str):
+            conn = Backend.get().connection
+            return self.dn and conn.search_s(self.dn, ldap.SCOPE_SUBTREE, filter)
+
+        return super().match_filter(filter)
+
+    @property
+    def identifier(self):
+        return self.rdn_value
+
+    def save(self, *args, **kwargs):
+        group_attr = self.python_attribute_to_ldap("groups")
+        if group_attr not in self.changes:
+            return super().save(*args, **kwargs)
+
+        # The LDAP attribute memberOf cannot directly be edited,
+        # so this is needed to update the Group.member attribute
+        # instead.
+        old_groups = self.state.get(group_attr) or []
+        new_groups = [
+            value if isinstance(value, Group) else Group.get(value)
+            for value in self.changes[group_attr]
+        ]
+        to_add = set(new_groups) - set(old_groups)
+        to_del = set(old_groups) - set(new_groups)
+        del self.changes[group_attr]
+
+        super().save(*args, **kwargs)
+
+        for group in to_add:
+            group.members = group.members + [self]
+            group.save()
+
+        for group in to_del:
+            # LDAP groups cannot be empty because groupOfNames.member
+            # is a MUST attribute.
+            # https://www.rfc-editor.org/rfc/rfc2256.html#section-7.10
+            # TODO: properly manage the situation where one wants to
+            # remove the last member of a group
+            group.members = [member for member in group.members if member != self]
+            group.save()
+
+        self.state[group_attr] = new_groups
+
+
+class Group(canaille.core.models.Group, LDAPObject):
+    attribute_map = {
+        "id": "entryUUID",
+        "created": "createTimestamp",
+        "last_modified": "modifyTimestamp",
+        "display_name": "cn",
+        "members": "member",
+        "description": "description",
+    }
+
+    @property
+    def identifier(self):
+        return self.rdn_value
+
+
+class Client(canaille.oidc.models.Client, LDAPObject):
+    ldap_object_class = ["oauthClient"]
+    base = "ou=clients,ou=oauth"
+    rdn_attribute = "oauthClientID"
+
+    client_info_attributes = {
+        "client_id": "oauthClientID",
+        "client_secret": "oauthClientSecret",
+        "client_id_issued_at": "oauthIssueDate",
+        "client_secret_expires_at": "oauthClientSecretExpDate",
+    }
+
+    client_metadata_attributes = {
+        "client_name": "oauthClientName",
+        "contacts": "oauthClientContact",
+        "client_uri": "oauthClientURI",
+        "redirect_uris": "oauthRedirectURIs",
+        "logo_uri": "oauthLogoURI",
+        "grant_types": "oauthGrantType",
+        "response_types": "oauthResponseType",
+        "scope": "oauthScope",
+        "tos_uri": "oauthTermsOfServiceURI",
+        "policy_uri": "oauthPolicyURI",
+        "jwks_uri": "oauthJWKURI",
+        "jwk": "oauthJWK",
+        "token_endpoint_auth_method": "oauthTokenEndpointAuthMethod",
+        "software_id": "oauthSoftwareID",
+        "software_version": "oauthSoftwareVersion",
+    }
+
+    attribute_map = {
+        "id": "entryUUID",
+        "created": "createTimestamp",
+        "last_modified": "modifyTimestamp",
+        "preconsent": "oauthPreconsent",
+        # post_logout_redirect_uris is not yet supported by authlib
+        "post_logout_redirect_uris": "oauthPostLogoutRedirectURI",
+        "audience": "oauthAudience",
+        **client_info_attributes,
+        **client_metadata_attributes,
+    }
+
+    @property
+    def identifier(self):
+        return self.rdn_value
+
+
+class AuthorizationCode(canaille.oidc.models.AuthorizationCode, LDAPObject):
+    ldap_object_class = ["oauthAuthorizationCode"]
+    base = "ou=authorizations,ou=oauth"
+    rdn_attribute = "oauthAuthorizationCodeID"
+    attribute_map = {
+        "id": "entryUUID",
+        "created": "createTimestamp",
+        "last_modified": "modifyTimestamp",
+        "authorization_code_id": "oauthAuthorizationCodeID",
+        "code": "oauthCode",
+        "client": "oauthClient",
+        "subject": "oauthSubject",
+        "redirect_uri": "oauthRedirectURI",
+        "response_type": "oauthResponseType",
+        "scope": "oauthScope",
+        "nonce": "oauthNonce",
+        "issue_date": "oauthAuthorizationDate",
+        "lifetime": "oauthAuthorizationLifetime",
+        "challenge": "oauthCodeChallenge",
+        "challenge_method": "oauthCodeChallengeMethod",
+        "revokation_date": "oauthRevokationDate",
+    }
+
+    @property
+    def identifier(self):
+        return self.rdn_value
+
+
+class Token(canaille.oidc.models.Token, LDAPObject):
+    ldap_object_class = ["oauthToken"]
+    base = "ou=tokens,ou=oauth"
+    rdn_attribute = "oauthTokenID"
+    attribute_map = {
+        "id": "entryUUID",
+        "created": "createTimestamp",
+        "last_modified": "modifyTimestamp",
+        "token_id": "oauthTokenID",
+        "access_token": "oauthAccessToken",
+        "client": "oauthClient",
+        "subject": "oauthSubject",
+        "type": "oauthTokenType",
+        "refresh_token": "oauthRefreshToken",
+        "scope": "oauthScope",
+        "issue_date": "oauthIssueDate",
+        "lifetime": "oauthTokenLifetime",
+        "revokation_date": "oauthRevokationDate",
+        "audience": "oauthAudience",
+    }
+
+    @property
+    def identifier(self):
+        return self.rdn_value
+
+
+class Consent(canaille.oidc.models.Consent, LDAPObject):
+    ldap_object_class = ["oauthConsent"]
+    base = "ou=consents,ou=oauth"
+    rdn_attribute = "cn"
+    attribute_map = {
+        "id": "entryUUID",
+        "created": "createTimestamp",
+        "last_modified": "modifyTimestamp",
+        "consent_id": "cn",
+        "subject": "oauthSubject",
+        "client": "oauthClient",
+        "scope": "oauthScope",
+        "issue_date": "oauthIssueDate",
+        "revokation_date": "oauthRevokationDate",
+    }
+
+    @property
+    def identifier(self):
+        return self.rdn_value
---
+++ packages/Canaille/canaille-0.0.53-cp311-cp311-manylinux_2_39_x86_64.whl/canaille/backends/ldap/utils.py
@@ -0,0 +1,97 @@
+import datetime
+from enum import Enum
+
+LDAP_NULL_DATE = "000001010000Z"
+
+
+class Syntax(str, Enum):
+    # fmt: off
+    BINARY =             "1.3.6.1.4.1.1466.115.121.1.5"
+    BOOLEAN =            "1.3.6.1.4.1.1466.115.121.1.7"
+    CERTIFICATE =        "1.3.6.1.4.1.1466.115.121.1.8"
+    COUNTRY_STRING =     "1.3.6.1.4.1.1466.115.121.1.11"
+    DISTINGUISHED_NAME = "1.3.6.1.4.1.1466.115.121.1.12"
+    DIRECTORY_STRING =   "1.3.6.1.4.1.1466.115.121.1.15"
+    FAX_IMAGE =          "1.3.6.1.4.1.1466.115.121.1.23"
+    GENERALIZED_TIME =   "1.3.6.1.4.1.1466.115.121.1.24"
+    IA5_STRING =         "1.3.6.1.4.1.1466.115.121.1.26"
+    INTEGER =            "1.3.6.1.4.1.1466.115.121.1.27"
+    JPEG =               "1.3.6.1.4.1.1466.115.121.1.28"
+    NUMERIC_STRING =     "1.3.6.1.4.1.1466.115.121.1.36"
+    OCTET_STRING =       "1.3.6.1.4.1.1466.115.121.1.40"
+    POSTAL_ADDRESS =     "1.3.6.1.4.1.1466.115.121.1.41"
+    PRINTABLE_STRING =   "1.3.6.1.4.1.1466.115.121.1.44"
+    TELEPHONE_NUMBER =   "1.3.6.1.4.1.1466.115.121.1.50"
+    UTC_TIME =           "1.3.6.1.4.1.1466.115.121.1.53"
+    # fmt: on
+
+
+def ldap_to_python(value, syntax):
+    from .ldapobject import LDAPObject
+
+    if syntax == Syntax.GENERALIZED_TIME:
+        value = value.decode("utf-8")
+        if value == LDAP_NULL_DATE:
+            # python cannot represent datetimes with year 0
+            return datetime.datetime.min
+        if value.endswith("Z"):
+            return datetime.datetime.strptime(value, "%Y%m%d%H%M%SZ").replace(
+                tzinfo=datetime.timezone.utc
+            )
+        return datetime.datetime.strptime(value, "%Y%m%d%H%M%S%z")
+
+    if syntax == Syntax.INTEGER:
+        return int(value.decode("utf-8"))
+
+    if syntax in (Syntax.JPEG, Syntax.FAX_IMAGE):
+        return value
+
+    if syntax == Syntax.BOOLEAN:
+        return value.decode("utf-8").upper() == "TRUE"
+
+    if syntax == Syntax.DISTINGUISHED_NAME:
+        return LDAPObject.get(value.decode("utf-8"))
+
+    return value.decode("utf-8")
+
+
+def python_to_ldap(value, syntax, encode=True):
+    encodable = True
+    if syntax == Syntax.GENERALIZED_TIME and isinstance(value, datetime.datetime):
+        if value == datetime.datetime.min:
+            value = LDAP_NULL_DATE
+        elif not value.tzinfo or value.tzinfo == datetime.timezone.utc:
+            value = value.strftime("%Y%m%d%H%M%SZ")
+        else:
+            value = value.strftime("%Y%m%d%H%M%S%z")
+
+    if syntax == Syntax.INTEGER and isinstance(value, int):
+        value = str(value)
+
+    if syntax in (Syntax.JPEG, Syntax.FAX_IMAGE):
+        encodable = False
+
+    if syntax == Syntax.BOOLEAN and isinstance(value, bool):
+        value = "TRUE" if value else "FALSE"
+
+    if syntax == Syntax.DISTINGUISHED_NAME:
+        value = value.dn if value else None
+
+    if not value:
+        return None
+
+    return value.encode() if encode and encodable else value
+
+
+def listify(value):
+    return value if isinstance(value, list) else [value]
+
+
+def cardinalize_attribute(python_unique, value):
+    if not value:
+        return None if python_unique else []
+
+    if python_unique:
+        return value[0]
+
+    return [v for v in value if v is not None]
