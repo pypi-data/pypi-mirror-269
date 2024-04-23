9f1d02412183a539f6a0b90b03593060458f73b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:00 +0000

    Add package siliconcompiler/0.21.10/siliconcompiler-0.21.10-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.21.10-cp38-cp38-macosx_10_15_x86_64.whl/siliconcompiler/tools/openroad/scripts/sc_screenshot.tcl
@@ -0,0 +1,21 @@
+gui::save_display_controls
+
+set sc_resolution \
+  [lindex [dict get $sc_cfg tool $sc_tool task $sc_task {var} show_vertical_resolution] 0]
+
+# Show the drc markers (if any)
+if { [file exists reports/${sc_design}_drc.rpt] } {
+  gui::load_drc reports/${sc_design}_drc.rpt
+}
+
+sc_image_setup_default
+
+sc_save_image "screenshot" "outputs/${sc_design}.png" $sc_resolution
+
+gui::restore_display_controls
+
+if { [dict exists $sc_cfg tool $sc_tool task $sc_task {var} include_report_images] &&
+     [lindex [dict get $sc_cfg tool $sc_tool task $sc_task {var} include_report_images] 0]
+     == "true" } {
+  source -echo "${sc_refdir}/sc_write_images.tcl"
+}
