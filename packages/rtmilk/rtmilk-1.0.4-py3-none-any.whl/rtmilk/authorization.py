e464417a2c293937456a454da0c74241e2f6ed0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:04:05 +0000

    Add package rtmilk/1.0.4/rtmilk-1.0.4-py3-none-any.whl

---
+++ packages/rtmilk/rtmilk-1.0.4-py3-none-any.whl/rtmilk/authorization.py
@@ -0,0 +1,17 @@
+from .api_sync import UnauthorizedAPI
+from ._sansio import ApiSig
+
+_AUTHORIZATION_URL = 'https://www.rememberthemilk.com/services/auth/'
+
+class AuthorizationSession:
+	"""Helper for authorizing an app against the RTM API"""
+
+	def __init__(self, apiKey, sharedSecret, perms):
+		self._api = UnauthorizedAPI(apiKey, sharedSecret)
+		self._frob = self._api.AuthGetFrob()
+		params = {'api_key': apiKey, 'perms': perms, 'frob': self._frob}
+		params['api_sig'] = ApiSig(sharedSecret, params)
+		self.url = _AUTHORIZATION_URL + '?' + '&'.join([f'{k}={v}' for k, v in params.items()])
+
+	def Done(self):
+		return self._api.AuthGetToken(self._frob)
