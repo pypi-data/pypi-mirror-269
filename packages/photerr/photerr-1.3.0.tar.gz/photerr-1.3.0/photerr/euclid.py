551f427b5a7492c2451470c2c5ed6b52106b5b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 06:03:51 +0000

    Add package photerr/1.3.0/photerr-1.3.0.tar.gz

---
+++ packages/photerr/photerr-1.3.0.tar.gz/photerr-1.3.0/photerr/euclid.py
@@ -0,0 +1,50 @@
+"""Photometric error model for Euclid."""
+
+from dataclasses import dataclass, field
+from typing import Any
+
+from photerr.model import ErrorModel
+from photerr.params import ErrorParams, param_docstring
+
+
+@dataclass
+class EuclidErrorParams(ErrorParams):
+    """Parameters for the Euclid photometric error model.
+
+    gamma and limiting magnitudes taken from page 4 of Graham 2020.
+    nYrObs and nVisYr set = 1, assuming that Euclid is point-and-stare.
+    """
+
+    __doc__ += param_docstring
+    __doc__ += "    Graham 2020 - https://arxiv.org/abs/2004.07885"
+    __doc__ += "\n    Scaramella 2021 - https://arxiv.org/abs/2108.01201"
+
+    nYrObs: float = 1.0
+    nVisYr: dict[str, float] | float = 1.0
+    gamma: dict[str, float] | float = 0.04
+
+    m5: dict[str, float] | float = field(
+        default_factory=lambda: {
+            "Y": 24.0,
+            "J": 24.2,
+            "H": 23.9,
+        }
+    )
+    theta: dict[str, float] | float = 0.18
+    airmass: float = 0
+
+
+class EuclidErrorModel(ErrorModel):
+    """Photometric error model for Euclid.
+
+    Below is the parameter docstring:
+    """
+
+    __doc__ += EuclidErrorParams.__doc__
+
+    def __init__(self, **kwargs: Any) -> None:
+        """Create a Euclid error model.
+
+        Keyword arguments override default values in EuclidErrorParams.
+        """
+        super().__init__(EuclidErrorParams(**kwargs))
---
+++ packages/photerr/photerr-1.3.0.tar.gz/photerr-1.3.0/photerr/roman.py
@@ -0,0 +1,58 @@
+"""Photometric error model for Roman."""
+
+from dataclasses import dataclass, field
+from typing import Any
+
+from photerr.model import ErrorModel
+from photerr.params import ErrorParams, param_docstring
+
+
+@dataclass
+class RomanErrorParams(ErrorParams):
+    """Parameters for the Roman photometric error model.
+
+    gamma and limiting magnitudes taken from page 4 of Graham 2020.
+    nYrObs and nVisYr set = 1, assuming that Roman is point-and-stare.
+    """
+
+    __doc__ += param_docstring
+    __doc__ += "    Graham 2020 - https://arxiv.org/abs/2004.07885"
+    __doc__ += "\n    Rubin 2021 - https://arxiv.org/abs/2102.05069"
+
+    nYrObs: float = 1.0
+    nVisYr: dict[str, float] | float = 1.0
+    gamma: dict[str, float] | float = 0.04
+
+    m5: dict[str, float] | float = field(
+        default_factory=lambda: {
+            "Y": 26.9,
+            "J": 26.95,
+            "H": 26.9,
+            "F": 26.25,
+        }
+    )
+    theta: dict[str, float] | float = field(
+        default_factory=lambda: {
+            "Y": 0.130,
+            "J": 0.136,
+            "H": 0.150,
+            "F": 0.166,
+        }
+    )
+    airmass: float = 0
+
+
+class RomanErrorModel(ErrorModel):
+    """Photometric error model for Roman.
+
+    Below is the parameter docstring:
+    """
+
+    __doc__ += RomanErrorParams.__doc__
+
+    def __init__(self, **kwargs: Any) -> None:
+        """Create a Roman error model.
+
+        Keyword arguments override default values in RomanErrorParams.
+        """
+        super().__init__(RomanErrorParams(**kwargs))
