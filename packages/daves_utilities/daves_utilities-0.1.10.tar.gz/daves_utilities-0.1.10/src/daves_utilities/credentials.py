493e899ea31ac1d715092a628aeca307e4d9beb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:48 +0000

    Add package daves_utilities/0.1.10/daves_utilities-0.1.10.tar.gz

---
+++ packages/daves_utilities/daves_utilities-0.1.10.tar.gz/daves_utilities-0.1.10/src/daves_utilities/credentials.py
@@ -0,0 +1,9 @@
+access = {
+    "postgres":{
+        "postgres_usr": 'ti',
+        "postgres_pwd": (
+            'gAAAAABk_1-X_7CiiUj5MIlXyYVGdFB90RKJPblTLgUNOaFtyaXZmoMzpF6h2'
+            'KyQu2TrKgjJBmdPwJtQvySgR0bHlYqJR9oHPw=='
+        )
+    },
+}
