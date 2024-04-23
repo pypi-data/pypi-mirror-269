de4edbd08af82b817403a23445993345a3560d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 03:04:51 +0000

    Add package blendedUx-Lang/1.0.1/blendedUx_Lang-1.0.1-py3-none-any.whl

---
+++ packages/blendedUx-Lang/blendedUx_Lang-1.0.1-py3-none-any.whl/blendedUxLang/__init__.py
@@ -0,0 +1,10 @@
+from blendedUxLang.blended.jinjaenv import BlendedEnvironment
+
+class BlendedUxLang():
+    """
+    """
+    def __init__(self):
+        pass
+
+    def BlendedEnvironment():
+        BlendedEnvironment
