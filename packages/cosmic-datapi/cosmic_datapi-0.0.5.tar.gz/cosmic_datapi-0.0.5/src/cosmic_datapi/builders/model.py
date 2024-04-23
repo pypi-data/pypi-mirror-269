4611afa1b10a01721e6ee915834f9ff688e5a22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:46 +0000

    Add package cosmic-datapi/0.0.5/cosmic_datapi-0.0.5.tar.gz

---
+++ packages/cosmic-datapi/cosmic_datapi-0.0.5.tar.gz/cosmic_datapi-0.0.5/src/cosmic_datapi/builders/model.py
@@ -0,0 +1,34 @@
+from ..api import AbstractBuilder, BuiltData
+from .block_state import BlockStateBuilder
+import os
+
+
+class AbstractModelBuilder(AbstractBuilder):
+    def __init__(self, mod: "Mod", path: str, name: str):
+        super().__init__(mod)
+        self._path = path
+        self._name = name
+        self._data = {}
+
+    def build(self) -> BuiltData:
+        path = os.path.join("models", self._path, f"{self._name}.json")
+        return BuiltData(path, self._data)
+
+
+class BlockModelBuilder(AbstractModelBuilder):
+    def __init__(self, mod: "Mod", name: str):
+        super().__init__(mod, "blocks", name)
+        self._data = {
+            "parent": "cube",
+            "textures": {}
+        }
+
+    def with_parent(self, id: str) -> "BlockModelBuilder":
+        self._data["parent"] = id
+        return self
+
+    def with_texture(self, kind: str, file_name: str) -> "BlockModelBuilder":
+        self._data["textures"]["kind"] = {
+            "fileName": file_name
+        }
+        return self
