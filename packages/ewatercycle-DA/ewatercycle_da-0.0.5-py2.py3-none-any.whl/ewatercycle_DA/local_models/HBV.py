24db4ebb1a697beef794c1f5c5bdbf591dc0d482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:36 +0000

    Add package ewatercycle-DA/0.0.5/ewatercycle_da-0.0.5-py2.py3-none-any.whl

---
+++ packages/ewatercycle-DA/ewatercycle_da-0.0.5-py2.py3-none-any.whl/ewatercycle_DA/local_models/HBV.py
@@ -0,0 +1,10 @@
+# for local mode
+from typing import Type
+from ewatercycle.base.model import LocalModel
+from ewatercycle_HBV.model import HBVMethods
+from HBV import HBV as HBV_bmi
+from bmipy import Bmi
+
+class HBVLocal(LocalModel, HBVMethods):
+    """The HBV eWaterCycle model, with the local BMI."""
+    bmi_class: Type[Bmi] = HBV_bmi
