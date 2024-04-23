47e60b3321858a2605af024893bff702adcc7bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 07:03:23 +0000

    Add package tck/1.0.0/tck-1.0.0-py3-none-any.whl

---
+++ packages/tck/tck-1.0.0-py3-none-any.whl/tck/TCK.py
@@ -0,0 +1,117 @@
+from concurrent.futures import ProcessPoolExecutor
+import numpy as np
+from tqdm.autonotebook import tqdm
+from sklearn.preprocessing import normalize
+from .GMM_MAP_EM import GMM_MAP_EM
+from .GMMposterior import GMMposterior
+
+class TCK:
+    def __init__(self, G=30, C=None):
+        self.G = G
+        self.C = C
+
+
+    def process_gmm(self, X, G, minN, minT, maxT, minV, maxV, I, missing, i):
+        """
+        Function to parallelize the training of the TCK.
+        """
+        c = (i // G) + 2
+        kwargs = {'C': c, 'minN': minN, 'minT': minT, 'maxT': maxT, 'minV': minV, 'maxV': maxV, 'I': I, 'missing': missing}
+        return GMM_MAP_EM(X, **kwargs)
+    
+
+    def update_matrix(self, i, mode, Xte):
+        """
+        Function to parallelize the training of the TCK.
+        """
+        if mode == 'tr-tr':
+            return np.dot(normalize(self.GMM[i][0], axis=1), normalize(self.GMM[i][0], axis=1).T)
+        else:
+            missing = np.isnan(Xte).any()
+            if mode == 'tr-te':
+                c = (i // self.G) + 2
+                return np.dot(normalize(self.GMM[i][0], axis=1), normalize(GMMposterior(Xte, c, *self.GMM[i][1:], missing), axis=1).T)
+            elif mode == 'te-te':
+                c = (i // self.G) + 2
+                posterior = normalize(GMMposterior(Xte, c, *self.GMM[i][1:], missing), axis=1)
+                return np.dot(posterior, posterior.T)
+            else:
+                raise ValueError('Invalid training mode')
+            
+
+    def fit(self, X, minN=0.8, minV=None, maxV=None, minT=6, maxT=None, I=20):
+        """
+        Fit the TCK model to the training data.
+
+        Parameters:
+        X (numpy.ndarray): Training data of size N x T x V.
+        minN (float): Minimum percentage of samples to be used in the training of the GMMs.
+        minV (int): Minimum number of attributes to be sampled from the dataset.
+        maxV (int): Maximum number of attributes to be sampled from the dataset.
+        minT (int): Minimum length of time segments to be sampled from the dataset.
+        maxT (int): Maximum length of time segments to be sampled from the dataset.
+        I (int): Number of iterations for the MAP-EM algorithm.
+
+        Returns:
+        TCK: The fitted TCK model.
+        """
+        
+        N, T, V = X.shape  # Unpack the shape of X        
+        if self.C is None:
+            self.C = 10 if N < 100 else 40
+        if minV is None:
+            minV = 1 if V == 1 else 2
+        if maxV is None:
+            maxV = min(int(np.ceil(0.9 * V)), 15)
+        if maxT is None:
+            maxT = min(int(np.floor(0.8 * T)), 25)
+
+        self.GMM = []
+        
+        # Check for missing data
+        missing = np.isnan(X).any()
+        missing_data_msg = "The dataset contains missing data\n" if missing else "The dataset does not contain missing data\n"
+        print(missing_data_msg)
+        
+        # Training message
+        training_msg = f"Training the TCK using the following parameters:\n\tC = {self.C}, G = {self.G}\n"
+        training_msg += f"\tNumber of MTS for each GMM: {int(np.floor(minN*N))} - {N} ({int(np.floor(minN*100))} - 100 percent)\n"
+        training_msg += f"\tNumber of attributes sampled from [{minV}, {maxV}]\n"
+        training_msg += f"\tLength of time segments sampled from [{minT}, {maxT}]\n\n"
+        print(training_msg)
+        
+        # Parallel processing
+        with ProcessPoolExecutor() as executor:
+            # Pass additional arguments required by process_gmm
+            futures = [executor.submit(self.process_gmm, X, self.G, minN, minT, maxT, minV, maxV, I, missing, i) for i in range(self.G * (self.C - 1))]
+            for future in tqdm(futures, desc='Fitting GMMs'):
+                self.GMM.append(future.result())
+      
+        return self
+
+    
+    def predict(self, mode, Xte=None):
+        """
+        Predict the similarity matrix for the test data.
+
+        Parameters:
+        mode (str): The mode of the prediction. Possible values are 'tr-tr', 'tr-te', and 'te-te'.
+        Xte (numpy.ndarray): The test data of size N x T x V.
+
+        Returns:
+        numpy.ndarray: The similarity matrix.
+        """
+
+        if mode == 'tr-tr':
+            K = np.zeros((self.GMM[0][0].shape[0], self.GMM[0][0].shape[0]))
+        elif mode == 'tr-te':
+            K = np.zeros((self.GMM[0][0].shape[0], Xte.shape[0]))
+        elif mode == 'te-te':
+            K = np.zeros((Xte.shape[0], Xte.shape[0]))
+
+        with ProcessPoolExecutor() as executor:
+            futures = [executor.submit(self.update_matrix, i, mode, Xte) for i in range(self.G * (self.C - 1))]
+            for future in tqdm(futures, desc=f"Computing TCK ({mode})"):
+                K += future.result()
+        
+        return K
---
+++ packages/tck/tck-1.0.0-py3-none-any.whl/tck/datasets.py
@@ -0,0 +1,68 @@
+import requests
+import numpy as np
+from io import BytesIO
+import warnings
+
+class DataLoader:
+    def __init__(self):
+        self.datasets = {
+            'AtrialFibrillation': ('https://zenodo.org/records/10852712/files/AF.npz?download=1', 'Multivariate time series classification.\nSamples: 5008 (4823 training, 185 test)\nFeatures: 2\nClasses: 3\nTime series length: 45'),
+            'ArabicDigits': ('https://zenodo.org/records/10852747/files/ARAB.npz?download=1', 'Multivariate time series classification.\nSamples: 8800 (6600 training, 2200 test)\nFeatures: 13\nClasses: 10\nTime series length: 93'),
+            'Auslan': ('https://zenodo.org/records/10839959/files/Auslan.npz?download=1', 'Multivariate time series classification.\nSamples: 2565 (1140 training, 1425 test)\nFeatures: 22\nClasses: 95\nTime series length: 136'),
+            'CharacterTrajectories': ('https://zenodo.org/records/10852786/files/CHAR.npz?download=1', 'Multivariate time series classification.\nSamples: 2858 (300 training, 2558 test)\nFeatures: 3\nClasses: 20\nTime series length: 205'),
+            'CMUsubject16': ('https://zenodo.org/records/10852831/files/CMU.npz?download=1', 'Multivariate time series classification.\nSamples: 58 (29 training, 29 test)\nFeatures: 62\nClasses: 2\nTime series length: 580'),
+            'ECG2D': ('https://zenodo.org/records/10839881/files/ECG_2D.npz?download=1', 'Multivariate time series classification.\nSamples: 200 (100 training, 100 test)\nFeatures: 2\nClasses: 2\nTime series length: 152'),
+            'Japanese_Vowels': ('https://zenodo.org/records/10837602/files/Japanese_Vowels.npz?download=1', 'Multivariate time series classification.\nSamples: 640 (270 training, 370 test)\nFeatures: 12\nClasses: 9\nTime series length: 29'),
+            'KickvsPunch': ('https://zenodo.org/records/10852865/files/KickvsPunch.npz?download=1', 'Multivariate time series classification.\nSamples: 26 (16 training, 10 test)\nFeatures: 62\nClasses: 2\nTime series length: 841'),
+            'Libras': ('https://zenodo.org/records/10852531/files/LIB.npz?download=1', 'Multivariate time series classification.\nSamples: 360 (180 training, 180 test)\nFeatures: 2\nClasses: 15\nTime series length: 45'),
+            'NetFlow': ('https://zenodo.org/records/10840246/files/NET.npz?download=1', 'Multivariate time series classification.\nSamples: 1337 (803 training, 534 test)\nFeatures: 4\nClasses: 2\nTime series length: 997'),
+            'RobotArm': ('https://zenodo.org/records/10852893/files/Robot.npz?download=1', 'Multivariate time series classification.\nSamples: 164 (100 training, 64 test)\nFeatures: 6\nClasses: 5\nTime series length: 15'),
+            'UWAVE': ('https://zenodo.org/records/10852667/files/UWAVE.npz?download=1', 'Multivariate time series classification.\nSamples: 628 (200 training, 428 test)\nFeatures: 3\nClasses: 8\nTime series length: 315'),
+            'Wafer': ('https://zenodo.org/records/10839966/files/Wafer.npz?download=1', 'Multivariate time series classification.\nSamples: 1194 (298 training, 896 test)\nFeatures: 6\nClasses: 2\nTime series length: 198'),
+            'Chlorine': ('https://zenodo.org/records/10840284/files/CHLO.npz?download=1', 'Univariate time series classification.\nSamples: 4307 (467 training, 3840 test)\nFeatures: 1\nClasses: 3\nTime series length: 166'), 
+            'Phalanx': ('https://zenodo.org/records/10852613/files/PHAL.npz?download=1', 'Univariate time series classification.\nSamples: 539 (400 training, 139 test)\nFeatures: 1\nClasses: 3\nTime series length: 80'),
+            'SwedishLeaf': ('https://zenodo.org/records/10840000/files/SwedishLeaf.npz?download=1', 'Univariate time series classification.\nSamples: 1125 (500 training, 625 test)\nFeatures: 1\nClasses: 15\nTime series length: 128'),
+        }
+
+    def available_datasets(self, details=False):
+        print("Available datasets:\n")
+        for alias, (_, description) in self.datasets.items():
+            if details:
+                print(f"{alias}\n-----------\n{description}\n")
+            else:
+                print(alias)
+
+    def get_data(self, alias):
+
+        if alias not in self.datasets:
+            raise ValueError(f"Dataset {alias} not found.")
+
+        url, _ = self.datasets[alias]
+        response = requests.get(url)
+        if response.status_code == 200:
+
+            data = np.load(BytesIO(response.content))
+            Xtr = data['Xtr']  # shape is [N,T,V]
+            if len(Xtr.shape) < 3:
+                Xtr = np.atleast_3d(Xtr)
+            Ytr = data['Ytr']  # shape is [N,1]
+            Xte = data['Xte']
+            if len(Xte.shape) < 3:
+                Xte = np.atleast_3d(Xte)
+            Yte = data['Yte']
+            n_classes_tr = len(np.unique(Ytr))
+            n_classes_te = len(np.unique(Yte))
+            if n_classes_tr != n_classes_te:
+                warnings.warn(f"Number of classes in training and test sets do not match for {alias} dataset.")
+            print(f"Loaded {alias} dataset.\nNumber of classes: {n_classes_tr}\nData shapes:\n  Xtr: {Xtr.shape}\n  Ytr: {Ytr.shape}\n  Xte: {Xte.shape}\n  Yte: {Yte.shape}")
+
+            return (Xtr, Ytr, Xte, Yte)
+        else:
+            print(f"Failed to download {alias} dataset.")
+            return None
+
+if __name__ == '__main__':
+    # Example usage
+    downloader = DataLoader()
+    downloader.available_datasets()  # Describe available datasets
+    Xtr, Ytr, Xte, Yte = downloader.get_data('NetFlow')  # Download dataset and return data
