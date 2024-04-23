ff61e6d6f1369345b3d33ea274751d92904441b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:04:07 +0000

    Add package replit-river/0.1.7b3/replit_river-0.1.7b3.tar.gz

---
+++ packages/replit-river/replit_river-0.1.7b3.tar.gz/replit_river-0.1.7b3/replit_river/transport.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+from typing import Dict, Tuple
+
+import nanoid  # type: ignore
+
+from replit_river.rpc import (
+    GenericRpcHandler,
+)
+from replit_river.session import Session
+from replit_river.transport_options import TransportOptions
+
+PROTOCOL_VERSION = "v1"
+
+
+class Transport:
+    def __init__(
+        self,
+        transport_id: str,
+        transport_options: TransportOptions,
+        is_server: bool,
+        handlers: Dict[Tuple[str, str], Tuple[str, GenericRpcHandler]] = {},
+    ) -> None:
+        self._transport_id = transport_id
+        self._transport_options = transport_options
+        self._is_server = is_server
+        self._sessions: Dict[str, Session] = {}
+        self._handlers = handlers
+        self._session_lock = asyncio.Lock()
+
+    async def close_all_sessions(self) -> None:
+        sessions = self._sessions.values()
+        logging.info(
+            f"start closing transport {self._transport_id}, number sessions : "
+            f"{len(sessions)}"
+        )
+        sessions_to_close = list(sessions)
+        for session in sessions_to_close:
+            await session.close(False)
+        logging.info(f"Transport closed {self._transport_id}")
+
+    async def _delete_session(self, session: Session) -> None:
+        async with self._session_lock:
+            if session._to_id in self._sessions:
+                del self._sessions[session._to_id]
+
+    def generate_nanoid(self) -> str:
+        return str(nanoid.generate())
