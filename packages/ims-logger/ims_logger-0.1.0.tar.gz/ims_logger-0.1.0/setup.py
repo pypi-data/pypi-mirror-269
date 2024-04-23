42a2e1fba21d09c2f34ce8628778f2d76ff97645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:04:10 +0000

    Add package ims-logger/0.1.0/ims_logger-0.1.0.tar.gz

---
+++ packages/ims-logger/ims_logger-0.1.0.tar.gz/ims_logger-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ims_logger',
+    packages=find_packages(include=['ims_logger']),
+    version='0.1.0',
+    author='Allen',
+    description='Library created for logging in IMS application'
+)
