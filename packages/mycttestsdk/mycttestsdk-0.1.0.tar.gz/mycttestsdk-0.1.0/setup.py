f265d471930ab30551290b5b4d315dc292050898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:05:17 +0000

    Add package mycttestsdk/0.1.0/mycttestsdk-0.1.0.tar.gz

---
+++ packages/mycttestsdk/mycttestsdk-0.1.0.tar.gz/mycttestsdk-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import pathlib
+ 
+setup(
+    name='mycttestsdk',  # Replace with your actual package name
+    # url = 'https://github.com/enomis-dev/test-package', # Replace with your github project link
+    version='0.1.0',
+    description="Used for integration with Application monitoring systems",
+    long_description = pathlib.Path("README.rst").read_text(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        # Your project dependencies
+    ],
+)
