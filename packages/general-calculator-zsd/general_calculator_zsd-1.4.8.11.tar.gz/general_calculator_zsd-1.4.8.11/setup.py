d2442fd2462e7a0b53e27d03baf62ec1bbb84aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:04:39 +0000

    Add package general-calculator-zsd/1.4.8.11/general_calculator_zsd-1.4.8.11.tar.gz

---
+++ packages/general-calculator-zsd/general_calculator_zsd-1.4.8.11.tar.gz/general_calculator_zsd-1.4.8.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='general_calculator_zsd',
+    version='1.4.8.11',
+    author='harden.jiang',
+    author_email='zsd0830@163.com',
+    description='修改bilibili_cookie',
+    long_description_content_type="""text/markdown""",
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
+
