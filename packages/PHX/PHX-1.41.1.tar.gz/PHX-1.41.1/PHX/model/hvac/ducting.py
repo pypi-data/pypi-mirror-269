620ed6e9c023779f7a61fc489e46a93de458cabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:50 +0000

    Add package PHX/1.41.1/PHX-1.41.1.tar.gz

---
+++ packages/PHX/PHX-1.41.1.tar.gz/PHX-1.41.1/PHX/model/hvac/ducting.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+# -*- Python Version: 3.10 -*-
+
+"""PHX Ventilation Ducting Distribution Objects."""
+
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import ClassVar, Dict, List, Optional
+
+from ladybug_geometry.geometry3d.polyline import LineSegment3D
+
+from PHX.model.enums.hvac import PhxVentDuctType
+
+
+@dataclass
+class PhxDuctSegment:
+    """An individual Duct Segment Segment."""
+
+    identifier: str
+    display_name: str
+    geometry: LineSegment3D
+    diameter: float
+    height: Optional[float]
+    width: Optional[float]
+    insulation_thickness: float
+    insulation_conductivity: float
+    insulation_reflective: bool
+
+    @property
+    def length(self) -> float:
+        return self.geometry.length
+
+
+@dataclass
+class PhxDuctElement:
+    """A Duct Element / Run made of one or more PhxDuctSegments."""
+
+    _count: ClassVar[int] = 0
+    id_num: int = field(init=False, default=0)
+
+    identifier: str
+    display_name: str
+    duct_type: PhxVentDuctType = field(init=False, default=PhxVentDuctType.SUPPLY)
+    vent_unit_id: int
+    _segments: Dict = field(default_factory=dict)
+
+    def __post_init__(self) -> None:
+        self.__class__._count += 1
+        self.id_num = self.__class__._count
+
+    @property
+    def quantity(self) -> int:
+        return 1
+
+    @property
+    def segments(self) -> List[PhxDuctSegment]:
+        return list(self._segments.values())
+
+    @property
+    def length(self) -> float:
+        return sum(_.length for _ in self.segments)
+
+    @property
+    def diameter(self) -> float:
+        weighted_total = 0.0
+        for seg in self.segments:
+            weighted_total += seg.length * seg.diameter
+
+        try:
+            return weighted_total / self.length
+        except ZeroDivisionError:
+            return 0.0
+
+    @property
+    def height(self) -> float:
+        weighted_total = 0.0
+        for seg in self.segments:
+            weighted_total += seg.length * (seg.height or 0.0)
+
+        try:
+            return weighted_total / self.length
+        except ZeroDivisionError:
+            return 0.0
+
+    @property
+    def width(self) -> float:
+        weighted_total = 0.0
+        for seg in self.segments:
+            weighted_total += seg.length * (seg.width or 0.0)
+
+        try:
+            return weighted_total / self.length
+        except ZeroDivisionError:
+            return 0.0
+
+    @property
+    def insulation_thickness(self) -> float:
+        weighted_total = 0.0
+        for seg in self.segments:
+            weighted_total += seg.length * seg.insulation_thickness
+
+        try:
+            return weighted_total / self.length
+        except ZeroDivisionError:
+            return 0.0
+
+    @property
+    def insulation_conductivity(self) -> float:
+        weighted_total = 0.0
+        for seg in self.segments:
+            weighted_total += seg.length * seg.insulation_conductivity
+
+        try:
+            return weighted_total / self.length
+        except ZeroDivisionError:
+            return 0.0
+
+    @property
+    def duct_shape(self) -> int:
+        if self.height and self.width:
+            return 2  # Rectangular Duct
+        else:
+            return 1  # Round Duct
+
+    @property
+    def is_reflective(self) -> bool:
+        return any({seg.insulation_reflective for seg in self.segments})
+
+    @property
+    def assigned_vent_unit_ids(self) -> List[int]:
+        return [self.vent_unit_id]
+
+    def add_segment(self, _s: PhxDuctSegment) -> None:
+        self._segments[_s.identifier] = _s
