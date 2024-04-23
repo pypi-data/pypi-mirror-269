1f2d214625241084e00931c9c9c7f713a9ed2c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:46 +0000

    Add package deephaven-plugin-ui/0.13.1/deephaven_plugin_ui-0.13.1.tar.gz

---
+++ packages/deephaven-plugin-ui/deephaven_plugin_ui-0.13.1.tar.gz/deephaven_plugin_ui-0.13.1/src/deephaven/ui/hooks/use_row_list.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import Any
+import pandas as pd
+
+from deephaven.table import Table
+
+from .use_table_data import use_table_data
+from ..types import Sentinel
+
+
+def _row_list(data: pd.DataFrame | Sentinel, is_sentinel: bool) -> list[Any] | Sentinel:
+    """
+    Return the first row of the table as a list.
+
+    Args:
+        data: The dataframe to extract the row from or the sentinel value.
+        is_sentinel: Whether the sentinel value was returned.
+
+    Returns:
+        The first row of the table as a list.
+    """
+    try:
+        return data if is_sentinel else data.iloc[0].values.tolist()
+    except IndexError:
+        # if there is a static table with no rows, we will get an IndexError
+        raise IndexError("Cannot get row list from an empty table")
+
+
+def use_row_list(table: Table, sentinel: Sentinel = None) -> list[Any] | Sentinel:
+    """
+    Return the first row of the table as a list. The table should already be filtered to only have a single row.
+
+    Args:
+        table: The table to extract the row from.
+        sentinel: The sentinel value to return if the table is ticking but empty. Defaults to None.
+
+    Returns:
+        The first row of the table as a list or the sentinel value.
+    """
+    return use_table_data(table, sentinel, _row_list)
