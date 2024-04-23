09b85be3183dc827b89a8de4c5313b3290595d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:47 +0000

    Add package jinns/0.8.5/jinns-0.8.5.tar.gz

---
+++ packages/jinns/jinns-0.8.5.tar.gz/jinns-0.8.5/tests/runtests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pytest dataGenerator_tests/*
+if [ $? -ne 0 ]; then
+   exit $?
+fi
+pytest -s sharding_tests/*
+if [ $? -ne 0 ]; then
+   exit $?
+fi
+pytest -s save_load_tests/*
+if [ $? -ne 0 ]; then
+   exit $?
+fi
+pytest solver_tests/*
+if [ $? -ne 0 ]; then
+   exit $?
+fi
+pytest solver_tests_spinn/*
+exit $?
