52ba116c953afb6221f611e1df0bbf49b4ad602b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 03:05:15 +0000

    Add package decorate-all/0.1.0/decorate_all-0.1.0.tar.gz

---
+++ packages/decorate-all/decorate_all-0.1.0.tar.gz/decorate_all-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='decorate_all',
+    version='0.1.0',
+    packages=['decorate_all'],
+    url='https://github.com/emendir/py-decorate-all',
+    description='A short description of your package',
+    long_description=open('ReadMe.md').read(),
+    long_description_content_type='text/markdown',
+)
