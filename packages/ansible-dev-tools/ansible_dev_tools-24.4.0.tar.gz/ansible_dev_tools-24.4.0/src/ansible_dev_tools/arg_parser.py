a6dc36869fd060aa1c3d990b1b11f236ee85a014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:48 +0000

    Add package ansible-dev-tools/24.4.0/ansible_dev_tools-24.4.0.tar.gz

---
+++ packages/ansible-dev-tools/ansible_dev_tools-24.4.0.tar.gz/ansible_dev_tools-24.4.0/src/ansible_dev_tools/arg_parser.py
@@ -0,0 +1,26 @@
+"""Parse the command line arguments."""
+
+import argparse
+
+from .version_builder import version_builder
+
+
+def parse() -> argparse.Namespace:
+    """Parse the command line arguments.
+
+    Returns:
+        The arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="The ansible content development kit.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=version_builder(),
+        help="Print the included tool versions and exit.",
+    )
+
+    return parser.parse_args()
