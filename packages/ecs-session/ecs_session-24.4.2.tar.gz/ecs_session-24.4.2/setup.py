48acddf8164a9b41ed128de1c19054c3c2408302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:57 +0000

    Add package ecs-session/24.4.2/ecs_session-24.4.2.tar.gz

---
+++ packages/ecs-session/ecs_session-24.4.2.tar.gz/ecs_session-24.4.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ecs_session']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3', 'configargparse', 'shtab', 'simple_term_menu']
+
+entry_points = \
+{'console_scripts': ['ecs-session = ecs_session:main']}
+
+setup_kwargs = {
+    'name': 'ecs-session',
+    'version': '24.4.2',
+    'description': 'ECS SSM tool',
+    'long_description': '# ecs-session\n\nInspired by [ecsgo](https://github.com/tedsmitt/ecsgo) (`ecspy` is in use already).\n\nProvides a tool to interact with AWS ECS tasks. Basically just wraps `aws`.\n\nCurrently provides:\n\n* interactive execute-command (e.g. shell)\n* port-forwarding\n\nYou can supply command-line arguments to specify which cluster/service/task/... to use or will be prompted\nwith a nice menu.\n\n## Installation\n\n```\npip install ecs-session\n```\n\n## Pre-requisites\n\n### Tools\n\n* [aws-cli](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)\n* [session-manager-plugin](https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html)\n\nMacOS users can alternatively install this via Homebrew:\n`brew install --cask session-manager-plugin awscli`\n\n### Infrastructure\n\nUse [ecs-exec-checker](https://github.com/aws-containers/amazon-ecs-exec-checker) to check for the pre-requisites to use ECS exec.\n\n\n## Usage\n\nSee `ecs-session --help`.\n',
+    'author': 'Stefan Heitmüller',
+    'author_email': 'stefan.heitmueller@gmx.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/morph027/ecs-session',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+}
+
+
+setup(**setup_kwargs)
