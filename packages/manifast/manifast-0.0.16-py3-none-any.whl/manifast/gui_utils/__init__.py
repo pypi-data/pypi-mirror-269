a2c737c7fbc55aec3aaf4d4d88ff8482f9e8256d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:04:41 +0000

    Add package manifast/0.0.16/manifast-0.0.16-py3-none-any.whl

---
+++ packages/manifast/manifast-0.0.16-py3-none-any.whl/manifast/gui_utils/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+'''
+brief        :  
+Author       : knightdby knightdby@163.com
+Date         : 2023-02-27 09:41:52
+FilePath     : /manifast/manifast/gui_utils/__init__.py
+Description  : 
+LastEditTime : 2023-02-27 09:41:55
+LastEditors  : knightdby
+Copyright (c) 2023 by Inc, All Rights Reserved.
+'''
