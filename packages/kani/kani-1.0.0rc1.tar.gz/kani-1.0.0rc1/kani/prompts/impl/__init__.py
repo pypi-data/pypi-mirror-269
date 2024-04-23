deff69d48f3af979fd6a202e2afeee8bb6cf1f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:35 +0000

    Add package kani/1.0.0rc1/kani-1.0.0rc1.tar.gz

---
+++ packages/kani/kani-1.0.0rc1.tar.gz/kani-1.0.0rc1/kani/prompts/impl/__init__.py
@@ -0,0 +1,8 @@
+"""Implementations of pipelines for popular models."""
+
+from .gemma import GEMMA_PIPELINE
+from .llama2 import LLAMA2_PIPELINE
+from .llama3 import LLAMA3_PIPELINE
+from .vicuna import VICUNA_PIPELINE
+
+MISTRAL_PIPELINE = LLAMA2_PIPELINE
