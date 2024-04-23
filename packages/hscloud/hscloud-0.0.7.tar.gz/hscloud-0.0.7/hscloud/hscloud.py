2d6bec459b0e34cb25bf2a88fb81c356878a6ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:05:17 +0000

    Add package hscloud/0.0.7/hscloud-0.0.7.tar.gz

---
+++ packages/hscloud/hscloud-0.0.7.tar.gz/hscloud-0.0.7/hscloud/hscloud.py
@@ -0,0 +1,24 @@
+from .helpers import Helpers
+
+class HsCloud:
+
+    def __init__(self, username=None, password=None):
+        super().__init__()
+        self.username = username
+        self.password = password
+        self.access_token = None
+
+    def login(self) -> tuple:
+        response = Helpers.call_api("/api/oauth/login", "post", Helpers.headers(), Helpers.params(), Helpers.login_body(self.username, self.password))
+        self.access_token = response[1].get("access_token")
+        return response
+
+    def get_devices(self) -> tuple:
+        return Helpers.call_api("/api/device/list", "get", Helpers.headers(self.access_token), Helpers.params())
+
+    def get_status(self, access_token, devicesn) -> tuple:
+        return Helpers.call_api("/api/device/state", "get", Helpers.headers(access_token), Helpers.params(devicesn))
+
+    def update(self, access_token, devicesn, **kwargs) -> bool:
+        response = Helpers.call_api("/api/device/control", "post", Helpers.headers(access_token), Helpers.params(), Helpers.update_body(devicesn, **kwargs))
+        return response[0]
