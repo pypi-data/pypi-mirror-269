d1ffe670b6b4e612516e809875442c4846cf7c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 06:04:31 +0000

    Add package polars-ts/0.0.9/polars_ts-0.0.9.tar.gz

---
+++ packages/polars-ts/polars_ts-0.0.9.tar.gz/polars_ts-0.0.9/tests/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+def get_config_filename(name: str) -> str:
+    path = Path("tests", "00_config", name).absolute()
+    return str(path)
---
+++ packages/polars-ts/polars_ts-0.0.9.tar.gz/polars_ts-0.0.9/tests/futures/test_continuous_contract.py
@@ -0,0 +1,70 @@
+from datetime import datetime, UTC
+import polars as pl
+
+import polars_ts  # noqa
+
+from ..config import get_config_filename
+from polars_ts.futures_helper.roll_calendar import guess_security_meta
+from polars_ts.futures_helper.generic_symbols import generic_symbol_universe
+
+
+def test_continuous_contract():
+    market_start_date = datetime(1990, 1, 1, 23, 0, tzinfo=UTC)
+    market_end_date = datetime(2000, 1, 1, 23, 0, tzinfo=UTC)
+
+    roll_config_filename = get_config_filename("roll_config.csv")
+    roll_config = pl.LazyFrame().io.read_csv(roll_config_filename)
+
+    generic_symbols = generic_symbol_universe(
+        roll_config, market_start_date, market_end_date
+    )
+    security_meta = guess_security_meta(generic_symbols, roll_config)
+
+    security_dates = (
+        security_meta.select(
+            "asset",
+            "instrument_id",
+            "fut_first_trade_dt",
+            expiry_date=pl.min_horizontal("last_tradeable_dt", "fut_notice_first"),
+        )
+        .collect()
+        .lazy()
+    )
+
+    roll_calendar = pl.LazyFrame().future.create_roll_calendar(
+        roll_config, security_dates
+    )
+
+    instrument_prices = security_dates.dummymkt.fetch_instrument_prices(
+        market_start_date.date(),
+        market_end_date.date(),
+        remove_weekends=True,
+    )
+
+    adjusted_prices = (
+        pl.LazyFrame()
+        .future.prepare_unadjusted_for_stitching(roll_calendar, instrument_prices)
+        .future.stitch_panama_backwards()
+    )
+
+    adjusted_prices_schema = list(adjusted_prices.schema.items())
+
+    expected_schema = [
+        ("time", pl.Date),
+        ("asset", pl.Categorical(ordering="physical")),
+        (
+            "instrument_id",
+            pl.Struct({"tenor": pl.Categorical(ordering="physical"), "year": pl.Int32}),
+        ),
+        ("unadjusted", pl.Float64),
+        ("panama_backwards", pl.Float64),
+    ]
+
+    assert adjusted_prices_schema == expected_schema
+
+    result = adjusted_prices.collect()
+
+    assert result.shape == (7830, 5)
+
+    # replace that with a stable hash
+    assert result.hash_rows().sum() == 11359005816526693312
