afccdea42314f33b48ad33a90a5c53347b19b434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:05:09 +0000

    Add package IISRapi/1.2/IISRapi-1.2.tar.gz

---
+++ packages/IISRapi/IISRapi-1.2.tar.gz/IISRapi-1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IISRapi',
+    version='1.2',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        'flair',
+        'torch',
+        'torchvision',
+    ],
+)
