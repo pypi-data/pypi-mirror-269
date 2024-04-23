416b709545181220b2641a1ff7073150404e8b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:06:55 +0000

    Add package BatchPlan/0.1.2/BatchPlan-0.1.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/BatchPlan/BatchPlan-0.1.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/batchplan/filters.py
@@ -0,0 +1,12 @@
+def in_list(ls, el):
+    return any([el.is_a(e) for e in ls])
+
+
+def default_filter():
+    # ls = ("IfcSite", "IfcSpace", "IfcOpeningElement")
+    ls = tuple()
+
+    def fn(el):
+        return el.is_a("IfcProduct") and (el.Representation is not None) and (not in_list(ls, el))
+
+    return fn
