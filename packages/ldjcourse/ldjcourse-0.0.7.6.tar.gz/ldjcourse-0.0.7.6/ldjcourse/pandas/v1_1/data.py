e5c5c154bd81e8ebe1fdc89ffa8fea647c2c5fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 03:04:49 +0000

    Add package ldjcourse/0.0.7.6/ldjcourse-0.0.7.6.tar.gz

---
+++ packages/ldjcourse/ldjcourse-0.0.7.6.tar.gz/ldjcourse-0.0.7.6/ldjcourse/pandas/v1_1/data.py
@@ -0,0 +1,103 @@
+import pandas as pd
+
+
+def data1():
+    # 定义行索引
+    index = ['01', '02', '03']
+
+    # 定义多级列索引
+    columns = [
+        ('var1', 'before'),
+        ('var1', 'post'),
+        ('var2', 'before'),
+        ('var2', 'post'),
+    ]
+    names = ['var', 'stage']
+    columns = pd.MultiIndex.from_tuples(columns, names=names)
+
+    # 定义数据
+    data = [
+        [11, 21, 31, 41],
+        [12, 22, 32, 42],
+        [13, 23, 33, 43],
+    ]
+
+    # 创建 DataFrame
+    df = pd.DataFrame(data, index=index, columns=columns)
+    return df
+
+
+def data2():
+    # 定义行索引
+    index = ['01', '02', '03']
+
+    # 定义多级列索引
+    columns = [
+        ('var1', 'before', 2),
+        ('var1', 'before', 1),
+        ('var1', 'post', 1),
+        ('var1', 'post', 2),
+        ('var2', 'before', 2),
+        ('var2', 'before', 1),
+        ('var2', 'post', 1),
+        ('var2', 'post', 2),
+    ]
+    names = ['var', 'stage1', 'stage2']
+    columns = pd.MultiIndex.from_tuples(columns, names=names)
+
+    # 定义数据
+    data = [
+        [11, 21, 31, 41, 51, 61, 71, 81],
+        [12, 22, 32, 42, 52, 62, 72, 82],
+        [13, 23, 33, 43, 53, 63, 73, 83],
+    ]
+
+    # 创建 DataFrame
+    df = pd.DataFrame(data, index=index, columns=columns)
+    return df
+
+
+def data3(type=0):
+    df = data2()
+    df = df.copy()
+
+    columns = list(df.columns)
+    if type == 0:
+        columns[3] = ('var1', 'post', 1)
+    elif type == 1:
+        columns[3] = ('var1', 'post', '')
+
+    df.columns = pd.MultiIndex.from_tuples(columns, names=df.columns.names)
+    df.sort_index(axis=1, inplace=True)
+    return df
+
+
+def data4():
+    # 定义多级行索引
+    index = [
+        ('01', 2023),
+        ('01', 2024),
+        ('02', 2023),
+        ('02', 2024),
+        ('03', 2023),
+        ('03', 2024),
+    ]
+    names = ['id', 'year']
+    index = pd.MultiIndex.from_tuples(index, names=names)
+
+    # 定义多级列索引
+    columns = ['var1', 'var2', 'var3']
+
+    # 定义数据
+    data = [
+        [11, 21, 31],
+        [12, 22, 32],
+        [13, 23, 33],
+        [14, 24, 34],
+        [15, 25, 35],
+        [16, 26, 36],
+    ]
+
+    # 创建 DataFrame
+    df = pd.DataFrame(data, index=index, columns=columns)
+    return df
