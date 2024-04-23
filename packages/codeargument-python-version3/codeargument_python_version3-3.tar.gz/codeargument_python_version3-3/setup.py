bf25c383de96d7d9e6377327fc7418b0f040868f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:04:41 +0000

    Add package codeargument-python-version3/3/codeargument_python_version3-3.tar.gz

---
+++ packages/codeargument-python-version3/codeargument_python_version3-3.tar.gz/codeargument_python_version3-3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+    name="codeargument_python_version3",
+    version="3",
+    author='shejunhan',
+    author_email='1715948422@qq.com',
+    packages=find_packages(),
+    insatll_requires=[],
+)
