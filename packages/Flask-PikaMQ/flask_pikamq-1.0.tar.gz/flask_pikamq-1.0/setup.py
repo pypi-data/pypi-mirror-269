63bd8671996ccc9ab643c455ede45feb59cc0ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:05:09 +0000

    Add package Flask-PikaMQ/1.0/flask_pikamq-1.0.tar.gz

---
+++ packages/Flask-PikaMQ/flask_pikamq-1.0.tar.gz/flask_pikamq-1.0/setup.py
@@ -0,0 +1,37 @@
+"""
+Flask-SQLite3
+-------------
+
+This is the description for that library
+"""
+
+from setuptools import setup
+
+
+setup(
+    name="Flask-PikaMQ",
+    version="1.0",
+    url="",
+    license="BSD",
+    author="Rasco Developers",
+    author_email="rezahartono.rasco@gmail.com",
+    description="This is Extension for RabbitMQ Microservice",
+    long_description=__doc__,
+    py_modules=["pika_rabbitmq"],
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    # packages=['flask_sqlite3'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=["Flask", "pika"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
