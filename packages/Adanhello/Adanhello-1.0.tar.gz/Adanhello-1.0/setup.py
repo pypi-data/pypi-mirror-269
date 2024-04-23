17e43062ec545407b056d6c29aa79ee04b4c1456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:56 +0000

    Add package Adanhello/1.0/Adanhello-1.0.tar.gz

---
+++ packages/Adanhello/Adanhello-1.0.tar.gz/Adanhello-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Adanhello",
+    version='1.0',
+    description="Test Hello",
+    url="http://xf.maiok.com",
+    author="zbj",
+    author_email="22@qq.com",
+    license="MIT",
+    packages=find_packages()
+)
