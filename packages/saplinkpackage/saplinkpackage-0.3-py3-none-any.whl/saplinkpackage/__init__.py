a3b00c08ac7c195d8ed86ee497f4ee003fb5f590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:06:31 +0000

    Add package saplinkpackage/0.3/saplinkpackage-0.3-py3-none-any.whl

---
+++ packages/saplinkpackage/saplinkpackage-0.3-py3-none-any.whl/saplinkpackage/__init__.py
@@ -0,0 +1,28 @@
+import win32com.client
+import xlrd
+import math
+from pyautogui import hotkey
+from saplinkpackage.LicenseLock import LicenseLockCheck
+from coordinateconverter.Rotate3D import rotate_line, rotate_rect
+from numpy import array
+from coordinateconverter.TriangleCalculation import triangle_angle_calculation
+from xlrd import open_workbook
+from saplinkpackage.SapAUTO import SapModalDraw
+from pyautogui import hotkey
+from time import sleep
+from math import sin, cos, radians
+import numpy as np
+from time import sleep
+import win32com.client
+import xlrd
+from pyautogui import hotkey
+from saplinkpackage.LicenseLock import LicenseLockCheck
+from wordlinkpackage.WordBaseFunction import WordLinkBaseFunction
+import os
+import sys
+import winreg
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMessageBox
+import wmi
+import datetime
+from requests import get as requests_get
