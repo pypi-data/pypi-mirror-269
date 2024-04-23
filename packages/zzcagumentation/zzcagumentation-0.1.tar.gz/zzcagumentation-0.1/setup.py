ea9bf4f7ac54b9fb032ed180da381f5052191fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 06:04:33 +0000

    Add package zzcagumentation/0.1/zzcagumentation-0.1.tar.gz

---
+++ packages/zzcagumentation/zzcagumentation-0.1.tar.gz/zzcagumentation-0.1/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup,find_packages
+setup(name='zzcagumentation',
+      version='0.1',
+      packages=find_packages(),author='hjc,zzc',
+      install_requires=[],author_email='2711625204@qq.com')
