e12539d899ffcb6f83996187f6cb9845d7c042e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:04:34 +0000

    Add package skleanr/5.2.8/skleanr-5.2.8.tar.gz

---
+++ packages/skleanr/skleanr-5.2.8.tar.gz/skleanr-5.2.8/skleanr/1_IPC_Server.py
@@ -0,0 +1,16 @@
+import socket
+
+# Set up server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 8888))
+server.listen(5)
+
+print("Server listening on port 8888")
+
+while True:
+    # Accept connection and handle data
+    client, _ = server.accept()
+    data = client.recv(1024)
+    print("Received:", data.decode())
+    client.send(data)
+    client.close()
