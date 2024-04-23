d863f9de774ce1274b256bf60b793f67ffc96559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:04:04 +0000

    Add package chargespot/0.0.2/chargespot-0.0.2.tar.gz

---
+++ packages/chargespot/chargespot-0.0.2.tar.gz/chargespot-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chargespot",
+    version="0.0.2",
+    keywords=("api test", "automation", "testing", "chargespot"),
+    description="api test tools",
+    long_description="simplify api testing",
+    url="",
+    author="chargespot",
+    author_email="",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    platforms="any",
+    install_requires=['requests', 'configobj', 'pyyaml', 'click', 'jinja2', 'pytest', 'pytest-html'],
+    entry_points='''
+    [console_scripts]
+    chargespot=chargespot.cli:chargespot_cli
+    '''
+)
