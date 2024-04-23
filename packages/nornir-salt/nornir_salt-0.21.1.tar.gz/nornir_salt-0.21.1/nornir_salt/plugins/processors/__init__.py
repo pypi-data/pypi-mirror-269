0c8de58561946d7cb3ab0a01b14d5a549a3d728e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:05:20 +0000

    Add package nornir-salt/0.21.1/nornir_salt-0.21.1.tar.gz

---
+++ packages/nornir-salt/nornir_salt-0.21.1.tar.gz/nornir_salt-0.21.1/nornir_salt/plugins/processors/__init__.py
@@ -0,0 +1,13 @@
+from .ToFileProcessor import ToFileProcessor
+from .TestsProcessor import TestsProcessor
+from .DiffProcessor import DiffProcessor
+from .DataProcessor import DataProcessor
+from .SaltEventProcessor import SaltEventProcessor
+
+__all__ = (
+    "ToFileProcessor",
+    "TestsProcessor",
+    "DiffProcessor",
+    "DataProcessor",
+    "SaltEventProcessor",
+)
