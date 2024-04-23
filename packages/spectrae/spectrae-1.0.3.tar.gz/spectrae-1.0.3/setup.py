bf3d21758df0ef8ac3f01810c0f23c9ba59173ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 06:03:45 +0000

    Add package spectrae/1.0.3/spectrae-1.0.3.tar.gz

---
+++ packages/spectrae/spectrae-1.0.3.tar.gz/spectrae-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='spectrae',
+    version='1.0.3',    
+    description='SPECTRA: The spectral framework for model evaluation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mims-harvard/SPECTRA/',
+    author='Yasha Ektefaie',
+    author_email='yasha_ektefaie@g.harvard.edu',
+    license='MIT License',
+    packages=['spectrae'],
+    install_requires=['networkx',
+                      'numpy', 
+                      'torch', 
+                      'scikit-learn',
+                      'pandas',
+                      'tqdm',            
+                      ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+    ],
+)
