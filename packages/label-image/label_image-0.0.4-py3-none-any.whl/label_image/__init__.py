25f7098ce37b0d2aa549289d76b301ddeefc4303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 03:04:02 +0000

    Add package label-image/0.0.4/label_image-0.0.4-py3-none-any.whl

---
+++ packages/label-image/label_image-0.0.4-py3-none-any.whl/label_image/__init__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+# @Author: SWHL
+# @Contact: liekkaskono@163.com
+import sys
+from pathlib import Path
+
+cur_dir = Path(__file__).resolve().parent
+sys.path.append(str(cur_dir))
