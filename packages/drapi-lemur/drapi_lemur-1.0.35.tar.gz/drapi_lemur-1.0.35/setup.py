20e87608cf87f3763ce374aa9102296597d1d52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 14:03:46 +0000

    Add package drapi-lemur/1.0.35/drapi_lemur-1.0.35.tar.gz

---
+++ packages/drapi-lemur/drapi_lemur-1.0.35.tar.gz/drapi_lemur-1.0.35/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from pathlib import Path
+from setuptools import find_namespace_packages
+
+SCRIPTS = sorted([str(fpath) for fpath in Path("./src/scripts").iterdir()])
+
+setup(name='drapi-lemur',
+      package_dir={"": "src"},
+      include_package_data=True,
+      packages=find_namespace_packages(where="src"),
+      scripts=SCRIPTS,
+      version='1.0.35',
+      description='Data Request API for the Integrated Data Repository Research Services of University of Florida.',
+      long_description="Data Request API for the Integrated Data Repository Research Services of University of Florida.",
+      author='Herman Autore',
+      author_email='hf.autore+drapi@gmail.com',
+      url='https://github.com/ChemGuy88/hermanCode/archive/refs/tags/v1.0.0.tar.gz',
+      keywords=['CTSI',
+                'Clinical and Translational Science Institute',
+                'IDR',
+                'Integrated Data Repository',
+                'Integrated Data Repository Research Services',
+                'ODSRI',
+                'Office of Data Science and Research Implementation',
+                'Shands',
+                'Sloth 🦥',
+                'UF',
+                'UF Health',
+                'UFHealth',
+                'University of Florida'])
