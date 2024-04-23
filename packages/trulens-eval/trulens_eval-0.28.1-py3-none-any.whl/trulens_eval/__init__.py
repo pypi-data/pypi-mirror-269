89da9e1a67687398f92ce1791c9768e3eb53d2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 07:03:25 +0000

    Add package trulens-eval/0.28.1/trulens_eval-0.28.1-py3-none-any.whl

---
+++ packages/trulens-eval/trulens_eval-0.28.1-py3-none-any.whl/trulens_eval/__init__.py
@@ -0,0 +1,164 @@
+"""
+# Trulens-eval LLM Evaluation Library
+
+This top-level import should include everything to get started.
+
+## Module organization/dependency
+
+Modules on lower lines should not import modules on same or above lines as
+otherwise you might get circular import errors.
+
+- `tests/`
+
+- `__init__.py`
+
+- all UI/dashboard components, `pages`, `react_components`, `ux`, `Leaderboard.py`, `appui.py`
+
+- `appui.py`
+
+- `tru_chain.py` `tru_custom_app.py` `tru_virtual.py`
+
+- `tru_llama.py` (note: llama_index uses langchain internally for some things)
+
+- `tru.py`
+
+- `utils`
+
+    - `llama.py` `langchain.py` `trulens.py`
+
+- `feedback`
+
+    - `__init__.py`
+
+    - `provider`
+
+        - `__init__.py` 
+
+        - `endpoint`
+
+            - `__init__.py`
+
+            - `openai.py` `hugs.py` `bedrock.py` `litellm.py` `langchain.py`
+
+            - `base.py` 
+
+        - `hugs.py` `openai.py` `bedrock.py` `litellm.py` `langchain.py`
+
+        - `base.py`
+
+    - `groundedness.py` `groundtruth.py` `prompts.py` `embeddings.py`
+
+    - `feedback.py`
+
+- `tru_basic_app.py` TODO: bad placement
+
+- `app.py`
+
+- `instruments.py`
+
+- `schema.py`
+
+- `utils`
+
+    - `json.py`
+
+- `keys.py`
+
+- `utils`
+
+    - `pyschema.py`
+
+    - `threading.py` `serial.py`
+
+    - `python.py` `text.py` `generated.py` `containers.py` `imports.py`
+
+TO PLACE
+- `database/`
+- `feedback/v2/`
+- `utils/command_line.py`
+- `utils/notebook_utils.py`
+- `utils/asynchro.py`
+- `utils/text.py`
+- `utils/__init__.py`
+
+"""
+
+__version_info__ = (0, 28, 1)
+__version__ = '.'.join(map(str, __version_info__))
+
+# This check is intentionally done ahead of the other imports as we want to
+# print out a nice warning/error before an import error happens further down
+# this sequence.
+from trulens_eval.utils.imports import check_imports
+
+check_imports()
+
+from trulens_eval.feedback.feedback import Feedback
+from trulens_eval.feedback.provider.base import Provider
+from trulens_eval.feedback.provider.hugs import Huggingface
+from trulens_eval.feedback.provider.langchain import Langchain
+from trulens_eval.schema import FeedbackMode
+from trulens_eval.schema import Select
+from trulens_eval.tru import Tru
+from trulens_eval.tru_basic_app import TruBasicApp
+from trulens_eval.tru_chain import TruChain
+from trulens_eval.tru_custom_app import TruCustomApp
+from trulens_eval.tru_virtual import TruVirtual
+from trulens_eval.utils.imports import OptionalImports
+from trulens_eval.utils.imports import REQUIREMENT_BEDROCK
+from trulens_eval.utils.imports import REQUIREMENT_LITELLM
+from trulens_eval.utils.imports import REQUIREMENT_LLAMA
+from trulens_eval.utils.imports import REQUIREMENT_OPENAI
+from trulens_eval.utils.imports import REQUIREMENT_RAILS
+from trulens_eval.utils.threading import TP
+
+# Optional provider types.
+
+with OptionalImports(messages=REQUIREMENT_LITELLM):
+    from trulens_eval.feedback.provider.litellm import LiteLLM
+
+with OptionalImports(messages=REQUIREMENT_BEDROCK):
+    from trulens_eval.feedback.provider.bedrock import Bedrock
+
+with OptionalImports(messages=REQUIREMENT_OPENAI):
+    from trulens_eval.feedback.provider.openai import AzureOpenAI
+    from trulens_eval.feedback.provider.openai import OpenAI
+
+# Optional app types.
+
+with OptionalImports(messages=REQUIREMENT_LLAMA):
+    from trulens_eval.tru_llama import TruLlama
+
+with OptionalImports(messages=REQUIREMENT_RAILS):
+    from trulens_eval.tru_rails import TruRails
+
+__all__ = [
+    "Tru",  # main interface
+
+    # app types
+    "TruBasicApp",
+    "TruCustomApp",
+    "TruChain",
+    "TruLlama",
+    "TruVirtual",
+    "TruRails",
+
+    # app setup
+    "FeedbackMode",
+
+    # feedback setup
+    "Feedback",
+    "Select",
+
+    # feedback providers
+    "Provider",
+    "AzureOpenAI",
+    "OpenAI",
+    "Langchain",
+    "LiteLLM",
+    "Bedrock",
+    "Huggingface",
+
+    # misc utility
+    "TP",
+]
