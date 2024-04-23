f5b6fefcb01667e6dd6563e1a605e482e077535f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:05:13 +0000

    Add package mom-test-py/1.0.0/mom_test_py-1.0.0.tar.gz

---
+++ packages/mom-test-py/mom_test_py-1.0.0.tar.gz/mom_test_py-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mom_test_py",
+    version="1.0.0",
+    description="Helper library for working with tests",
+    packages=find_packages(),
+    install_requires=[
+        # add dependencies here.
+        # e.g. 'numpy>=1.11.1
+    ]
+)
