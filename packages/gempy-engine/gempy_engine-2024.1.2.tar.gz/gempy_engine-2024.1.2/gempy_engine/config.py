752fec8b6a174a2221bb968da5e612b8ee46e039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:57 +0000

    Add package gempy-engine/2024.1.2/gempy_engine-2024.1.2.tar.gz

---
+++ packages/gempy-engine/gempy_engine-2024.1.2.tar.gz/gempy_engine-2024.1.2/gempy_engine/config.py
@@ -0,0 +1,31 @@
+from importlib.util import find_spec
+from enum import Enum, auto, Flag
+
+
+class AvailableBackends(Flag):
+    numpy = auto()
+    tensorflow = auto()
+    PYTORCH = auto()
+    
+    # Legacy
+    aesara = auto()
+    legacy = auto()
+
+
+# ! Careful what we commit here!
+DEBUG_MODE = True
+OPTIMIZE_MEMORY = True
+DEFAULT_BACKEND = AvailableBackends.numpy
+DEFAULT_PYKEOPS = False
+DEFAULT_TENSOR_DTYPE = 'float64'
+LINE_PROFILER_ENABLED = False
+SET_RAW_ARRAYS_IN_SOLUTION = True
+NOT_MAKE_INPUT_DEEP_COPY = False 
+
+is_numpy_installed = find_spec("numpy") is not None
+is_tensorflow_installed = find_spec("tensorflow") is not None
+is_pytorch_installed = find_spec("torch")
+is_pykeops_installed = find_spec("pykeops") is not None
+
+
+
---
+++ packages/gempy-engine/gempy_engine-2024.1.2.tar.gz/gempy_engine-2024.1.2/setup.py
@@ -0,0 +1,61 @@
+﻿from os import path
+
+from setuptools import setup, find_packages
+
+
+def read_requirements(file_name):
+    requirements = []
+    with open(file_name, "r", encoding="utf-8") as f:
+        for line in f:
+            # Strip whitespace and ignore comments
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
+
+            # Handle -r directive
+            if line.startswith("-r "):
+                referenced_file = line.split()[1]  # Extract the file name
+                requirements.extend(read_requirements(referenced_file))  # Recursively read referenced file
+            else:
+                requirements.append(line)
+
+    return requirements
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gempy_engine",
+    # version=version,
+    author="Miguel de la Varga",
+    author_email="miguel@terranigma-solutions.com",
+    description="A Python package for GemPy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gempy-project/gempy_engine",
+    packages=find_packages(exclude=('test', 'docs', 'examples')),
+    license='EUPL-1.2',
+    classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.10",
+    install_requires=read_requirements("requirements/requirements.txt"),
+    extras_require={
+            "dev"   : read_requirements("requirements/dev-requirements.txt"),
+            "opt"   : read_requirements("requirements/optional-requirements.txt"),
+            "server": read_requirements("requirements/server-requirements.txt")
+    },
+    setup_requires=['setuptools_scm'],
+    use_scm_version={
+            "root"            : ".",
+            "relative_to"     : __file__,
+            "write_to"        : path.join("gempy_engine", "_version.py"),
+            "fallback_version": "3.0.0"
+    },
+)
