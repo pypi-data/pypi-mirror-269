6f8d22c97ce12a9d1a80473c0032e32fee31cab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 14:03:59 +0000

    Add package swarms/4.8.7/swarms-4.8.7.tar.gz

---
+++ packages/swarms/swarms-4.8.7.tar.gz/swarms-4.8.7/swarms/structs/schemas.py
@@ -0,0 +1,166 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Any
+
+from pydantic import BaseModel, Field
+
+
+class TaskInput(BaseModel):
+    task: Any = Field(
+        ...,
+        description=(
+            "The input parameters for the task. Any value is allowed."
+        ),
+        examples=['{\n"debug": false,\n"mode": "benchmarks"\n}'],
+    )
+
+
+class Artifact(BaseModel):
+    """
+    Represents an artifact.
+
+    Attributes:
+        artifact_id (str): Id of the artifact.
+        file_name (str): Filename of the artifact.
+        relative_path (str, optional): Relative path of the artifact in the agent's workspace.
+    """
+
+    artifact_id: str = Field(
+        ...,
+        description="Id of the artifact",
+        examples=["b225e278-8b4c-4f99-a696-8facf19f0e56"],
+    )
+    file_name: str = Field(
+        ...,
+        description="Filename of the artifact",
+        examples=["main.py"],
+    )
+    relative_path: str | None = Field(
+        None,
+        description=(
+            "Relative path of the artifact in the agent's workspace"
+        ),
+        examples=["python/code/"],
+    )
+
+
+class ArtifactUpload(BaseModel):
+    file: bytes = Field(..., description="File to upload")
+    relative_path: str | None = Field(
+        None,
+        description=(
+            "Relative path of the artifact in the agent's workspace"
+        ),
+        examples=["python/code/"],
+    )
+
+
+class StepInput(BaseModel):
+    step: Any = Field(
+        ...,
+        description=(
+            "Input parameters for the task step. Any value is" " allowed."
+        ),
+        examples=['{\n"file_to_refactor": "models.py"\n}'],
+    )
+
+
+class StepOutput(BaseModel):
+    step: Any = Field(
+        ...,
+        description=(
+            "Output that the task step has produced. Any value is"
+            " allowed."
+        ),
+        examples=['{\n"tokens": 7894,\n"estimated_cost": "0,24$"\n}'],
+    )
+
+
+class TaskRequestBody(BaseModel):
+    input: str | None = Field(
+        None,
+        description="Input prompt for the task.",
+        examples=["Write the words you receive to the file 'output.txt'."],
+    )
+    additional_input: TaskInput | None = None
+
+
+class Task(TaskRequestBody):
+    task_id: str = Field(
+        ...,
+        description="The ID of the task.",
+        examples=["50da533e-3904-4401-8a07-c49adf88b5eb"],
+    )
+    artifacts: list[Artifact] = Field(
+        [],
+        description="A list of artifacts that the task has produced.",
+        examples=[
+            [
+                "7a49f31c-f9c6-4346-a22c-e32bc5af4d8e",
+                "ab7b4091-2560-4692-a4fe-d831ea3ca7d6",
+            ]
+        ],
+    )
+
+
+class StepRequestBody(BaseModel):
+    input: str | None = Field(
+        None,
+        description="Input prompt for the step.",
+        examples=["Washington"],
+    )
+    additional_input: StepInput | None = None
+
+
+class Status(Enum):
+    created = "created"
+    running = "running"
+    completed = "completed"
+
+
+class Step(BaseModel):
+    task_id: str = Field(
+        ...,
+        description="The ID of the task this step belongs to.",
+        examples=["50da533e-3904-4401-8a07-c49adf88b5eb"],
+    )
+    step_id: int = Field(
+        ...,
+        description="The ID of the task step.",
+        examples=["6bb1801a-fd80-45e8-899a-4dd723cc602e"],
+    )
+    name: str | None = Field(
+        None,
+        description="The name of the task step.",
+        examples=["Write to file"],
+    )
+    output: str | None = Field(
+        None,
+        description="Output of the task step.",
+        examples=[
+            "I am going to use the write_to_file command and write"
+            " Washington to a file called output.txt"
+            " <write_to_file('output.txt', 'Washington')"
+        ],
+    )
+    artifacts: list[Artifact] = Field(
+        [],
+        description="A list of artifacts that the step has produced.",
+    )
+    max_loops: int = Field(
+        1,
+        description="The maximum number of times to run the workflow.",
+    )
+
+
+class ManySteps(BaseModel):
+    task_id: str = Field(
+        ...,
+        description="The ID of the task this step belongs to.",
+        examples=["50da533e-3904-4401-8a07-c49adf88b5eb"],
+    )
+    steps: list[Step] = Field(
+        [],
+        description="A list of task steps.",
+    )
---
+++ packages/swarms/swarms-4.8.7.tar.gz/swarms-4.8.7/swarms/structs/sequential_workflow.py
@@ -0,0 +1,203 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional
+
+from termcolor import colored
+
+# from swarms.utils.logger import logger
+from swarms.structs.agent import Agent
+from swarms.structs.conversation import Conversation
+from swarms.structs.task import Task
+from swarms.utils.loguru_logger import logger
+
+
+# SequentialWorkflow class definition using dataclasses
+@dataclass
+class SequentialWorkflow:
+    """
+    SequentialWorkflow class for running a sequence of task_pool using N number of autonomous agents.
+
+    Args:
+        max_loops (int): The maximum number of times to run the workflow.
+        dashboard (bool): Whether to display the dashboard for the workflow.
+
+
+    Attributes:
+        task_pool (List[Task]): The list of task_pool to execute.
+        max_loops (int): The maximum number of times to run the workflow.
+        dashboard (bool): Whether to display the dashboard for the workflow.
+
+
+    Examples:
+    >>> from swarms.models import OpenAIChat
+    >>> from swarms.structs import SequentialWorkflow
+    >>> llm = OpenAIChat(openai_api_key="")
+    >>> workflow = SequentialWorkflow(max_loops=1)
+    >>> workflow.add("What's the weather in miami", llm)
+    >>> workflow.add("Create a report on these metrics", llm)
+    >>> workflow.run()
+    >>> workflow.task_pool
+
+    """
+
+    name: str = None
+    description: str = None
+    task_pool: List[Task] = None
+    max_loops: int = 1
+    autosave: bool = False
+    saved_state_filepath: Optional[str] = "sequential_workflow_state.json"
+    restore_state_filepath: Optional[str] = None
+    dashboard: bool = False
+    agents: List[Agent] = None
+
+    def __post_init__(self):
+        self.conversation = Conversation(
+            system_prompt=f"Objective: {self.description}",
+            time_enabled=True,
+            autosave=True,
+        )
+
+        # Logging
+        logger.info("Number of agents activated:")
+        if self.agents:
+            logger.info(f"Agents: {len(self.agents)}")
+        else:
+            logger.info("No agents activated.")
+
+        if self.task_pool:
+            logger.info(f"Task Pool Size: {len(self.task_pool)}")
+        else:
+            logger.info("Task Pool is empty.")
+
+    def add(
+        self,
+        task: Optional[Task] = None,
+        tasks: Optional[List[Task]] = None,
+        *args,
+        **kwargs,
+    ) -> None:
+        """
+        Add a task to the workflow.
+
+        Args:
+            agent (Union[Callable, Agent]): The model or agent to execute the task.
+            task (str): The task description or the initial input for the Agent.
+
+            *args: Additional arguments to pass to the task execution.
+            **kwargs: Additional keyword arguments to pass to the task execution.
+        """
+        for agent in self.agents:
+            out = agent(str(self.description))
+            self.conversation.add(agent.agent_name, out)
+            prompt = self.conversation.return_history_as_string()
+            out = agent(prompt)
+
+        return out
+
+    def reset_workflow(self) -> None:
+        """Resets the workflow by clearing the results of each task."""
+        try:
+            for task in self.task_pool:
+                task.result = None
+                logger.info(
+                    f"[INFO][SequentialWorkflow] Reset task {task} in"
+                    " workflow"
+                )
+        except Exception as error:
+            logger.error(
+                colored(f"Error resetting workflow: {error}", "red"),
+            )
+
+    def get_task_results(self) -> Dict[str, Any]:
+        """
+        Returns the results of each task in the workflow.
+
+        Returns:
+            Dict[str, Any]: The results of each task in the workflow
+        """
+        try:
+            return {
+                task.description: task.result for task in self.task_pool
+            }
+        except Exception as error:
+            logger.error(
+                colored(f"Error getting task results: {error}", "red"),
+            )
+
+    def remove_task(self, task: Task) -> None:
+        """Remove task_pool from sequential workflow"""
+        try:
+            self.task_pool.remove(task)
+            logger.info(
+                f"[INFO][SequentialWorkflow] Removed task {task} from"
+                " workflow"
+            )
+        except Exception as error:
+            logger.error(
+                colored(
+                    f"Error removing task from workflow: {error}",
+                    "red",
+                ),
+            )
+
+    def run(self) -> None:
+        """
+        Run the workflow.
+
+        Raises:
+            ValueError: If an Agent instance is used as a task and the 'task' argument is not provided.
+
+        """
+        self.workflow_bootup()
+        loops = 0
+        while loops < self.max_loops:
+            for i, agent in enumerate(self.agents):
+                logger.info(f"Agent {i+1} is executing the task.")
+                out = agent(self.description)
+                self.conversation.add(agent.agent_name, str(out))
+                prompt = self.conversation.return_history_as_string()
+                print(prompt)
+                print("Next agent...........")
+                out = agent(prompt)
+
+            return out
+        # try:
+        #     self.workflow_bootup()
+        #     loops = 0
+        #     while loops < self.max_loops:
+        #         for i in range(len(self.task_pool)):
+        #             task = self.task_pool[i]
+        #             # Check if the current task can be executed
+        #             if task.result is None:
+        #                 # Get the inputs for the current task
+        #                 task.context(task)
+
+        #                 result = task.execute()
+
+        #                 # Pass the inputs to the next task
+        #                 if i < len(self.task_pool) - 1:
+        #                     next_task = self.task_pool[i + 1]
+        #                     next_task.description = result
+
+        #                 # Execute the current task
+        #                 task.execute()
+
+        #                 # Autosave the workflow state
+        #                 if self.autosave:
+        #                     self.save_workflow_state(
+        #                         "sequential_workflow_state.json"
+        #                     )
+
+        #         self.workflow_shutdown()
+        #         loops += 1
+        # except Exception as e:
+        #     logger.error(
+        #         colored(
+        #             (
+        #                 "Error initializing the Sequential workflow:"
+        #                 f" {e} try optimizing your inputs like the"
+        #                 " agent class and task description"
+        #             ),
+        #             "red",
+        #             attrs=["bold", "underline"],
+        #         )
+        #     )
