70a387ceef1888c0083f86f2be913808214dc900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 06:04:40 +0000

    Add package lint4jira/0.1/lint4jira-0.1.tar.gz

---
+++ packages/lint4jira/lint4jira-0.1.tar.gz/lint4jira-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lint4jira',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'jira',
+        'requests',
+        'argparse',
+        'curses'
+    ],
+    entry_points={
+        'console_scripts': [
+            'lint4jira=lint4jira.lint4jira:main',
+        ],
+    },
+    author='Alameddin Çelik',
+    author_email='alameddinc@gmail.com',
+    description='lint4jira is a calculator for Jira tasks or sprints health. ' \
+                'It calculates the health of the tasks in a sprint or a single task in Jira.',
+    license='MIT',
+    keywords='jira health sprint task',
+    url='https://github.com/alameddinc/lint4jira',
+
+)
