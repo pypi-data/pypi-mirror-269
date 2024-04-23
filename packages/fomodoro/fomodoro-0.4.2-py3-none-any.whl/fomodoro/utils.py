6951f5f97df980b28b4499def69856c2ffa6dbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:57 +0000

    Add package fomodoro/0.4.2/fomodoro-0.4.2-py3-none-any.whl

---
+++ packages/fomodoro/fomodoro-0.4.2-py3-none-any.whl/fomodoro/utils.py
@@ -0,0 +1,19 @@
+"""Utilities."""
+from enum import Enum
+from os.path import dirname, abspath, join
+
+
+TIME_FORMAT = '%H:%M:%S'
+
+INFO_FILE = join(dirname(abspath(__file__)), 'info.json')
+
+BELL_SOUND_FILE_NAME = "bell.mp3"
+
+BELL_SOUND_FILE = join(dirname(abspath(__file__)), BELL_SOUND_FILE_NAME)
+
+class States(Enum):
+    """States of the stopwatch and the timer."""
+    WITHOUT_START: str = 'Without start'
+    START: str = 'Start'
+    PAUSE: str = 'Pause'
+    STOP: str = 'Stop'
