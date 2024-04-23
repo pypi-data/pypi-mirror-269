6c516455ef76cedd0efb6a37a8c15a96a684193c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 14:04:12 +0000

    Add package pyavaccess/0.2.0/pyavaccess-0.2.0-py3-none-any.whl

---
+++ packages/pyavaccess/pyavaccess-0.2.0-py3-none-any.whl/pyavaccess/config/matrix_devices.py
@@ -0,0 +1,26 @@
+import re
+
+# Define regular expressions for common response patterns
+PATTERN_ALL = re.compile(r".* in(\d+) all")
+PATTERN_OUT = re.compile(r".* in(\d+) out(\d+)")
+
+# Define configuration for matrix devices
+MatrixDevices = {
+    # Pick device config by version
+    "VER 1.0.2": {
+        "4KMX42-H2A": {
+            "inputCount": 4,
+            "outputCount": 2,
+            "audioOutputs": [
+                "hdmiaudioout1",
+                "hdmiaudioout2",
+                "audioout1",
+                "spdifaudioout2",
+            ],
+            "EDIDParamCount": 11,
+            "maxDelayInMin": 30,
+            "irModeCount": 2,
+            "commandCount": 24,
+        }
+    }
+}
