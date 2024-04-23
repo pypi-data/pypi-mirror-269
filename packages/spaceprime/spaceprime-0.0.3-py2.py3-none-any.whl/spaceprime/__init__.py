1ea3a2af03d2588efc882775eb76dc6e976f5154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 06:03:43 +0000

    Add package spaceprime/0.0.3/spaceprime-0.0.3-py2.py3-none-any.whl

---
+++ packages/spaceprime/spaceprime-0.0.3-py2.py3-none-any.whl/spaceprime/__init__.py
@@ -0,0 +1,27 @@
+"""
+spaceprime is a python package to facilitate spatially explicit coalescent modeling in msprime.
+Please see the documentation at https://connor-french.github.io/spaceprime
+for more information.
+"""
+
+__author__ = """Connor French"""
+__email__ = "french.connor.m@gmail.com"
+__version__ = "0.0.3"
+
+
+from spaceprime.utilities import (
+    create_raster,
+    raster_to_demes,
+    calc_migration_matrix,
+    split_landscape_by_pop,
+    max_thresh_from_coords,
+    coords_to_sample_dict,
+    anc_to_deme_dict,
+    sampled_cells_to_coords,
+)
+
+from spaceprime.demography import (
+    stepping_stone_2d,
+    add_landscape_change,
+    add_ancestral_populations,
+)
