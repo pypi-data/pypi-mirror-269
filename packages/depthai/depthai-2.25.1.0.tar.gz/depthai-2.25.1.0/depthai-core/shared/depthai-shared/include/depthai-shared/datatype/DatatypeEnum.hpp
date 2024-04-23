2885c32e2a13076b6215d6bd452a285640686148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:46 +0000

    Add package depthai/2.25.1.0/depthai-2.25.1.0.tar.gz

---
+++ packages/depthai/depthai-2.25.1.0.tar.gz/depthai-2.25.1.0/depthai-core/shared/depthai-shared/include/depthai-shared/datatype/DatatypeEnum.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdint>
+
+namespace dai {
+
+enum class DatatypeEnum : std::int32_t {
+    Buffer,
+    ImgFrame,
+    EncodedFrame,
+    NNData,
+    ImageManipConfig,
+    CameraControl,
+    ImgDetections,
+    SpatialImgDetections,
+    SystemInformation,
+    SpatialLocationCalculatorConfig,
+    SpatialLocationCalculatorData,
+    EdgeDetectorConfig,
+    AprilTagConfig,
+    AprilTags,
+    Tracklets,
+    IMUData,
+    StereoDepthConfig,
+    FeatureTrackerConfig,
+    ToFConfig,
+    PointCloudConfig,
+    PointCloudData,
+    TrackedFeatures,
+    MessageGroup,
+};
+bool isDatatypeSubclassOf(DatatypeEnum parent, DatatypeEnum children);
+
+}  // namespace dai
