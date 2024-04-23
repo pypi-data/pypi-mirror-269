c3a10392c6a9311ad58bdd53c599105ae725acca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:51 +0000

    Add package secretvalidator/0.1.1/secretvalidator-0.1.1.tar.gz

---
+++ packages/secretvalidator/secretvalidator-0.1.1.tar.gz/secretvalidator-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='secretvalidator',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        secretvalidator=secretvalidator.cli:validate
+    ''',
+)
