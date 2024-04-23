e544b0546d23e16e1a78cf727ae88fbec8179001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:59 +0000

    Add package danila-lib/1.3.2/danila_lib-1.3.2-py3-none-any.whl

---
+++ packages/danila-lib/danila_lib-1.3.2-py3-none-any.whl/data/neuro/models.py
@@ -0,0 +1,3 @@
+RAMA_CLASSIFY_MODEL_ADDRESS = 'https://disk.yandex.ru/d/4xtNt0zftHFFVg'
+RAMA_NO_SPRING_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/sE5iSXYWL3xEkQ'
+RAMA_SPRING_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/zAEw0_ALgtBg9Q'
