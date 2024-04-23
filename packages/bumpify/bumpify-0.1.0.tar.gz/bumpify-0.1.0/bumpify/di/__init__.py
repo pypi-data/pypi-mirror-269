3bb65d547e0bbf4a2ef51bb9697f17161f49b815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:03:46 +0000

    Add package bumpify/0.1.0/bumpify-0.1.0.tar.gz

---
+++ packages/bumpify/bumpify-0.1.0.tar.gz/bumpify-0.1.0/bumpify/di/__init__.py
@@ -0,0 +1,20 @@
+from pydio.api import Provider
+
+from bumpify.context import Context
+
+from . import api, config, console, filesystem, semver, vcs
+
+provider = Provider()
+provider.attach(api.provider)
+provider.attach(config.provider)
+provider.attach(semver.provider)
+provider.attach(filesystem.provider)
+provider.attach(vcs.provider)
+provider.attach(console.provider)
+
+
+@provider.provides(Context)
+def make_context():
+    # NOTE: Context is created by injector to ensure that all providers will
+    # use same instance of it. It must be initialized shortly after creation.
+    return Context()
---
+++ packages/bumpify/bumpify-0.1.0.tar.gz/bumpify-0.1.0/bumpify/di/semver.py
@@ -0,0 +1,26 @@
+from pydio.api import Provider
+
+from bumpify import utils
+from bumpify.core.config.objects import LoadedConfig, LoadedModuleConfig
+from bumpify.core.filesystem.interface import IFileSystemReaderWriter
+from bumpify.core.semver.implementation import SemVerApi
+from bumpify.core.semver.interface import ISemVerApi
+from bumpify.core.semver.objects import SemVerConfig
+from bumpify.core.vcs.interface import IVcsReaderWriter
+
+provider = Provider()
+
+
+@provider.provides(ISemVerApi)
+def make_semver_api(injector):
+    semver_config = utils.inject_type(injector, LoadedModuleConfig[SemVerConfig])
+    filesystem_reader_writer = utils.inject_type(injector, IFileSystemReaderWriter)
+    vcs_reader_writer = utils.inject_type(injector, IVcsReaderWriter)
+    return SemVerApi(semver_config, filesystem_reader_writer, vcs_reader_writer)
+
+
+@provider.provides(LoadedModuleConfig[SemVerConfig])
+def make_loaded_semver_config(injector):
+    loaded_config = utils.inject_type(injector, LoadedConfig)
+    loaded_semver_config = loaded_config.require_module_config(SemVerConfig)
+    return loaded_semver_config
