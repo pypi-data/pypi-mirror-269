100cdc116cb5f2edad097fb933a84e6ff8c273c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 05:04:48 +0000

    Add package conciergelib-aiohttp/0.0.1/conciergelib_aiohttp-0.0.1.tar.gz

---
+++ packages/conciergelib-aiohttp/conciergelib_aiohttp-0.0.1.tar.gz/conciergelib_aiohttp-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="conciergelib-aiohttp",
+    version="0.0.1",
+    description="conciergelib-aiohttp package lock",
+    packages=["conciergelib-aiohttp"],
+)
