5c2d5ec18f077c440219b9f6a94db88f0132321a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:04:24 +0000

    Add package Flask-PikaRMQ/1.0.1/flask_pikarmq-1.0.1.tar.gz

---
+++ packages/Flask-PikaRMQ/flask_pikarmq-1.0.1.tar.gz/flask_pikarmq-1.0.1/setup.py
@@ -0,0 +1,29 @@
+"""
+Flask-PikaMQ
+-------------
+
+ADD RABBITMQ_URL TO FLASK CONFIG
+"""
+
+from setuptools import setup
+
+
+setup(
+    name="Flask-PikaRMQ",
+    version="1.0.1",
+    url="",
+    license="BSD",
+    author="Rasco Developers",
+    author_email="rezahartono.rasco@gmail.com",
+    description="This is Extension for RabbitMQ Microservice",
+    long_description=__doc__,
+    py_modules=["flask_pikarmq"],
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    # packages=['flask_sqlite3'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=["Flask", "pika"],
+    classifiers=[],
+)
