5ab9ebb83dcfbbedd350e7711d32f5ca9b15d672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:05:08 +0000

    Add package CoreStart/0.1.4/CoreStart-0.1.4.tar.gz

---
+++ packages/CoreStart/CoreStart-0.1.4.tar.gz/CoreStart-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CoreStart',
+    version='0.1.4',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    url='https://github.com/data-jeong/CoreStart',
+    entry_points={
+        'console_scripts': [
+            'core_start=corestart.core_start:run',
+        ]
+    },
+    install_requires=[
+        # 여기에 필요한 의존성을 추가
+    ],
+    python_requires='>=3.6',
+)
+
