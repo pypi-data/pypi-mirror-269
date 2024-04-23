df606463ef2b8e72b0cd0ca5b04f1af07949b29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 16:05:14 +0000

    Add package forta-bot-sdk/0.2.3/forta_bot_sdk-0.2.3.tar.gz

---
+++ packages/forta-bot-sdk/forta_bot_sdk-0.2.3.tar.gz/forta_bot_sdk-0.2.3/src/forta_bot_sdk/cli/run_transaction.py
@@ -0,0 +1,20 @@
+from typing import Callable
+from web3 import AsyncWeb3
+from ..common import ScanEvmOptions
+from ..handlers import RunHandlersOnTransaction
+
+RunTransaction = Callable[[str, ScanEvmOptions, AsyncWeb3, int], None]
+
+
+def provide_run_transaction(run_handlers_on_transaction: RunHandlersOnTransaction):
+
+    async def run_transaction(tx_hash: str, options: ScanEvmOptions, provider: AsyncWeb3, chain_id: int) -> None:
+        tx_hashes = [tx_hash]
+        # support for specifying multiple transactions with comma-delimited list
+        if tx_hash.find(",") >= 0:
+            tx_hashes = tx_hash.split(",")
+
+        for hash in tx_hashes:
+            await run_handlers_on_transaction(hash, options, provider, chain_id)
+
+    return run_transaction
