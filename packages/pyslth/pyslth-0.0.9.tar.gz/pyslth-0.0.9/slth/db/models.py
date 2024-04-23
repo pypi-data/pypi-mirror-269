6769636b11d38945786c1432b433412d7fe92660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 04:05:56 +0000

    Add package pyslth/0.0.9/pyslth-0.0.9.tar.gz

---
+++ packages/pyslth/pyslth-0.0.9.tar.gz/pyslth-0.0.9/slth/db/models.py
@@ -0,0 +1,101 @@
+from django.db.models import Model as DjangoModel
+from django.db.models import *
+from django.utils.translation import gettext_lazy as _
+from .. import ModelMixin
+
+class CharField(CharField):
+    def __init__(self, *args, **kwargs):
+        self.mask = kwargs.pop('mask', None)
+        kwargs.setdefault('max_length', 255)
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, *args, **kwargs):
+        field = super().formfield(*args, **kwargs)
+        field.mask = self.mask
+        return field
+    
+class IntegerField(IntegerField):
+    def __init__(self, *args, **kwargs):
+        self.mask = kwargs.pop('mask', None)
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, *args, **kwargs):
+        field = super().formfield(*args, **kwargs)
+        field.mask = self.mask
+        return field
+
+class ForeignKey(ForeignKey):
+    def __init__(self, to, on_delete=None, **kwargs):
+        self.pick = kwargs.pop('pick', False)
+        self.addable = kwargs.pop('addable', False)
+        super().__init__(to, on_delete or CASCADE, **kwargs)
+    
+    def formfield(self, *args, **kwargs):
+        field = super().formfield(*args, **kwargs)
+        return field
+
+class ManyToManyField(ManyToManyField):
+    def __init__(self, *args, **kwargs):
+        self.pick = kwargs.pop('pick', False)
+        self.addable = kwargs.pop('addable', False)
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, *args, **kwargs):
+        field = super().formfield(*args, **kwargs)
+        return field
+
+class OneToManyField(ManyToManyField):
+    def __init__(self, *args, **kwargs):
+        self.fields = kwargs.pop('fields', '__all__')
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, *args, **kwargs):
+        from .. import forms
+        kwargs.update(form_class=forms.OneToManyField, fields=self.fields)
+        field = super().formfield(*args, model=self.related_model, **kwargs)
+        field.label = self.verbose_name
+        return field
+
+
+class OneToOneField(OneToOneField):
+    def __init__(self, *args, **kwargs):
+        self.fields = kwargs.pop('fields', '__all__')
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, *args, **kwargs):
+        from .. import forms
+        kwargs.update(form_class=forms.OneToOneField, fields=self.fields)
+        field = super().formfield(*args, model=self.related_model, **kwargs)
+        field.label = self.verbose_name
+        field.required2 = not self.blank
+        return field
+
+
+class DecimalField(DecimalField):
+    def __init__(self, *args, **kwargs):
+        kwargs['decimal_places'] = kwargs.pop('decimal_places', 2)
+        kwargs['max_digits'] = kwargs.pop('max_digits', 9)
+        super().__init__(*args, **kwargs)
+
+    def formfield(self, *args, **kwargs):
+        from .. import forms
+        kwargs.update(form_class=forms.DecimalField)
+        return super().formfield(*args, **kwargs)
+    
+class ColorField(CharField):
+
+    def formfield(self, *args, **kwargs):
+        from .. import forms
+        kwargs.update(form_class=forms.ColorField)
+        return super().formfield(*args, **kwargs)
+
+
+class TextField(TextField):
+    def __init__(self, *args, **kwargs):
+        self.formatted= kwargs.pop('formatted', False)
+        super().__init__(*args, **kwargs)
+
+
+class Model(DjangoModel, ModelMixin):
+    class Meta:
+        abstract = True
---
+++ packages/pyslth/pyslth-0.0.9.tar.gz/pyslth-0.0.9/slth/utils.py
@@ -0,0 +1,42 @@
+
+
+def build_url(request, path=None):
+    url = ''
+    if path and path.startswith('http'):
+        url = path
+    elif request:
+        port = request.META.get('HTTP_X_FORWARDED_PORT', request.get_port())
+        port = '' if port in (80, 443) else f':{port}'
+        url = "{}://{}{}{}".format(
+            request.META.get('HTTP_X_FORWARDED_PROTO', request.scheme),
+            request.get_host().split(':')[0], port, path or request.path
+        )
+    return url
+
+def absolute_url(request, *querystrings):
+    url = build_url(request)
+    if url:
+        querstring = request.META.get('QUERY_STRING')
+        url = append_url(url, querstring)
+        for querystring in querystrings:
+            url = append_url(url, querystring)
+    return url
+
+def append_url(url, *querystrings):
+    params = {}
+    if '?' in url:
+        url, querystring = url.split('?')
+        if querystring:
+            for param in querystring.split('&'):
+                k, v = param.split('=')
+                params[k] = v
+    else:
+        url, querystring = url, ''
+    for querystring in querystrings:
+        if querystring:
+            for param in querystring.split('&'):
+                k, v = param.split('=')
+                params[k] = v
+    if params:
+        url = '{}?{}'.format(url, '&'.join([f'{k}={v}' for k, v in params.items()]))
+    return url
