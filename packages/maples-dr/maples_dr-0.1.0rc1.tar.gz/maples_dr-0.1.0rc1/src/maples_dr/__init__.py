cfa723e3ecf4ba4f6f86bc8707b0d267802efce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 07:03:28 +0000

    Add package maples-dr/0.1.0rc1/maples_dr-0.1.0rc1.tar.gz

---
+++ packages/maples-dr/maples_dr-0.1.0rc1.tar.gz/maples_dr-0.1.0rc1/src/maples_dr/__init__.py
@@ -0,0 +1,16 @@
+__all__ = ["configure", "Dataset", "export_test_set", "export_train_set", "load_test_set", "load_train_set"]
+
+import os
+
+if os.environ.get("MAPLES-DR_SILENT_IMPORT", None) is None:
+    from rich.console import Console
+
+    console = Console()
+    console.print(
+        "[b]Thanks for using MAPLES-DR![/b]\n"
+        "  When using this dataset in academic works,\n"
+        "  please cite: [u cyan link=https://arxiv.org/abs/2402.04258]https://arxiv.org/abs/2402.04258[/u cyan link]"
+    )
+
+from .dataset import DataSample, Dataset
+from .quick_api import clear_cache, configure, export_test_set, export_train_set, load_test_set, load_train_set
