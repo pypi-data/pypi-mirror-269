81a3de8284895e1bd377af53d4544d9a5f783972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:05:03 +0000

    Add package hermesbaby/0.1.0/hermesbaby-0.1.0.tar.gz

---
+++ packages/hermesbaby/hermesbaby-0.1.0.tar.gz/hermesbaby-0.1.0/src/hermesbaby/cmd/hb.py
@@ -0,0 +1,7 @@
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    # Define other arguments here
+    args = parser.parse_args()
+    print("Welcome to HermesBaby command line tool!")
+    # Your command line tool logic here
