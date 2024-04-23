468313f44fd5249cf9e79c389725f2332f87cd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 15:04:21 +0000

    Add package kytest/0.0.48/kytest-0.0.48-py3-none-any.whl

---
+++ packages/kytest/kytest-0.0.48-py3-none-any.whl/kytest/ios/__init__.py
@@ -0,0 +1,13 @@
+from .element import IosElem as Elem
+from .driver import IosDriver, connected
+from .case import TestCase
+from .page import Page
+
+__all__ = [
+    "Elem",
+    "IosDriver",
+    "connected",
+    "TestCase",
+    "Page"
+]
+
