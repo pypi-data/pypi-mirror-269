fc58d3bc8528c0d08babd1550bc8c118e037853f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 03:04:06 +0000

    Add package sphinx-doxysummary/2.3.3/sphinx-doxysummary-2.3.3.tar.gz

---
+++ packages/sphinx-doxysummary/sphinx-doxysummary-2.3.3.tar.gz/sphinx-doxysummary-2.3.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 17 13:50:43 2022
+
+@author: dn266595
+
+Future implementation: set_verbosity for debugging, mute auto generate and
+allowing set alias as page title
+"""
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup()
