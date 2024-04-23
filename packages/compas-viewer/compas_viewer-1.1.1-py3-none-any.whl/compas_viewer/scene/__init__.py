55116afbf877548335db8488ce5e2ac2e06a5aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-23 03:04:05 +0000

    Add package compas-viewer/1.1.1/compas_viewer-1.1.1-py3-none-any.whl

---
+++ packages/compas-viewer/compas_viewer-1.1.1-py3-none-any.whl/compas_viewer/scene/__init__.py
@@ -0,0 +1,136 @@
+"""
+This package provides scene object plugins for visualizing COMPAS objects in `compas_viewer`.
+"""
+
+from typing import Union
+from compas.scene import register
+from compas.plugins import plugin
+from compas.datastructures import Mesh
+from compas.datastructures import Graph
+from compas.geometry import (
+    Point,
+    Line,
+    Vector,
+    Circle,
+    Box,
+    Polyline,
+    Torus,
+    Polygon,
+    Sphere,
+    Plane,
+    Cylinder,
+    Ellipse,
+    Cone,
+    Capsule,
+    Frame,
+    NurbsSurface,
+    Geometry,
+)
+
+from .sceneobject import ViewerSceneObject
+from .meshobject import MeshObject
+from .graphobject import GraphObject
+from .pointobject import PointObject
+from .lineobject import LineObject
+from .vectorobject import VectorObject
+from .tagobject import TagObject, Tag
+from .frameobject import FrameObject
+from .circleobject import CircleObject
+from .boxobject import BoxObject
+from .torusobject import TorusObject
+from .polylineobject import PolylineObject
+from .polygonobject import PolygonObject
+from .sphereobject import SphereObject
+from .planeobject import PlaneObject
+from .cylinderobject import CylinderObject
+from .ellipseobject import EllipseObject
+from .coneobject import ConeObject
+from .capsuleobject import CapsuleObject
+from .nurbssurfaceobject import NurbsSurfaceObject
+from .collectionobject import CollectionObject
+from .geometryobject import GeometryObject
+
+
+@plugin(category="drawing-utils", requires=["compas_viewer"])
+def clear(guids: list[str]):
+    for obj in guids:
+        del obj
+
+
+@plugin(category="drawing-utils", requires=["compas_viewer"])
+def redraw():
+    pass
+
+
+@plugin(category="factories", requires=["compas_viewer"])
+def register_scene_objects():
+    register(Mesh, MeshObject, context="Viewer")
+    register(Graph, GraphObject, context="Viewer")
+    register(Point, PointObject, context="Viewer")
+    register(Line, LineObject, context="Viewer")
+    register(Tag, TagObject, context="Viewer")
+    register(Frame, FrameObject, context="Viewer")
+    register(Vector, VectorObject, context="Viewer")
+    register(Circle, CircleObject, context="Viewer")
+    register(Box, BoxObject, context="Viewer")
+    register(Polyline, PolylineObject, context="Viewer")
+    register(Torus, TorusObject, context="Viewer")
+    register(Polygon, PolygonObject, context="Viewer")
+    register(Sphere, SphereObject, context="Viewer")
+    register(Plane, PlaneObject, context="Viewer")
+    register(Cylinder, CylinderObject, context="Viewer")
+    register(Ellipse, EllipseObject, context="Viewer")
+    register(Cone, ConeObject, context="Viewer")
+    register(Capsule, CapsuleObject, context="Viewer")
+    register(NurbsSurface, NurbsSurfaceObject, context="Viewer")
+    register(list[Union[Geometry, Mesh]], CollectionObject, context="Viewer")
+
+    try:
+        from compas_occ.brep import OCCBrep
+        from .brepobject import BRepObject
+
+        register(OCCBrep, BRepObject, context="Viewer")
+    except ImportError:
+        pass
+
+
+__all__ = [
+    "ViewerSceneObject",
+    "Mesh",
+    "MeshObject",
+    "Point",
+    "PointObject",
+    "Line",
+    "LineObject",
+    "Tag",
+    "TagObject",
+    "Frame",
+    "FrameObject",
+    "Vector",
+    "VectorObject",
+    "Circle",
+    "CircleObject",
+    "Polyline",
+    "PolylineObject",
+    "Box",
+    "BoxObject",
+    "Torus",
+    "TorusObject",
+    "Polygon",
+    "PolygonObject",
+    "Sphere",
+    "SphereObject",
+    "Plane",
+    "PlaneObject",
+    "Cylinder",
+    "CylinderObject",
+    "Ellipse",
+    "EllipseObject",
+    "Cone",
+    "ConeObject",
+    "Capsule",
+    "CapsuleObject",
+    "NurbsSurface",
+    "NurbsSurfaceObject",
+    "GeometryObject",
+]
---
+++ packages/compas-viewer/compas_viewer-1.1.1-py3-none-any.whl/compas_viewer/scene/collectionobject.py
@@ -0,0 +1,111 @@
+from typing import Union
+
+import numpy as np
+from numpy import array
+
+from compas.data import Data
+from compas.datastructures import Mesh
+from compas.geometry import Geometry
+from compas.scene import GeometryObject
+
+from .sceneobject import ShaderDataType
+from .sceneobject import ViewerSceneObject
+
+
+class Collection(Data):
+    """Viewer scene object for displaying a collection of COMPAS geometries."""
+
+    def __init__(self, items: list[Union[Geometry, Mesh]], **kwargs):
+        super().__init__(**kwargs)
+        self.items = items
+
+    @property
+    def __data__(self):
+        return self.items
+
+
+class CollectionObject(ViewerSceneObject, GeometryObject):
+    """Viewer scene object for displaying a collection of COMPAS geometries."""
+
+    def __init__(self, items: list[Union[Geometry, Mesh]], **kwargs):
+        self.collection = Collection(items)
+        super().__init__(geometry=self.collection, **kwargs)
+        self.objects = [ViewerSceneObject(item=item, **kwargs) for item in self.collection.items]
+
+    def _read_points_data(self) -> ShaderDataType:
+        positions = []
+        colors = []
+        elements = []
+        count = 0
+        for obj in self.objects:
+            p, c, e = obj._read_points_data() or ([], [], [])
+            positions += p
+            colors += c
+            elements += (array(e) + count).tolist()
+            count += len(p)
+        return positions, colors, elements
+
+    def _read_lines_data(self) -> ShaderDataType:
+        positions = []
+        colors = []
+        elements = []
+        count = 0
+        for obj in self.objects:
+            p, c, e = obj._read_lines_data() or ([], [], [])
+            positions += p
+            colors += c
+            elements += (array(e) + count).tolist()
+            count += len(p)
+        return positions, colors, elements
+
+    def _read_frontfaces_data(self) -> ShaderDataType:
+        positions = []
+        colors = []
+        opacities = []
+        elements = []
+        count = 0
+        for obj in self.objects:
+            if obj.use_rgba:
+                p, c, o, e = obj._read_frontfaces_data() or ([], [], [])
+                positions += p
+                colors += c
+                opacities += o
+                elements += (np.array(e) + count).tolist()
+                count += len(p)
+            else:
+                p, c, e = obj._read_frontfaces_data() or ([], [], [])
+                positions += p
+                colors += c
+                elements += (np.array(e) + count).tolist()
+                count += len(p)
+
+        if opacities:
+            return positions, colors, opacities, elements
+        else:
+            return positions, colors, elements
+
+    def _read_backfaces_data(self) -> ShaderDataType:
+        positions = []
+        colors = []
+        opacities = []
+        elements = []
+        count = 0
+        for obj in self.objects:
+            if obj.use_rgba:
+                p, c, o, e = obj._read_backfaces_data() or ([], [], [])
+                positions += p
+                colors += c
+                opacities += o
+                elements += (np.array(e) + count).tolist()
+                count += len(p)
+            else:
+                p, c, e = obj._read_backfaces_data() or ([], [], [])
+                positions += p
+                colors += c
+                elements += (np.array(e) + count).tolist()
+                count += len(p)
+
+        if opacities:
+            return positions, colors, opacities, elements
+        else:
+            return positions, colors, elements
