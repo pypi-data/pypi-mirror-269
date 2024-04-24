d9245c5ea9822916681e9da2b6a82c69d72baeef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:24 +0000

    Add package gridworks-protocol/0.7.4/gridworks_protocol-0.7.4-py3-none-any.whl

---
+++ packages/gridworks-protocol/gridworks_protocol-0.7.4-py3-none-any.whl/gwproto/data_classes/telemetry_tuple.py
@@ -0,0 +1,13 @@
+from typing import NamedTuple
+
+from gwproto.data_classes.sh_node import ShNode
+from gwproto.enums import TelemetryName
+
+
+class TelemetryTuple(NamedTuple):
+    AboutNode: ShNode
+    SensorNode: ShNode
+    TelemetryName: TelemetryName
+
+    def __repr__(self):
+        return f"TT({self.AboutNode.alias} {self.TelemetryName.value} read by {self.SensorNode.alias})"
