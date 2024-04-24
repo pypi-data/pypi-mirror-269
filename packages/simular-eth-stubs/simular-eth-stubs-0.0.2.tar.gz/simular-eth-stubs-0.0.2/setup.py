1276d97f8730243278b955a779b2a8d07643a604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:11 +0000

    Add package simular-eth-stubs/0.0.2/simular-eth-stubs-0.0.2.tar.gz

---
+++ packages/simular-eth-stubs/simular-eth-stubs-0.0.2.tar.gz/simular-eth-stubs-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+PKG_NAME = "simular-eth-stubs"
+VERSION = "0.0.2"
+setup(
+    name=PKG_NAME,
+    version=VERSION,
+    packages=['simular-stubs'],
+    description="Type stubs for simular-eth",
+    url=f"https://github.com/james4ever0/{PKG_NAME}",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    # install_requires=REQUIREMENTS,
+    install_requires = open('requirements.txt').read().splitlines(),
+)
