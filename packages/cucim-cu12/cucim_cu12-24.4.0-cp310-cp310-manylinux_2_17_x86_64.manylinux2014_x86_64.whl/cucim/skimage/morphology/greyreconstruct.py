78e4e27fc82f77822d0fbf74afee3e5dc547f1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 06:04:58 +0000

    Add package cucim-cu12/24.4.0/cucim_cu12-24.4.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cucim-cu12/cucim_cu12-24.4.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cucim/skimage/morphology/greyreconstruct.py
@@ -0,0 +1,10 @@
+import warnings
+
+from .grayreconstruct import reconstruction  # noqa
+
+warnings.warn(
+    "Importing from cucim.skimage.morphology.greyreconstruct is deprecated. "
+    "Please import from cucim.skimage.morphology instead.",
+    FutureWarning,
+    stacklevel=2,
+)
