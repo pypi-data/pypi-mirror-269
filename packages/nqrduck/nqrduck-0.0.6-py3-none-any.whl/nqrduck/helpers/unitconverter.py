d3aecc4c43d68687624c3a24c33e11e7504bff79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:47 +0000

    Add package nqrduck/0.0.6/nqrduck-0.0.6-py3-none-any.whl

---
+++ packages/nqrduck/nqrduck-0.0.6-py3-none-any.whl/nqrduck/helpers/unitconverter.py
@@ -0,0 +1,29 @@
+"""Helper used for unit conversion."""
+import decimal
+class UnitConverter:
+    """This class provides methods for unit conversion."""
+
+    @classmethod
+    def to_decimal(cls, value : str) -> decimal.Decimal:
+        """This method checks if the last character of the string is a suffix.
+
+        The available suffixes are:
+        - n for nano
+        - u for micro
+        - m for milli
+        
+        Args:
+            value (str): The value to be converted
+
+        Returns:
+            decimal.Decimal: The converted value
+        """
+        if value[-1] == "n":
+            return decimal.Decimal(value[:-1]) * decimal.Decimal("1e-9")
+        elif value[-1] == "u":
+            return decimal.Decimal(value[:-1]) * decimal.Decimal("1e-6")
+        elif value[-1] == "m":
+            return decimal.Decimal(value[:-1]) * decimal.Decimal("1e-3")
+        else:
+            return decimal.Decimal(value)
+
