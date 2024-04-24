5a407effc9a24f8dc761d2c7378fb4ce2971bdde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 14:04:56 +0000

    Add package pdbgui/0.3.0/pdbgui-0.3.0.tar.gz

---
+++ packages/pdbgui/pdbgui-0.3.0.tar.gz/pdbgui-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='pdbgui',
+version='0.3.0',
+author='Aarthi',
+author_email='nunnaaarthi@gmail.com',
+description='This package is created to understand package creation in python',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
