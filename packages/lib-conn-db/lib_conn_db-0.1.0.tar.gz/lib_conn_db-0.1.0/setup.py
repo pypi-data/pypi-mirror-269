36e319ab1eb0f30f664f291bd46c5ae5485b34c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:18 +0000

    Add package lib-conn-db/0.1.0/lib_conn_db-0.1.0.tar.gz

---
+++ packages/lib-conn-db/lib_conn_db-0.1.0.tar.gz/lib_conn_db-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='lib_conn_db',
+    version='0.1.0',
+    author='Kogui',
+    description='Uma lib de conexão de testt',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lucasaarrudaa/lib-repo-test',  
+    packages=find_packages(),
+    install_requires=[
+        'pyodbc',
+    ],
+    python_requires='>=3.6',  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
