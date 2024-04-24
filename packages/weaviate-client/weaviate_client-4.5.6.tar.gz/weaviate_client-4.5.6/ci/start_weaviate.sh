21ebe34894c943e12722887bf09fccccc69e76bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:10 +0000

    Add package weaviate-client/4.5.6/weaviate_client-4.5.6.tar.gz

---
+++ packages/weaviate-client/weaviate_client-4.5.6.tar.gz/weaviate_client-4.5.6/ci/start_weaviate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+export WEAVIATE_VERSION=$1
+
+echo "Run Docker compose"
+nohup docker compose -f ci/docker-compose.yml up -d
+nohup docker compose -f ci/docker-compose-async.yml up -d
+nohup docker compose -f ci/docker-compose-azure.yml up -d
+nohup docker compose -f ci/docker-compose-okta-cc.yml up -d
+nohup docker compose -f ci/docker-compose-okta-users.yml up -d
+nohup docker compose -f ci/docker-compose-wcs.yml up -d
+nohup docker compose -f ci/docker-compose-generative.yml up -d
+nohup docker compose -f ci/docker-compose-cluster.yml up -d
+nohup docker compose -f ci/docker-compose-rerank.yml up -d
+nohup docker compose -f ci/docker-compose-proxy.yml up -d
