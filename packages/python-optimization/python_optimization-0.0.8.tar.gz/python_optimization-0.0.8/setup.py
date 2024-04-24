36ac3050cf0f9cedc51977e1bafec2c33c1ca60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:06 +0000

    Add package python-optimization/0.0.8/python_optimization-0.0.8.tar.gz

---
+++ packages/python-optimization/python_optimization-0.0.8.tar.gz/python_optimization-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.8'
+DESCRIPTION = 'python optimization'
+LONG_DESCRIPTION = 'A package that allows python optimization'
+
+# Setting up
+setup(
+    name="python_optimization",
+    version=VERSION,
+    author="founder synergy",
+    author_email="founders.synergy@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
