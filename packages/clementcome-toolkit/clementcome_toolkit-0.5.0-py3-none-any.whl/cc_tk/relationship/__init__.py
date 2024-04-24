fa09c789ab622ea783a3e742b99981b98f88e854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:05:58 +0000

    Add package clementcome-toolkit/0.5.0/clementcome_toolkit-0.5.0-py3-none-any.whl

---
+++ packages/clementcome-toolkit/clementcome_toolkit-0.5.0-py3-none-any.whl/cc_tk/relationship/__init__.py
@@ -0,0 +1,5 @@
+"""`relationship` module is dedicated to testing statistical relationship."""
+from cc_tk.relationship.significance import (
+    get_significance,  # noqa: F401
+)
+from cc_tk.relationship.summary import RelationshipSummary  # noqa: F401
