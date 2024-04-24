e2569a22bf9daa7b1220485a1ba0f212a290dcaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:12 +0000

    Add package ludic/0.4.1/ludic-0.4.1.tar.gz

---
+++ packages/ludic/ludic-0.4.1.tar.gz/ludic-0.4.1/ludic/catalog/pages.py
@@ -0,0 +1,155 @@
+from typing import override
+
+from ludic.attrs import Attrs, NoAttrs
+from ludic.html import body, head, html, script, style, title
+from ludic.types import AnyChildren, Component, ComponentStrict
+
+
+class HtmlHeadAttrs(Attrs, total=False):
+    title: str
+
+
+class HtmlBodyAttrs(Attrs, total=False):
+    htmx_path: str
+    htmx_enabled: bool
+    htmx_version: str
+
+
+class Head(Component[AnyChildren, HtmlHeadAttrs]):
+    @override
+    def render(self) -> head:
+        return head(
+            title(self.attrs.get("title", "Ludic App")),
+            style.load(cache=True),
+            *self.children,
+        )
+
+
+class Body(Component[AnyChildren, HtmlBodyAttrs]):
+    @override
+    def render(self) -> body:
+        scripts = []
+        if htmx_path := self.attrs.get("htmx_path"):
+            scripts.append(script(src=htmx_path))
+        elif self.attrs.get("htmx_enabled", "htmx_version" in self.attrs):
+            htmx_version = self.attrs.get("htmx_version", "latest")
+            scripts.append(script(src=f"https://unpkg.com/htmx.org@{htmx_version}"))
+        return body(*self.children, *scripts)
+
+
+class HtmlPage(ComponentStrict[Head, Body, NoAttrs]):
+    styles = style.use(
+        lambda theme: {
+            # global styling
+            "*": {
+                "box-sizing": "border-box",
+                "max-inline-size": theme.measure,
+                "font-family": theme.fonts.plain,
+                "font-size": theme.fonts.size,
+                "color": theme.colors.dark,
+                "overflow-wrap": "break-word",
+                "margin": "0",
+                "padding": "0",
+                "line-height": theme.line_height,
+            },
+            ("html", "body", "div", "header", "nav", "main", "footer"): {
+                "max-inline-size": "none",
+            },
+            # elements styling
+            "h1": {
+                "font-size": theme.headers.h1.size,
+                "font-family": theme.fonts.serif,
+            },
+            "h2": {
+                "font-size": theme.headers.h2.size,
+                "font-family": theme.fonts.serif,
+            },
+            "h3": {
+                "font-size": theme.headers.h3.size,
+                "font-family": theme.fonts.serif,
+            },
+            "h4": {
+                "font-size": theme.headers.h4.size,
+                "font-family": theme.fonts.serif,
+            },
+            "h5": {
+                "font-size": theme.headers.h5.size,
+                "font-family": theme.fonts.serif,
+            },
+            "h6": {
+                "font-size": theme.headers.h6.size,
+                "font-family": theme.fonts.serif,
+            },
+            "a": {
+                "color": theme.colors.primary.darken(0.3),
+                "text-decoration": "none",
+            },
+            "a:hover": {
+                "text-decoration": "underline",
+            },
+            "pre": {
+                "overflow": "auto",
+            },
+            ("code", "pre", "pre *"): {
+                "font-family": theme.fonts.mono,
+                "font-size": theme.fonts.size * 0.9,
+            },
+            "dl": {
+                "margin-block": "0",
+            },
+            "dl dt": {
+                "margin-block-start": theme.sizes.xxs,
+            },
+            "dl dd": {
+                "margin-block-start": theme.sizes.xxxs,
+            },
+            "dt": {
+                "font-weight": "bold",
+            },
+            "dd": {
+                "margin-left": "0",
+            },
+            ("ul", "ol"): {
+                "padding-inline-start": theme.sizes.xl,
+            },
+            ("img", "svg"): {
+                "width": "100%",
+            },
+            "button": {
+                "line-height": theme.line_height - 0.2,
+            },
+            # utilities
+            ".text-align-center": {
+                "text-align": "center",
+            },
+            ".text-align-right": {
+                "text-align": "right",
+            },
+            ".text-align-left": {
+                "text-align": "left",
+            },
+            ".no-border": {
+                "border": "none !important",
+                "border-radius": "0 !important",
+            },
+            ".no-padding": {
+                "padding": "0 !important",
+            },
+            ".no-inline-padding": {
+                "padding-inline": "0 !important",
+            },
+            ".no-margin": {
+                "margin": "0 !important",
+            },
+            ".no-inline-margin": {
+                "margin-inline": "0 !important",
+            },
+        }
+    )
+
+    @override
+    def render(self) -> html:
+        return html(
+            self.children[0].render(),
+            self.children[1].render(),
+        )
---
+++ packages/ludic/ludic-0.4.1.tar.gz/ludic-0.4.1/tests/test_components.py
@@ -0,0 +1,135 @@
+from ludic.catalog.forms import Form, InputField, TextAreaField
+from ludic.catalog.items import Key, Pairs, Value
+from ludic.catalog.navigation import Navigation, NavItem
+from ludic.catalog.tables import Table, TableHead, TableRow
+from ludic.catalog.typography import Link, Paragraph
+from ludic.html import b
+
+
+def test_link() -> None:
+    link = Link("A link!", to="https://example.com")
+    assert link.to_html() == '<a href="https://example.com" target="_blank">A link!</a>'
+    link = Link("A link!", to="/home")
+    assert link.to_html() == '<a href="/home">A link!</a>'
+
+
+def test_paragraph() -> None:
+    paragraph = Paragraph(
+        "Hello, how ",
+        b("are you"),
+        "? Click ",
+        Link("here", to="https://example.com"),
+        ".",
+    )
+    assert isinstance(paragraph.children[3], Link)
+    assert paragraph.children[3].attrs["to"] == "https://example.com"
+    assert paragraph.to_string(pretty=False) == (
+        "<Paragraph>"
+          'Hello, how <b>are you</b>? Click <Link to="https://example.com">here</Link>.'
+        "</Paragraph>"
+    )  # fmt: skip
+    assert paragraph.to_html() == (
+        '<p>'
+            'Hello, how <b>are you</b>? '
+            'Click <a href="https://example.com" target="_blank">here</a>.'
+        '</p>'
+    )  # fmt: skip
+
+
+def test_navigation() -> None:
+    navigation = Navigation(
+        NavItem("Home", to="/"),
+        NavItem("About", to="/about"),
+        id="nav",
+    )
+    assert navigation.to_html() == (
+        '<nav id="nav" class="navigation">'
+            '<ul class="stack small">'
+                '<li class="nav-item"><a href="/" class="small btn link">Home</a></li>'
+                '<li class="nav-item">'
+                    '<a href="/about" class="small btn link">About</a>'
+                '</li>'
+            "</ul>"
+        "</nav>"
+    )  # fmt: skip
+
+
+def test_pairs() -> None:
+    pairs = Pairs(
+        Key("Name"),
+        Value("John"),
+        Key("Age"),
+        Value(42),
+    )
+    assert pairs.to_html() == (
+        '<dl class="stack small">'
+            '<dt>Name</dt>'
+            '<dd>John</dd>'
+            '<dt>Age</dt>'
+            '<dd>42</dd>'
+        "</dl>"
+    )  # fmt: skip
+
+
+def test_tables() -> None:
+    table = Table(
+        TableHead("Name", "Age"),
+        TableRow("John", 42),
+        TableRow("Jane", 43),
+    )
+
+    assert table.header == ("Name", "Age")
+    assert table.getlist("Name") == ["John", "Jane"]
+    assert table.getlist("Age") == [42, 43]
+    assert table.getlist("NOT_FOUND") == []
+
+    assert len(table.children) > 2
+    assert table.children[2].get_value(123) is None
+
+    assert table.to_html() == (
+        '<table class="table">'
+            "<thead>"
+                '<tr><th>Name</th><th>Age</th></tr>'
+            "</thead>"
+            "<tbody>"
+                '<tr><td>John</td><td>42</td></tr>'
+                '<tr><td>Jane</td><td>43</td></tr>'
+            "</tbody>"
+        "</table>"
+    )  # fmt: skip
+
+
+def test_form_fields() -> None:
+    form = Form(
+        InputField(value="Name", name="name"),
+        TextAreaField("Description", name="description"),
+    )
+
+    assert form.to_html() == (
+        '<form class="form stack">'
+            '<div class="form-field">'
+                '<input value="Name" name="name" id="name" />'
+            "</div>"
+            '<div class="form-field">'
+                '<textarea name="description" id="description">Description</textarea>'
+            "</div>"
+        "</form>"
+    )  # fmt: skip
+
+    form = Form(
+        InputField(value="Name", name="name", label="Foo"),
+        TextAreaField("Description", name="description", label="Bar"),
+    )
+
+    assert form.to_html() == (
+        '<form class="form stack">'
+            '<div class="form-field">'
+                '<label for="name">Foo</label>'
+                '<input value="Name" name="name" id="name" />'
+            "</div>"
+            '<div class="form-field">'
+                '<label for="description">Bar</label>'
+                '<textarea name="description" id="description">Description</textarea>'
+            "</div>"
+        "</form>"
+    )  # fmt: skip
