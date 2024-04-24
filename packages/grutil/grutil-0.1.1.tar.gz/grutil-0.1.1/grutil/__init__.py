8693ddbba1977485bc52e976c10964e4f3aed807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:14 +0000

    Add package grutil/0.1.1/grutil-0.1.1.tar.gz

---
+++ packages/grutil/grutil-0.1.1.tar.gz/grutil-0.1.1/grutil/__init__.py
@@ -0,0 +1,9 @@
+from .afm import is_valid_afm
+from .amka import is_valid_amka
+from .dates import (
+    date2grdate,
+    grdate2date,
+    grdate2isodate,
+    isodate2gr,
+    isodate2yearmonth,
+)
