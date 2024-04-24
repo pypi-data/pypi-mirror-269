cb6c433b21c59dcd146c85ca3fb2e679c5218f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 06:04:14 +0000

    Add package cucim-cu12/24.4.0/cucim_cu12-24.4.0-cp311-cp311-manylinux_2_28_aarch64.whl

---
+++ packages/cucim-cu12/cucim_cu12-24.4.0-cp311-cp311-manylinux_2_28_aarch64.whl/cucim/skimage/util/lookfor.py
@@ -0,0 +1,31 @@
+import sys
+
+import numpy as np
+
+
+def lookfor(what):
+    """Do a keyword search on scikit-image docstrings.
+
+    Parameters
+    ----------
+    what : str
+        Words to look for.
+
+    Examples
+    --------
+    >>> import cucim.skimage
+    >>> cucim.skimage.lookfor('median')  # doctest: +SKIP
+    Search results for 'median'
+    ---------------------------
+    cucim.skimage.filters.median
+        Return local median of an image.
+    cucim.skimage.measure.block_reduce
+        Downsample image by applying function `func` to local blocks.
+    cucim.skimage.filters.threshold_local
+        Compute a threshold mask image based on local pixel neighborhood.
+    cucim.skimage.registration.optical_flow_ilk
+        Coarse to fine optical flow estimator.
+    cucim.skimage.registration.optical_flow_tvl1
+        Coarse to fine optical flow estimator.
+    """
+    return np.lookfor(what, sys.modules[__name__.split(".")[0]])
