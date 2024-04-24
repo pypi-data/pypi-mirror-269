1660997bf4f6186fb719a44321943fcb58c021cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 15:04:47 +0000

    Add package qualesim/1.0.2/qualesim-1.0.2-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/qualesim/qualesim-1.0.2-cp310-cp310-manylinux2014_x86_64.whl/qualesim/__init__.py
@@ -0,0 +1,11 @@
+"""Python API for DQCsim: the Delft Quantum & Classical Simulator
+
+This library allows you to write DQCsim host programs and plugins in Python.
+"""
+
+__all__ = ["raw", "common", "plugin", "host"]
+
+# Don't try to output documentation for the test module.
+__pdoc__ = {"tests": False}
+
+import dqcsim._dqcsim as raw
