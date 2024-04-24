bd91a29cb22e2717ca13c7c8014954435c8990f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 16:05:14 +0000

    Add package HomePy/0.0.2/HomePy-0.0.2-py3-none-any.whl

---
+++ packages/HomePy/HomePy-0.0.2-py3-none-any.whl/HomePy/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023-12-22 17:36
+# @Author  : Jack
+# @File    : __init__.py
+
+"""
+__init__
+"""
---
+++ packages/HomePy/HomePy-0.0.2-py3-none-any.whl/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023-12-22 16:55
+# @Author  : Jack
+# @File    : __init__.py.py
+
+"""
+__init__.py
+"""
+
+__version__ = "0.0.2"
