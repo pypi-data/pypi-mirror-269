05c383035d7eda55c8bdfd6e07ccd94864ec7c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 06:04:19 +0000

    Add package fal/0.15.0/fal-0.15.0.tar.gz

---
+++ packages/fal/fal-0.15.0.tar.gz/fal-0.15.0/tests/conftest.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from functools import partial
+
+import pytest
+
+from fal import function
+
+
+@pytest.fixture(scope="function")
+def isolated_client():
+    return partial(function, machine_type="XS", keep_alive=0)
