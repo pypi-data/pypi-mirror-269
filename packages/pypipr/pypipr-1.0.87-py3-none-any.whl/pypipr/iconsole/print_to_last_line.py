8f8c444c784632912c0ed6d9ebfc833d0ae03c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:30 +0000

    Add package pypipr/1.0.87/pypipr-1.0.87-py3-none-any.whl

---
+++ packages/pypipr/pypipr-1.0.87-py3-none-any.whl/pypipr/iconsole/print_to_last_line.py
@@ -0,0 +1,16 @@
+def print_to_last_line(text: str):
+    """
+    Melakukan print ke konsol tetapi akan menimpa baris terakhir.
+    Berguna untuk memberikan progress secara interaktif.
+
+    ```py
+    c = input("masukkan apa saja : ")
+    print_to_last_line(f"masukkan apa saja : {c} [ok]")
+    ```
+    """
+    print(f"\033[F{text}")
+
+
+if __name__ == "__main__":
+    c = input("masukkan apa saja : ")
+    print_to_last_line(f"masukkan apa saja : {c} [ok]")
