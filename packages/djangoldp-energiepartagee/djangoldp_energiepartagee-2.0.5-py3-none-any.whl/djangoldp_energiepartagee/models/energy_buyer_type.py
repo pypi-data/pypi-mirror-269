8f0d0846f64432ca32ec59129cee9af1b6a32df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:06:21 +0000

    Add package djangoldp-energiepartagee/2.0.5/djangoldp_energiepartagee-2.0.5-py3-none-any.whl

---
+++ packages/djangoldp-energiepartagee/djangoldp_energiepartagee-2.0.5-py3-none-any.whl/djangoldp_energiepartagee/models/energy_buyer_type.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from djangoldp.models import Model
+from djangoldp.permissions import AuthenticatedOnly, ReadOnly
+
+
+class ContractType(Model):
+    type = models.CharField(max_length=250, blank=True, null=True, verbose_name="Type")
+
+    class Meta(Model.Meta):
+        ordering = ["pk"]
+        permission_classes = [AuthenticatedOnly, ReadOnly]
+        rdf_type = "energiepartagee:contract_type"
+        verbose_name = _("Type de contrat")
+        verbose_name_plural = _("Types de contrats")
+
+    def __str__(self):
+        if self.type:
+            return self.type
+        else:
+            return self.urlid
