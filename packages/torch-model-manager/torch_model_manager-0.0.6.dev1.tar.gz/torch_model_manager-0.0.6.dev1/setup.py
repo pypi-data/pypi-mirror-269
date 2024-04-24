618fce176b25645a18f638d55dedb5cf32d3b2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 16:04:32 +0000

    Add package torch-model-manager/0.0.6.dev1/torch_model_manager-0.0.6.dev1.tar.gz

---
+++ packages/torch-model-manager/torch_model_manager-0.0.6.dev1.tar.gz/torch_model_manager-0.0.6.dev1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='torch-model-manager',
+    version='0.0.6.dev1',
+    description='A package for managing PyTorch models',
+    author='Billal MOKHTARI',
+    author_email='mokhtaribillal1@gmail.com',
+    packages=find_packages(exclude=['tests', 'docs', 'build.sh', 'requirements.sh', 'resources.md']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/Billal-MOKHTARI/torch-model-manager',
+    keywords=['PyTorch', 'Deep Learning', 'Machine Learning', 'High Level Programming'],
+    install_requires=[
+        'torch',
+        'numpy',
+        'torchvision',
+        'torch'
+        # Add any other dependencies here
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+
+)
