2acb2875362dc54a6e8f945809ba142029596dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 14:05:01 +0000

    Add package factorlib/1.0.0/factorlib-1.0.0.tar.gz

---
+++ packages/factorlib/factorlib-1.0.0.tar.gz/factorlib-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       	name="factorlib", 
+        version=VERSION,
+        author="Jake Zur",
+        author_email="jakezur@umich.edu",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
