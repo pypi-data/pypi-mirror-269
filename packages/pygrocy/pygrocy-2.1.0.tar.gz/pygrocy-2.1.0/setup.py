a177d4b873b133c74af15649556115153e26fd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:35 +0000

    Add package pygrocy/2.1.0/pygrocy-2.1.0.tar.gz

---
+++ packages/pygrocy/pygrocy-2.1.0.tar.gz/pygrocy-2.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygrocy",
+    version="2.1.0",
+    author="Sebastian Rutofski",
+    author_email="kontakt@sebastian-rutofski.de",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sebrut/pygrocy",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "backports.zoneinfo;python_version<'3.9'",  # backports can be removed when python 3.8 support is dropped
+        "deprecation~=2.1.0",
+        "pydantic>=1.8.2,<1.11.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
