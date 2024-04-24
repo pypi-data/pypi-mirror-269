6660ee0b0c86f4bae2ae64b06e579d9d97b856c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:05:46 +0000

    Add package nestipy-ioc/0.1.20/nestipy_ioc-0.1.20.tar.gz

---
+++ packages/nestipy-ioc/nestipy_ioc-0.1.20.tar.gz/nestipy_ioc-0.1.20/src/nestipy_ioc/__init__.py
@@ -0,0 +1,25 @@
+from .annotation import Annotation
+from .container import NestipyContainer
+from .context_container import NestipyContextContainer
+from .dependency import Inject, Res, Req, Session, Query, Body, Args, Context, Files, SocketServer, SocketClient, \
+    SocketData
+from .provider import ModuleProviderDict
+
+__all__ = [
+    "Annotation",
+    "NestipyContainer",
+    "NestipyContextContainer",
+    "ModuleProviderDict",
+    "Inject",
+    "Res",
+    "Req",
+    "Session",
+    "Query",
+    "Body",
+    "Args",
+    "Context",
+    "Files",
+    "SocketServer",
+    "SocketClient",
+    "SocketData"
+]
