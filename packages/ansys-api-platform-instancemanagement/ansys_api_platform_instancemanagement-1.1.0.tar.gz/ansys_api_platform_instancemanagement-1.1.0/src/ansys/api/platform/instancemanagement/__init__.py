d3b10654778e27702844f2e889f2145c6976c5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 16:04:40 +0000

    Add package ansys-api-platform-instancemanagement/1.1.0/ansys_api_platform_instancemanagement-1.1.0.tar.gz

---
+++ packages/ansys-api-platform-instancemanagement/ansys_api_platform_instancemanagement-1.1.0.tar.gz/ansys_api_platform_instancemanagement-1.1.0/src/ansys/api/platform/instancemanagement/__init__.py
@@ -0,0 +1,8 @@
+"""Auto-generated Python gRPC interface package for the ``ansys-api-platform-instancemanagement package``."""
+
+import pathlib
+
+__all__ = ["__version__"]
+
+with open(pathlib.Path(__file__).parent / "VERSION", encoding="utf-8") as f:
+    __version__ = f.read().strip()
