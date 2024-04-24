80507f695cc0497d4772c991e9a58382d8816d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:04 +0000

    Add package pandaz/1.1.3/pandaz-1.1.3.tar.gz

---
+++ packages/pandaz/pandaz-1.1.3.tar.gz/pandaz-1.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.3'
+DESCRIPTION = 'Helps users code efficently'
+LONG_DESCRIPTION = 'A package that allows to build simple codes in a well defined and efficient manner.'
+
+# Setting up
+setup(
+    name="pandaz",
+    version=VERSION,
+    author="Tom Hanks",
+    author_email="<tomhanks02@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
+
