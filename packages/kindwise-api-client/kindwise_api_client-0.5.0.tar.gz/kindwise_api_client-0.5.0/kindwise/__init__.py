e0b34f1472b2a1c497e1306fa09f0e9b85fb6b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:05:00 +0000

    Add package kindwise-api-client/0.5.0/kindwise_api_client-0.5.0.tar.gz

---
+++ packages/kindwise-api-client/kindwise_api_client-0.5.0.tar.gz/kindwise_api_client-0.5.0/kindwise/__init__.py
@@ -0,0 +1,5 @@
+from kindwise.crop_health import CropHealthApi
+from kindwise.insect import InsectApi, InsectKBType
+from kindwise.models import Result, Identification, Input, UsageInfo, ClassificationLevel, SearchResult
+from kindwise.mushroom import MushroomApi, MushroomKBType
+from kindwise.plant import PlantApi, PlantIdentification, HealthAssessment, RawPlantIdentification, PlantKBType
