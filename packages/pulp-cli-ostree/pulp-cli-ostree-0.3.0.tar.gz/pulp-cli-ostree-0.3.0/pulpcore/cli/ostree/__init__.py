f200695fe6c34a9b008b12d87db7c2b5aa4982d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:10 +0000

    Add package pulp-cli-ostree/0.3.0/pulp-cli-ostree-0.3.0.tar.gz

---
+++ packages/pulp-cli-ostree/pulp-cli-ostree-0.3.0.tar.gz/pulp-cli-ostree-0.3.0/pulpcore/cli/ostree/__init__.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+import click
+from pulpcore.cli.common.generic import pulp_group
+
+from pulpcore.cli.ostree.distribution import distribution
+from pulpcore.cli.ostree.remote import remote
+from pulpcore.cli.ostree.repository import repository
+
+__version__ = "0.3.0"
+
+
+@pulp_group("ostree")
+def ostree_group() -> None:
+    pass
+
+
+def mount(main: click.Group, **kwargs: Any) -> None:
+    ostree_group.add_command(distribution)
+    ostree_group.add_command(remote)
+    ostree_group.add_command(repository)
+    main.add_command(ostree_group)
