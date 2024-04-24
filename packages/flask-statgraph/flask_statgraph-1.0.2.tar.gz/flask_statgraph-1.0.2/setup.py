d2cdf4e13ed20d01a9afb6a720d9c4187275b612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 07:06:31 +0000

    Add package flask-statgraph/1.0.2/flask_statgraph-1.0.2.tar.gz

---
+++ packages/flask-statgraph/flask_statgraph-1.0.2.tar.gz/flask_statgraph-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="flask-statgraph",
+    version="1.0.2",
+    author="Ephreal",
+    original_creator="HealYouDown",
+    description="Package to collect statistics in Flask.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ephreal/flask-statgraph",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "flask", "flask-sqlalchemy"
+    ]
+)
