4cf28958917271818ca173b799a183a0b7b01f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:06:05 +0000

    Add package cat-win/1.7.6/cat_win-1.7.6-py3-none-any.whl

---
+++ packages/cat-win/cat_win-1.7.6-py3-none-any.whl/cat_win/src/const/defaultconstants.py
@@ -0,0 +1,22 @@
+"""
+defaultconstants
+"""
+
+
+class DKW:
+    """
+    The collection of all default constants
+    """
+    DEFAULT_COMMAND_LINE = 'default_command_line'
+    DEFAULT_FILE_ENCODING = 'default_file_encoding'
+    LARGE_FILE_SIZE = 'large_file_size'
+    STRIP_COLOR_ON_PIPE = 'strip_color_on_pipe'
+    IGNORE_UNKNOWN_BYTES = 'ignore_unknown_bytes'
+    PEEK_SIZE = 'peek_size'
+    EDITOR_INDENTATION = 'editor_indentation'
+    EDITOR_AUTO_INDENT = 'editor_auto_indent'
+    STRINGS_MIN_SEQUENCE_LENGTH = 'strings_minimum_sequence_length'
+    STRINGS_DELIMETER = 'strings_delimeter'
+    UNICODE_ESCAPED_FIND = 'unicode_escaped_find'
+    UNICODE_ESCAPED_REPLACE = 'unicode_escaped_replace'
+    MORE_STEP_LENGTH = 'more_step_length'
