6941d00b24ca61cd1ee6387b89a08c9ec83241ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:03:58 +0000

    Add package infoindia/1.0.0/infoindia-1.0.0.tar.gz

---
+++ packages/infoindia/infoindia-1.0.0.tar.gz/infoindia-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='infoindia',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Vijay Chouhan',
+    author_email='vijay977364@gmail.com',
+    description='Wrapper for State Data APIs',
+    url='https://github.com/NarutoUzumvki/state_data_api',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
