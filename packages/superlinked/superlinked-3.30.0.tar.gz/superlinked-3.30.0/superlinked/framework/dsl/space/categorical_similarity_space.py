c7c339f0c5ffda6af3772628e3d23305ed1d6991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:55 +0000

    Add package superlinked/3.30.0/superlinked-3.30.0.tar.gz

---
+++ packages/superlinked/superlinked-3.30.0.tar.gz/superlinked-3.30.0/superlinked/framework/dsl/space/categorical_similarity_space.py
@@ -0,0 +1,112 @@
+# Copyright 2024 Superlinked, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import Mapping, cast
+
+from superlinked.framework.common.dag.categorical_similarity_node import (
+    CategoricalSimilarityNode,
+)
+from superlinked.framework.common.dag.node import Node
+from superlinked.framework.common.dag.schema_field_node import SchemaFieldNode
+from superlinked.framework.common.data_types import Vector
+from superlinked.framework.common.schema.schema_object import (
+    SchemaField,
+    SchemaObject,
+    String,
+)
+from superlinked.framework.common.space.normalization import L2Norm
+from superlinked.framework.dsl.space.space import Space
+from superlinked.framework.dsl.space.space_field_set import SpaceFieldSet
+
+
+class CategoricalSimilaritySpace(Space):
+    """
+    Represents a space for encoding categorical similarity.
+
+    A CategoricalSimilaritySpace is designed to measure the similarity between items that
+    are grouped into a finite number of textual categories. The categories are represented
+    in a one-hot encoded vector, with the option to apply a negative filter for unmatched
+    categories. Negative_filter allows for filtering out unmatched categories, by setting
+    it to a large negative value, effectively resulting in large negative similarity between
+    non-matching category items. A category input not present in categories will be encoded
+    as other category. These categories will be similar to each other by default. Set
+    uncategorised_as_category parameter to False in order to suppress this behaviour.
+
+    Attributes:
+        category_input (Union[String, List[String]]): The schema field containing input
+            category or categories to be considered in the similarity space.
+        categories (List[str]): A list of categories that defines the dimensionality of the
+            one-hot encoded vector. Any category not listed is considered as 'other'.
+        negative_filter (float): A value to represent unmatched categories in the one-hot vector.
+            Instead of using 0, which typically represents the absence of a category, this allows
+            for a different representation - resulting in effectively filtering out items that has
+            non-matching categories.
+        uncategorised_as_category (bool): If set to False, the similarity between other categories will be
+            set to 0, or negative_filter if set. By this we can control if a category_input not in
+            categories will be similar to other category_inputs not in categories. Note that the same
+            category_inputs not in categories will not be similar to each other either.
+
+    Raises:
+        InvalidSchemaException: If a schema object does not have a corresponding node in the
+            similarity space.
+    """
+
+    def __init__(
+        self,
+        category_input: String | list[String],
+        categories: list[str],
+        negative_filter: float = 0.0,
+        uncategorised_as_category: bool = True,
+    ) -> None:
+        """
+        Initialize the CategoricalSimilaritySpace.
+
+        Args:
+            category_input (String | list[String]): The category input Schema field.
+            categories (list[str]): This controls the set of categories represented in the one-hot vector,
+                else it falls into the other category. It is needed to control dimensionality.
+            negative_filter (float): Not matched category vector elements are not initialized as 0,
+                but as negative_filter
+        """
+        super().__init__(category_input, String)
+        self.__category = SpaceFieldSet(self, cast(set[SchemaField], self._field_set))
+        self.__uncategorised_as_category: bool = uncategorised_as_category
+        normalization = L2Norm()
+        unchecked_category_node_map = {
+            single_category: CategoricalSimilarityNode(
+                parent=SchemaFieldNode(single_category),
+                categories=categories,
+                negative_filter=negative_filter,
+                uncategorised_as_category=self.uncategorised_as_category,
+                normalization=normalization,
+            )
+            for single_category in self._field_set
+        }
+        self.__schema_node_map: dict[SchemaObject, CategoricalSimilarityNode] = {
+            schema_field.schema_obj: node
+            for schema_field, node in unchecked_category_node_map.items()
+        }
+
+    @property
+    def _node_by_schema(self) -> Mapping[SchemaObject, Node[Vector]]:
+        return self.__schema_node_map
+
+    @property
+    def uncategorised_as_category(self) -> bool:
+        return self.__uncategorised_as_category
+
+    @property
+    def category(self) -> SpaceFieldSet:
+        return self.__category
