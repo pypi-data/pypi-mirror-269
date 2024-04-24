67580bef8e5a9a88beb66b1dbae9cd0f55839b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 07:06:42 +0000

    Add package kappaschedules/0.0.31/kappaschedules-0.0.31.tar.gz

---
+++ packages/kappaschedules/kappaschedules-0.0.31.tar.gz/kappaschedules-0.0.31/kappaschedules/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.31"
+
+from .factory import object_to_schedule
+from .schedules import *
---
+++ packages/kappaschedules/kappaschedules-0.0.31.tar.gz/kappaschedules-0.0.31/kappaschedules/schedules/constant_schedule.py
@@ -0,0 +1,13 @@
+from .base import ScheduleBase
+
+
+class ConstantSchedule(ScheduleBase):
+    def __init__(self, value, **kwargs):
+        super().__init__(**kwargs)
+        self.value = value
+
+    def __str__(self):
+        return f"{type(self).__name__}(value={self.value})"
+
+    def _get_value(self, step: int, total_steps: int, abs_step: int = None) -> float:
+        return self.value
