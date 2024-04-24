39bf80000ff41f2f4fec7106cb94da527b8733ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:26 +0000

    Add package mep-tools/1.2.78/mep_tools-1.2.78.tar.gz

---
+++ packages/mep-tools/mep_tools-1.2.78.tar.gz/mep_tools-1.2.78/setup.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+# Fix and optimise the library installation
+setup(
+    name="mep_tools",
+    version="1.2.78",
+    author="Khai",
+    author_email="sarraj@marksmen.nl",
+    license="MIT",
+    packages=find_packages(),
+    package_data={'mep_tools': ['*/*.pyi', '*.pyi']},
+    description="grpc for rep",
+    long_description="hold the code for the grpc files",
+    install_requires=requirements,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+)
