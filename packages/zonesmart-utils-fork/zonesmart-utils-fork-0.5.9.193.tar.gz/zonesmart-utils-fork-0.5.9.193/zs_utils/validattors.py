45936fb1f50849ffa64d178731a3badfe9680ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:24 +0000

    Add package zonesmart-utils-fork/0.5.9.193/zonesmart-utils-fork-0.5.9.193.tar.gz

---
+++ packages/zonesmart-utils-fork/zonesmart-utils-fork-0.5.9.193.tar.gz/zonesmart-utils-fork-0.5.9.193/zs_utils/validattors.py
@@ -0,0 +1,10 @@
+import uuid
+
+from rest_framework.exceptions import ValidationError
+
+
+def validate_uuid(value):
+    try:
+        uuid.UUID(str(value))
+    except ValueError:
+        raise ValidationError(f"Неверный формат UUID. {value}")
