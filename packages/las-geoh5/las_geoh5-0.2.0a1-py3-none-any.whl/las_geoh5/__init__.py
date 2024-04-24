3cd3b51c6380e3cc9963c86a50fa339227ff593f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:55 +0000

    Add package las-geoh5/0.2.0a1/las_geoh5-0.2.0a1-py3-none-any.whl

---
+++ packages/las-geoh5/las_geoh5-0.2.0a1-py3-none-any.whl/las_geoh5/__init__.py
@@ -0,0 +1,11 @@
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#
+#  This file is part of las_geoh5 project.
+#
+#  las-geoh5 is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+#
+
+# flake8: noqa
+
+__version__ = "0.2.0-alpha.1"
