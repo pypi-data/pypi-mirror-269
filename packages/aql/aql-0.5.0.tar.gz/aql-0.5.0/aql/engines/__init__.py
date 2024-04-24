0f8fbe8dbe695eb078426f57c8f2cf378365ce1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 15:04:57 +0000

    Add package aql/0.5.0/aql-0.5.0.tar.gz

---
+++ packages/aql/aql-0.5.0.tar.gz/aql-0.5.0/aql/engines/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 Amethyst Reese
+# Licensed under the MIT license
+
+from .base import Connection, Engine
+from .mysql import MysqlConnection, MysqlEngine
+from .sql import SqlEngine
+from .sqlite import SqliteConnection, SqliteEngine
---
+++ packages/aql/aql-0.5.0.tar.gz/aql-0.5.0/aql/tests/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022 Amethyst Reese
+# Licensed under the MIT license
+
+from .column import ColumnTest
+from .connector import ConnectorTest
+from .engines import *  # noqa: F403
+from .query import QueryTest
+from .table import TableTest
+from .types import TypesTest
