1725705214a6108e2f58cfec3231af5dbe939605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 15:04:07 +0000

    Add package sanic-cache/0.1.1/sanic_cache-0.1.1.tar.gz

---
+++ packages/sanic-cache/sanic_cache-0.1.1.tar.gz/sanic_cache-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='sanic_cache',
+    version='0.1.1',
+    packages=['sanic_cache'],
+    url='https://github.com/nahongyan1997/sanic_cache',
+    license='MIT',
+    author='Na haha',
+    author_email='547840062@qq.com',
+    description='flask-cache -> sanic-cache',
+    install_requires=[
+        "sanic >= 23.12.1",
+        "sanic_ext",
+        "redis >= 5.0.0",
+    ]
+)
