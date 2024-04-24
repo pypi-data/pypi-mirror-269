9913b146597885f3de1880bda16ce584eb30065b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 15:04:40 +0000

    Add package rtsf-web/1.3.7/rtsf-web-1.3.7.tar.gz

---
+++ packages/rtsf-web/rtsf-web-1.3.7.tar.gz/rtsf-web-1.3.7/webuidriver/__about__.py
@@ -0,0 +1,11 @@
+#! python3
+# -*- encoding: utf-8 -*-
+
+__title__           = "rtsf-web"
+__short_desc__      = "only for web ui test, base on rtsf"
+__version__         = "1.3.7"
+__autor__           = u"罗科峰"
+__author_email__    = "lkf20031988@163.com"
+__license__         = 'MIT'
+
+HOME_PAGE           = "https://github.com/RockFeng0/rtsf-web"
