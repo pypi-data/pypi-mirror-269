32d70b171d29a9197e55e24244f4cb62931d9ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:06:45 +0000

    Add package pynumstim/0.2.10/pynumstim-0.2.10-py3-none-any.whl

---
+++ packages/pynumstim/pynumstim-0.2.10-py3-none-any.whl/pynumstim/__init__.py
@@ -0,0 +1,12 @@
+""" Python library to create symbolic number and arithmetic stimuli for psychological experiments
+
+creating math problem stimuli with latex formulas and dvipng
+"""
+
+__author__ = "Oliver Lindemann"
+__version__ = "0.2.10"
+
+from ._problem import MathProblem
+from ._mplist import MathProblemList
+from ._data_sets import Datasets
+from ._number import Num, TNum, TPyNum
