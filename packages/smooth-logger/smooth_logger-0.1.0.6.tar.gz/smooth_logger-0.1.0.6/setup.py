1f199832e507391c0db3ddb1faa67885797f7e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 06:04:34 +0000

    Add package smooth-logger/0.1.0.6/smooth_logger-0.1.0.6.tar.gz

---
+++ packages/smooth-logger/smooth_logger-0.1.0.6.tar.gz/smooth_logger-0.1.0.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    return open('README.md', 'r').read()
+
+
+setup(
+    name="smooth_logger",
+    version="0.1.0.6",
+    author="Murdo Maclachlan",
+    author_email="murdomaclachlan@duck.com",
+    description=(
+        "A simple logger made primarily for my own personal use. Made from a combination of"
+        + " necessity and so much sloth that it overflowed into productivity."
+    ),
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://codeberg.org/MurdoMaclachlan/smooth_logger",
+    packages=find_packages(),
+    install_requires=[
+        "plyer",
+        "smooth_progress"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    license='AGPLv3+'
+)
