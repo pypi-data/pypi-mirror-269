abb8d18e8e31bf5fc7b5dca23cf4cc21ac0a688f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:04 +0000

    Add package thirdai/0.7.46/thirdai-0.7.46-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/thirdai/thirdai-0.7.46-cp311-cp311-macosx_11_0_arm64.whl/thirdai/neural_db/__init__.py
@@ -0,0 +1,24 @@
+import nltk
+
+from . import parsing_utils
+from .constraint_matcher import AnyOf, EqualTo, GreaterThan, InRange, LessThan
+from .documents import (
+    CSV,
+    DOCX,
+    PDF,
+    URL,
+    Document,
+    InMemoryText,
+    Reference,
+    SalesForce,
+    SentenceLevelDOCX,
+    SentenceLevelPDF,
+    SharePoint,
+    SQLDatabase,
+    Unstructured,
+)
+from .model_bazaar import Login, ModelBazaar
+from .neural_db import CancelState, CheckpointConfig, NeuralDB, Strength, Sup
+from .trainer import training_data_manager, training_progress_manager
+
+nltk.download("punkt")
