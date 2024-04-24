c8308331ae7bec8f216326aca7e33079c76d4cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 15:04:40 +0000

    Add package load-envfile/0.4/load_envfile-0.4.tar.gz

---
+++ packages/load-envfile/load_envfile-0.4.tar.gz/load_envfile-0.4/load_envfile/load_envfile.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+
+def load_envfile():
+  # Obtener el nombre del archivo de entorno de los argumentos de la línea de comandos
+  env_file = None
+  if "--env-file" in sys.argv:
+      index = sys.argv.index("--env-file")
+      if index + 1 < len(sys.argv):
+          env_file = sys.argv[index + 1]
+
+  # Cargar las variables de entorno desde el archivo especificado
+  if env_file:
+      load_dotenv(env_file)
---
+++ packages/load-envfile/load_envfile-0.4.tar.gz/load_envfile-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='load_envfile',
+    version='0.4',
+    packages=find_packages(),
+    description='Una biblioteca para cargar variables de entorno desde un archivo',
+    author='María Muñoz Cruzado',
+    author_email='maria.munozcruzado@telefonica.com',
+    install_requires=[
+        'python-dotenv',
+        'os'
+    ],
+
+    # Configuración específica para ruedas
+    setup_requires=['wheel'],
+
+    # Indica que quieres que se construyan ruedas durante la distribución
+    python_requires='>=3.6',
+)
