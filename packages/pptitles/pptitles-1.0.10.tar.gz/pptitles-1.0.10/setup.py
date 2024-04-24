f8e127bd6c6c33660897135a7dbb1da34c2549f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 07:06:27 +0000

    Add package pptitles/1.0.10/pptitles-1.0.10.tar.gz

---
+++ packages/pptitles/pptitles-1.0.10.tar.gz/pptitles-1.0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pptitles',
+    version='1.0.10',
+    scripts=['pptitles'],  # Adjust as needed if it's a file or use glob.glob() for multiple files
+    author='phx',
+    author_email='phx@example.com',
+    description='Extract the slide number and title from all slides in a PowerPoint presentation.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/phx/pptitles',
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+    install_requires=['python-pptx>=0.6.23']
+)
+
