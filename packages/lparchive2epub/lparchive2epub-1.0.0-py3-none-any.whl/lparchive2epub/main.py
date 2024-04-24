251cad8f9472e53cef82349e410f445ef3246764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:05:16 +0000

    Add package lparchive2epub/1.0.0/lparchive2epub-1.0.0-py3-none-any.whl

---
+++ packages/lparchive2epub/lparchive2epub-1.0.0-py3-none-any.whl/lparchive2epub/main.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser, ArgumentTypeError
+from multiprocessing import Pool
+from urllib.parse import urlparse
+
+from requests import Session
+
+from lparchive2epub.lib import lparchive2epub
+from tqdm import tqdm
+
+def is_lparchive_url(arg):
+    url = urlparse(arg)
+    if not "lparchive.org" in url.geturl():
+        raise ArgumentTypeError(f"{arg} is not an lparchive.org URL")
+    if not url.path or url.path == "/":
+        raise ArgumentTypeError(f"{arg} is not a let's play archive")
+    return arg
+
+
+arg_parser = ArgumentParser(
+    prog="lparchive2epub",
+    description="A tool to transform a Let's Play from lparchive.org to epub format."
+)
+
+arg_parser.add_argument("url", metavar="URL", nargs=1, type=is_lparchive_url)
+arg_parser.add_argument("output", metavar="OUTPUT_FILE", nargs=1, type=str)
+
+
+def main():
+    args = arg_parser.parse_args()
+    session = Session()
+    with Pool() as pool:
+        lparchive2epub(tqdm, session, pool, args.url[0], args.output[0])
+
+
+if __name__ == '__main__':
+    main()
