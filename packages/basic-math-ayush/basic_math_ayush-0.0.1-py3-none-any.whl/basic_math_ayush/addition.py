3c3e8825f90277941a98ce307633e2ecb71af633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 07:06:28 +0000

    Add package basic-math-ayush/0.0.1/basic_math_ayush-0.0.1-py3-none-any.whl

---
+++ packages/basic-math-ayush/basic_math_ayush-0.0.1-py3-none-any.whl/basic_math_ayush/addition.py
@@ -0,0 +1,2 @@
+def add(a: float,b: float) -> float:
+    return round(a+b,10)
---
+++ packages/basic-math-ayush/basic_math_ayush-0.0.1-py3-none-any.whl/example.py
@@ -0,0 +1,5 @@
+import basic_math_ayush
+
+print(basic_math_ayush.add(1.3, 2.2)) #3.5
+print(basic_math_ayush.sub(5.4, 2)) #3.4
+print(basic_math_ayush.mult(4.8, 2)) #9.6
