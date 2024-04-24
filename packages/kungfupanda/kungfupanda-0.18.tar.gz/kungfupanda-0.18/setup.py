b08766b87ca59ab87e11f1470828dd8ef17a83e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 16:05:43 +0000

    Add package kungfupanda/0.18/kungfupanda-0.18.tar.gz

---
+++ packages/kungfupanda/kungfupanda-0.18.tar.gz/kungfupanda-0.18/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+setup(
+    name='kungfupanda',
+    version='0.18',
+    author='The big fat panda',
+    author_email='po_the_big_fat_panda@gmail.com',
+    description="A simple package to make everything easy for programmers for doing everything the cool way",
+    long_description="""A simple package for the people who are not bound by society rules 
+    made by an cute panda who trained to be a dragon warrior
+    For all the story watch the original triology
+    Kung Fu Panda 1
+    Kung Fu Panda 2
+    Kung Fu Panda 3
+    """,
+    url='',
+    packages=find_packages(),
+    install_requires=[
+        'pandas','cv2'
+    ]
+)
