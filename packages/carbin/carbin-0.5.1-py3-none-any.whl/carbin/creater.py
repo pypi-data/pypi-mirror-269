1017f59202058906ffc4370058ba78056e1a4924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:29 +0000

    Add package carbin/0.5.1/carbin-0.5.1-py3-none-any.whl

---
+++ packages/carbin/carbin-0.5.1-py3-none-any.whl/carbin/creater.py
@@ -0,0 +1,178 @@
+#
+# Copyright 2023 The Turbo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import click, os, shutil
+import carbin.util as util
+
+
+class Creater:
+    def __init__(self, prefix, name, test, examples, benchmark, requirements):
+        self.prefix = prefix
+        self.name = name
+        self.test = test
+        self.examples = examples
+        self.benchmark = benchmark
+        self.requirements = requirements
+        self.top_dir = prefix.get_private_path("cmake")
+
+    def fetch(self):
+        util.mkdir(self.top_dir)
+        url = 'https://github.com/gottingen/carbin-template/archive/master.tar.gz'
+        self.prefix.log("fetch:", url)
+        f = util.retrieve_url(url, self.top_dir, copy=True, insecure=True, hash=None)
+        if os.path.isfile(f):
+            click.echo("Extracting archive {0} ...".format(f))
+            util.extract_ar(archive=f, dst=self.top_dir)
+        return next(util.get_dirs(self.top_dir))
+
+    def create_project(self):
+        d = self.fetch()
+        self.copy_project_cmakelist(d)
+        self.deal_src_cmakelist(d)
+        self.deal_carbin_cmake(d)
+        self.deal_cmake(d)
+        self.create_example_dir(d)
+        self.create_tests_dir(d)
+        self.create_benchmark_dir(d)
+        self.create_requires_file(d)
+
+    def upgrade_carbin(self):
+        d = self.fetch()
+        self.deal_carbin_cmake(d)
+
+    def copy_project_cmakelist(self, d):
+        p = os.path.join(d, 'CMakeLists.txt')
+        fr = open(p, 'r')
+        content = fr.read()
+        fr.close()
+        wc = str.replace(content, "changeme", self.name)
+        if not self.test:
+            wc = str.replace(wc, "add_subdirectory(tests)", "#add_subdirectory(tests)")
+
+        if not self.benchmark:
+            wc = str.replace(wc, "add_subdirectory(benchmark)", "#add_subdirectory(benchmark)")
+
+        if not self.examples:
+            wc = str.replace(wc, "add_subdirectory(examples)", "#add_subdirectory(examples)")
+
+        dp = 'CMakeLists.txt'
+        fw = open(dp, 'w')
+        fw.write(wc)
+        fw.close()
+
+    def deal_src_cmakelist(self, d):
+        dp = self.name + '/CMakeLists.txt'
+        # do not modify user defines cmake
+        if os.path.exists(self.name):
+            return
+        src = os.path.join(d, 'changeme')
+        util.copy_dir(src, self.name)
+        os.remove(dp)
+        srcfile = os.path.join(d, 'changeme/CMakeLists.txt')
+        fr = open(srcfile, 'r')
+        content = fr.read()
+        fr.close()
+        wc = str.replace(content, "changeme", self.name)
+        fw = open(dp, 'w')
+        fw.write(wc)
+        fw.close()
+        shared_dp = self.name + '/shared.cc'
+        shared_src = os.path.join(d, 'changeme/shared.cc')
+        os.remove(shared_dp)
+        fs = open(shared_src, 'r')
+        shared_content = fs.read()
+        fr.close()
+        shared_new = str.replace(shared_content, "changeme", self.name)
+        sw = open(shared_dp, 'w')
+        sw.write(shared_new)
+        sw.close()
+
+
+
+
+    def deal_carbin_cmake(self, d):
+        if os.path.exists('carbin_cmake'):
+            shutil.rmtree('carbin_cmake')
+        cm = os.path.join(d, 'carbin_cmake')
+        util.copy_dir(cm, 'carbin_cmake')
+
+    ########
+    # user's cmake .do not edit
+    def deal_cmake(self, d):
+        if os.path.exists('cmake'):
+            return
+        cm = os.path.join(d, 'cmake')
+        util.copy_dir(cm, 'cmake')
+        dir_list = os.listdir('cmake')
+        for f in dir_list:
+            if f.startswith('changeme'):
+                rf = f.replace('changeme', self.name)
+                os.rename(os.path.join('cmake', f), os.path.join('cmake', rf))
+
+        if not os.path.exists('conda'):
+            cc = os.path.join(d, 'conda')
+            util.copy_dir(cc, 'conda')
+
+
+    def create_example_dir(self, d):
+        if self.examples:
+            util.mkdir('examples')
+            src = os.path.join(d, 'examples/CMakeLists.txt')
+            util.copy_to(src, 'examples')
+
+    def create_tests_dir(self, d):
+        src = os.path.join(d, 'tests')
+        if self.test:
+            if not os.path.exists('tests'):
+                util.copy_dir(src, 'tests')
+
+    def create_benchmark_dir(self, d):
+        if self.benchmark:
+            util.mkdir('benchmark')
+            src = os.path.join(d, 'benchmark/CMakeLists.txt')
+            util.copy_to(src, 'benchmark')
+
+    def create_requires_file(self, d):
+        if not self.requirements:
+            return
+        if os.path.exists('carbin_deps.txt'):
+            return
+        src = os.path.join(d, 'carbin_deps.txt')
+        if os.path.exists(src):
+            util.copy_to(src, '.')
+            return
+        f = fw = open('carbin_deps.txt', 'w')
+        content = '''
+#
+# Copyright 2023 The titan-search Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###########
+# turbo
+#gottingen/turbo@v0.9.86 -DCARBIN_BUILD_TEST=OFF -DCARBIN_BUILD_BENCHMARK=OFF -DCARBIN_BUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBRARY=OFF -DBUILD_STATIC_LIBRARY=ON -DCMAKE_BUILD_TYPE=release
+        '''
+        f.write(content)
+        f.close()
