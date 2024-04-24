74707d14c68c35121ce16b3296eacb2a9e680491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 06:04:34 +0000

    Add package wagtail-tabbed-structblock/0.0.5/wagtail_tabbed_structblock-0.0.5-py3-none-any.whl

---
+++ packages/wagtail-tabbed-structblock/wagtail_tabbed_structblock-0.0.5-py3-none-any.whl/src/tabbed_structblock/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='wagtail_tabbed_structblock',
+    version='0.0.5',
+    description='Tab UI implementation for Wagtail',
+    url='https://github.com/AgenciaEgo/wagtail_tabbed_structblock',
+    author='Eric Kersten',
+    author_email='eric.kersten@egodesign.io',
+    license='MIT',
+    packages=['wagtail_tabbed_structblock'],
+    package_dir={'wagtail_tabbed_structblock': 'src'},
+    zip_safe=False,
+)
