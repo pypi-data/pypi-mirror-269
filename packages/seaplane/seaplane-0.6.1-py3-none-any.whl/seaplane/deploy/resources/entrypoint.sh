536ce47815c750df81393329dc8897fa21ae86da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:41 +0000

    Add package seaplane/0.6.1/seaplane-0.6.1-py3-none-any.whl

---
+++ packages/seaplane/seaplane-0.6.1-py3-none-any.whl/seaplane/deploy/resources/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+chmod go+w /data
+
+PROJECT_FOLDER=<PROJECT>
+
+cd "${PROJECT_FOLDER}"
+
+# unzip -o "project.zip"
+# rm project.zip
+
+project_name=$(toml get --toml-path pyproject.toml tool.poetry.name)
+main_file=$(toml get --toml-path pyproject.toml tool.seaplane.main)
+
+# cd ..
+# cp -a "${PROJECT_FOLDER}" "${project_name}" #rename folder to project name
+# cd "${project_name}"
+
+poetry lock
+poetry install
+
+export DEBUG_MODE=1
+export PYTHONUNBUFFERED=1
+poetry run python3 "${project_name}/${main_file}"
