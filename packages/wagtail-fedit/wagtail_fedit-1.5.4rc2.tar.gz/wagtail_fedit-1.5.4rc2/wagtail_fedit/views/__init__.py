98132506784857829fcfa0b3ee5e29e424d2ef30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:30 +0000

    Add package wagtail-fedit/1.5.4rc2/wagtail_fedit-1.5.4rc2.tar.gz

---
+++ packages/wagtail-fedit/wagtail_fedit-1.5.4rc2.tar.gz/wagtail_fedit-1.5.4rc2/wagtail_fedit/views/__init__.py
@@ -0,0 +1,11 @@
+from .adapters import (
+    BaseAdapterView,
+    EditAdapterView,
+)
+from .editable import (
+    FEditableView,
+    PublishView,
+    UnpublishView,
+    SubmitView,
+    CancelView,
+)
