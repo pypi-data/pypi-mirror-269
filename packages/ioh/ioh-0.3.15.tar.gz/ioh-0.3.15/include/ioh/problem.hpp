11e45df7d021d88d84c2c58ca86412365c4a0fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:05:36 +0000

    Add package ioh/0.3.15/ioh-0.3.15.tar.gz

---
+++ packages/ioh/ioh-0.3.15.tar.gz/ioh-0.3.15/include/ioh/problem.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "problem/transformation.hpp"
+#include "problem/cec.hpp"
+#include "problem/problem.hpp"
+#include "problem/single.hpp"
+#include "problem/wrap_function.hpp"
+#include "problem/bbob.hpp"
+#include "problem/pbo.hpp"
+#include "problem/wmodel.hpp"
+#include "problem/submodular.hpp"
+
+#ifdef USING_MKLANDSCAPE
+#include "problem/mklandscape/cliqueTreeC.hpp"
+#endif
+
+#include "problem/star_discrepancy/real.hpp"
+#include "problem/star_discrepancy/integer.hpp"
