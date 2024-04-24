e6addf933cd41f994ffbf340ee71a6da8cfa15ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:45 +0000

    Add package topologicpy/0.5.8/topologicpy-0.5.8.tar.gz

---
+++ packages/topologicpy/topologicpy-0.5.8.tar.gz/topologicpy-0.5.8/src/topologicpy/__init__.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2024
+# Wassim Jabi <wassim.jabi@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+import os, re
+from sys import platform
+
+__version__ = '0.5.8'
+__version_info__ = tuple([ int(num) for num in __version__.split('.')])
+
+if platform == 'win32':
+    os_name = 'windows'
+elif platform == "darwin":
+    os_name = 'macos'
+else:
+    os_name = 'linux'
+
+sitePackagesFolderName = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin", os_name)
+test1 = os.path.dirname(os.path.realpath(__file__))
+topologicFolderName = [filename for filename in os.listdir(sitePackagesFolderName) if filename.startswith("topologic")][0]
+topologicPath = os.path.join(sitePackagesFolderName, topologicFolderName)
+sys.path.append(topologicPath.replace(".libs",""))
+
+import topologic
+
