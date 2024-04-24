4470d545c097b06dd15529dece67c256ef09b5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:13 +0000

    Add package meantools/1.0.1/meantools-1.0.1.tar.gz

---
+++ packages/meantools/meantools-1.0.1.tar.gz/meantools-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meantools',
+    version='1.0.1',
+    description='Integration of multi-omics data for biosynthetic pathway discovery',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kumarsaurabh20/meantools',
+    author='Kumar Saurabh Singh',
+    author_email='kumar.singh@wur.nl',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'matplotlib', 'rdkit-pypi', 'itertools', 'networkx', 'pandas', 'sqlite3', 'tqdm', 'svgutils', 'svgelements', 'scipy', 'markov_clustering', 'seaborn', 'subprocess'],
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    include_package_data=True,
+    package_data={'data': ['data/*.csv', 'data/chem_prop.tsv', 'data/lotus_v101023.sqlite', 'data/mvc.db', 'cluster_one-1.0.jar']},
+    python_requires='>=3.8',
+    scripts=['corrMultiomics_mod.py', 'format_databases.py', 'gizmos.py', 'heraldPathways_mod.py', 'mutual_ranks.py', 'pathMassTransitions_mod.py', 'paveWays.py', 'queryMassNPDB_mod.py', 'clusters.py', 'graphics.py']
+)
+
