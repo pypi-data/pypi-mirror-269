3e9bc5a5a90252a2fea17c59df8e481ad0470317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:29 +0000

    Add package mimllearning/0.1.4/mimllearning-0.1.4.tar.gz

---
+++ packages/mimllearning/mimllearning-0.1.4.tar.gz/mimllearning-0.1.4/src/miml/tutorial/demopypi.py
@@ -0,0 +1,14 @@
+import mimllearning as miml
+
+dataset_train = load_dataset("../datasets/miml_birds_random_80train.arff", delimiter="'")
+dataset_test = load_dataset("../datasets/miml_birds_random_20test.arff", delimiter="'")
+
+#classifier = MIMLtoMLClassifier(KNeighborsClassifier(), ArithmeticTransformation())
+classifier = MIMLtoMIBRClassifier(AllPositiveAPRClassifier())
+
+classifier.fit(dataset_train)
+
+print(classifier.predict_bag(dataset_test.get_bag("366")))
+print(dataset_test.get_bag("366").get_labels()[0])
+
+classifier.evaluate(dataset_test)
---
+++ packages/mimllearning/mimllearning-0.1.4.tar.gz/mimllearning-0.1.4/todo.md
@@ -0,0 +1,18 @@
+
+- Tipos devueltos
+- Revisar todos los nombres
+- Ver como poner un boolean trained para los modelos
+- quitar super.fit()
+- tutorial files
+  - train_test to ml y mi
+  - rama to ml y mi clasificadores
+  - MananingMLInsatances
+- sklearn.classification_report
+- parametros con metricas
+- que los guarde en excel
+
+- Cambiar los np.array por np.ndarray con sus dimensiones
+Preguntar si arithmetic, geometric, minmax le añado al
+nombre lo de transformation
+
+
