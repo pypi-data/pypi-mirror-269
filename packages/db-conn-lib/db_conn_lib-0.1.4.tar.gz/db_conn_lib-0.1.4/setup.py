d4ba0ae91d84f798dcdd2b19b714b5798c61d8a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:13 +0000

    Add package db-conn-lib/0.1.4/db_conn_lib-0.1.4.tar.gz

---
+++ packages/db-conn-lib/db_conn_lib-0.1.4.tar.gz/db_conn_lib-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='db_conn_lib',
+    version='0.1.4',
+    author='Kogui',
+    description='Uma biblioteca de conexão de teste',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lucasaarrudaa/lib-repo-test',  
+    packages=find_packages(),
+    install_requires=[
+        'pyodbc',
+    ],
+    python_requires='>=3.6',  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
