c26fb96b6388d5dd0d4cae2459f7dfe0db6e1c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:06:02 +0000

    Add package intake-esgf/2024.4.23/intake_esgf-2024.4.23-py3-none-any.whl

---
+++ packages/intake-esgf/intake_esgf-2024.4.23-py3-none-any.whl/intake_esgf/__init__.py
@@ -0,0 +1,35 @@
+# An intake and intake-esm inpsired catalog for ESGF
+
+import warnings
+
+import xarray as xr
+from pkg_resources import DistributionNotFound, get_distribution
+
+warnings.simplefilter("ignore", category=xr.SerializationWarning)
+
+
+def in_notebook() -> bool:
+    """Check if the code is running in a jupyter notebook"""
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":  # Jupyter notebook, Spyder or qtconsole
+            return True
+        elif shell == "TerminalInteractiveShell":
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
+IN_NOTEBOOK = in_notebook()
+
+from intake_esgf.catalog import ESGFCatalog  # noqa
+from intake_esgf.config import conf  # noqa
+
+__all__ = ["ESGFCatalog", "conf", "IN_NOTEBOOK"]
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:  # pragma: no cover
+    __version__ = "0.0.0"  # pragma: no cover
