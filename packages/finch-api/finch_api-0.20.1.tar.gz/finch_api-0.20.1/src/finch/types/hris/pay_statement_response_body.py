b97f568080f640409ddcc4cfb3a52f1805067772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:05:29 +0000

    Add package finch-api/0.20.1/finch_api-0.20.1.tar.gz

---
+++ packages/finch-api/finch_api-0.20.1.tar.gz/finch_api-0.20.1/src/finch/types/hris/pay_statement_response_body.py
@@ -0,0 +1,16 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+
+from ..shared import Paging
+from ..._models import BaseModel
+from .pay_statement import PayStatement
+
+__all__ = ["PayStatementResponseBody"]
+
+
+class PayStatementResponseBody(BaseModel):
+    paging: Optional[Paging] = None
+
+    pay_statements: Optional[List[PayStatement]] = None
+    """The array of pay statements for the current payment."""
---
+++ packages/finch-api/finch_api-0.20.1.tar.gz/finch_api-0.20.1/src/finch/types/sandbox/connection_create_params.py
@@ -0,0 +1,22 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["ConnectionCreateParams"]
+
+
+class ConnectionCreateParams(TypedDict, total=False):
+    provider_id: Required[str]
+
+    authentication_type: Literal["credential", "api_token", "oauth", "assisted"]
+
+    employee_size: int
+    """Optional: the size of the employer to be created with this connection.
+
+    Defaults to 20
+    """
+
+    products: List[str]
