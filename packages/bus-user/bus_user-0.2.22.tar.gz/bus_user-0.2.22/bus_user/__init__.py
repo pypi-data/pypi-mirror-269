03b309aab050be723fd5c8f4175593d8258ec36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:50 +0000

    Add package bus-user/0.2.22/bus_user-0.2.22.tar.gz

---
+++ packages/bus-user/bus_user-0.2.22.tar.gz/bus_user-0.2.22/bus_user/__init__.py
@@ -0,0 +1,10 @@
+from .history import *
+from .serial_client import *
+from .serial_server import *
+from .serial_derivatives import *
+
+# from .bus_i2c import *  # import only Linux supported modul return exx
+try:
+    from .i2c_client import *    # import only Linux supported modul return exx
+except:
+    pass
