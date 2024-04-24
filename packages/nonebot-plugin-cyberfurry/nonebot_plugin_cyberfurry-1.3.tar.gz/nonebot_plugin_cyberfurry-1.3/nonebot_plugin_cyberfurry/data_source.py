2fac953e9225218e480cc9179b947dcd18ed7e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:06:44 +0000

    Add package nonebot-plugin-cyberfurry/1.3/nonebot_plugin_cyberfurry-1.3.tar.gz

---
+++ packages/nonebot-plugin-cyberfurry/nonebot_plugin_cyberfurry-1.3.tar.gz/nonebot_plugin_cyberfurry-1.3/nonebot_plugin_cyberfurry/data_source.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class aidata_demo(BaseModel):
+    name :      str ="幼龙云"     #easycyberfurry_001
+    ethnic :    str ="龙"       #easycyberfurry_001
+    cfConAge :  str ="child"    #easycyberfurry_001
+    cfConStyle: str ="chilly"    #easycyberfurry_001
+    story :     str ="幼龙云端核心服务器集群中诞生的智慧生命体"         #easycyberfurry_001
+    promptPatch:str =""         #yinyingllm_v2
+    model :     str ="yinyingllm-v2" #yinyingllm_v1_v3    
+
+class userdata_demo(BaseModel):
+    user_id:    str =""
+    name:       str =""
+    ethnic:     str =""
