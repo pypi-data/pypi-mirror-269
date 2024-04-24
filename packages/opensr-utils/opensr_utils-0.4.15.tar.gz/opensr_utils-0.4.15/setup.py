4bd2829771ac36b418e0aae2cd8fdd054c79d10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:05:00 +0000

    Add package opensr-utils/0.4.15/opensr_utils-0.4.15.tar.gz

---
+++ packages/opensr-utils/opensr_utils-0.4.15.tar.gz/opensr_utils-0.4.15/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# read the contents of README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='opensr-utils',
+    version='0.4.15',
+    author = "Simon Donike, Cesar Aybar, Luis Gomez Chova, Freddie Kalaitzis",
+    author_email = "accounts@donike.net",
+    description = "Utilities supporting the ESA opensr-model package for Super-Resolution of Senintel-2 Imagery",
+    url = "https://isp.uv.es/opensr/",
+    project_urls={'Source Code': 'https://github.com/ESAopenSR/opensr-utils'},
+    license='MIT',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+	'numpy==1.23.5',
+	'einops==0.6.0',
+	'rasterio==1.3.6',
+	'tqdm==4.64.1',
+	'torch==1.13.1',
+	'scikit-image==0.19.3',
+        'torchvision==0.14.1',
+        'pytorch-lightning==1.9.0'],
+)
