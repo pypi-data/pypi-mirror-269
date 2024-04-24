d81c5899ed0c8710ade2cbb6cafb3f8412701d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:06:17 +0000

    Add package chai-cli/0.3.0/chai_cli-0.3.0.tar.gz

---
+++ packages/chai-cli/chai_cli-0.3.0.tar.gz/chai_cli-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='chai-cli',
+    version='0.3.0',
+    author='Ritik Sahni',
+    author_email='ritiksahni0203@gmail.com',
+    description='AI in your CLI.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['ai', 'openai', 'cli'],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'openai',
+        'argparse',
+        'pyperclip'
+    ],
+    entry_points={
+        'console_scripts': [
+            'chai = chai:main',
+        ]
+    }
+)
+
