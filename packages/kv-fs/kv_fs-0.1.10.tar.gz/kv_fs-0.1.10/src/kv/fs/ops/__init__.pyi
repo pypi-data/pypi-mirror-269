2c0f550fcc9b572c94e0ac7c64d0c6c45425761b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 16:04:46 +0000

    Add package kv-fs/0.1.10/kv_fs-0.1.10.tar.gz

---
+++ packages/kv-fs/kv_fs-0.1.10.tar.gz/kv_fs-0.1.10/src/kv/fs/ops/__init__.pyi
@@ -0,0 +1,5 @@
+from .ops import insert, update, read, delete, filenames, files, ensure_path, append, move, copy
+
+__all__ = [
+  'insert', 'update', 'read', 'delete', 'filenames', 'files', 'ensure_path', 'append', 'move', 'copy'
+]
