99209b5995e11ccc0c09494c7348f665cf0bd7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 06:04:24 +0000

    Add package field-generator/0.2.1/field-generator-0.2.1.tar.gz

---
+++ packages/field-generator/field-generator-0.2.1.tar.gz/field-generator-0.2.1/README.md
@@ -0,0 +1,1 @@
+This code was made for our Slope Field Generator project in CS 488 for Liz to give to their Calc 1 class.
---
+++ packages/field-generator/field-generator-0.2.1.tar.gz/field-generator-0.2.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="field-generator",
+    version="0.2.1",
+    author="EQIKE",
+    author_email="lc20-0098@lclark.edu",
+    packages=["field_generator"],
+    description="A simple slope field generator",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamawestruck/Slope-Field-Generator",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=["matplotlib", "scipy"]
+)
