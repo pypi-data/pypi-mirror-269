42929fd4d580002be1ea2b967df8d5aa4ec1d202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:05:09 +0000

    Add package numerous/0.0.39/numerous-0.0.39.tar.gz

---
+++ packages/numerous/numerous-0.0.39.tar.gz/numerous-0.0.39/src/numerous/__init__.py
@@ -0,0 +1,6 @@
+"""The python SDK for numerous."""
+
+__all__ = ("action", "app", "container", "field", "html", "slider")
+__version__ = "0.0.39"
+
+from .apps import action, app, container, field, html, slider
