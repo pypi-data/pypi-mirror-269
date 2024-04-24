5350cdc9ae410a0178e8e50b17757a2d78bd0623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 16:05:18 +0000

    Add package sensestreet/1.0.6/sensestreet-1.0.6.tar.gz

---
+++ packages/sensestreet/sensestreet-1.0.6.tar.gz/sensestreet-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='sensestreet',
+    version='1.0.6',
+    license='Apache',
+    packages=find_packages('src'),
+    author="Sense Street",
+    author_email="engineering@sensestreet.com",
+    url="https://sensestreet.com",
+    package_dir={'': 'src'},
+    install_requires=[
+          'requests',
+          'PyJWT',
+          'cryptography'
+      ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+
+)
