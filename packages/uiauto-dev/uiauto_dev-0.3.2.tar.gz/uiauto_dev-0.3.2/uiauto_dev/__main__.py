c94c42ff10f9811883ccbfef832ae8d47eaf2956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 04:05:11 +0000

    Add package uiauto-dev/0.3.2/uiauto_dev-0.3.2.tar.gz

---
+++ packages/uiauto-dev/uiauto_dev-0.3.2.tar.gz/uiauto_dev-0.3.2/uiauto_dev/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Created on Sun Feb 18 2024 14:20:15 by codeskyblue
+"""
+
+from uiauto_dev.cli import main
+
+if __name__ == "__main__":
+    main()
