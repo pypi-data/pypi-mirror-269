5e0573c4de39c86ecd3b9a5433237a18b082bc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 15:04:07 +0000

    Add package pytzx/0.5.0/pytzx-0.5.0.tar.gz

---
+++ packages/pytzx/pytzx-0.5.0.tar.gz/pytzx-0.5.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytzx',
+    version='0.5.0',
+    description='A simple API for creating TZX files in Python',
+    maintainer='Sergei Morozov',
+    maintainer_email='morozov@tut.by',
+    url='https://github.com/morozov/pytzx',
+    license='GPLv2',
+    packages=find_packages(),
+    python_requires='>=3',
+)
