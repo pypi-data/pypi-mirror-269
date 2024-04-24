8876813c121a6c5bbf02d6b7b0e7268bfc2593a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 06:04:24 +0000

    Add package prettymapp/0.3.0/prettymapp-0.3.0.tar.gz

---
+++ packages/prettymapp/prettymapp-0.3.0.tar.gz/prettymapp-0.3.0/README.md
@@ -0,0 +1,84 @@
+# prettymapp 🖼️
+
+**Prettymapp is a webapp and Python package to create beautiful maps from OpenStreetMap data**
+
+---
+<h3 align="center">
+    🎈 Try it out here: <a href="https://prettymapp.streamlit.app/">prettymapp on streamlit 🎈 </a>
+</h3>
+
+---
+
+
+
+<p align="center">
+    <a href="https://prettymapp.streamlit.app/"><img src="./streamlit-prettymapp/example_prints/demo.gif" width=700></a>
+</p>
+
+<br>
+
+<table>
+    <tr><td><img src="./streamlit-prettymapp/example_prints/macau.png"></td><td><img src="./streamlit-prettymapp/example_prints/barcelona.png"></td></tr>
+</table>
+
+## Based on the prettymaps project
+
+Prettymapp is based on a rewrite of the fantastic [prettymaps](https://github.com/marceloprates/prettymaps) project by
+[@marceloprates](https://github.com/marceloprates). All credit for the original idea, designs and implementation go to him.
+The prettymapp rewrite focuses on speed and adapted configuration to interface with the webapp.
+It drops more complex configuration options in favour of improved speed, reduced code complexity and 
+simplified configuration interfaces. It is partially tested and adds a [streamlit](https://streamlit.io/) webapp component.
+
+## Running the app locally
+
+```bash
+git clone https://github.com/chrieke/prettymapp.git
+cd prettymapp
+pip install -r streamlit-prettymapp/requirements.txt
+streamlit run streamlit-prettymapp/app.py
+```
+
+## Python package
+
+You can also use prettymapp without the webapp, directly in Python. This lets you customize the functionality or 
+build your own application.
+
+**Installation:**
+
+```bash
+pip install prettymapp
+```
+
+**Define the area, download and plot the OSM data:**
+
+```python
+from prettymapp.geo import get_aoi
+from prettymapp.osm import get_osm_geometries
+from prettymapp.plotting import Plot
+from prettymapp.settings import STYLES
+
+aoi = get_aoi(address="Praça Ferreira do Amaral, Macau", radius=1100, rectangular=False)
+df = get_osm_geometries(aoi=aoi)
+
+fig = Plot(
+    df=df,
+    aoi_bounds=aoi.bounds,
+    draw_settings=STYLES["Peach"]
+).plot_all()
+
+fig.savefig("map.jpg")
+```
+
+You can also plot exported OSM XML files e.g. from openstreetmap.org:
+
+```python
+from prettymapp.osm import get_osm_geometries_from_xml
+
+df = get_osm_geometries_from_xml(filepath="Berlin.osm")
+aoi_bounds = df.total_bounds
+...
+```
+
+To customize the map appearance, use the additional arguments of the [`Plot`](plotting.py#L36) class (e.g. `shape`, 
+`contour_width` etc.). Check the preconfigured [styles](prettymapp/settings.py#L35) and 
+webapp [examples](streamlit-prettymapp/examples.json) for inspiration.
---
+++ packages/prettymapp/prettymapp-0.3.0.tar.gz/prettymapp-0.3.0/prettymapp/osm.py
@@ -0,0 +1,89 @@
+from typing import Union
+from pathlib import Path
+
+from osmnx.features import features_from_polygon, features_from_xml
+from osmnx import settings
+from geopandas import clip, GeoDataFrame
+from shapely.geometry import Polygon
+
+from prettymapp.geo import explode_multigeometries
+from prettymapp.settings import LC_SETTINGS
+
+settings.use_cache = True
+settings.log_console = False
+
+
+def get_osm_tags():
+    """
+    Get relevant OSM tags for use with prettymapp
+    """
+    tags: dict = {}
+    for d in LC_SETTINGS.values():  # type: ignore
+        for k, v in d.items():  # type: ignore
+            try:
+                tags.setdefault(k, []).extend(v)
+            except TypeError:  # e.g. "building": True
+                tags[k] = v
+    return tags
+
+
+def cleanup_osm_df(df: GeoDataFrame, aoi: Union[Polygon, None] = None) -> GeoDataFrame:
+    """
+    Cleanup of queried osm geometries to relevant level for use with prettymapp
+    """
+    df = df.droplevel(level=0)
+    df = df[~df.geometry.geom_type.isin(["Point", "MultiPoint"])]
+    if aoi is not None:
+        df = clip(df, aoi)
+    df = explode_multigeometries(df)
+
+    df["landcover_class"] = None
+    for lc_class, osm_tags in LC_SETTINGS.items():
+        tags_in_columns = list(set(osm_tags.keys()).intersection(list(df.columns)))  # type: ignore
+        mask_lc_class = df[tags_in_columns].notna().sum(axis=1) != 0
+        # Remove mask elements that belong to other subtag
+        listed_osm_tags = {
+            k: v
+            for k, v in osm_tags.items()  # type: ignore
+            if isinstance(v, list) and k in tags_in_columns
+        }
+        for tag, subtags in listed_osm_tags.items():
+            mask_from_different_subtag = ~df[tag].isin(subtags) & df[tag].notna()
+            mask_lc_class[mask_from_different_subtag] = False
+        df.loc[mask_lc_class, "landcover_class"] = lc_class
+    # Drop not assigned elements (part of multiple classes)
+    df = df[~df["landcover_class"].isnull()]
+    df = df.drop(
+        df.columns.difference(["geometry", "landcover_class", "highway"]), axis=1
+    )
+
+    return df
+
+
+def get_osm_geometries(aoi: Polygon) -> GeoDataFrame:
+    """
+    Query OSM features within a polygon geometry.
+
+    Args:
+        aoi: Polygon geometry query boundary.
+    """
+    tags = get_osm_tags()
+    df = features_from_polygon(polygon=aoi, tags=tags)
+    df = cleanup_osm_df(df, aoi)
+    return df
+
+
+def get_osm_geometries_from_xml(
+    filepath: Union[str, Path], aoi: Union[Polygon, None] = None
+) -> GeoDataFrame:
+    """
+    Query OSM features in an OSM-formatted XML file.
+
+    Args:
+        filepath: path to file containing OSM XML data
+        aoi: Optional geographic boundary to filter elements
+    """
+    tags = get_osm_tags()
+    df = features_from_xml(filepath, polygon=aoi, tags=tags)
+    df = cleanup_osm_df(df, aoi)
+    return df
