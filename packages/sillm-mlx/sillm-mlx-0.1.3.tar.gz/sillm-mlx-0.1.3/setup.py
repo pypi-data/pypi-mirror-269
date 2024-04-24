408dfab7fd2841d14b118e4ced6805257ee20888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:05:13 +0000

    Add package sillm-mlx/0.1.3/sillm-mlx-0.1.3.tar.gz

---
+++ packages/sillm-mlx/sillm-mlx-0.1.3.tar.gz/sillm-mlx-0.1.3/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+pkg_dir = pathlib.Path(__file__).parent.resolve()
+readme = (pkg_dir / "README.md").read_text(encoding="utf-8")
+requirements = (pkg_dir / "requirements.txt").read_text(encoding="utf-8").splitlines()
+requirements_server = (pkg_dir / "requirements-server.txt").read_text(encoding="utf-8").splitlines()
+
+setup(
+    name = "sillm-mlx",
+    version = "0.1.3",
+    description = "Running and training LLMs on Apple Silicon via MLX",
+    long_description = readme,
+    long_description_content_type = "text/markdown",
+    readme="README.md",
+    url = "https://github.com/armbues/SiLLM",
+    install_requires=requirements,
+    extras_require={
+        "server": requirements_server
+    },
+    packages = find_packages(exclude="examples"),
+    include_package_data=True,
+    package_data = {
+        'sillm': ['templates/*.jinja']
+    },
+    python_requires = ">=3.9"
+)
---
+++ packages/sillm-mlx/sillm-mlx-0.1.3.tar.gz/sillm-mlx-0.1.3/sillm/utils/common.py
@@ -0,0 +1,21 @@
+import numpy as np
+import mlx.core as mx
+
+import yaml
+
+def seed(seed):
+    """
+    Seed random number generators.
+    
+    Args:
+        seed: The seed value.
+    """
+    mx.random.seed(seed)
+    np.random.seed(seed)
+
+def load_yaml(fpath, args):
+    with open(fpath, "r") as f:
+        config = yaml.safe_load(f)
+
+        for key, value in config.items():
+            setattr(args, key, value)
