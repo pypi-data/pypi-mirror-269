96fa7a3fe2d39501bc8290e7933e7edeeed34844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 07:06:42 +0000

    Add package chess2vec/0.3.1b2/chess2vec-0.3.1b2.tar.gz

---
+++ packages/chess2vec/chess2vec-0.3.1b2.tar.gz/chess2vec-0.3.1b2/chess2vec/utils/__init__.py
@@ -0,0 +1,8 @@
+def convert_return(_type, **ckwargs):
+    def decorator(fn):
+        def wrapper(*args, **kwargs):
+            return _type(fn(*args, **kwargs), **ckwargs)
+
+        return wrapper
+
+    return decorator
