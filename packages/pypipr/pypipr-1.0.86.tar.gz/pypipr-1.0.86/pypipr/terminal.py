3fef45bb576d1ea1e09ff8981abf4b6cea373c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 03:04:28 +0000

    Add package pypipr/1.0.86/pypipr-1.0.86.tar.gz

---
+++ packages/pypipr/pypipr-1.0.86.tar.gz/pypipr-1.0.86/pypipr/terminal.py
@@ -0,0 +1,66 @@
+import pypipr
+
+
+def main():
+    m = [x for x in dir(pypipr) if not x.startswith("__")]
+    a = pypipr.iargv(1)
+    pd = False
+    while isinstance(m, list):
+        if a is None:
+            pd = True
+            for i, v in dict(enumerate(m)).items():
+                print(f"{i}. {v}")
+            pypipr.print_colorize("Masukan Nomor Urut atau Nama Fungsi : ", text_end="")
+            a = input()
+            print()
+            pypipr.exit_if_empty(len(a))
+
+        if a.isdigit():
+            m = pypipr.get_from_index(m, int(a))
+        else:
+            m = [v for v in m if v.__contains__(a)]
+            if len(m) == 1:
+                m = pypipr.get_from_index(m, 0)
+        pypipr.exit_if_empty(m)
+        a = None if a != m else m
+
+    f = getattr(pypipr, m)
+
+    if pd:
+        pypipr.print_colorize(m)
+        print(f.__doc__)
+
+    if pypipr.inspect.isclass(f):
+        pypipr.print_log("Class tidak dapat dijalankan.")
+    elif pypipr.inspect.ismodule(f):
+        pypipr.print_log("Module tidak dapat dijalankan.")
+    elif pypipr.inspect.isfunction(f):
+        s = pypipr.inspect.signature(f)
+
+        if not a:
+            print(m, end="")
+            pypipr.print_colorize(s)
+
+        k = {}
+        for i, v in s.parameters.items():
+            print(f"{i} [{v.default}] : ", end="")
+            o = input()
+            if len(o):
+                try:
+                    k[i] = eval(o)
+                except Exception:
+                    pypipr.print_colorize(
+                        "Input harus dalam syntax python.",
+                        color=pypipr.colorama.Fore.RED,
+                    )
+
+        # print()
+        f = f(**k)
+    else:
+        pass
+
+    pypipr.iprint(f)
+
+
+if __name__ == "__main__":
+    main()
