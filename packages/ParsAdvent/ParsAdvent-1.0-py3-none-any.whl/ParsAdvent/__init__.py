2ffc115e2b55e6d861a62f91b369dd0540d1a7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:25 +0000

    Add package ParsAdvent/1.0/ParsAdvent-1.0-py3-none-any.whl

---
+++ packages/ParsAdvent/ParsAdvent-1.0-py3-none-any.whl/ParsAdvent/__init__.py
@@ -0,0 +1,7 @@
+from .Funciones import Token
+from .Funciones import TablaSimbolos
+from .Funciones import Personaje
+from .Funciones import Texto
+from .Funciones import Lugar
+from .Funciones import Mundo
+from .Funciones import Comunicador
