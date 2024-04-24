4b5f0cd2f44a59645fb0ff1d1a329b322061654d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 15:04:17 +0000

    Add package dataidea/0.2.5/dataidea-0.2.5.tar.gz

---
+++ packages/dataidea/dataidea-0.2.5.tar.gz/dataidea-0.2.5/dataidea/statistics.py
@@ -0,0 +1,6 @@
+from .packages import *
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif as fClassif
+from sklearn.feature_selection import f_regression as fRegression
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import RFE
