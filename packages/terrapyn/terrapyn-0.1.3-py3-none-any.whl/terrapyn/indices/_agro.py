240c9ee555be072b45e2764b45a7c1b7dd53838d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 05:04:39 +0000

    Add package terrapyn/0.1.3/terrapyn-0.1.3-py3-none-any.whl

---
+++ packages/terrapyn/terrapyn-0.1.3-py3-none-any.whl/terrapyn/indices/_agro.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def growing_degree_days(
+    tmax: np.ndarray = None, tmin: np.ndarray = None, t_base: float = 0, t_cutoff: float = None
+) -> np.ndarray:
+    """
+    Calculate the modified growing (thermal) degree days (GDD/TDD) from daily maximum and minimum temperatures.
+    If `t_cutoff` is given, `tmax` is thresholded to `t_cutoff`. If the daily mean of the adjusted tmax and
+    tmin is below `t_base`, the daily value is set to 0.
+
+    Args:
+        tmax: Daily maximum temperature.
+        tmin: Daily minimum temperature.
+        t_base: Base temperature, below which the rate of growth or development is zero.
+        t_cutoff: Upper developmental threshold/cutoff temperature, above which the rate of growth or
+        development begins to decrease or stop.
+    """
+    if tmax is None or tmin is None:
+        raise ValueError("Both tmax and tmin must be provided.")
+
+    if t_base >= t_cutoff:
+        raise ValueError("t_base must be less than t_cutoff.")
+
+    gdd = (((tmax.clip(max=t_cutoff) + tmin) / 2) - t_base).clip(min=0)
+
+    return gdd
