0bc1ba71bbda24eb666a3c4e051fe3b623544252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-24 16:05:17 +0000

    Add package multidst/0.1.2/multidst-0.1.2.tar.gz

---
+++ packages/multidst/multidst-0.1.2.tar.gz/multidst-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='multidst',
+    version='0.1.2',
+    packages=find_packages(),  # Automatically find all packages and subpackages
+    install_requires=[
+        'numpy', 
+        'pandas',
+        'scipy',
+        'statsmodels',
+        'matplotlib'
+    ],
+    extras_require = {
+        "dev":["twine>4.0.2"],
+    },
+    python_requires=">=3.10",
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
+
