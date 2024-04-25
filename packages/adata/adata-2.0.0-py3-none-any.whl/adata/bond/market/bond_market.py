f8b20f07cda51da5a36dfc3ccf4b11a7e00f3992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:08:53 +0000

    Add package adata/2.0.0/adata-2.0.0-py3-none-any.whl

---
+++ packages/adata/adata-2.0.0-py3-none-any.whl/adata/bond/market/bond_market.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@desc:
+http://www.iwencai.com/unifiedwap/result?typed=1&preParams=&ts=1&f=1&qs=1&selfsectsn=&querytype=&searchfilter=&tid=stockpick&w=127093
+
+@author: 1nchaos
+@time:2023/4/5
+@log: 
+"""
+
+
+class BondMarket(object):
+    """bond 行情"""
+
+    def __init__(self) -> None:
+        super().__init__()
