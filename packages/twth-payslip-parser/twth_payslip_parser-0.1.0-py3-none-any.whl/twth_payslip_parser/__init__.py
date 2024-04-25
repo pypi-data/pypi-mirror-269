df836487f75ae5476a6ce91bdbd163e439cc37aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:06:16 +0000

    Add package twth-payslip-parser/0.1.0/twth_payslip_parser-0.1.0-py3-none-any.whl

---
+++ packages/twth-payslip-parser/twth_payslip_parser-0.1.0-py3-none-any.whl/twth_payslip_parser/__init__.py
@@ -0,0 +1,19 @@
+import argparse
+import sys
+import json
+import os
+
+from PyPDF2 import PdfReader
+from twth_payslip_parser.parser import parse
+from twth_payslip_parser.decrypter import decrypt
+
+
+def main():
+    parsed = parse(
+        decrypt(
+            PdfReader(sys.argv[1]),
+            os.getenv("PDF_PASSWORD")
+        )
+    )
+
+    print(json.dumps(parsed, default=str))
