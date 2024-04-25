90f47b779021131b3191c1a0f1bf225feeaafa57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 07:07:02 +0000

    Add package np-upload-validation/0.1.27/np_upload_validation-0.1.27.tar.gz

---
+++ packages/np-upload-validation/np_upload_validation-0.1.27.tar.gz/np_upload_validation-0.1.27/tests/test_npc.py
@@ -0,0 +1,12 @@
+import pytest
+
+from np_upload_validation import npc
+
+
+@pytest.mark.onprem
+@pytest.mark.parametrize("args, expected", [
+    (("DRpilot_686176_20231207", ), True),
+    (("686176_2023-12-07", ), True),
+])
+def test_is_session_id(args, expected) -> None:  # noqa: ANN001
+    assert npc.is_session_id(*args) == expected
