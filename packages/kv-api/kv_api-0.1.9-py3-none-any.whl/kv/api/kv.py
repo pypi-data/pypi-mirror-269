de9a0841b21c00a59f16e6fdb8720f04a81a77ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:30 +0000

    Add package kv-api/0.1.9/kv_api-0.1.9-py3-none-any.whl

---
+++ packages/kv-api/kv_api-0.1.9-py3-none-any.whl/kv/api/kv.py
@@ -0,0 +1,63 @@
+from typing import TypeVar, Generic, overload, Literal, AsyncIterable
+from abc import ABC, abstractmethod
+from haskellian import Either, Left, IsLeft, either as E, Promise, promise as P, AsyncIter, asyn_iter as AI
+from .errors import ExistentItem, InexistentItem, DBError, InvalidData, ReadError
+
+T = TypeVar('T')
+
+class KV(ABC, Generic[T]):
+  
+  @abstractmethod
+  def insert(self, key: str, value: T) -> Promise[Either[DBError, None]]: ...
+
+  @abstractmethod
+  def read(self, key: str) -> Promise[Either[ReadError, T]]: ...
+
+  @abstractmethod
+  def delete(self, key: str) -> Promise[Either[DBError | InexistentItem, None]]: ...
+
+  @abstractmethod
+  def items(self, batch_size: int | None = None) -> AsyncIter[Either[DBError | InvalidData, tuple[str, T]]]: ...
+
+  @P.lift
+  async def has(self, key: str) -> Either[DBError, bool]:
+    try:
+      keys = await self.keys().map(E.unsafe).sync()
+      return E.Right(key in keys)
+    except IsLeft as e:
+      return E.Left(e.value)
+
+  
+  @AI.lift
+  async def keys(self, batch_size: int | None = None) -> AsyncIterable[Either[DBError, str]]:
+    async for e in self.items(batch_size):
+      yield e.fmap(lambda it: it[0])
+  
+  @AI.lift
+  async def values(self, batch_size: int | None = None) -> AsyncIterable[Either[DBError, T]]:
+    async for e in self.items(batch_size):
+      yield e.fmap(lambda it: it[1])
+
+  @overload
+  def copy(self, key: str, to: 'KV[T]', to_key: str, *, replace: Literal[False]) -> Promise[Either[DBError|InexistentItem|ExistentItem, None]]: ...
+  @overload
+  def copy(self, key: str, to: 'KV[T]', to_key: str, *, replace: bool = True) -> Promise[Either[DBError|InexistentItem, None]]: ...
+  @P.lift
+  async def copy(self, key: str, to: 'KV[T]', to_key: str, *, replace: bool = True):
+    try:
+      value = (await self.read(key)).unsafe()
+      return await to.insert(to_key, value, replace=replace)
+    except IsLeft as e:
+      return Left(e.value)
+
+  @overload
+  def move(self, key: str, to: 'KV[T]', to_key: str, *, replace: Literal[False]) -> Promise[Either[DBError|InexistentItem|ExistentItem, None]]: ...
+  @overload
+  def move(self, key: str, to: 'KV[T]', to_key: str, *, replace: bool = True) -> Promise[Either[DBError|InexistentItem, None]]: ...
+  @P.lift
+  async def move(self, key: str, to: 'KV[T]', to_key: str, *, replace: bool = True):
+    try:
+      (await self.copy(key, to, to_key, replace=replace)).unsafe()
+      (await self.delete(key)).unsafe()
+    except IsLeft as e:
+      return Left(e.value)
