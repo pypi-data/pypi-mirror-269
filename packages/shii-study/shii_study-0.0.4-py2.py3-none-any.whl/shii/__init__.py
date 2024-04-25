c3a97a0bb2a0a91e64c95bce1bca438f0e71168f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:07:03 +0000

    Add package shii-study/0.0.4/shii_study-0.0.4-py2.py3-none-any.whl

---
+++ packages/shii-study/shii_study-0.0.4-py2.py3-none-any.whl/shii/__init__.py
@@ -0,0 +1,3 @@
+from shii.src.study import study, study_return_text, no_study_return_text, study_say
+__all__ = ['study', 'study_return_text', 'study_say', 'no_study_return_text', 'del_study']
+version = '0.0.4'
