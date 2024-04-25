e7c4980c0655f15ec1e3f9fb3aa103e9ed0fc717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 14:04:49 +0000

    Add package geneweaver-core/0.10.0a0/geneweaver_core-0.10.0a0-py3-none-any.whl

---
+++ packages/geneweaver-core/geneweaver_core-0.10.0a0-py3-none-any.whl/geneweaver/core/schema/score.py
@@ -0,0 +1,28 @@
+"""Pydantic schema for defining score types."""
+
+from typing import Optional
+
+from geneweaver.core.enum import ScoreType
+from pydantic import BaseModel
+
+
+class GenesetScoreType(BaseModel):
+    """Pydantic schema for defining score types."""
+
+    score_type: ScoreType
+    threshold_low: Optional[float] = None
+    threshold: float = 0.05
+
+    def __str__(self: "GenesetScoreType") -> str:
+        """Return a string representation of the score type."""
+        name = self.score_type.name.title().replace("_", "-")
+        if self.threshold_low:
+            return f"{self.threshold_low} < {name} < {self.threshold}"
+        return f"{name} < {self.threshold}"
+
+    def threshold_as_db_string(self) -> str:
+        """Return a string representation of the score type for the database."""
+        if self.threshold_low is not None:
+            return f"{self.threshold_low},{self.threshold}"
+        else:
+            return str(self.threshold)
