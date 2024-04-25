b1ca428663b40d33d579622225490581f6daf428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:06:08 +0000

    Add package chess2vec/0.3.6/chess2vec-0.3.6-py3-none-any.whl

---
+++ packages/chess2vec/chess2vec-0.3.6-py3-none-any.whl/chess2vec/components.py
@@ -0,0 +1,86 @@
+import abc
+from abc import ABC
+from typing import Iterable
+
+import chess
+import chess.pgn
+import numpy as np
+
+from chess2vec import utils
+
+
+class Component(ABC):
+    @abc.abstractmethod
+    def __call__(self, board: chess.Board): ...
+
+    def __repr__(self) -> str:
+        return f"{'.'.join((self.__module__, self.__class__.__qualname__))}()"
+
+
+class MetaComponent(ABC):
+    @abc.abstractmethod
+    def __call__(self, game: chess.pgn.Game): ...
+
+    def __repr__(self) -> str:
+        return f"{'.'.join((self.__module__, self.__class__.__qualname__))}()"
+
+
+class Composition:
+    def __init__(
+        self, comp: Iterable[Component] = None, meta: Iterable[MetaComponent] = None
+    ) -> None:
+        self.comp = tuple(comp) if comp else tuple()
+        self.meta = tuple(meta) if meta else tuple()
+
+    def compose(self, game: chess.pgn.Game):
+        board = game.board()
+        yield tuple(c(board) for c in self.comp)
+
+        for move in game.mainline_moves():
+            board.push(move)
+            yield tuple(c(board) for c in self.comp)
+
+    def __call__(self, game: chess.pgn.Game):
+        return {
+            "meta": tuple(m(game) for m in self.meta),
+            "comp": tuple(self.compose(game)),
+        }
+
+    def __repr__(self) -> str:
+        return f"{'.'.join((self.__module__, self.__class__.__qualname__))}(comp={self.comp}, meta={self.meta})"
+
+
+class Actions(Component):
+    @utils.convert_result(np.array, dtype=np.uint16)
+    @utils.convert_result(list)
+    def __call__(self, board):
+
+        for move in board.legal_moves:
+            pt = board.piece_type_at(move.from_square)
+
+            if not board.turn:
+                move.from_square ^= 0x38
+                move.to_square ^= 0x38
+
+            yield 64 * (64 * (pt - 1) + move.from_square) + move.to_square
+
+
+class Pieces(Component):
+    def __init__(self, relative=True) -> None:
+        self.relative = relative
+
+    @utils.convert_result(np.array, dtype=np.uint16)
+    @utils.convert_result(list)
+    def __call__(self, board):
+        for sq in chess.scan_reversed(board.occupied):
+            piece = board.piece_at(sq)
+
+            if self.relative and not board.turn:
+                sq ^= 0x38
+
+            yield 64 * (6 * (int(board.turn) ^ piece.color) + piece.piece_type) + sq
+
+
+class FEN(Component):
+    def __call__(self, board) -> str:
+        return board.fen()
---
+++ packages/chess2vec/chess2vec-0.3.6-py3-none-any.whl/chess2vec/utils/__init__.py
@@ -0,0 +1,36 @@
+import gzip
+import pickle
+
+
+def convert_result(_type, **ckwargs):
+    def decorator(fn):
+        def wrapper(*args, **kwargs):
+            return _type(fn(*args, **kwargs), **ckwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def load_gzip(file):
+    with gzip.open(file) as f:
+        while True:
+            try:
+                yield pickle.load(f)
+            except EOFError:
+                return
+
+
+def dump_gzip(obj, file, mode, batch_size=2**16):
+    with gzip.open(file, mode) as f:
+        for batch in batched(obj, batch_size):
+            pickle.dump(batch, f)
+
+
+def batched(obj, batch_size):
+    for idx in range(0, len(obj), batch_size):
+        yield obj[idx : idx + batch_size]
+
+
+def flatten(obj):
+    yield from (pos for game in obj for pos in game)
