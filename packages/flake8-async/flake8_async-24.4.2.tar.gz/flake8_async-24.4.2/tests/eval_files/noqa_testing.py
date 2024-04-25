b9ec490b904e2c718b01583baec42a5e9b053803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:08:54 +0000

    Add package flake8-async/24.4.2/flake8_async-24.4.2.tar.gz

---
+++ packages/flake8-async/flake8_async-24.4.2.tar.gz/flake8_async-24.4.2/tests/eval_files/noqa_testing.py
@@ -0,0 +1,8 @@
+# AUTOFIX
+# ARG --enable=ASYNC911
+import trio
+
+
+async def foo_0():
+    yield  # ASYNC911: 4, "yield", Statement("function definition", lineno-1)
+    await trio.lowlevel.checkpoint()
