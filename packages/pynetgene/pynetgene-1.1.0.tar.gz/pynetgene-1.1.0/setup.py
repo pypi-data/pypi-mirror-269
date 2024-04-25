7e8315b7371f3ce7904dfaa15bd86f825cd07d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:56 +0000

    Add package pynetgene/1.1.0/pynetgene-1.1.0.tar.gz

---
+++ packages/pynetgene/pynetgene-1.1.0.tar.gz/pynetgene-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pynetgene',                      # The name of your package
+    version='1.1.0',                       # The current version of the package
+    author="Catalin Baba",                 # The name of the package author
+    author_email="catalin.viorelbaba@gmail.com",  # Email address of the author
+    packages=find_packages(),              # Automatically find all packages (Python directories with an __init__.py file)
+    install_requires=[                     # List of dependencies needed to run the package
+        'pytest>=8.1.1',                   # Specify minimum version requirement for pytest
+    ],
+    description="PyNetgene: A Python Library for Building the Genetic Algorithm"  # A short description of the package
+)
