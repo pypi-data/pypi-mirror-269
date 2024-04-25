d49189375cba3b96c6d2a1dab658e69a94ee9bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:09:04 +0000

    Add package ctypes-windows-sdk/0.0.11/ctypes_windows_sdk-0.0.11-py3-none-any.whl

---
+++ packages/ctypes-windows-sdk/ctypes_windows_sdk-0.0.11-py3-none-any.whl/cwinsdk/shared/guiddef.py
@@ -0,0 +1,20 @@
+from ctypes import POINTER, Structure, c_ubyte, c_ulong, c_ushort
+
+
+class GUID(Structure):
+    _fields_ = [
+        ("Data1", c_ulong),
+        ("Data2", c_ushort),
+        ("Data3", c_ushort),
+        ("Data4", c_ubyte * 8),
+    ]
+
+
+IID = GUID
+LPIID = POINTER(IID)
+CLSID = GUID
+LPCLSID = POINTER(CLSID)
+FMTID = GUID
+LPFMTID = POINTER(FMTID)
+REFCLSID = POINTER(IID)  # const POINTER
+REFIID = POINTER(IID)  # const POINTER
