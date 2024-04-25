5bee7d1b781a323fbb7f2c0b8a260de83e7f81ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:16 +0000

    Add package NEURON-nightly/9.0a1.dev223/NEURON_nightly-9.0a1.dev223-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/NEURON-nightly/NEURON_nightly-9.0a1.dev223-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/neuron/.data/include/nrnmpiuse.h
@@ -0,0 +1,19 @@
+#ifndef usenrnmpi_h
+#define usenrnmpi_h
+
+/* define to 1 if you want MPI specific features activated */
+#define NRNMPI 1
+
+/* define to 1 if you want mpi dynamically loaded instead of linked normally */
+#define NRNMPI_DYNAMICLOAD 1
+
+/* define to 1 if you want the MUSIC - MUlti SImulation Coordinator */
+/* #undef NRN_MUSIC */
+
+/* define to the dll path if you want to load automatically */
+#define DLL_DEFAULT_FNAME "aarch64/.libs/libnrnmech.so"
+
+/* define if needed */
+/* #undef ALWAYS_CALL_MPI_INIT */
+
+#endif
