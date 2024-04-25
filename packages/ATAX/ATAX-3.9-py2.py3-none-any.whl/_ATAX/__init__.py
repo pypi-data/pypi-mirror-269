616e7411932a09f405c02e8b794fe4f4e97f083c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:10:14 +0000

    Add package ATAX/3.9/ATAX-3.9-py2.py3-none-any.whl

---
+++ packages/ATAX/ATAX-3.9-py2.py3-none-any.whl/_ATAX/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2017 Christian Tanzer. All rights reserved
+# tanzer@gg32.com                                      https://www.gg32.com
+# ****************************************************************************
+# This module is part of the package ATAX.
+#
+# This module is licensed under the terms of the BSD 3-Clause License
+# <https://www.gg32.com/license/bsd_3c.html>.
+# ****************************************************************************
+#
+#++
+# Name
+#    ATAX.__init__
+#
+# Purpose
+#    Initialize package `ATAX`
+#
+# Revision Dates
+#    11-Feb-2006 (CT) Creation
+#    11-Oct-2016 (CT) Add `__version__`
+#    22-Feb-2017 (CT) Remove `__version__`
+#    ««revision-date»»···
+#--
+
+from _TFL.Package_Namespace import Package_Namespace
+
+ATAX = Package_Namespace ()
+
+del Package_Namespace
+
+### __END__ ATAX.__init__
