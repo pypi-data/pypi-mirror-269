50de7fb00ec466435d2b80d4950f13c1c9f8e500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:06:24 +0000

    Add package superT5/0.0.1/supert5-0.0.1-py3-none-any.whl

---
+++ packages/superT5/supert5-0.0.1-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,6 @@
+from src.heads.t5_heads import (T5ForConditionalGeneration, T5ForQuestionAnswering, 
+                                    T5ForSequenceClassification, T5ForTokenClassification)
+
+from src.heads.encoder_heads import (T5EncoderForQuestionAnswering, T5EncoderForTextClassification, T5ForTokenClassification)
+
+from src.model.config import T5Config
