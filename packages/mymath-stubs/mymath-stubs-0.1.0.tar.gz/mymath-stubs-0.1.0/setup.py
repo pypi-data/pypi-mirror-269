a43a0aae435841a31f49ebcca270e7f2fb7df41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:15:19 +0000

    Add package mymath-stubs/0.1.0/mymath-stubs-0.1.0.tar.gz

---
+++ packages/mymath-stubs/mymath-stubs-0.1.0.tar.gz/mymath-stubs-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+
+setup(
+    name='mymath-stubs',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github/alaamer12/package_t',
+    license='MIT',
+    author='Alaamer',
+    author_email='alaamerthefirst@gmail.com',
+    description='This is my first stub package',
+    long_description="This is my first stub package, lg",
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    keywords=['hello', 'name'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+
+)
