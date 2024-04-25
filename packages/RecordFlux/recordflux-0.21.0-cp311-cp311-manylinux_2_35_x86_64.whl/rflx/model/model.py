4deb2365258f93e2a1c4d91b498a736adc4eefab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:08:56 +0000

    Add package RecordFlux/0.21.0/recordflux-0.21.0-cp311-cp311-manylinux_2_35_x86_64.whl

---
+++ packages/RecordFlux/recordflux-0.21.0-cp311-cp311-manylinux_2_35_x86_64.whl/rflx/model/model.py
@@ -0,0 +1,273 @@
+from __future__ import annotations
+
+import itertools
+import logging
+from collections.abc import Sequence
+from dataclasses import dataclass
+from pathlib import Path
+
+from rflx import const
+from rflx.common import Base, unique, verbose_repr
+from rflx.error import RecordFluxError, Severity, Subsystem
+from rflx.identifier import ID
+
+from . import message, session, top_level_declaration, type_
+from .cache import Cache, Digest
+from .package import Package
+from .type_ import BUILTIN_TYPES
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class UncheckedModel(Base):
+    declarations: Sequence[top_level_declaration.UncheckedTopLevelDeclaration]
+    error: RecordFluxError
+
+    def checked(
+        self,
+        cache: Cache,
+        workers: int = 1,
+    ) -> Model:
+        error = RecordFluxError(self.error)
+        declarations: list[top_level_declaration.TopLevelDeclaration] = []
+
+        for d in self.declarations:
+            try:
+                unverified = d.checked(declarations, skip_verification=True)
+                digest = Digest(unverified)
+                if cache.is_verified(digest):
+                    checked = unverified
+                else:
+                    log.info("Verifying %s", d.identifier)
+                    checked = d.checked(declarations, workers=workers)
+                declarations.append(checked)
+                cache.add_verified(digest)
+            except RecordFluxError as e:  # noqa: PERF203
+                error.extend(e)
+
+        return Model(declarations, error)
+
+
+class Model(Base):
+    def __init__(
+        self,
+        declarations: Sequence[top_level_declaration.TopLevelDeclaration] | None = None,
+        error: RecordFluxError | None = None,
+    ) -> None:
+        self._declarations = declarations or []
+
+        error = error or RecordFluxError()
+        error += _check_duplicates(self._declarations)
+        self._declarations = self._add_type_dependencies(self._declarations)
+        error += _check_conflicts(self._declarations)
+        error.propagate()
+
+    def __repr__(self) -> str:
+        return verbose_repr(self, ["declarations"])
+
+    def __str__(self) -> str:
+        return "\n\n".join(self.create_specifications().values())
+
+    @property
+    def declarations(self) -> Sequence[top_level_declaration.TopLevelDeclaration]:
+        return self._declarations
+
+    @property
+    def types(self) -> list[type_.Type]:
+        return [d for d in self._declarations if isinstance(d, type_.Type)]
+
+    @property
+    def messages(self) -> list[message.Message]:
+        return [d for d in self._declarations if isinstance(d, message.Message)]
+
+    @property
+    def refinements(self) -> list[message.Refinement]:
+        return [d for d in self._declarations if isinstance(d, message.Refinement)]
+
+    @property
+    def sessions(self) -> list[session.Session]:
+        return [d for d in self._declarations if isinstance(d, session.Session)]
+
+    @property
+    def packages(self) -> dict[ID, list[top_level_declaration.TopLevelDeclaration]]:
+        return {p: list(d) for p, d in itertools.groupby(self._declarations, lambda x: x.package)}
+
+    def create_specifications(self) -> dict[ID, str]:
+        pkgs: dict[ID, Package] = {}
+        for d in self.declarations:
+            if isinstance(d, type_.Type):
+                if not type_.is_builtin_type(d.name) and not type_.is_internal_type(d.name):
+                    pkg_name: ID = d.package
+                    pkg = pkgs.setdefault(pkg_name, Package(pkg_name))
+                    pkg.imports |= {dep.package for dep in d.direct_dependencies}
+                    pkg.declarations.append(d)
+            else:
+                pkg_name = d.package
+                pkgs.setdefault(pkg_name, Package(pkg_name)).declarations.append(d)
+        return {identifier: str(pkg) for identifier, pkg in pkgs.items()}
+
+    def write_specification_files(self, output_dir: Path) -> None:
+        """Write corresponding specification files (one per package) into given directory."""
+        for package, specification in self.create_specifications().items():
+            header = (
+                "-- style: disable = line-length\n\n"
+                if any(len(l) > 120 for l in specification.split("\n"))
+                else ""
+            )
+            (output_dir / f"{package.flat.lower()}.rflx").write_text(f"{header}{specification}")
+
+    @staticmethod
+    def _add_type_dependencies(
+        declarations: Sequence[top_level_declaration.TopLevelDeclaration],
+    ) -> list[top_level_declaration.TopLevelDeclaration]:
+        """Add missing type dependencies to list of declarations."""
+        result: list[top_level_declaration.TopLevelDeclaration] = []
+
+        for d in declarations:
+            if isinstance(d, type_.Type):
+                result.extend(d.dependencies)
+                result.append(d)
+
+            if isinstance(d, session.Session):
+                result.extend(d.direct_dependencies.values())
+                result.append(d)
+
+        return list(unique(result))
+
+
+def _check_duplicates(
+    declarations: Sequence[top_level_declaration.TopLevelDeclaration],
+) -> RecordFluxError:
+    error = RecordFluxError()
+    seen: dict[ID, top_level_declaration.TopLevelDeclaration] = {}
+
+    for d in declarations:
+        if d.package in (const.BUILTINS_PACKAGE, const.INTERNAL_PACKAGE):
+            continue
+
+        if type_.is_builtin_type(d.identifier.name) or type_.is_internal_type(d.identifier.name):
+            error.extend(
+                [
+                    (
+                        f'illegal redefinition of built-in type "{d.identifier.name}"',
+                        Subsystem.MODEL,
+                        Severity.ERROR,
+                        d.location,
+                    ),
+                ],
+            )
+
+        elif d.identifier in seen:
+            error.extend(
+                [
+                    (
+                        (
+                            f'conflicting refinement of "{d.pdu.identifier}" with'
+                            f' "{d.sdu.identifier}"'
+                            if isinstance(d, message.Refinement)
+                            else (
+                                f'name conflict for type "{d.identifier}"'
+                                if isinstance(d, type_.Type)
+                                else f'name conflict for session "{d.identifier}"'
+                            )
+                        ),
+                        Subsystem.MODEL,
+                        Severity.ERROR,
+                        d.location,
+                    ),
+                    (
+                        (
+                            "previous occurrence of refinement"
+                            if isinstance(d, message.Refinement)
+                            else f'previous occurrence of "{d.identifier}"'
+                        ),
+                        Subsystem.MODEL,
+                        Severity.INFO,
+                        seen[d.identifier].location,
+                    ),
+                ],
+            )
+        seen[d.identifier] = d
+
+    return error
+
+
+def _check_conflicts(
+    declarations: Sequence[top_level_declaration.TopLevelDeclaration],
+) -> RecordFluxError:
+    error = RecordFluxError()
+
+    for e1, e2 in [
+        (e1, e2)
+        for i1, e1 in enumerate(declarations)
+        for i2, e2 in enumerate(declarations)
+        if (
+            isinstance(e1, type_.Enumeration)
+            and isinstance(e2, type_.Enumeration)
+            and i1 < i2
+            and (
+                e1.package in (e2.package, const.BUILTINS_PACKAGE)
+                or e2.package == const.BUILTINS_PACKAGE
+            )
+        )
+    ]:
+        identical_literals = set(e2.literals) & set(e1.literals)
+
+        if identical_literals:
+            literals_message = ", ".join([f"{l}" for l in sorted(identical_literals)])
+            error.extend(
+                [
+                    (
+                        f"conflicting literals: {literals_message}",
+                        Subsystem.MODEL,
+                        Severity.ERROR,
+                        e2.location,
+                    ),
+                    *[
+                        (
+                            f'previous occurrence of "{l}"',
+                            Subsystem.MODEL,
+                            Severity.INFO,
+                            l.location,
+                        )
+                        for l in sorted(identical_literals)
+                    ],
+                ],
+            )
+
+    literals = [
+        ID(d.package * l, location=l.location)
+        for d in declarations
+        if isinstance(d, type_.Enumeration)
+        for l in d.literals
+    ]
+    name_conflicts = {
+        *(set(literals) & {d.identifier for d in declarations}),
+        *({l.name for l in literals} & {t.name for t in BUILTIN_TYPES}),
+    }
+    if name_conflicts:
+        for literal, conflicting_type in [
+            (l, d)
+            for l in literals
+            for d in declarations
+            if {l, l.name} & name_conflicts and l.name == d.identifier.name
+        ]:
+            error.extend(
+                [
+                    (
+                        f'literal "{literal.name}" conflicts with type declaration',
+                        Subsystem.MODEL,
+                        Severity.ERROR,
+                        literal.location,
+                    ),
+                    (
+                        f'conflicting type "{conflicting_type.identifier}"',
+                        Subsystem.MODEL,
+                        Severity.INFO,
+                        conflicting_type.location,
+                    ),
+                ],
+            )
+
+    return error
---
+++ packages/RecordFlux/recordflux-0.21.0-cp311-cp311-manylinux_2_35_x86_64.whl/rflx/typing_.py
@@ -0,0 +1,492 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from collections import abc
+from pathlib import Path
+from typing import ClassVar, Final, Optional, Union
+
+import attr
+
+from rflx import const
+from rflx.error import Location, RecordFluxError, Severity, Subsystem
+from rflx.identifier import ID
+
+
+@attr.s(frozen=True)
+class Bounds:
+    lower: Optional[int] = attr.ib()
+    upper: Optional[int] = attr.ib()
+
+    def __attrs_post_init__(self) -> None:
+        assert self.lower is None or self.upper is None or self.lower <= self.upper
+
+    def __bool__(self) -> bool:
+        return self.lower is not None and self.upper is not None
+
+    def __contains__(self, item: object) -> bool:
+        if isinstance(item, int):
+            if self.lower is None or self.upper is None:
+                return False
+            return self.lower <= item <= self.upper
+        if isinstance(item, Bounds):
+            if not self and not item:
+                return True
+            if (
+                self.lower is not None
+                and self.upper is not None
+                and item.lower is not None
+                and item.upper is not None
+            ):
+                return self.lower <= item.lower and self.upper >= item.upper
+        return False
+
+    def __str__(self) -> str:
+        if not self:
+            return "undefined"
+        if self.lower == self.upper:
+            return str(self.lower)
+        return f"{self.lower} .. {self.upper}"
+
+    @staticmethod
+    def union(left: Bounds, right: Bounds) -> Bounds:
+        if left.lower is None or left.upper is None or right.lower is None or right.upper is None:
+            return Bounds(None, None)
+        return Bounds(min(left.lower, right.lower), max(left.upper, right.upper))
+
+
+class Type:
+    DESCRIPTIVE_NAME: ClassVar[str]
+
+    def __str__(self) -> str:
+        return self.DESCRIPTIVE_NAME
+
+    @abstractmethod
+    def is_compatible(self, other: Type) -> bool:
+        raise NotImplementedError
+
+    def is_compatible_strong(self, other: Type) -> bool:
+        return (
+            other.is_compatible_strong(self)
+            if isinstance(other, Integer)
+            else self.is_compatible(other)
+        )
+
+    @abstractmethod
+    def common_type(self, other: Type) -> Type:
+        raise NotImplementedError
+
+
+@attr.s(frozen=True)
+class Undefined(Type):
+    DESCRIPTIVE_NAME: ClassVar[str] = "undefined type"
+
+    def is_compatible(self, _other: Type) -> bool:
+        return False
+
+    def common_type(self, _other: Type) -> Type:
+        return self
+
+
+UNDEFINED: Final = Undefined()
+
+
+@attr.s(frozen=True)
+class Any(Type):
+    DESCRIPTIVE_NAME: ClassVar[str] = "any type"
+
+    def is_compatible(self, other: Type) -> bool:
+        return not isinstance(other, Undefined)
+
+    def common_type(self, other: Type) -> Type:
+        return other
+
+
+@attr.s(frozen=True)
+class NamedType(Any):
+    identifier: ID = attr.ib(converter=ID)
+
+    def is_compatible(self, other: Type) -> bool:
+        return other == Any() or (
+            isinstance(other, self.__class__) and self.identifier == other.identifier
+        )
+
+    def common_type(self, other: Type) -> Type:
+        if other == Any() or self == other:
+            return self
+        return Undefined()
+
+
+@attr.s(frozen=True)
+class Enumeration(NamedType):
+    DESCRIPTIVE_NAME: ClassVar[str] = "enumeration type"
+    literals: abc.Sequence[ID] = attr.ib()
+    always_valid: bool = attr.ib(default=False)
+    location: Optional[Location] = attr.ib(default=None, cmp=False)
+
+    def __str__(self) -> str:
+        return f'{self.DESCRIPTIVE_NAME} "{self.identifier}"'
+
+
+@attr.s(frozen=True)
+class AnyInteger(Any):
+    DESCRIPTIVE_NAME: ClassVar[str] = "integer type"
+
+    def is_compatible(self, other: Type) -> bool:
+        return other == Any() or isinstance(other, AnyInteger)
+
+    def common_type(self, other: Type) -> Type:
+        if other == Any() or self == other:
+            return self
+        if isinstance(other, AnyInteger):
+            return other
+        return Undefined()
+
+
+@attr.s(frozen=True)
+class UndefinedInteger(AnyInteger):
+    def common_type(self, other: Type) -> Type:
+        if other == Any() or isinstance(other, AnyInteger):
+            return self
+        return Undefined()
+
+
+@attr.s(frozen=True)
+class BoundedInteger(AnyInteger):
+    bounds: Bounds = attr.ib(Bounds(None, None))
+
+
+@attr.s(frozen=True)
+class UniversalInteger(BoundedInteger):
+    DESCRIPTIVE_NAME: ClassVar[str] = "type universal integer"
+    bounds: Bounds = attr.ib(Bounds(None, None))
+
+    def __str__(self) -> str:
+        return f"{self.DESCRIPTIVE_NAME} ({self.bounds})"
+
+    def common_type(self, other: Type) -> Type:
+        if isinstance(other, UndefinedInteger):
+            return UndefinedInteger()
+        if isinstance(other, UniversalInteger) and self.bounds != other.bounds:
+            return UniversalInteger(Bounds.union(self.bounds, other.bounds))
+        if isinstance(other, Integer):
+            return other
+        if other == Any() or other == AnyInteger() or self == other:
+            return self
+        return Undefined()
+
+
+@attr.s(frozen=True)
+class Integer(BoundedInteger, NamedType):
+    DESCRIPTIVE_NAME: ClassVar[str] = "integer type"
+    identifier: ID = attr.ib(converter=ID)
+    bounds: Bounds = attr.ib(Bounds(None, None))
+    location: Optional[Location] = attr.ib(default=None, cmp=False)
+
+    def __str__(self) -> str:
+        return f'{self.DESCRIPTIVE_NAME} "{self.identifier}" ({self.bounds})'
+
+    def is_compatible_strong(self, other: Type) -> bool:
+        return (
+            self == other
+            or other == Any()
+            or other == AnyInteger()
+            or isinstance(other, UniversalInteger)
+        )
+
+    def common_type(self, other: Type) -> Type:
+        if isinstance(other, UndefinedInteger):
+            return UndefinedInteger()
+        if isinstance(other, UniversalInteger):
+            return self
+        if isinstance(other, Integer) and (
+            self.identifier != other.identifier or self.bounds != other.bounds
+        ):
+            return UndefinedInteger()
+        if other == Any() or other == AnyInteger() or self == other:
+            return self
+        return Undefined()
+
+
+@attr.s(frozen=True)
+class Composite(Any):
+    DESCRIPTIVE_NAME: ClassVar[str] = "composite type"
+    element: Type = attr.ib()
+
+
+@attr.s(frozen=True)
+class Aggregate(Composite):
+    DESCRIPTIVE_NAME: ClassVar[str] = "aggregate"
+    element: Type = attr.ib()
+
+    def __str__(self) -> str:
+        return f"{self.DESCRIPTIVE_NAME} with element {self.element}"
+
+    def is_compatible(self, other: Type) -> bool:
+        return (
+            other == Any()
+            or isinstance(other, Aggregate)
+            or (
+                isinstance(other, Sequence)
+                and (
+                    other.element == Any()
+                    or (
+                        isinstance(self.element, BoundedInteger)
+                        and isinstance(other.element, BoundedInteger)
+                        and self.element.is_compatible_strong(other.element)
+                        and self.element.bounds in other.element.bounds
+                    )
+                    or (
+                        not isinstance(self.element, BoundedInteger)
+                        and self.element.is_compatible_strong(other.element)
+                    )
+                )
+            )
+        )
+
+    def common_type(self, other: Type) -> Type:
+        if isinstance(other, Sequence) and self.is_compatible(other):
+            return other
+        if isinstance(other, Aggregate) and self.element != other.element:
+            return Aggregate(self.element.common_type(other.element))
+        if other == Any() or self == other:
+            return self
+        return Undefined()
+
+
+@attr.s(frozen=True)
+class Sequence(Composite, NamedType):
+    DESCRIPTIVE_NAME: ClassVar[str] = "sequence type"
+    identifier: ID = attr.ib(converter=ID)
+    element: Type = attr.ib()
+
+    def __str__(self) -> str:
+        return f'{self.DESCRIPTIVE_NAME} "{self.identifier}" with element {self.element}'
+
+    def is_compatible(self, other: Type) -> bool:
+        return (
+            other == Any()
+            or (
+                isinstance(other, Aggregate)
+                and (
+                    other.element == Any()
+                    or (
+                        isinstance(self.element, BoundedInteger)
+                        and isinstance(other.element, BoundedInteger)
+                        and other.element.is_compatible_strong(self.element)
+                        and other.element.bounds in self.element.bounds
+                    )
+                    or (
+                        not isinstance(self.element, BoundedInteger)
+                        and other.element.is_compatible_strong(self.element)
+                    )
+                )
+            )
+            or (
+                isinstance(other, Sequence)
+                and self.identifier == other.identifier
+                and self.element == other.element
+            )
+        )
+
+    def common_type(self, other: Type) -> Type:
+        if isinstance(other, Aggregate) and self.is_compatible(other):
+            return self
+        if other == Any() or self == other:
+            return self
+        return Undefined()
+
+
+OPAQUE = Sequence(const.INTERNAL_PACKAGE * "Opaque", Integer("Byte", Bounds(0, 255)))
+
+
+@attr.s(frozen=True)
+class Refinement:
+    field: ID = attr.ib(converter=ID)
+    sdu: Message = attr.ib()
+    package: ID = attr.ib(converter=ID)
+
+
+@attr.s(frozen=True)
+class Compound(NamedType):
+    """Base type for any type consisting of multiple fields of different types."""
+
+    DESCRIPTIVE_NAME: ClassVar[str]
+    identifier: ID = attr.ib(converter=ID)
+    field_combinations: set[tuple[str, ...]] = attr.ib(factory=set)
+    parameter_types: abc.Mapping[ID, Type] = attr.ib(factory=dict)
+    field_types: abc.Mapping[ID, Type] = attr.ib(factory=dict)
+
+    @property
+    def parameters(self) -> set[ID]:
+        return set(self.parameter_types.keys())
+
+    @property
+    def fields(self) -> set[ID]:
+        return set(self.field_types.keys())
+
+    @property
+    def types(self) -> abc.Mapping[ID, Type]:
+        return {**self.parameter_types, **self.field_types}
+
+    def __str__(self) -> str:
+        return f'{self.DESCRIPTIVE_NAME} "{self.identifier}"'
+
+
+@attr.s(frozen=True)
+class Structure(Compound):
+    DESCRIPTIVE_NAME = "structure type"
+
+
+@attr.s(frozen=True)
+class Message(Compound):
+    DESCRIPTIVE_NAME = "message type"
+    refinements: abc.Sequence[Refinement] = attr.ib(factory=list)
+    is_definite: bool = attr.ib(default=False)
+
+
+@attr.s(frozen=True)
+class Channel(Any):
+    DESCRIPTIVE_NAME: ClassVar[str] = "channel"
+    readable: bool = attr.ib()
+    writable: bool = attr.ib()
+
+    def __str__(self) -> str:
+        mode = {
+            (True, False): "readable ",
+            (False, True): "writable ",
+            (True, True): "readable and writable ",
+        }
+        return f"{mode[(self.readable, self.writable)]}{self.DESCRIPTIVE_NAME}"
+
+    def is_compatible(self, other: Type) -> bool:
+        return other == Any() or (
+            isinstance(other, self.__class__)
+            and (self.readable or not other.readable)
+            and (self.writable or not other.writable)
+        )
+
+    def common_type(self, other: Type) -> Type:
+        if other == Any() or self == other:
+            return self
+        return Undefined()
+
+
+def common_type(types: abc.Sequence[Type]) -> Type:
+    result: Type = Any()
+
+    for t in types:
+        result = result.common_type(t)
+
+    return result
+
+
+def check_type(
+    actual: Type,
+    expected: Union[Type, tuple[Type, ...]],
+    location: Optional[Location],
+    description: str,
+) -> RecordFluxError:
+    assert expected, "empty expected types"
+
+    if actual == Undefined():
+        return _undefined_type(location, description)
+
+    error = RecordFluxError()
+
+    expected_types = [expected] if isinstance(expected, Type) else list(expected)
+
+    if Undefined() not in [actual, expected] and all(
+        not actual.is_compatible(t) for t in expected_types
+    ):
+        desc = (
+            " or ".join(map(str, expected_types)) if isinstance(expected, tuple) else str(expected)
+        )
+        error.extend(
+            [
+                (f"expected {desc}", Subsystem.MODEL, Severity.ERROR, location),
+                (f"found {actual}", Subsystem.MODEL, Severity.INFO, location),
+            ],
+        )
+
+    return error
+
+
+def check_type_instance(
+    actual: Type,
+    expected: Union[type[Type], tuple[type[Type], ...]],
+    location: Optional[Location],
+    description: str = "",
+) -> RecordFluxError:
+    assert expected, "empty expected types"
+
+    if actual == Undefined():
+        return _undefined_type(location, description)
+
+    error = RecordFluxError()
+
+    if not isinstance(actual, expected) and actual != Any():
+        desc = (
+            " or ".join(e.DESCRIPTIVE_NAME for e in expected)
+            if isinstance(expected, tuple)
+            else expected.DESCRIPTIVE_NAME
+        )
+        error.extend(
+            [
+                (f"expected {desc}", Subsystem.MODEL, Severity.ERROR, location),
+                (f"found {actual}", Subsystem.MODEL, Severity.INFO, location),
+            ],
+        )
+
+    return error
+
+
+def _undefined_type(location: Optional[Location], description: str = "") -> RecordFluxError:
+    error = RecordFluxError()
+    error.extend(
+        [
+            (
+                "undefined" + (f" {description}" if description else ""),
+                Subsystem.MODEL,
+                Severity.ERROR,
+                location,
+            ),
+        ],
+    )
+    return error
+
+
+BOOLEAN = Enumeration(
+    const.BUILTINS_PACKAGE * "Boolean",
+    [ID("False"), ID("True")],
+    location=Location((0, 0), Path(str(const.BUILTINS_PACKAGE)), (0, 0)),
+)
+
+# Assumes a Bit_Length type semantically equivalent to:
+#
+# type Bit_Length is range 0 .. Integer'Last * 8.
+#
+# Potentially failing range checks will not be detected in the model, if a custom Bit_Length type
+# with different bounds is used (eng/recordflux/RecordFlux#317).
+BIT_LENGTH = Integer(
+    const.BUILTINS_PACKAGE * "Bit_Length",
+    Bounds(0, (2**31 - 1) * 8),
+    location=Location((0, 0), Path(str(const.BUILTINS_PACKAGE)), (0, 0)),
+)
+
+BASE_INTEGER = Integer(
+    const.BUILTINS_PACKAGE * "Base_Integer",
+    Bounds(0, 2**const.MAX_SCALAR_SIZE - 1),
+    location=Location((0, 0), Path(str(const.BUILTINS_PACKAGE)), (0, 0)),
+)
+
+INDEX = Integer(
+    const.BUILTINS_PACKAGE * "Index",
+    Bounds(1, 2**31 - 1),
+    location=Location((0, 0), Path(str(const.BUILTINS_PACKAGE)), (0, 0)),
+)
+
+BIT_INDEX = Integer(
+    const.BUILTINS_PACKAGE * "Bit_Index",
+    Bounds(1, BIT_LENGTH.bounds.upper),
+    location=Location((0, 0), Path(str(const.BUILTINS_PACKAGE)), (0, 0)),
+)
