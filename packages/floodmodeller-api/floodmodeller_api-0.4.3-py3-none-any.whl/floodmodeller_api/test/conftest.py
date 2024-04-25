4db0936b00f60d96b307edc828c8ee57e7851174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:07:16 +0000

    Add package floodmodeller-api/0.4.3/floodmodeller_api-0.4.3-py3-none-any.whl

---
+++ packages/floodmodeller-api/floodmodeller_api-0.4.3-py3-none-any.whl/floodmodeller_api/test/conftest.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def test_workspace():
+    return Path(os.path.dirname(__file__), "test_data")
---
+++ packages/floodmodeller-api/floodmodeller_api-0.4.3-py3-none-any.whl/floodmodeller_api/test/test_logs_lf.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
+from floodmodeller_api import IEF, LF1
+
+
+@pytest.fixture
+def lf1_fp(test_workspace):
+    return Path(test_workspace, "ex3.lf1")
+
+
+def test_lf1_info_dict(lf1_fp):
+    """LF1: Check info dictionary"""
+    lf1 = LF1(lf1_fp)
+    assert lf1.info["version"] == "5.0.0.7752"
+    assert lf1.info["max_system_volume"] == 270549
+    assert lf1.info["mass_balance_error"] == -0.03
+    assert lf1.info["progress"] == 100
+
+
+def test_lf1_report_progress(lf1_fp):
+    """LF1: Check report_progress()"""
+    lf1 = LF1(lf1_fp)
+    assert lf1.report_progress() == 100
+
+
+def test_lf1_to_dataframe(lf1_fp):
+    """LF1: Check to_dataframe()"""
+    lf1 = LF1(lf1_fp)
+    df = lf1.to_dataframe()
+    assert df.loc[df.index[0], "iter"] == 6
+    assert df.loc[df.index[-1], "outflow"] == 21.06
+    assert df.loc[df.index[4], "mass_error"] == -0.07
+
+
+def test_lf1_from_ief(lf1_fp, test_workspace):
+    """LF1: Check IEF.get_lf1()"""
+    lf1 = LF1(lf1_fp)
+
+    ief_fp = Path(test_workspace, "ex3.ief")
+    ief = IEF(ief_fp)
+    lf1_from_ief = ief.get_log()
+
+    assert lf1._filepath == lf1_from_ief._filepath
+    assert lf1.info == lf1_from_ief.info
+    pd.testing.assert_frame_equal(lf1.to_dataframe(), lf1_from_ief.to_dataframe())
