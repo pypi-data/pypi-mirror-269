3e7b06656604906e75803ee5d724d7c6c1d771fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:26 +0000

    Add package dplib-py/0.7.4/dplib_py-0.7.4-py3-none-any.whl

---
+++ packages/dplib-py/dplib_py-0.7.4-py3-none-any.whl/dplib/actions/metadata/check.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import List, Union
+
+from ... import settings, types
+from ...errors.metadata import MetadataError
+from ...helpers.data import read_data
+from ...helpers.profile import check_profile
+
+
+def check_metadata(
+    metadata: Union[str, types.IDict], *, type: types.IMetadataType
+) -> List[MetadataError]:
+    if isinstance(metadata, str):
+        metadata = read_data(metadata)
+
+    # Get default profile
+    if type == "dialect":
+        default_profile = settings.PROFILE_DEFAULT_DIALECT
+    elif type == "package":
+        default_profile = settings.PROFILE_DEFAULT_PACKAGE
+    elif type == "resource":
+        default_profile = settings.PROFILE_DEFAULT_RESOURCE
+    elif type == "schema":
+        default_profile = settings.PROFILE_DEFAULT_SCHEMA
+    else:
+        raise ValueError(f"Invalid metadata type: {type}")
+
+    # Validate metadata
+    profile = metadata.get("$schema", default_profile)
+    errors = check_profile(metadata=metadata, profile=profile)
+
+    return errors
