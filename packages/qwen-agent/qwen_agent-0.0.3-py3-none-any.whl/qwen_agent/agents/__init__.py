d2510a00c9a893732e0f5a0a22e0a8028064991e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:08:04 +0000

    Add package qwen-agent/0.0.3/qwen_agent-0.0.3-py3-none-any.whl

---
+++ packages/qwen-agent/qwen_agent-0.0.3-py3-none-any.whl/qwen_agent/agents/__init__.py
@@ -0,0 +1,30 @@
+from qwen_agent import Agent
+
+from .article_agent import ArticleAgent
+from .assistant import Assistant
+# DocQAAgent is the default solution for long document question answering.
+# The actual implementation of DocQAAgent may change with every release.
+from .doc_qa.basic_doc_qa import BasicDocQA as DocQAAgent
+from .fncall_agent import FnCallAgent
+from .group_chat import GroupChat
+from .group_chat_auto_router import GroupChatAutoRouter
+from .group_chat_creator import GroupChatCreator
+from .react_chat import ReActChat
+from .router import Router
+from .user_agent import UserAgent
+from .write_from_scratch import WriteFromScratch
+
+__all__ = [
+    'Agent',
+    'DocQAAgent',
+    'Assistant',
+    'ArticleAgent',
+    'ReActChat',
+    'Router',
+    'UserAgent',
+    'GroupChat',
+    'WriteFromScratch',
+    'GroupChatCreator',
+    'GroupChatAutoRouter',
+    'FnCallAgent',
+]
