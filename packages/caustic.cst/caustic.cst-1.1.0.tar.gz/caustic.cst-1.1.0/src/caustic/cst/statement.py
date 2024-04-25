eac0dd56061c18af885dec9980bc64f511a30014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:06:17 +0000

    Add package caustic.cst/1.1.0/caustic.cst-1.1.0.tar.gz

---
+++ packages/caustic.cst/caustic.cst-1.1.0.tar.gz/caustic.cst-1.1.0/src/caustic/cst/statement.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+'''CST nodes for statements'''
+
+#> Imports
+import typing
+from dataclasses import dataclass, field
+
+from .bases import CausticASTNode, BaseStatement
+from .block import Block, Line
+#</Imports
+
+#> Header >/
+__all__ = ('ImportStatement', 'ExportStatement',
+           'IfStatement', 'ElifStatement', 'ElseStatement',
+           'ForStatement', 'WhileStatement')
+
+_dc = dataclass(slots=True)
+
+# External-related
+@_dc
+class ImportStatement(BaseStatement):
+    '''Represents an import'''
+    target: CausticASTNode
+    i_from: CausticASTNode | None = field(default=None, kw_only=True)
+    i_as:   CausticASTNode | None = field(default=None, kw_only=True)
+@_dc
+class ExportStatement(BaseStatement):
+    '''Represents an export'''
+    target: CausticASTNode
+    e_as: str | None = field(default=None, kw_only=True)
+    extern: bool = field(default=False, kw_only=True)
+
+# Conditional-related
+@_dc
+class IfStatement(BaseStatement):
+    '''Represents an if statement'''
+    condition: CausticASTNode
+    block: Block
+    next: typing.Union['ElifStatement', 'ElseStatement'] | None = None
+class ElifStatement(IfStatement):
+    '''Represents an else-if statement'''
+    __slots__ = ()
+@_dc
+class ElseStatement(BaseStatement):
+    '''Represents an else statement'''
+    block: Block
+
+# Loop-related
+@_dc
+class ForStatement(BaseStatement):
+    '''Represents a for loop'''
+    clause: tuple[Line, Line, Line] | CausticASTNode
+    block: Block
+    for_in: bool = field(default=False, kw_only=True)
+@_dc
+class WhileStatement(BaseStatement):
+    '''Represents a while loop'''
+    clause: CausticASTNode
+    block: Block
+    do_while: bool = field(default=False, kw_only=True)
