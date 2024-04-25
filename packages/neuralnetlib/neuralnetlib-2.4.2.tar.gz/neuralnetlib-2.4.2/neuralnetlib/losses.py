20876bc1471c0eeae10f919f3f00370faa58dfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:08:58 +0000

    Add package neuralnetlib/2.4.2/neuralnetlib-2.4.2.tar.gz

---
+++ packages/neuralnetlib/neuralnetlib-2.4.2.tar.gz/neuralnetlib-2.4.2/neuralnetlib/losses.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+
+class LossFunction:
+    EPSILON = 1e-15
+
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        raise NotImplementedError
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+
+    def get_config(self) -> dict:
+        return {"name": self.__class__.__name__}
+
+    @staticmethod
+    def from_config(config: dict) -> 'LossFunction':
+        if config['name'] == 'MeanSquaredError':
+            return MeanSquaredError()
+        elif config['name'] == 'BinaryCrossentropy':
+            return BinaryCrossentropy()
+        elif config['name'] == 'CategoricalCrossentropy':
+            return CategoricalCrossentropy()
+        elif config['name'] == 'MeanAbsoluteError':
+            return MeanAbsoluteError()
+        elif config['name'] == 'HuberLoss':
+            return HuberLoss(config['delta'])
+        else:
+            raise ValueError(f'Unknown loss function: {config["name"]}')
+
+
+class MeanSquaredError(LossFunction):
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.mean(np.power(y_true - y_pred, 2))
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        y_true_reshaped = y_true.reshape(-1, 1)
+        return 2 * (y_pred - y_true_reshaped) / y_true.shape[0]
+
+    def __str__(self):
+        return "MeanSquaredError"
+
+
+class BinaryCrossentropy(LossFunction):
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        y_pred = np.clip(y_pred, LossFunction.EPSILON, 1 - LossFunction.EPSILON)
+        return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        y_pred = np.clip(y_pred, LossFunction.EPSILON, 1 - LossFunction.EPSILON)
+        return y_pred - y_true
+
+    def __str__(self):
+        return "BinaryCrossentropy"
+
+
+class CategoricalCrossentropy(LossFunction):
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        y_pred = np.clip(y_pred, LossFunction.EPSILON, 1 - LossFunction.EPSILON)
+        return -np.sum(y_true * np.log(y_pred)) / y_true.shape[0]
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        try:
+            y_pred = np.clip(y_pred, LossFunction.EPSILON, 1 - LossFunction.EPSILON)
+            return (y_pred - y_true) / y_true.shape[0]
+        except Exception as e:
+            print(e, "Make sure to one-hot encode your labels.", sep="\n")
+
+    def __str__(self):
+        return "CategoricalCrossentropy"
+
+
+class MeanAbsoluteError(LossFunction):
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.mean(np.abs(y_true - y_pred))
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        return np.where(y_pred > y_true, 1, -1)
+
+    def __str__(self):
+        return "MeanAbsoluteError"
+
+
+class HuberLoss(LossFunction):
+    def __init__(self, delta: float = 1.0):
+        super().__init__()
+        self.delta = delta
+
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        error = y_true - y_pred
+        is_small_error = np.abs(error) <= self.delta
+        squared_loss = 0.5 * np.square(error)
+        linear_loss = self.delta * (np.abs(error) - 0.5 * self.delta)
+        return np.mean(np.where(is_small_error, squared_loss, linear_loss))
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        error = y_true - y_pred
+        return np.where(np.abs(error) <= self.delta, error, self.delta * np.sign(error))
+
+    def __str__(self):
+        return f"HuberLoss(delta={self.delta})"
+
+    def get_config(self) -> dict:
+        return {"name": self.__class__.__name__, "delta": self.delta}
