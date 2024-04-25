8648626352f60e7534290a1af038777cc1b1e3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 14:05:13 +0000

    Add package latex-dependency-scanner/0.1.3/latex_dependency_scanner-0.1.3.tar.gz

---
+++ packages/latex-dependency-scanner/latex_dependency_scanner-0.1.3.tar.gz/latex_dependency_scanner-0.1.3/src/latex_dependency_scanner/__init__.py
@@ -0,0 +1,16 @@
+"""The name space for the package."""
+
+from __future__ import annotations
+
+from latex_dependency_scanner.compile import compile_pdf
+from latex_dependency_scanner.scanner import scan
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    # broken installation, we don't even try unknown only works because we do poor mans
+    # version compare
+    __version__ = "unknown"
+
+
+__all__ = ["__version__", "compile_pdf", "scan"]
