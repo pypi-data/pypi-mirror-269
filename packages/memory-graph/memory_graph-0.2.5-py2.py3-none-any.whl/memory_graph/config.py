96f34b0ac8f2ee73918bd41fbb51b2d76c30ed17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:06:07 +0000

    Add package memory-graph/0.2.5/memory_graph-0.2.5-py2.py3-none-any.whl

---
+++ packages/memory-graph/memory_graph-0.2.5-py2.py3-none-any.whl/memory_graph/config.py
@@ -0,0 +1,15 @@
+""" Configuration file for the graph visualizer. The configuration values are set later by the 'config_default.py' file. """
+
+max_number_nodes = 0
+max_string_length = 0
+
+no_reference_types = {}
+no_child_references_types = set()
+
+type_to_node = { }
+
+type_to_color = { }
+
+type_to_vertical_orientation = { }
+
+type_to_slicer = { }
