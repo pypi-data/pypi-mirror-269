b321b286b92222e729bfe7d21930005f0af8e488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:08:59 +0000

    Add package PokeCorky/1.0.2/pokecorky-1.0.2.tar.gz

---
+++ packages/PokeCorky/pokecorky-1.0.2.tar.gz/pokecorky-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PokeCorky',
+    version='1.0.2',
+    author= "Victor Diaz",
+    author_email="20222_diaz@tesch.edu.mx",
+    description="Una biblioteca que contiene la clase RandomPokemon para generar un pokemon aleatorio",
+    packages=[
+        "PokeCorky",
+    ],
+    package_data={'PokeCorky': ['pokemon.csv']},
+    install_requires=[
+        "pandas",
+    ],
+)
