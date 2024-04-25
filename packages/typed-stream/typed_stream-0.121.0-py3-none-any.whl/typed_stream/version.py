f1298d11d5941d556e4f83211f18fcbae11c46f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 06:04:49 +0000

    Add package typed-stream/0.121.0/typed_stream-0.121.0-py3-none-any.whl

---
+++ packages/typed-stream/typed_stream-0.121.0-py3-none-any.whl/typed_stream/version.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This script contains the version of this package.
+
+Update this to create a new release.
+To get the current version run this script.
+"""
+
+from __future__ import annotations
+
+VERSION = "0.121.0"
+
+if __name__ == "__main__":  # pragma: no cover
+    print(VERSION)
