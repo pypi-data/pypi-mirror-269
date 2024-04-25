55b3d94633ed85bd0d1fe13d8efd7d1ff28af2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:08:44 +0000

    Add package pymindcore/0.2.11/pymindcore-0.2.11.tar.gz

---
+++ packages/pymindcore/pymindcore-0.2.11.tar.gz/pymindcore-0.2.11/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymindcore',
+    version='0.2.11',
+    packages=find_packages(),
+    description='A Python library for AI, easy to use, freely available for any use',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Dogz-R-Godz/Pymind',
+    author='Dogz R Godz',
+    author_email='doggocam01@gmail.com',
+    license='CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    install_requires=[
+        'numpy',
+        'Pillow',
+        'numexpr', 
+        'numba'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    ],
+)
