07177574b22d68e100a4dbd68fe62965e664346e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 07:06:56 +0000

    Add package dyff-client/0.3.2/dyff_client-0.3.2.tar.gz

---
+++ packages/dyff-client/dyff_client-0.3.2.tar.gz/dyff_client-0.3.2/dyff/client/_generated/operations/_patch.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2024 UL Research Institutes
+# SPDX-License-Identifier: Apache-2.0
+
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+"""Customize generated code here.
+
+Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
+"""
+from typing import List
+
+from ._operations import AuditsOperations as _AuditsOperationsGenerated
+
+
+class AuditsOperations(_AuditsOperationsGenerated):
+    def upload(self, *args, **kwargs):
+        raise NotImplementedError()
+
+
+__all__: List[str] = [
+    "AuditsOperations"
+]  # Add all objects you want publicly available to users at this package level
+
+
+def patch_sdk():
+    """Do not remove from this file.
+
+    `patch_sdk` is a last resort escape hatch that allows you to do customizations
+    you can't accomplish using the techniques described in
+    https://aka.ms/azsdk/python/dpcodegen/python/customize
+    """
