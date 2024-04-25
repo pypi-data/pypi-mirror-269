7a684c97136cb85a5a21fe7ebea1cd86b09391de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 06:04:46 +0000

    Add package healthcare_puller/0.2.3/healthcare_puller-0.2.3-py3-none-any.whl

---
+++ packages/healthcare_puller/healthcare_puller-0.2.3-py3-none-any.whl/healthcare_puller/michigan_legislature/bill_and_analysis_puller.py
@@ -0,0 +1,4 @@
+import requests
+
+MI_HOUSE_ANALYSIS_PDF_FILE_URL = "https://www.legislature.mi.gov/documents/2023-2024/billanalysis/house/pdf/"
+MI_SENATE_ANALYSIS_PDF_FILE_URL = "https://www.legislature.mi.gov/documents/2023-2024/billanalysis/senate/pdf/"
