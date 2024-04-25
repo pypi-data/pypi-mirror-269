02ac3d85545c0d1e9f435152cb8b809b55043b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:07:44 +0000

    Add package types-mock/5.1.0.20240425/types-mock-5.1.0.20240425.tar.gz

---
+++ packages/types-mock/types-mock-5.1.0.20240425.tar.gz/types-mock-5.1.0.20240425/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+
+name = "types-mock"
+description = "Typing stubs for mock"
+long_description = '''
+## Typing stubs for mock
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`mock`](https://github.com/testing-cabal/mock) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`mock`.
+
+This version of `types-mock` aims to provide accurate annotations
+for `mock==5.1.*`.
+The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/mock. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `273e9ea6ddc2d5cad2db7dd25a0383a61be15675` and was tested
+with mypy 1.10.0, pyright 1.1.360, and
+pytype 2024.4.11.
+'''.lstrip()
+
+setup(name=name,
+      version="5.1.0.20240425",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/mock.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['mock-stubs'],
+      package_data={'mock-stubs': ['__init__.pyi', 'backports.pyi', 'mock.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0 license",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
