246f42283d452cb1dfd33b1e74e997cee3ba8d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:08:52 +0000

    Add package cdk-pyproject/0.1.2/cdk_pyproject-0.1.2-py3-none-any.whl

---
+++ packages/cdk-pyproject/cdk_pyproject-0.1.2-py3-none-any.whl/cdk_pyproject/py_project.py
@@ -0,0 +1,64 @@
+import importlib.resources
+import os.path
+import tomllib
+from pathlib import Path
+from typing import Self
+
+from aws_cdk import BundlingOptions, DockerImage, aws_lambda
+from pyproject_metadata import StandardMetadata
+
+dockerfiles = importlib.resources.files("cdk_pyproject.dockerfiles")
+
+
+class PyProject:
+    def __init__(self, path: str, runtime: aws_lambda.Runtime, image: DockerImage) -> None:
+        self.runtime = runtime
+        self.image = image
+        self.path = path
+
+    @classmethod
+    def from_pyproject(cls, path: str, runtime: aws_lambda.Runtime) -> Self:
+        image = DockerImage.from_build(
+            path=path,
+            build_args={"IMAGE": runtime.bundling_image.image},
+            file=os.path.relpath(str(dockerfiles.joinpath("pyproject.Dockerfile")), start=path),
+        )
+
+        return cls(path, runtime, image)
+
+    @classmethod
+    def from_rye(cls, path: str, runtime: aws_lambda.Runtime) -> Self:
+        image = DockerImage.from_build(
+            path=path,
+            build_args={"IMAGE": runtime.bundling_image.image},
+            file=os.path.relpath(str(dockerfiles.joinpath("rye.Dockerfile")), start=path),
+        )
+
+        return cls(path, runtime, image)
+
+    @classmethod
+    def from_poetry(cls, path: str, runtime: aws_lambda.Runtime) -> Self:
+        raise NotImplementedError
+
+    def get_root_project_name(self) -> str:
+        pyproject = Path(self.path, "pyproject.toml")
+        metadata = StandardMetadata.from_pyproject(tomllib.loads(pyproject.read_text()))
+        return metadata.name
+
+    def code(self, project: str | None) -> aws_lambda.Code:
+        if project is None:
+            project = self.get_root_project_name()
+
+        return aws_lambda.Code.from_asset(
+            path=".",
+            bundling=BundlingOptions(
+                image=self.image,
+                command=[
+                    "bash",
+                    "-eux",
+                    "-c",
+                    f"pip install --find-links /tmp/wheelhouse --no-index --target /asset-output {project}",
+                ],
+                user="root",
+            ),
+        )
