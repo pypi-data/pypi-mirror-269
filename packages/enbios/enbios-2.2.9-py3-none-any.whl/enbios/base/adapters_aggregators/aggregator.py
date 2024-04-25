deec3469cad33869051a26ea5be9da079fa933ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:42 +0000

    Add package enbios/2.2.9/enbios-2.2.9-py3-none-any.whl

---
+++ packages/enbios/enbios-2.2.9-py3-none-any.whl/enbios/base/adapters_aggregators/aggregator.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Any
+
+from enbios.generic.enbios2_logging import get_logger
+from enbios.generic.tree.basic_tree import BasicTreeNode
+from enbios.models.models import NodeOutput, ScenarioResultNodeData
+
+
+class EnbiosAggregator(ABC):
+    @abstractmethod
+    def validate_config(self, config: Optional[dict[str, Any]]):
+        pass
+
+    @abstractmethod
+    def validate_node(self, node_name: str, node_config: Any):
+        pass
+
+    @abstractmethod
+    def aggregate_node_output(
+        self,
+        node: BasicTreeNode[ScenarioResultNodeData],
+        scenario_name: Optional[str] = "",
+    ) -> list[NodeOutput]:
+        pass
+
+    @abstractmethod
+    def aggregate_node_result(self, node: BasicTreeNode[ScenarioResultNodeData]):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def node_indicator() -> str:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def name() -> str:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_config_schemas() -> dict:
+        pass
+
+    def result_extras(self, node_name: str) -> dict[str, Any]:
+        return {}
+
+    def get_logger(self):
+        return get_logger(f"__name__ ({self.name})")
