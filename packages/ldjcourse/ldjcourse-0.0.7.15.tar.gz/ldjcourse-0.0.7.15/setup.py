2c278fa31555feec1c18055a264ce71f7263cf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:08:41 +0000

    Add package ldjcourse/0.0.7.15/ldjcourse-0.0.7.15.tar.gz

---
+++ packages/ldjcourse/ldjcourse-0.0.7.15.tar.gz/ldjcourse-0.0.7.15/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7.15'
+
+setup(
+    name='ldjcourse',
+    version=VERSION,
+    author='元蓝先生',
+    description='B站：元蓝先生 https://space.bilibili.com/3546564903569609',
+    license='MIT',
+
+    packages=find_packages()
+)
+
+# 打包
+# python setup.py sdist
+
+# 上传
+# twine upload dist/*
+
+'''
+pypi-AgEIcHlwaS5vcmcCJGY4M2VlNDcxLWY1ZDktNDIwOC1hNzUyLTUzNDVjNWQ1ZWRiYQACEVsxLFsibGRqY291cnNlIl1dAAIsWzIsWyI5ZDdjMjczNy1kMTlhLTQyMWYtOTRjZC05OTdlOGY2NDJkM2IiXV0AAAYgXBrVqbE583sfdqq2LgXMHZRW_DyZKnJoUQDpVYQKEh4
+
+
+'''
