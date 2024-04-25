1f8d5fa5e9d7a81d9dc1878e6db92cf55c493602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:07:41 +0000

    Add package Mensajes-yeru/5.0/mensajes_yeru-5.0.tar.gz

---
+++ packages/Mensajes-yeru/mensajes_yeru-5.0.tar.gz/mensajes_yeru-5.0/mensajes/adios/despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adios me despedido desde despedidas.despedir()")
+
+class Despedida:
+    def __init__(self):
+        print("Hola, me despedio desde Despedida.__init__()")
