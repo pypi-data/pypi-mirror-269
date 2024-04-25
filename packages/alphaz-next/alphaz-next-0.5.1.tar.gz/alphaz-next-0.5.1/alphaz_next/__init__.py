e036d242856d4c9a082bd35c1ba55ccfa8db7164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:10:13 +0000

    Add package alphaz-next/0.5.1/alphaz-next-0.5.1.tar.gz

---
+++ packages/alphaz-next/alphaz-next-0.5.1.tar.gz/alphaz-next-0.5.1/alphaz_next/__init__.py
@@ -0,0 +1,9 @@
+from pysql_repo import DataBase as DataBase
+from pysql_repo import (
+    Repository as Repository,
+    Service as Service,
+    with_session as with_session,
+    LoadingTechnique as LoadingTechnique,
+    Operators as Operators,
+    RelationshipOption as RelationshipOption,
+)
