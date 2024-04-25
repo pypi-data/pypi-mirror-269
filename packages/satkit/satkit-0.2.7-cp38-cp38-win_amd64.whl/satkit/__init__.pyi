0812dfaa58962375f51da85438f446d52c7a79eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:07:47 +0000

    Add package satkit/0.2.7/satkit-0.2.7-cp38-cp38-win_amd64.whl

---
+++ packages/satkit/satkit-0.2.7-cp38-cp38-win_amd64.whl/satkit/__init__.pyi
@@ -0,0 +1,33 @@
+from .satkit import *
+
+
+from . import jplephem
+from . import frametransform
+from . import moon
+from . import sun
+from . import satprop
+from . import density
+from . import utils
+
+__all__ = [
+    "time",
+    "duration",
+    "timescale",
+    "quaternion",
+    "sgp4",
+    "gravmodel",
+    "gravity",
+    "nrlmsise00",
+    "solarsystem",
+    "TLE",
+    "itrfcoord",
+    "consts",
+    "frametransform",
+    "jplephem",
+    "utils",
+    "moon",
+    "sun",
+    "satprop",
+    "density",
+    "utils"
+]
