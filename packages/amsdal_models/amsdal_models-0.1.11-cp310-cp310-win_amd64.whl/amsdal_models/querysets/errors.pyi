c02472396e32b76191b08a0b3170c8eabc5d32b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:09:02 +0000

    Add package amsdal_models/0.1.11/amsdal_models-0.1.11-cp310-cp310-win_amd64.whl

---
+++ packages/amsdal_models/amsdal_models-0.1.11-cp310-cp310-win_amd64.whl/amsdal_models/querysets/errors.pyi
@@ -0,0 +1,8 @@
+from amsdal_utils.errors import AmsdalError
+
+class AmsdalQuerySetError(AmsdalError):
+    """Base class for all queryset errors"""
+class ObjectDoesNotExistError(AmsdalQuerySetError):
+    """The requested object does not exist"""
+class MultipleObjectsReturnedError(AmsdalQuerySetError):
+    """The query returned multiple objects when only one was expected."""
