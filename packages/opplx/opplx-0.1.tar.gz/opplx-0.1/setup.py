71fd31130b7ae0005d04b2fcc5fc7bf3589810e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 06:05:17 +0000

    Add package opplx/0.1/opplx-0.1.tar.gz

---
+++ packages/opplx/opplx-0.1.tar.gz/opplx-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opplx',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'opplx = ollama.__init__:main',
+        ],
+    },
+    install_requires=[
+        'langchain',
+        'googlesearch-python',
+        'twine',
+        'setuptools',
+        'wheel',
+        'faiss-cpu'
+    ],
+)
