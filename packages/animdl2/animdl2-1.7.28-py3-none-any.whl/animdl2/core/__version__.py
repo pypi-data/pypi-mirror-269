f49ee49fed64038c2a7454e2559c8a3bdb086044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:13:50 +0000

    Add package animdl2/1.7.28/animdl2-1.7.28-py3-none-any.whl

---
+++ packages/animdl2/animdl2-1.7.28-py3-none-any.whl/animdl2/core/__version__.py
@@ -0,0 +1,13 @@
+import importlib.metadata
+
+try:
+    __core__ = importlib.metadata.version("animdl")
+except importlib.metadata.PackageNotFoundError:
+    import pathlib
+
+    from animdl2.utils.optopt import regexlib
+
+    __core__ = regexlib.search(
+        r'name = "animdl"\nversion = "(.+?)"',
+        (pathlib.Path(__file__).parent.parent / "pyproject.toml").read_text(),
+    ).group(1)
