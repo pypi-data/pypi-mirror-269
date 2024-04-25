1bf3bce45840f49b94d979f3001844c8c051b879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:07:02 +0000

    Add package simplecalculatorkms/0.0.3/simplecalculatorkms-0.0.3.tar.gz

---
+++ packages/simplecalculatorkms/simplecalculatorkms-0.0.3.tar.gz/simplecalculatorkms-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='simplecalculatorkms',
+    version='0.0.3',
+    description='A very basic simplecalculatorkms',
+    url='',
+    author='meenakshisundaram',
+    author_email='kmstpm@gamil.org',
+    classifiers=classifiers,
+    keywords='simplecalculatorkms',
+    packages=find_packages(),
+    install_requires=['']
+)
