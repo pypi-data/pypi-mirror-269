5638e7c0cc990b6a303f0f99417fab770e2051d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:07:30 +0000

    Add package NStudyPy/0.0.1/NstudyPy-0.0.1-py3-none-any.whl

---
+++ packages/NStudyPy/NstudyPy-0.0.1-py3-none-any.whl/tools.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024-04-24 18:34
+# @Author  : Jack
+# @File    : tools
+
+"""
+tools
+"""
+
+
+def hello():
+    """
+    初始化项目测试
+    :return:
+    """
+    return "hello world!"
