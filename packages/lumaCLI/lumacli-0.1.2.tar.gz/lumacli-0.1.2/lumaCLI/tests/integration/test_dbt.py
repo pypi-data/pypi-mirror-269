c9e36b3284d972a123d9b996ef714f2b887d05fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:09:02 +0000

    Add package lumaCLI/0.1.2/lumacli-0.1.2.tar.gz

---
+++ packages/lumaCLI/lumacli-0.1.2.tar.gz/lumacli-0.1.2/lumaCLI/tests/integration/test_dbt.py
@@ -0,0 +1,21 @@
+from typer.testing import CliRunner
+
+from lumaCLI.main import app
+
+runner = CliRunner()
+
+
+def test_ingest_v1_7(METADATA_DIR_V1_7):
+    result = runner.invoke(
+        app,
+        [
+            "dbt",
+            "ingest",
+            "--metadata-dir",
+            METADATA_DIR_V1_7 / "model_metadata",
+            "--luma-url",
+            "http://localhost:8000",
+            "--dry-run",
+        ],
+    )
+    assert result.exit_code == 0
