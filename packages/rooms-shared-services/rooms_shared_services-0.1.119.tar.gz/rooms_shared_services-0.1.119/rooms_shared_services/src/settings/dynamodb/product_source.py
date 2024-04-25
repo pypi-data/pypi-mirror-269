85689f5bbb58c7b84d2bf1a9cd903a9f58db09c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:15:15 +0000

    Add package rooms-shared-services/0.1.119/rooms_shared_services-0.1.119.tar.gz

---
+++ packages/rooms-shared-services/rooms_shared_services-0.1.119.tar.gz/rooms_shared_services-0.1.119/rooms_shared_services/src/settings/dynamodb/product_source.py
@@ -0,0 +1,11 @@
+from rooms_shared_services.src.settings.dynamodb.base import BaseDynamodbSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class Settings(BaseDynamodbSettings):
+    model_config = SettingsConfigDict(env_prefix="product_source_")
+
+    sku_indexname: str
+    original_ident_indexname: str
+    ident_code: str
+    ident_key: str    
