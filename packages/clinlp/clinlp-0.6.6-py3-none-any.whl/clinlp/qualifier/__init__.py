c0f9add1096a9084d8cfdb99f508366bfe810bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:48 +0000

    Add package clinlp/0.6.6/clinlp-0.6.6-py3-none-any.whl

---
+++ packages/clinlp/clinlp-0.6.6-py3-none-any.whl/clinlp/qualifier/__init__.py
@@ -0,0 +1,23 @@
+import importlib.util
+
+from .context_algorithm import ContextAlgorithm, ContextRule, ContextRuleDirection
+from .qualifier import (
+    ATTR_QUALIFIERS,
+    Qualifier,
+    QualifierDetector,
+    QualifierFactory,
+    get_qualifiers,
+    set_qualifiers,
+)
+
+if importlib.util.find_spec("transformers") is not None:
+    from .transformer import (
+        ExperiencerTransformer,
+        NegationTransformer,
+        QualifierTransformer,
+    )
+
+__all__ = ['ContextAlgorithm', 'ContextRule', 'ContextRuleDirection', 'ATTR_QUALIFIERS',
+           'Qualifier', 'QualifierDetector', 'QualifierFactory', 'get_qualifiers',
+           'set_qualifiers', 'ExperiencerTransformer', 'NegationTransformer',
+           'QualifierTransformer']
