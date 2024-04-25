39dbeb326f495d0cccbaa42bf12218c8b8f71b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:09:00 +0000

    Add package clump-python/0.1.6/clump_python-0.1.6-py3-none-any.whl

---
+++ packages/clump-python/clump_python-0.1.6-py3-none-any.whl/CLUMP/examples/Example_Euclidean_3D.py
@@ -0,0 +1,26 @@
+# Script to run GenerateClump_Euclidean_3D
+# 2021 © V. Angelidakis, S. Nadimi, M. Otsubo, S. Utili.
+
+# 2021 MATLAB implementation by V. Angelidakis <v.angelidakis@qub.ac>
+# 2024 Translated from MATLAB to Python by U.A. Canbolat <utku.canbolat@fau.de>
+
+import sys
+sys.path.append('/home/vas/Desktop/Utku_Code_Review/CLUMP_Python/')
+
+from CLUMP.GenerateClump_Euclidean_3D import GenerateClump_Euclidean_3D
+import sys
+sys.path.append('../')
+
+# Load particle shape from stl file
+inputGeom = 'ParticleGeometries/Hexahedron.stl'
+N = 21
+rMin = 0
+div = 102
+overlap = 0.6
+output = 'EU_Hexahedron.txt'
+outputVTK = 'EU_Hexahedron.vtk'
+visualise = True
+
+mesh,clump = GenerateClump_Euclidean_3D(inputGeom=inputGeom, N=N, rMin=rMin, div=div, overlap=overlap, output=output, outputVTK=outputVTK, visualise=visualise)
+
+print('Total number of spheres: '+ str(clump.numSpheres))
