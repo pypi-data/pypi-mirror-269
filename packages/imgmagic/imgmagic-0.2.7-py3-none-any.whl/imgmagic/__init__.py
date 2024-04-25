f2d1656d3c6e3cf5654917835c9968f4a15b575a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:08:54 +0000

    Add package imgmagic/0.2.7/imgmagic-0.2.7-py3-none-any.whl

---
+++ packages/imgmagic/imgmagic-0.2.7-py3-none-any.whl/imgmagic/__init__.py
@@ -0,0 +1,5 @@
+from .display import greyscale
+from .geometric import crop, rotate, flipping, reverse, scaling#, downsampling
+from .histogram import adjustContrast, equalization, negative#, correctionGamma, histogram
+from .filter import GaussianNoise, SaltnPepperNoise, filtering, sharpening, crosscorrelation
+from .fourier import ftfiltering, UnsharpMasking, HighFreqEmph#, LaplacianFilter, hardthresholding
