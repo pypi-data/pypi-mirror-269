f046e40fd50766640ebcb894fe4823fcfec57b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:09:01 +0000

    Add package passage-identity/2.5.0/passage_identity-2.5.0.tar.gz

---
+++ packages/passage-identity/passage_identity-2.5.0.tar.gz/passage_identity-2.5.0/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="passage-identity",
+    version="2.5.0",
+    author="Passage Identity, Inc",
+    author_email="support@passage.id",
+    description="Python library to help manage your Passage application and users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/passageidentity/passage-python",
+    project_urls={
+        "Bug Tracker": "https://github.com/passageidentity/passage-python/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages = [
+        'passageidentity', 
+        'passageidentity.models', 
+        'passageidentity.openapi_client', 
+        'passageidentity.openapi_client.api', 
+        'passageidentity.openapi_client.models'
+    ],
+    package_dir = {
+        'passageidentity': 'passageidentity',
+        'passageidentity.models': 'passageidentity/models',
+        'passageidentity.openapi_client': 'passageidentity/openapi_client',
+        'passageidentity.openapi_client.api': 'passageidentity/openapi_client/api',
+        'passageidentity.openapi_client.models': 'passageidentity/openapi_client/models',
+    },
+    install_requires=[
+        'aenum',
+        'cryptography',
+        'django < 5', 
+        'Flask', 
+        'importlib-metadata >= 1.0 ; python_version < "3.12"',
+        'pydantic',
+        'pyjwt',
+        "python-dateutil",
+        'requests', 
+        "typing_extensions >= 4.7.1",
+        "urllib3 >= 1.25.3, < 2.1.0",
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.4.3'],
+    test_suite='tests',
+    python_requires=">=3.7",
+)
