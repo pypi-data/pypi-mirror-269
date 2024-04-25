319c4560b23ce1f9e121138a860ab4858921bd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:46 +0000

    Add package neo4j-genai/0.1.3/neo4j_genai-0.1.3.tar.gz

---
+++ packages/neo4j-genai/neo4j_genai-0.1.3.tar.gz/neo4j_genai-0.1.3/src/neo4j_genai/retrievers.py
@@ -0,0 +1,233 @@
+#  Copyright (c) "Neo4j"
+#  Neo4j Sweden AB [https://neo4j.com]
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from abc import abstractmethod, ABC
+from typing import Optional, Any
+from pydantic import ValidationError
+from neo4j import Driver, Record
+from .embedder import Embedder
+from .types import (
+    SimilaritySearchModel,
+    VectorSearchRecord,
+    VectorCypherSearchModel,
+)
+
+
+class Retriever(ABC):
+    """
+    Abstract class for Neo4j retrievers
+    """
+
+    def __init__(self, driver: Driver):
+        self.driver = driver
+
+    def _verify_version(self) -> None:
+        """
+        Check if the connected Neo4j database version supports vector indexing.
+
+        Queries the Neo4j database to retrieve its version and compares it
+        against a target version (5.18.1) that is known to support vector
+        indexing. Raises a ValueError if the connected Neo4j version is
+        not supported.
+        """
+        records, _, _ = self.driver.execute_query("CALL dbms.components()")
+        version = records[0]["versions"][0]
+
+        if "aura" in version:
+            version_tuple = (
+                *tuple(map(int, version.split("-")[0].split("."))),
+                0,
+            )
+            target_version = (5, 18, 0)
+        else:
+            version_tuple = tuple(map(int, version.split(".")))
+            target_version = (5, 18, 1)
+
+        if version_tuple < target_version:
+            raise ValueError(
+                "This package only supports Neo4j version 5.18.1 or greater"
+            )
+
+    @abstractmethod
+    def search(self, *args, **kwargs) -> Any:
+        pass
+
+
+class VectorRetriever(Retriever):
+    """
+    Provides retrieval method using vector search over embeddings.
+    If an embedder is provided, it needs to have the required Embedder type.
+    """
+
+    def __init__(
+        self,
+        driver: Driver,
+        index_name: str,
+        embedder: Optional[Embedder] = None,
+        return_properties: Optional[list[str]] = None,
+    ) -> None:
+        super().__init__(driver)
+        self._verify_version()
+        self.index_name = index_name
+        self.return_properties = return_properties
+        self.embedder = embedder
+
+    def search(
+        self,
+        query_vector: Optional[list[float]] = None,
+        query_text: Optional[str] = None,
+        top_k: int = 5,
+    ) -> list[VectorSearchRecord]:
+        """Get the top_k nearest neighbor embeddings for either provided query_vector or query_text.
+        See the following documentation for more details:
+
+        - [Query a vector index](https://neo4j.com/docs/cypher-manual/current/indexes-for-vector-search/#indexes-vector-query)
+        - [db.index.vector.queryNodes()](https://neo4j.com/docs/operations-manual/5/reference/procedures/#procedure_db_index_vector_queryNodes)
+
+        Args:
+            query_vector (Optional[list[float]], optional): The vector embeddings to get the closest neighbors of. Defaults to None.
+            query_text (Optional[str], optional): The text to get the closest neighbors of. Defaults to None.
+            top_k (int, optional): The number of neighbors to return. Defaults to 5.
+
+        Raises:
+            ValueError: If validation of the input arguments fail.
+            ValueError: If no embedder is provided.
+
+        Returns:
+            list[VectorSearchRecord]: The `top_k` neighbors found in vector search with their nodes and scores.
+        """
+        try:
+            validated_data = SimilaritySearchModel(
+                index_name=self.index_name,
+                top_k=top_k,
+                query_vector=query_vector,
+                query_text=query_text,
+            )
+        except ValidationError as e:
+            error_details = e.errors()
+            raise ValueError(f"Validation failed: {error_details}")
+
+        parameters = validated_data.model_dump(exclude_none=True)
+
+        if query_text:
+            if not self.embedder:
+                raise ValueError("Embedding method required for text query.")
+            query_vector = self.embedder.embed_query(query_text)
+            parameters["query_vector"] = query_vector
+            del parameters["query_text"]
+
+        db_query_string = """
+        CALL db.index.vector.queryNodes($index_name, $top_k, $query_vector)
+        YIELD node, score
+        """
+
+        if self.return_properties:
+            return_properties_cypher = ", ".join(
+                [f".{prop}" for prop in self.return_properties]
+            )
+            db_query_string += (
+                f"RETURN node {{{return_properties_cypher}}} as node, score"
+            )
+
+        records, _, _ = self.driver.execute_query(db_query_string, parameters)
+
+        try:
+            return [
+                VectorSearchRecord(node=record["node"], score=record["score"])
+                for record in records
+            ]
+        except ValidationError as e:
+            error_details = e.errors()
+            raise ValueError(
+                f"Validation failed while constructing output: {error_details}"
+            )
+
+
+class VectorCypherRetriever(Retriever):
+    """
+    Provides retrieval method using vector similarity and custom Cypher query.
+    If an embedder is provided, it needs to have the required Embedder type.
+    """
+
+    def __init__(
+        self,
+        driver: Driver,
+        index_name: str,
+        retrieval_query: str,
+        embedder: Optional[Embedder] = None,
+    ) -> None:
+        super().__init__(driver)
+        self._verify_version()
+        self.index_name = index_name
+        self.retrieval_query = retrieval_query
+        self.embedder = embedder
+
+    def search(
+        self,
+        query_vector: Optional[list[float]] = None,
+        query_text: Optional[str] = None,
+        top_k: int = 5,
+        query_params: Optional[dict[str, Any]] = None,
+    ) -> list[Record]:
+        """Get the top_k nearest neighbor embeddings for either provided query_vector or query_text.
+        See the following documentation for more details:
+
+        - [Query a vector index](https://neo4j.com/docs/cypher-manual/current/indexes-for-vector-search/#indexes-vector-query)
+        - [db.index.vector.queryNodes()](https://neo4j.com/docs/operations-manual/5/reference/procedures/#procedure_db_index_vector_queryNodes)
+
+        Args:
+            query_vector (Optional[list[float]], optional): The vector embeddings to get the closest neighbors of. Defaults to None.
+            query_text (Optional[str], optional): The text to get the closest neighbors of. Defaults to None.
+            top_k (int, optional): The number of neighbors to return. Defaults to 5.
+            query_params (Optional[dict[str, Any]], optional): Parameters for the Cypher query. Defaults to None.
+
+        Raises:
+            ValueError: If validation of the input arguments fail.
+            ValueError: If no embedder is provided.
+
+        Returns:
+            list[Record]: The results of the search query
+        """
+        try:
+            validated_data = VectorCypherSearchModel(
+                index_name=self.index_name,
+                top_k=top_k,
+                query_vector=query_vector,
+                query_text=query_text,
+                query_params=query_params,
+            )
+        except ValidationError as e:
+            raise ValueError(f"Validation failed: {e.errors()}")
+
+        parameters = validated_data.model_dump(exclude_none=True)
+
+        if query_text:
+            if not self.embedder:
+                raise ValueError("Embedding method required for text query.")
+            parameters["query_vector"] = self.embedder.embed_query(query_text)
+            del parameters["query_text"]
+
+        if query_params:
+            for key, value in query_params.items():
+                if key not in parameters:
+                    parameters[key] = value
+            del parameters["query_params"]
+
+        query_prefix = """
+                CALL db.index.vector.queryNodes($index_name, $top_k, $query_vector)
+                YIELD node, score
+                """
+        search_query = query_prefix + self.retrieval_query
+        records, _, _ = self.driver.execute_query(search_query, parameters)
+        return records
