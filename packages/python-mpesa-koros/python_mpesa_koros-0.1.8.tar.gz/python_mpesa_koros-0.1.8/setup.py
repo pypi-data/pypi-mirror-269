69cc4e18ca259ed6d938ba9201bb159baeb97e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:07:01 +0000

    Add package python-mpesa-koros/0.1.8/python_mpesa_koros-0.1.8.tar.gz

---
+++ packages/python-mpesa-koros/python_mpesa_koros-0.1.8.tar.gz/python_mpesa_koros-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_mpesa_koros',
+    version='0.1.8',
+    author='Kevin Ongulu',
+    author_email='kevinongulu@gmail.com',
+    description='A python package for connecting to MPESA APIs',
+    #packages=find_packages(),
+    packages = ['python_mpesa_koros'], 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests', #requests>=2.26.0
+        'python-dotenv',
+        'pycryptodome'
+    ],
+    python_requires='>=3.6',
+)
