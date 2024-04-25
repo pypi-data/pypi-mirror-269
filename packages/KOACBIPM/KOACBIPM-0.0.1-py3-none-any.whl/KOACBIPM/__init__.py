c78971daa1ea821e0af20bff502143829b13f555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:07:49 +0000

    Add package KOACBIPM/0.0.1/KOACBIPM-0.0.1-py3-none-any.whl

---
+++ packages/KOACBIPM/KOACBIPM-0.0.1-py3-none-any.whl/KOACBIPM/__init__.py
@@ -0,0 +1,13 @@
+# package
+# __init__.py
+import pandas as pd
+import numpy as np
+import os
+from sklearn import preprocessing
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.decomposition import PCA
+
+
+
+__all__=["KOACBIPM"]
+
