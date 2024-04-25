2bf859e802b255fbe421dcff867f6732384b1ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 07:06:55 +0000

    Add package python-active-versions/1.10.0/python_active_versions-1.10.0.tar.gz

---
+++ packages/python-active-versions/python_active_versions-1.10.0.tar.gz/python_active_versions-1.10.0/python_active_versions/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023 Gabriele Pongelli
+#
+# SPDX-License-Identifier: MIT
+
+"""Top-level package for python active versions."""
+
+__author__ = "Gabriele Pongelli"
+__email__ = "gabriele.pongelli@gmail.com"
+__version__ = "1.10.0"
