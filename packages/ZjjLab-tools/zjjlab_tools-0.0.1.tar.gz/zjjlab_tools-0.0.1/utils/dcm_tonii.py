f0872ade132d711d81fdb2cd6d5e684823bc06ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:06:35 +0000

    Add package ZjjLab-tools/0.0.1/zjjlab_tools-0.0.1.tar.gz

---
+++ packages/ZjjLab-tools/zjjlab_tools-0.0.1.tar.gz/zjjlab_tools-0.0.1/utils/dcm_tonii.py
@@ -0,0 +1,18 @@
+import os
+from joblib import Parallel, delayed
+__all__ = ['dcm_tonii']
+def do_dcm2nii(input,output,dirname):
+        os.makedirs(output,exist_ok=True)
+        if dirname:
+            tmp=input.split('/')
+            output+='/'+tmp[-1]
+            os.makedirs(output,exist_ok=True)
+        os.system('/home/dell/mricron/dcm2niix -z o -o {}  {}'.format(output,input))
+        print(output)
+
+def dcm_tonii(listdrs,output,dirname=True):
+    Parallel(n_jobs=40)(delayed(do_dcm2nii)(listdrs[i],output,dirname) for i in range(0, len(listdrs)))
+
+
+
+
