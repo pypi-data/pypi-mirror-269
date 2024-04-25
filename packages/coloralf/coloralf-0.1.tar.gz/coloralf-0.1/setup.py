f0e2ce3ac97c491a4d669a986f0a49de5d941987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:08:48 +0000

    Add package coloralf/0.1/coloralf-0.1.tar.gz

---
+++ packages/coloralf/coloralf-0.1.tar.gz/coloralf-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+	desc = f.read()
+
+setup(
+	name='coloralf',
+	version='0.1',
+	packages=find_packages(),
+	install_requires=[],
+
+	long_description=desc,
+	long_description_content_type="text/markdown"
+)
