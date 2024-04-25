0145e035a721111519f44828ba7333e5f9dc7bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:10:16 +0000

    Add package openai-messages-token-helper/0.0.5/openai_messages_token_helper-0.0.5.tar.gz

---
+++ packages/openai-messages-token-helper/openai_messages_token_helper-0.0.5.tar.gz/openai_messages_token_helper-0.0.5/CONTRIBUTING.md
@@ -0,0 +1,24 @@
+# How to contribute
+
+## Development
+
+Install the project dependencies:
+
+```sh
+python3 -m pip install -e '.[dev]'
+pre-commit install
+```
+
+Run the tests:
+
+```sh
+python3 -m pytest
+```
+
+## Publishing
+
+Publish to PyPi:
+
+```sh
+flit publish
+```
---
+++ packages/openai-messages-token-helper/openai_messages_token_helper-0.0.5.tar.gz/openai_messages_token_helper-0.0.5/tests/test_messagebuilder.py
@@ -0,0 +1,35 @@
+from openai_messages_token_helper import build_messages, count_tokens_for_message
+
+from .messages import system_message_short, system_message_unicode, user_message, user_message_unicode
+
+
+def test_messagebuilder():
+    messages = build_messages("gpt-35-turbo", system_message_short["message"]["content"])
+    assert messages == [system_message_short["message"]]
+    assert count_tokens_for_message("gpt-35-turbo", messages[0]) == system_message_short["count"]
+
+
+def test_messagebuilder_append():
+    messages = build_messages(
+        "gpt-35-turbo", system_message_short["message"]["content"], new_user_message=user_message["message"]["content"]
+    )
+    assert messages == [system_message_short["message"], user_message["message"]]
+    assert count_tokens_for_message("gpt-35-turbo", messages[0]) == system_message_short["count"]
+    assert count_tokens_for_message("gpt-35-turbo", messages[1]) == user_message["count"]
+
+
+def test_messagebuilder_unicode():
+    messages = build_messages("gpt-35-turbo", system_message_unicode["message"]["content"])
+    assert messages == [system_message_unicode["message"]]
+    assert count_tokens_for_message("gpt-35-turbo", messages[0]) == system_message_unicode["count"]
+
+
+def test_messagebuilder_unicode_append():
+    messages = build_messages(
+        "gpt-35-turbo",
+        system_message_unicode["message"]["content"],
+        new_user_message=user_message_unicode["message"]["content"],
+    )
+    assert messages == [system_message_unicode["message"], user_message_unicode["message"]]
+    assert count_tokens_for_message("gpt-35-turbo", messages[0]) == system_message_unicode["count"]
+    assert count_tokens_for_message("gpt-35-turbo", messages[1]) == user_message_unicode["count"]
