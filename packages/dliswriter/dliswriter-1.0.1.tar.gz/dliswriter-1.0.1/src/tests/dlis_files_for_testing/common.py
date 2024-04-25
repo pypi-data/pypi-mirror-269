f69b63099545ec18e5b9886dc7cf58cc5aa3d293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:06:24 +0000

    Add package dliswriter/1.0.1/dliswriter-1.0.1.tar.gz

---
+++ packages/dliswriter/dliswriter-1.0.1.tar.gz/dliswriter-1.0.1/src/tests/dlis_files_for_testing/common.py
@@ -0,0 +1,20 @@
+from dliswriter import DLISFile, StorageUnitLabel
+from dliswriter.logical_record.eflr_types import FileHeaderSet, FileHeaderItem
+
+
+def make_file_header() -> FileHeaderItem:
+    return FileHeaderItem(
+        "DEFAULT FHLR",
+        sequence_number=1,
+        parent=FileHeaderSet()
+    )
+
+
+def make_sul() -> StorageUnitLabel:
+    return StorageUnitLabel("DEFAULT STORAGE SET", sequence_number=1)
+
+
+def make_df() -> DLISFile:
+    df = DLISFile(storage_unit_label=make_sul(), file_header=make_file_header())
+    df.add_origin("DEFINING ORIGIN", creation_time="2050/03/02 15:30:00", file_set_number=1)
+    return df
