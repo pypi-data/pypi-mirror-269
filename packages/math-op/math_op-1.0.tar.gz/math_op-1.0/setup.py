b8187dbdd717edcb46e931c2c54fcef05e18392e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:05:53 +0000

    Add package math-op/1.0/math_op-1.0.tar.gz

---
+++ packages/math-op/math_op-1.0.tar.gz/math_op-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_op',
+    version='1.0',
+    packages=find_packages(),
+    license='MIT',
+    description='A Python package for basic mathematical operations.',
+    author='Bijeet Basak',
+    author_email='bijeet.basak2018@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
