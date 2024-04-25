6d6951c115315e28b6cc15f941e079004189bfe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:08:22 +0000

    Add package mat3ra-made/2024.4.25.post0/mat3ra_made-2024.4.25.post0.tar.gz

---
+++ packages/mat3ra-made/mat3ra_made-2024.4.25.post0.tar.gz/mat3ra_made-2024.4.25.post0/tests/py/unit/test_tools_analyze.py
@@ -0,0 +1,19 @@
+import numpy as np
+from ase.build import bulk
+from mat3ra.made.tools.analyze import get_average_interlayer_distance
+from mat3ra.made.tools.analyze import get_surface_area
+
+
+def test_calculate_average_interlayer_distance():
+    substrate = bulk("Si", cubic=True)
+    film = bulk("Cu", cubic=True)
+    interface_atoms = substrate + film
+    interface_atoms.set_tags([1] * len(substrate) + [2] * len(film))
+    distance = get_average_interlayer_distance(interface_atoms, 1, 2)
+    assert np.isclose(distance, 4.0725)
+
+
+def test_calculate_surface_area():
+    atoms = bulk("Si", cubic=False)
+    area = get_surface_area(atoms)
+    assert np.isclose(area, 12.7673)
