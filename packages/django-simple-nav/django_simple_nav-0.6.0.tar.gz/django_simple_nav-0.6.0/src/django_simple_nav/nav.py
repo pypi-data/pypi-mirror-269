5d2cf47d1296457c6f4018ffb5356c261fa2e646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:07:45 +0000

    Add package django-simple-nav/0.6.0/django_simple_nav-0.6.0.tar.gz

---
+++ packages/django-simple-nav/django_simple_nav-0.6.0.tar.gz/django_simple_nav-0.6.0/src/django_simple_nav/nav.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from dataclasses import field
+from typing import Any
+
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.urls.exceptions import NoReverseMatch
+from django.utils.safestring import mark_safe
+
+from django_simple_nav.permissions import check_item_permissions
+
+
+@dataclass(frozen=True)
+class Nav:
+    template_name: str | None = field(init=False, default=None)
+    items: list[NavGroup | NavItem] | None = field(init=False, default=None)
+
+    def get_template_name(self) -> str:
+        if self.template_name is not None:
+            return self.template_name
+
+        msg = f"{self.__class__!r} must define 'template_name' or override 'get_template_name()'"
+        raise ImproperlyConfigured(msg % self.__class__.__name__)
+
+    def get_items(self, request: HttpRequest) -> list[RenderedNavItem]:
+        if self.items is not None:
+            return [
+                RenderedNavItem(item, request)
+                for item in self.items
+                if check_item_permissions(item, request)
+            ]
+
+        msg = f"{self.__class__!r} must define 'items' or override 'get_items()'"
+        raise ImproperlyConfigured(msg)
+
+    def get_context_data(self, request: HttpRequest) -> dict[str, Any]:
+        return {
+            "items": self.get_items(request),
+            "request": request,
+        }
+
+    def render(self, request: HttpRequest, template_name: str | None = None) -> str:
+        context = self.get_context_data(request)
+        return render_to_string(
+            template_name=template_name or self.get_template_name(),
+            context=context,
+            request=request,
+        )
+
+
+@dataclass(frozen=True)
+class NavGroup:
+    title: str
+    items: list[NavGroup | NavItem]
+    url: str | None = None
+    permissions: list[str] = field(default_factory=list)
+    extra_context: dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass(frozen=True)
+class NavItem:
+    title: str
+    url: str
+    permissions: list[str] = field(default_factory=list)
+    extra_context: dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass(frozen=True)
+class RenderedNavItem:
+    item: NavItem | NavGroup
+    request: HttpRequest
+
+    def __getattr__(self, name: str) -> Any:
+        if name == "extra_context":
+            return self.item.extra_context
+        elif hasattr(self.item, name):
+            return getattr(self.item, name)
+        else:
+            try:
+                return self.item.extra_context[name]
+            except KeyError as err:
+                msg = f"{self.item!r} object has no attribute {name!r}"
+                raise AttributeError(msg) from err
+
+    @property
+    def title(self) -> str:
+        return mark_safe(self.item.title)
+
+    @property
+    def items(self) -> list[RenderedNavItem] | None:
+        if not isinstance(self.item, NavGroup):
+            return None
+        return [RenderedNavItem(item, self.request) for item in self.item.items]
+
+    @property
+    def url(self) -> str | None:
+        if not self.item.url:
+            return None
+        try:
+            url = reverse(self.item.url)
+        except NoReverseMatch:
+            url = self.item.url
+        return url
+
+    @property
+    def active(self) -> bool:
+        if not self.url:
+            return False
+        return (
+            self.request.path.startswith(self.url)
+            and self.url != "/"
+            or self.request.path == self.url
+        )
---
+++ packages/django-simple-nav/django_simple_nav-0.6.0.tar.gz/django_simple_nav-0.6.0/tests/test_nav.py
@@ -0,0 +1,250 @@
+from __future__ import annotations
+
+import pytest
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.utils.module_loading import import_string
+from model_bakery import baker
+
+from django_simple_nav.nav import NavGroup
+from django_simple_nav.nav import NavItem
+from django_simple_nav.nav import RenderedNavItem
+from tests.navs import DummyNav
+from tests.utils import count_anchors
+
+pytestmark = pytest.mark.django_db
+
+
+def test_init():
+    nav = DummyNav
+
+    assert nav.template_name == "tests/dummy_nav.html"
+    assert len(nav.items) == 12
+
+    for item in nav.items:
+        assert item.title
+
+
+@pytest.mark.parametrize(
+    "nav, template_name, expected_count",
+    [
+        ("tests.navs.DummyNav", "tests/dummy_nav.html", 12),
+    ],
+)
+def test_dotted_path_loading(nav, template_name, expected_count):
+    nav = import_string(nav)
+
+    assert nav.template_name == template_name
+    assert len(nav.items) == expected_count
+
+
+@pytest.mark.parametrize(
+    "user, expected_count",
+    [
+        (AnonymousUser(), 7),
+        (None, 10),
+    ],
+)
+def test_nav_render(user, expected_count, req):
+    if not isinstance(user, AnonymousUser):
+        user = baker.make(get_user_model())
+
+    req.user = user
+
+    rendered_template = DummyNav().render(req)
+
+    assert count_anchors(rendered_template) == expected_count
+
+
+def test_dotted_path_nav_render(req):
+    req.user = baker.make(get_user_model())
+
+    nav = import_string("tests.navs.DummyNav")
+
+    assert nav().render(req)
+
+
+@pytest.mark.parametrize(
+    "permission, expected_count",
+    [
+        ("", 10),  # regular authenticated user
+        ("is_staff", 13),
+        ("is_superuser", 19),
+        ("tests.dummy_perm", 13),
+    ],
+)
+def test_nav_render_with_permissions(req, permission, expected_count):
+    user = baker.make(get_user_model())
+
+    if permission == "tests.dummy_perm":
+        dummy_perm = baker.make(
+            "auth.Permission",
+            codename="dummy_perm",
+            name="Dummy Permission",
+            content_type=baker.make("contenttypes.ContentType", app_label="tests"),
+        )
+        user.user_permissions.add(dummy_perm)
+    else:
+        setattr(user, permission, True)
+
+    user.save()
+
+    req.user = user
+    rendered_template = DummyNav().render(req)
+
+    assert count_anchors(rendered_template) == expected_count
+
+
+def test_nav_render_with_template_name(req):
+    req.user = AnonymousUser()
+
+    rendered_template = DummyNav().render(req, "tests/alternate.html")
+
+    assert "This is an alternate template." in rendered_template
+
+
+def test_extra_context(req):
+    item = NavItem(
+        title="Test",
+        url="/test/",
+        extra_context={"foo": "bar"},
+    )
+
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.foo == "bar"
+
+
+def test_extra_context_with_no_extra_context(req):
+    item = NavItem(
+        title="Test",
+        url="/test/",
+    )
+
+    rendered_item = RenderedNavItem(item, req)
+
+    with pytest.raises(AttributeError):
+        assert rendered_item.foo == "bar"
+
+
+def test_extra_context_shadowing(req):
+    item = NavItem(
+        title="Test",
+        url="/test/",
+        extra_context={"title": "Shadowed"},
+    )
+
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.title == "Test"
+
+
+def test_extra_context_iteration(req):
+    item = NavItem(
+        title="Test",
+        url="/test/",
+        extra_context={"foo": "bar", "baz": "qux"},
+    )
+
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.extra_context == {"foo": "bar", "baz": "qux"}
+    for key, value in rendered_item.extra_context.items():
+        assert getattr(rendered_item, key) == value
+
+
+def test_extra_context_builtins(req):
+    item = NavGroup(
+        title="Test",
+        items=[
+            NavItem(
+                title="Test",
+                url="/test/",
+                permissions=["is_staff"],
+                extra_context={"foo": "bar"},
+            ),
+        ],
+        url="/test/",
+        permissions=["is_staff"],
+        extra_context={"baz": "qux"},
+    )
+
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.title == "Test"
+    assert rendered_item.url == "/test/"
+    assert rendered_item.permissions == ["is_staff"]
+    assert rendered_item.extra_context == {"baz": "qux"}
+    assert rendered_item.baz == "qux"
+
+    assert rendered_item.items is not None
+    assert len(rendered_item.items) == 1
+
+    rendered_group_item = rendered_item.items[0]
+
+    assert rendered_group_item.title == "Test"
+    assert rendered_group_item.url == "/test/"
+    assert rendered_group_item.permissions == ["is_staff"]
+    assert rendered_group_item.extra_context == {"foo": "bar"}
+    assert rendered_group_item.foo == "bar"
+
+
+def test_get_context_data(req):
+    req.user = baker.make(get_user_model())
+
+    context = DummyNav().get_context_data(req)
+
+    assert context["items"]
+
+
+def test_get_context_data_override(req):
+    class OverrideNav(DummyNav):
+        def get_context_data(self, request):
+            return {"foo": "bar"}
+
+    req.user = baker.make(get_user_model())
+
+    context = OverrideNav().get_context_data(req)
+
+    assert context["foo"] == "bar"
+
+
+def test_get_context_data_override_render(req):
+    class OverrideNav(DummyNav):
+        def get_context_data(self, request):
+            return {"foo": "bar"}
+
+    req.user = baker.make(get_user_model())
+
+    rendered_template = OverrideNav().render(req)
+
+    assert count_anchors(rendered_template) == 0
+
+
+def test_rendered_nav_item_active(req):
+    item = NavItem(title="Test", url="/test/")
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.active is False
+
+    req.path = "/test/"
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.active is True
+
+
+def test_rendered_nav_group_active_no_url(req):
+    item = NavGroup(title="Test", items=[NavItem(title="Test", url="/test/")])
+
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.active is False
+
+
+def test_rendered_nav_item_active_named_url(req):
+    item = NavItem(title="Test", url="fake-view")
+
+    req.path = "/fake-view/"
+    rendered_item = RenderedNavItem(item, req)
+
+    assert rendered_item.active is True
