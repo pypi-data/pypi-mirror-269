0fd62d667eedb5b0730448a15a8108f041f0700e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 15:07:30 +0000

    Add package NStudyPy/0.0.1/nstudypy-0.0.1.tar.gz

---
+++ packages/NStudyPy/nstudypy-0.0.1.tar.gz/nstudypy-0.0.1/README.md
@@ -0,0 +1,7 @@
+# NstudyPy
+NstudyPy工具包
+
+## 版本更新历史
+
+## version 0.0.1
+- 初始化项目
---
+++ packages/NStudyPy/nstudypy-0.0.1.tar.gz/nstudypy-0.0.1/src/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024-04-24 18:33
+# @Author  : Jack
+# @File    : __init__.py
+
+"""
+__init__.py
+"""
+"""
+__init__.py
+"""
+
+__version__ = "0.0.1"
