0d7793e3a1a0bdd4529e7e3bb3689954eff4ac7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 06:04:46 +0000

    Add package cli-log/0.1.4/cli-log-0.1.4.tar.gz

---
+++ packages/cli-log/cli-log-0.1.4.tar.gz/cli-log-0.1.4/cli/__init__.py
@@ -0,0 +1,11 @@
+"""
+# Command Line Interface Log
+
+Developed by [DeltaBotics](https://github.com/DeltaBotics)
+See LICENSE for more details.
+"""
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+from .core import info, debug, warn, error
