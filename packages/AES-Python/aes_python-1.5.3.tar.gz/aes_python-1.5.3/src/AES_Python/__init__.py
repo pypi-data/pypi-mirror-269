788abc1e2926c9a6000d90238488c03ae9df53f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 16:06:59 +0000

    Add package AES-Python/1.5.3/aes_python-1.5.3.tar.gz

---
+++ packages/AES-Python/aes_python-1.5.3.tar.gz/aes_python-1.5.3/src/AES_Python/__init__.py
@@ -0,0 +1,15 @@
+from .main import AES
+
+# --------------------
+# Project information
+# --------------------
+__name__ = "AES-Python"
+__author__ = 'Gabriel Lindeblad'
+__copyright__ = 'Copyright 2024, Gabriel Lindeblad'
+__license__ = 'MIT'
+__email__ = 'gabriel.lindeblad@icloud.com'
+__status__ = 'Development'
+__date__ = '2024-04-25'
+__description__ = 'AES (Advanced Encryption Standard) implementation in Python-3.12'
+__platforms__ = 'Windows, Linux, MacOS'
+__all__ = ['AES']
