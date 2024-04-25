8b1ae1ac31ee962b7be24cc43285659d9299f620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 05:09:02 +0000

    Add package molSimplify/1.7.4/molSimplify-1.7.4-py3-none-any.whl

---
+++ packages/molSimplify/molSimplify-1.7.4-py3-none-any.whl/molSimplify/ml/layers.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from typing import List, Tuple
+
+
+class PermutationLayer(tf.keras.layers.Layer):
+
+    def __init__(self, permutations: List[Tuple[int]]):
+        super().__init__()
+        self.permutations = permutations
+
+    def call(self, inputs):
+        # Shape (batch_size, n_tmc_features)
+        tmc_inputs = inputs["tmc"]
+        # Shape (batch_size, n_ligands, n_ligand_features)
+        ligand_inputs = inputs["ligand"]
+        outputs = []
+        for p in self.permutations:
+            outputs.append(
+                tf.concat(
+                    [
+                        tmc_inputs,
+                    ]
+                    + [
+                        ligand_inputs[:, p_i, :] for p_i in p
+                    ],
+                    axis=-1,
+                )
+            )
+        return tf.stack(outputs, axis=1)
