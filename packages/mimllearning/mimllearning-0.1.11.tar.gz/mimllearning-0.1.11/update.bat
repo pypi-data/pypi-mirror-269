43da6bb9a01479be84eed1c4253afc696b9c4950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 14:05:22 +0000

    Add package mimllearning/0.1.11/mimllearning-0.1.11.tar.gz

---
+++ packages/mimllearning/mimllearning-0.1.11.tar.gz/mimllearning-0.1.11/update.bat
@@ -0,0 +1,4 @@
+python -m build
+python -m twine upload --repository pypi dist/* -p pypi-AgEIcHlwaS5vcmcCJGM0ZDM5ZDYxLTE4YmItNGNmNC05MzQwLTM0MjUxZTVkYzY2NQACKlszLCI0YThjYTE3OC1lNzU2LTQxMDMtODNiZC1hY2Y5OTY2MzBkOWUiXQAABiAH_u5z_gln-wOAV4MIRtWPKuAdhyeEmdibdnT7R3qE5Q
+pip uninstall mimllearning -y
+
