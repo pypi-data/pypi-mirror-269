9eb427576ffc8e6c70014acf25695f035bafbb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:19 +0000

    Add package gluepy/1.1.4/gluepy-1.1.4-py3-none-any.whl

---
+++ packages/gluepy/gluepy-1.1.4-py3-none-any.whl/gluepy/files/storages/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from gluepy.conf import default_settings
+from gluepy.utils.loading import LazyProxy, import_string, SingletonMixin
+from .local import LocalStorage
+from .s3 import S3Storage
+from .google import GoogleStorage
+
+
+default_storage = LazyProxy(lambda: import_string(default_settings.STORAGE_BACKEND)())
