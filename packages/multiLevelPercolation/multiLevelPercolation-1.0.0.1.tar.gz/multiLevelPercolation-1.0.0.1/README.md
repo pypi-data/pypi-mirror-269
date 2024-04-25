6d72623b16829cc59ef0bccdbcd359695914166a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:15:13 +0000

    Add package multiLevelPercolation/1.0.0.1/multiLevelPercolation-1.0.0.1.tar.gz

---
+++ packages/multiLevelPercolation/multiLevelPercolation-1.0.0.1.tar.gz/multiLevelPercolation-1.0.0.1/README.md
@@ -0,0 +1,5 @@
+# Identifying individual unique thresholds for structures educed from scalar fields
+
+For a complete description of the code see https://github.com/Phoenixfire1081/MultiLevelPercolationAnalysis.
+
+
---
+++ packages/multiLevelPercolation/multiLevelPercolation-1.0.0.1.tar.gz/multiLevelPercolation-1.0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools 
+  
+with open("README.md", "r") as fh: 
+    description = fh.read() 
+  
+setuptools.setup( 
+    name="multiLevelPercolation", 
+    version="1.0.0.1", 
+    author="Abhishek Harikrishnan", 
+    author_email="abhishek.harikrishnan@fu-berlin.de", 
+    packages=["multiLevelPercolation"], 
+    package_dir={'multiLevelPercolation': 'multiLevelPercolation'},
+    package_data={'multiLevelPercolation': ['*.bin']},
+    description="Identifying individual unique thresholds for structures educed from scalar fields", 
+    long_description=description, 
+    long_description_content_type="text/markdown", 
+    url="https://github.com/Phoenixfire1081/MultiLevelPercolationAnalysis", 
+    license='MIT', 
+    python_requires='>=3.8', 
+    install_requires=[] 
+) 
