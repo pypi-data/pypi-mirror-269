3d3110b485414050c55b7b0806e79cef3fc4b992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:46 +0000

    Add package netlify-dns-manager/0.4/netlify_dns_manager-0.4.tar.gz

---
+++ packages/netlify-dns-manager/netlify_dns_manager-0.4.tar.gz/netlify_dns_manager-0.4/netlify_dns_manage/parser.py
@@ -0,0 +1,19 @@
+import zonefile_parser
+from zonefile_parser.record import Record
+
+def parse_zone_file(domain_name, zone_file_path):
+    dns_records = []
+    with open(zone_file_path,"r") as stream:
+        content = stream.read()
+        records: list[Record] = zonefile_parser.parse(content)
+        for record in records:
+            new_name = record.name.replace(f".{domain_name}.","")
+            modified_record = {
+                "rtype": record.rtype,
+                "name": new_name,
+                "rclass": record.rclass,
+                "rdata": record.rdata,
+                "ttl": record.ttl
+            }
+            dns_records.append(modified_record)
+    return dns_records
