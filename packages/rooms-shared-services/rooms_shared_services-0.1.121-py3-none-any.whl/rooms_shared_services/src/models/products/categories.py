eee913de428c187287d277494e47b2d53265e169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 03:14:34 +0000

    Add package rooms-shared-services/0.1.121/rooms_shared_services-0.1.121-py3-none-any.whl

---
+++ packages/rooms-shared-services/rooms_shared_services-0.1.121-py3-none-any.whl/rooms_shared_services/src/models/products/categories.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+from pydantic import HttpUrl, field_validator, Field
+
+from rooms_shared_services.src.models.texts.translations import TextTranslations
+from rooms_shared_services.src.storage.models import BaseDynamodbModel
+from uuid import UUID, uuid4
+from rooms_shared_services.src.models.texts.languages import Language
+
+
+class ProductCategory(BaseDynamodbModel):
+    name: str
+    id: int
+    parent_id: int | None = None
+    parent_name: str | None = None
+    description: str | None = None
+    image: HttpUrl | None = None
+    name_translations: TextTranslations | None = None
+    description_translations: TextTranslations | None = None
+
+    @field_validator(
+        "image",
+        mode="before",
+    )
+    def coerce_image(cls, item_value: Any):
+        if isinstance(item_value, dict):
+            return item_value.get("image", None)
+        return item_value
+    
+    
+class ProductCategoryTranslation(BaseDynamodbModel):
+    id: UUID = Field(default_factory=uuid4)
+    name: str
+    language: Language
+    description: str | None = None
+    image: HttpUrl | None = None
+    
+    @field_validator(
+        "name",
+        mode="before"
+    )
+    def remove_underscores(cls, name_value: Any):
+        if isinstance(name_value, str):
+            return name_value.replace("_", " ")
+        return name_value
