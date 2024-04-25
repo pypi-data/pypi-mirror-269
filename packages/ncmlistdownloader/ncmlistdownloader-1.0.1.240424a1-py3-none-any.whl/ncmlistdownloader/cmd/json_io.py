9a70e3d7ac2c3949a53a88558736d4e10bb95aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-25 04:09:32 +0000

    Add package ncmlistdownloader/1.0.1.240424a1/ncmlistdownloader-1.0.1.240424a1-py3-none-any.whl

---
+++ packages/ncmlistdownloader/ncmlistdownloader-1.0.1.240424a1-py3-none-any.whl/ncmlistdownloader/cmd/json_io.py
@@ -0,0 +1,35 @@
+'''
+ncmlistdownloader/cmd/json_io.py
+Core.Ver.1.0.1.240424a1
+Author: CooooldWind_
+'''
+
+from ncmlistdownloader.song import *
+from ncmlistdownloader.playlist import *
+from ncmlistdownloader.cmd.common import *
+
+def json_save_list(pl: Playlist):
+    path = str(input_func(notice = "Input the file's page"))
+    d = {
+        'type': 'downloading_list_ncmld',
+        'global_config': GLOBAL_CONFIG_MODEL,
+        'track': [],
+    }
+    t = []
+    for i in pl.track:
+        t.append({
+            'type': 'song',
+            'info': {
+                'title': i.title,
+                'artist': i.artist,
+                'album': i.album,
+                'id': i.id,
+                'pic_url': i.url_info['album_pic']
+            },
+            'global': True,
+            'downloading_config': GLOBAL_CONFIG_MODEL,
+        })
+    d['track'] = t
+    with open(path, 'w+', encoding = 'utf-8') as file:
+        json.dump(d, file, ensure_ascii = False, sort_keys = True)
+    print(format_output("Saved!", type = "Info"))
