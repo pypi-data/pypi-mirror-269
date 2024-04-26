eccd4516ad312fa3fd2541a0b61cad3944c32ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:05:33 +0000

    Add package gameon/1.0.1/gameon-1.0.1.tar.gz

---
+++ packages/gameon/gameon-1.0.1.tar.gz/gameon-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='gameon',
+   version='1.0.1',
+   description='A useful game engine for beginners',
+   author='Morris El Helou',
+   author_email='morriselhelou816@gmail.com',
+   include_package_data=True,
+   packages=['GameOn'], 
+   package_data={"GameOn": ["*.ico"]},
+   install_requires=['pygame','keyboard','tymer'], #external packages as dependencies
+)
