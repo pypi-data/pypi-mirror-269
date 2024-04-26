4994abb02d99692806e1b66326949c88246548f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:54 +0000

    Add package ilcdlib/4.2.0/ilcdlib-4.2.0-py3-none-any.whl

---
+++ packages/ilcdlib/ilcdlib-4.2.0-py3-none-any.whl/ilcdlib/epd/dialect/oekobaudat.py
@@ -0,0 +1,50 @@
+#
+#  Copyright 2024 by C Change Labs Inc. www.c-change-labs.com
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This software was developed with support from the Skanska USA,
+#  Charles Pankow Foundation, Microsoft Sustainability Fund, Interface, MKA Foundation, and others.
+#  Find out more at www.BuildingTransparency.org
+#
+from ilcdlib.dto import ProductClassDef
+from ilcdlib.epd.reader import IlcdEpdReader
+from ilcdlib.utils import none_throws
+
+
+class OekobauDatIlcdXmlEpdReader(IlcdEpdReader):
+    """Reader for EPDs in the Oekobau.DAT specific ILCD XML format."""
+
+    @classmethod
+    def is_known_url(cls, url: str) -> bool:
+        """Return whether the URL recognized as a known Environdec URL."""
+        return "oekobaudat" in url.lower()
+
+    def post_init(self):
+        """Configure Oekobau.DAT specific settings."""
+        self.xml_parser.xml_ns["epd2019"] = self.xml_parser.xml_ns["epd2019_indata"]
+
+    def _product_classes_to_openepd(self, classes: dict[str, list[ProductClassDef]]) -> dict[str, list[str] | str]:
+        """
+        Convert the product classes to OpenEPD format.
+
+        The Oekobau.DAT format according to openEPD is a string containing full id and
+        the name of the most specific class. Example: "1.1.01 Cement"
+        """
+        result = super()._product_classes_to_openepd(classes)
+        for classification_name, class_defs in classes.items():
+            if classification_name.lower() == "oekobau.dat" and len(class_defs) > 0:
+                last_class = class_defs[-1]
+                del result[classification_name]
+                result["oekobau.dat"] = " ".join((none_throws(last_class.id), none_throws(last_class.name)))
+        return result
---
+++ packages/ilcdlib/ilcdlib-4.2.0-py3-none-any.whl/ilcdlib/mapping/common.py
@@ -0,0 +1,77 @@
+#
+#  Copyright 2024 by C Change Labs Inc. www.c-change-labs.com
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This software was developed with support from the Skanska USA,
+#  Charles Pankow Foundation, Microsoft Sustainability Fund, Interface, MKA Foundation, and others.
+#  Find out more at www.BuildingTransparency.org
+#
+__all__ = (
+    "BaseDataMapper",
+    "SimpleDataMapper",
+)
+
+import abc
+from typing import Generic, TypeVar, cast
+
+T = TypeVar("T")
+K = TypeVar("K")
+
+
+class BaseDataMapper(Generic[T, K], abc.ABC):
+    """Base class for all data mappers."""
+
+    @abc.abstractmethod
+    def map(self, input_value: T, default_value: K | None) -> K | None:
+        """
+        Map the input value to the output value.
+
+        :param input_value: The input value to map.
+        :param default_value: The default value to return if there is no mapping for input value.
+        """
+        pass
+
+
+class SimpleDataMapper(BaseDataMapper[T, T], Generic[T]):
+    """A data mapper that does not change the type of the input value."""
+
+    DATABASE: dict[T, T] = {}
+
+    def map(self, input_value: T, default_value: T | None) -> T | None:
+        """
+        Map the input value to the output value.
+
+        :param input_value: The input value to map.
+        :param default_value: The default value to return if there is no mapping for input value.
+        """
+        return self.DATABASE.get(input_value, default_value)
+
+
+class KeyValueMapper(BaseDataMapper[str, T], Generic[T]):
+    """A data mapper that maps input values to output values using keywords."""
+
+    KV: dict[str, list[T]] = {}
+
+    def map(self, input_value: str, default_value: T | None) -> T | None:
+        """
+        Map the input value to the output value using keywords.
+
+        :param input_value: The input value to map.
+        :param default_value: The default value to return if there is no mapping for input value.
+        """
+        for impact_name, keywords in self.KV.items():
+            for keyword in keywords:
+                if str(keyword).strip().lower() in input_value.strip().lower():
+                    return cast(T, impact_name)
+        return default_value
