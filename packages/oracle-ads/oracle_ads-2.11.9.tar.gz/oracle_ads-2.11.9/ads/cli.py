d957a7d229e343472f2fdfc2a7808b5a4d1a239d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:05:52 +0000

    Add package oracle-ads/2.11.9/oracle_ads-2.11.9.tar.gz

---
+++ packages/oracle-ads/oracle_ads-2.11.9.tar.gz/oracle_ads-2.11.9/ads/cli.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*--
+
+# Copyright (c) 2021, 2024 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+import traceback
+import sys
+
+import fire
+from dataclasses import is_dataclass
+from ads.common import logger
+
+try:
+    import click
+    import ads.opctl.cli
+    import ads.jobs.cli
+    import ads.pipeline.cli
+    import ads.opctl.operator.cli
+except Exception as ex:
+    print(
+        "Please run `pip install oracle-ads[opctl]` to install "
+        "the required dependencies for ADS CLI. \n"
+        f"{str(ex)}"
+    )
+    logger.debug(ex)
+    logger.debug(traceback.format_exc())
+    exit()
+
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-package-version
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+ADS_VERSION = metadata.version("oracle_ads")
+
+
+@click.group()
+@click.version_option(version=ADS_VERSION, prog_name="ads")
+@click.help_option("--help", "-h")
+def click_cli():
+    pass
+
+
+@click.command
+def aqua_cli():
+    """CLI for AQUA."""
+    # This is a dummy entry for click.
+    # The `ads aqua` commands are handled by AquaCommand
+
+
+click_cli.add_command(ads.opctl.cli.commands)
+click_cli.add_command(ads.jobs.cli.commands)
+click_cli.add_command(ads.pipeline.cli.commands)
+click_cli.add_command(ads.opctl.operator.cli.commands)
+click_cli.add_command(aqua_cli, name="aqua")
+
+
+# fix for fire issue with --help
+# https://github.com/google/python-fire/issues/258
+def _SeparateFlagArgs(args):
+    try:
+        index = args.index("--help")
+        args = args[:index]
+        return args, ["--help"]
+    except ValueError:
+        return args, []
+
+
+fire.core.parser.SeparateFlagArgs = _SeparateFlagArgs
+
+
+def serialize(data):
+    """Serialize dataclass objects or lists of dataclass objects.
+    Parameters:
+        data: A dataclass object or a list of dataclass objects.
+    Returns:
+        None
+    Prints:
+        The string representation of each dataclass object.
+    """
+    if isinstance(data, list):
+        [print(str(item)) for item in data]
+    else:
+        print(str(data))
+
+
+def cli():
+    if len(sys.argv) > 1 and sys.argv[1] == "aqua":
+        from ads.aqua.cli import AquaCommand
+
+        fire.Fire(
+            AquaCommand, command=sys.argv[2:], name="ads aqua", serialize=serialize
+        )
+    else:
+        click_cli()
+
+
+if __name__ == "__main__":
+    cli()
