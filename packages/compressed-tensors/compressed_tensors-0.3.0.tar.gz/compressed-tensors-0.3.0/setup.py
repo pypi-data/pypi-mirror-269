f35e3550fed8be652d08292bc60d57931639db7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:47 +0000

    Add package compressed-tensors/0.3.0/compressed_tensors-0.3.0.tar.gz

---
+++ packages/compressed-tensors/compressed_tensors-0.3.0.tar.gz/compressed-tensors-0.3.0/setup.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from setuptools import setup, find_packages
+from typing import List, Dict
+
+def _setup_packages() -> List:
+    return find_packages(
+        "src", include=["compressed_tensors", "compressed_tensors.*"], exclude=["*.__pycache__.*"]
+    )
+    
+def _setup_install_requires() -> List:
+    return ["torch>=1.7.0", "transformers<=4.40", "pydantic<2.7"]
+
+def _setup_extras() -> Dict:
+    return {"dev": ["black==22.12.0", "isort==5.8.0", "wheel>=0.36.2", "flake8>=3.8.3", "pytest>=6.0.0", "nbconvert>=7.16.3"]}
+
+setup(
+    name="compressed-tensors",
+    version="0.3.0",
+    author="Neuralmagic, Inc.",
+    author_email="support@neuralmagic.com",
+    description="Library for utilization of compressed safetensors of neural network models",
+    extras_require=_setup_extras(),
+    install_requires=_setup_install_requires(),
+    package_dir={"": "src"},
+    packages=_setup_packages(),
+)
