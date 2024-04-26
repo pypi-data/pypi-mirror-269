4bf7e63072623fea015e893cdf02a6aa861adf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:47 +0000

    Add package alphaz-next/0.5.2/alphaz_next-0.5.2-py3-none-any.whl

---
+++ packages/alphaz-next/alphaz_next-0.5.2-py3-none-any.whl/alphaz_next/asyncio/__init__.py
@@ -0,0 +1,6 @@
+from pysql_repo.asyncio import (
+    AsyncDataBase as AsyncDataBase,
+    AsyncRepository as AsyncRepository,
+    AsyncService as AsyncService,
+    with_async_session as with_async_session,
+)
