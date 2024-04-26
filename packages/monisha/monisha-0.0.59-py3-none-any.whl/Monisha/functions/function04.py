a4e53f22c09025f66225c6cd03274713a52bf623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:55 +0000

    Add package monisha/0.0.59/monisha-0.0.59-py3-none-any.whl

---
+++ packages/monisha/monisha-0.0.59-py3-none-any.whl/Monisha/functions/function04.py
@@ -0,0 +1,27 @@
+#============================================================================================================================
+
+class Eimes(object):
+
+    DATA01 = (".TXT", ".PDF", ".APK", ".EXE", ".ZIP", ".RAR", ".TAR", ".ISO")
+
+    DATA02 = (".MKV", ".MP4", ".MOV", ".WMV", ".3GP", ".MPG", ".WEBM", ".AVI", ".FLV", ".M4V", ".GIF")
+
+    DATA03 = (".MP3", ".M4A", ".M4B", ".FLAC", ".WAV", ".AIF", ".OGG", ".AAC", ".DTS", ".MID", ".AMR", ".MKA")
+
+    DATA04 = (".JPG", ".JPX", ".PNG", ".WEBP", ".CR2", ".TIF", ".BMP", ".JXR", ".PSD", ".ICO", ".HEIC", ".JPEG")
+
+#===========================================================================================================================  
+
+class Mimeo(object):
+
+    DATA01 = ("audio", "document", "photo", "sticker", "animation", "video", "voice", "video_note", "new_chat_photo")
+
+#============================================================================================================================
+
+class Mimes(object):
+
+    DATA01 = ['audio/mpeg', 'audio/x-flac', 'audio/x-wav', 'audio/m4a', 'audio/ogg']
+
+    DATA02 = ['video/3gpp', 'video/mp4', 'video/x-matroska', 'video/webm', 'video/mpeg', 'video/x-flv', 'video/x-msvideo']
+
+#============================================================================================================================
