cad3bce17cf84a69667200fecd26ec13432fdf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:50 +0000

    Add package opentelemetry-instrumentation-langchain/0.16.7/opentelemetry_instrumentation_langchain-0.16.7-py3-none-any.whl

---
+++ packages/opentelemetry-instrumentation-langchain/opentelemetry_instrumentation_langchain-0.16.7-py3-none-any.whl/opentelemetry/instrumentation/langchain/utils.py
@@ -0,0 +1,43 @@
+import logging
+import os
+from opentelemetry import context as context_api
+from opentelemetry.instrumentation.langchain.config import Config
+
+
+def _with_tracer_wrapper(func):
+    """Helper for providing tracer for wrapper functions."""
+
+    def _with_tracer(tracer, to_wrap):
+        def wrapper(wrapped, instance, args, kwargs):
+            return func(tracer, to_wrap, wrapped, instance, args, kwargs)
+
+        return wrapper
+
+    return _with_tracer
+
+
+def should_send_prompts():
+    return (
+        os.getenv("TRACELOOP_TRACE_CONTENT") or "true"
+    ).lower() == "true" or context_api.get_value("override_enable_content_tracing")
+
+
+def dont_throw(func):
+    """
+    A decorator that wraps the passed in function and logs exceptions instead of throwing them.
+
+    @param func: The function to wrap
+    @return: The wrapper function
+    """
+    # Obtain a logger specific to the function's module
+    logger = logging.getLogger(func.__module__)
+
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.warning("Failed to execute %s, error: %s", func.__name__, str(e))
+            if Config.exception_logger:
+                Config.exception_logger(e)
+
+    return wrapper
