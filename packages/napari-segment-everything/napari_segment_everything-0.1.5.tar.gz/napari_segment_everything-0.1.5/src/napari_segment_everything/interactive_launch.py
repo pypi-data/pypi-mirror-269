d203b87ea1efeb2ea29d4178e5c279708fb11d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:56 +0000

    Add package napari-segment-everything/0.1.5/napari_segment_everything-0.1.5.tar.gz

---
+++ packages/napari-segment-everything/napari_segment_everything-0.1.5.tar.gz/napari_segment_everything-0.1.5/src/napari_segment_everything/interactive_launch.py
@@ -0,0 +1,9 @@
+import napari
+from napari_segment_everything import segment_everything
+
+viewer = napari.Viewer()
+
+viewer.window.add_dock_widget(
+    segment_everything.NapariSegmentEverything(viewer)
+)
+napari.run()
