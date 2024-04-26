6e176f6fce19ba7c4b77fbcb4356371a9eb03285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:05:02 +0000

    Add package verificanumeroprimo/1.0.0/verificanumeroprimo-1.0.0.tar.gz

---
+++ packages/verificanumeroprimo/verificanumeroprimo-1.0.0.tar.gz/verificanumeroprimo-1.0.0/numeroPrimo/test.py
@@ -0,0 +1,7 @@
+from primo import is_primo
+
+number = int(input("Digite um numero: "))
+
+result = is_primo(number)
+
+print(f"{number} eh primo: {result}")
---
+++ packages/verificanumeroprimo/verificanumeroprimo-1.0.0.tar.gz/verificanumeroprimo-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='verificanumeroprimo',
+
+    version='1.0.0',
+
+    author='Luiz Claudio',
+
+    author_email='luiz.holanda@visionaespacial.com.br',
+
+    description='Pacote para verificar se um numero eh primo',
+
+    packages=find_packages(),
+
+)
