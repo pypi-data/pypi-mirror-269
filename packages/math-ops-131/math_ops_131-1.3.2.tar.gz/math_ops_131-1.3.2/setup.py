84757dc090117e3a1d03c456faad00d74607977d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:54 +0000

    Add package math-ops-131/1.3.2/math_ops_131-1.3.2.tar.gz

---
+++ packages/math-ops-131/math_ops_131-1.3.2.tar.gz/math_ops_131-1.3.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='math_ops_131',
+    version='1.3.2',
+    packages=find_packages(),
+    description='A Python package for basic mathematical operations.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Unnath Chittimalla',
+    license = 'MIT License',
+    author_email='unnath.chittimalla@iiitb.ac.in',
+    url='https://github.com/AspiringPianist/math_ops_131',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
