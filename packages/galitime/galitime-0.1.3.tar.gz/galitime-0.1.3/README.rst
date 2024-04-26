0a260c49be16bef7d9a9ada092961aec65c349d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:05:17 +0000

    Add package galitime/0.1.3/galitime-0.1.3.tar.gz

---
+++ packages/galitime/galitime-0.1.3.tar.gz/galitime-0.1.3/README.rst
@@ -0,0 +1,134 @@
+galitime
+========
+
+.. |info-badge| image:: https://img.shields.io/badge/Project-Info-blue
+    :target: https://github.com/karel-brinda/galitime
+.. |github-release-badge| image:: https://img.shields.io/github/release/karel-brinda/galitime.svg
+    :target: https://github.com/karel-brinda/galitime/releases/
+.. |pypi-badge| image:: https://img.shields.io/pypi/v/galitime.svg
+    :target: https://pypi.org/project/galitime/
+.. |zenodo-badge| image:: https://zenodo.org/badge/DOI/10.5281/zenodo.10953105.svg
+    :target: https://doi.org/10.5281/zenodo.10953105
+.. |ci-tests-badge| image:: https://github.com/karel-brinda/galitime/actions/workflows/ci.yml/badge.svg
+    :target: https://github.com/karel-brinda/galitime/actions/
+
+|info-badge| |github-release-badge| |pypi-badge| |zenodo-badge| |ci-tests-badge|
+
+
+Introduction
+------------
+
+Software for benchmarking programs using `GNU Time <https://www.gnu.org/software/time/>`_.
+
+
+
+Quick example
+-------------
+
+.. code-block:: bash
+
+    $ if [[ $(uname) == "Darwin" ]]; then brew install gnu-time; fi
+    $ conda install -y -c bioconda -c conda-forge galitime
+
+    $ galitime --log time.log ls
+
+    $ cat time.log
+    real_s  user_s  sys_s   percent_cpu max_ram_kb  exit_code   fs_inputs   fs_outputs  real_s_alt  command
+    0.03    0.00    0.00    10% 2904    0   0   0   0.076661    ls
+
+
+
+Installation
+------------
+
+Dependencies
+~~~~~~~~~~~~
+
+Galitime has no dependencies beyond Python 3. However, on OS X
+it requires the GNU version of the :code:`time` command (:code:`gtime`),
+which can be installed by :code:`brew install gnu-time`.
+
+
+Using Bioconda
+~~~~~~~~~~~~~~
+
+.. code-block:: bash
+
+    conda install -y -c bioconda -c conda-forge galitime
+
+
+Using PyPI
+~~~~~~~~~~
+
+Install the Galitime Python package:
+
+.. code-block:: bash
+
+    pip install -U galitime
+
+
+
+Command-line parameters
+-----------------------
+
+
+.. code-block::
+
+    $ galitime -h
+    usage: galitime [-h] --log LOG [--experiment EXPERIMENT] [-v] command
+
+    Benchmark a command.
+
+    positional arguments:
+      command               The command to be benchmarked
+
+    options:
+      -h, --help            show this help message and exit
+      --log LOG             Path to the log file with benchmark statistics (if the directory doesn't exist, it will be created).
+      --experiment EXPERIMENT
+                            Name of the experiment (to be attached to the output)
+      -v                    show program's version number and exit
+
+
+Output
+------
+
+* ``experiment`` - Name of the experiment, if provided via ``-n``
+* ``real_s`` - Real time in seconds (wall clock time)
+* ``user_s`` - User CPU time in seconds (user mode, excluding system calls)
+* ``sys_s`` - System CPU time in seconds (kernel mode)
+* ``percent_cpu`` - CPU usage percentage
+* ``max_ram_kb`` - Maximum RAM usage in kilobytes
+* ``exit_code`` - Exit code of the command (0 if everything ok)
+* ``fs_inputs`` - File system read read operations count
+* ``fs_outputs`` - File system write operations count
+* ``real_s_alt`` - Python-measured real time in seconds (slightly higher than ``real_s``)
+* ``command`` - Command executed, with tabs replaced by spaces
+
+
+
+
+
+Issues
+------
+
+Please use `Github issues <https://github.com/karel-brinda/galitime/issues>`_.
+
+
+Changelog
+---------
+
+See `Releases <https://github.com/karel-brinda/galitime/releases>`_.
+
+
+Licence
+-------
+
+`MIT <https://github.com/karel-brinda/galitime/blob/master/LICENSE.txt>`_
+
+
+Authors
+-------
+
+* `Karel Brinda <http://brinda.eu>`_ <karel.brinda@inria.fr>
+* `Leandro Lima <https://github.com/leoisl>`_
---
+++ packages/galitime/galitime-0.1.3.tar.gz/galitime-0.1.3/galitime/galitime.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+import argparse
+import collections
+import datetime
+import os
+import re
+import subprocess
+import sys
+import tempfile
+
+from pathlib import Path
+
+PROGRAM = 'galitime'
+DESC = 'benchmarking of computational experiments using GNU time'
+
+try:
+    sys.path.append(os.path.dirname(__file__))
+    import version
+    VERSION = version.VERSION
+except ImportError:
+    VERSION = "(version NA)"
+
+DEFAULT_L = "stderr"
+
+
+def get_time_command():
+    """
+    Returns the appropriate time command based on the operating system.
+
+    Returns:
+        str: The time command to be used.
+
+    Raises:
+        Exception: If the operating system is not supported.
+    """
+    if sys.platform == "linux":
+        time_command = "/usr/bin/time"
+    elif sys.platform == "darwin":
+        time_command = "gtime"
+    else:
+        raise Exception("Unsupported OS")
+    return time_command
+
+
+def run_single_instance(command, experiment):
+
+    d = collections.OrderedDict()
+
+    # 1) experiment name (if user-specified)
+    if experiment:
+        d['experiment'] = experiment
+
+    # 2) GNU time measurements
+    header = [
+        "real(s)", "sys(s)", "user(s)", "percent_CPU", "max_RAM(kb)", "FS_inputs", "FS_outputs",
+        "elapsed_time_alt(s)"
+    ]
+
+    with tempfile.TemporaryDirectory() as dir_fn:
+        tmp_fn = os.path.join(dir_fn, "gtime_output.txt")
+        gtime_columns = (
+            "real_s", "user_s", "sys_s", "percent_cpu", "max_ram_kb", "exit_code", "fs_inputs",
+            "fs_outputs"
+        )
+        gtime_columns_spec = "%e\t%U\t%S\t%P\t%M\t%x\t%I\t%O"
+        benchmark_wrapper = f'{get_time_command()} -o {tmp_fn} -f "{gtime_columns_spec}"'
+
+        start_time = datetime.datetime.now()
+        main_process = subprocess.Popen(
+            f'{benchmark_wrapper} {command}', shell=True, executable='/bin/bash'
+        )
+        return_code = main_process.wait()
+        end_time = datetime.datetime.now()
+
+        if return_code:
+            raise subprocess.CalledProcessError(
+                return_code, main_process.args, output=main_process.stdout,
+                stderr=main_process.stderr
+            )
+
+        with open(tmp_fn) as tmp_fo:
+            gtime_output_values = tmp_fo.readline().strip().split("\t")
+
+    for k, v in zip(gtime_columns, gtime_output_values):
+        d[k] = v
+
+    # 3) elapsed time
+    d["real_s_alt"] = str((end_time - start_time).total_seconds())
+
+    # 4) formatted command
+    d["command"] = " ".join(command.replace("\\\n", " ").strip().split())
+
+    return d
+
+
+def run(log_file, command, experiment):
+    """
+    Run a benchmarking command and log the results.
+
+    Args:
+        log_file (str): The path to the log file where the results will be logged.
+        experiment (str): Optional experiment name to include in the log.
+        command (str): The benchmarking command to run.
+
+    Raises:
+        subprocess.CalledProcessError: If the benchmarking command returns a non-zero exit code.
+
+    Returns:
+        None
+    """
+    d = run_single_instance(command, experiment)
+
+    output = "\t".join(d.keys()) + "\n" + "\t".join(d.values())
+
+    if log_file == "stdout":
+        print(output)
+    elif log_file == "stderr":
+        print(output, file=sys.stderr)
+    else:
+        Path(log_file).parent.mkdir(parents=True, exist_ok=True)
+        with open(log_file, "w") as fo:
+            print(output, file=fo)
+
+
+def main():
+    """
+    The main function of the script. It parses the command line arguments, runs the benchmarking command,
+    and logs the results.
+
+    Raises:
+        subprocess.CalledProcessError: If the benchmarking command returns a non-zero exit code.
+    """
+
+    class CustomArgumentParser(argparse.ArgumentParser):
+
+        def __init__(self, prog=None, **kwargs):
+            super().__init__(prog="galitime", **kwargs)
+
+        def print_help(self):
+            """
+            Prints the help message.
+
+            Returns:
+                None
+            """
+            msg = self.format_help()
+            repl = re.compile(r'\]\s+\[')
+            msg = repl.sub("] [", msg)
+            msg = msg.replace(" [-h] [-v]", "")
+            msg = msg.replace(", --help", "        ")
+            print(msg)
+
+        def format_help(self):
+            formatter = self._get_formatter()
+            formatter.add_text(" \n" + self.description)
+            formatter.add_usage(self.usage, self._actions, self._mutually_exclusive_groups)
+            formatter.add_text(self.epilog)
+
+            # positionals, optionals and user-defined groups
+            for action_group in self._action_groups:
+                formatter.start_section(action_group.title)
+                formatter.add_text(action_group.description)
+                formatter.add_arguments(action_group._group_actions)
+                formatter.end_section()
+
+            return formatter.format_help()
+
+    parser = CustomArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="Program: {} ({})\n".format(PROGRAM, DESC) + "Version: {}\n".format(VERSION) +
+        "Contact: Karel Brinda <karel.brinda@inria.fr>",
+    )
+
+    parser.add_argument('command', help='the command to be benchmarked')
+
+    parser.add_argument(
+        '-v',
+        action='version',
+        version='{} {}'.format(PROGRAM, VERSION),
+    )
+
+    parser.add_argument(
+        '-l', '--log', dest='log', metavar='FILE', default=DEFAULT_L,
+        help=f'output benchmarking file (or stderr/stdout) [{DEFAULT_L}]'
+    )
+
+    parser.add_argument(
+        '-n', '--name', metavar='STR', help='name of the experiment', dest='experiment',
+        default=None
+    )
+
+    args = parser.parse_args()
+    run(log_file=args.log, experiment=args.experiment, command=args.command)
+
+
+if __name__ == "__main__":
+    main()
