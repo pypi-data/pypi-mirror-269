eb1e7f2aca7f4a7c92d10f8d76724b2559843c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:48 +0000

    Add package cardpydentity/0.1.0/cardpydentity-0.1.0.tar.gz

---
+++ packages/cardpydentity/cardpydentity-0.1.0.tar.gz/cardpydentity-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cardpydentity',
+    version='0.1.0',
+    install_requires=[
+        'requests',
+        'fuzzywuzzy',
+        'python-Levenshtein',
+        'openai',
+        'appdirs'
+    ],
+)
