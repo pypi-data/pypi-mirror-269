3b0c007cf04ca25c54fdca5b61ff59a73772b99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:05:05 +0000

    Add package funcnodes-plotly/0.1.0/funcnodes_plotly-0.1.0.tar.gz

---
+++ packages/funcnodes-plotly/funcnodes_plotly-0.1.0.tar.gz/funcnodes_plotly-0.1.0/funcnodes_plotly/__init__.py
@@ -0,0 +1,41 @@
+from typing import Tuple, Any
+import plotly.graph_objects as go
+from plotly.basedatatypes import BaseTraceType
+import funcnodes as fn
+from exposedfunctionality.function_parser.types import add_type
+
+from .plots import make_scatter, make_bar
+from .figure import make_figue, add_trace, plot
+
+
+add_type(
+    go.Figure,
+    "plotly.Figure",
+)
+add_type(
+    BaseTraceType,
+    "plotly.Trace",
+)
+
+
+def figureencoder(figure: go.Figure, preview: bool = False) -> Tuple[Any, bool]:
+    if isinstance(figure, go.Figure):
+        return figure.to_plotly_json(), True
+    return figure, False
+
+
+fn.JSONEncoder.add_encoder(figureencoder)
+
+
+NODE_SHELF = fn.Shelf(
+    nodes=[
+        make_scatter,
+        make_bar,
+        make_figue,
+        add_trace,
+        plot,
+    ],
+    name="Plotly",
+    description="A collection of functions for creating plotly plots.",
+    subshelves=[],
+)
