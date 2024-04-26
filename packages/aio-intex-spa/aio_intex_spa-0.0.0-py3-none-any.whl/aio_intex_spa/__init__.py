9323b1c831fe31ea41f8581414ab4182d2293a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:05:23 +0000

    Add package aio-intex-spa/0.0.0/aio_intex_spa-0.0.0-py3-none-any.whl

---
+++ packages/aio-intex-spa/aio_intex_spa-0.0.0-py3-none-any.whl/aio_intex_spa/__init__.py
@@ -0,0 +1,7 @@
+"""Initialize the aio-intex-spa package."""
+
+from aio_intex_spa.intex_spa import IntexSpa
+from aio_intex_spa.intex_spa_exceptions import (
+    IntexSpaUnreachableException,
+    IntexSpaDnsException,
+)
---
+++ packages/aio-intex-spa/aio_intex_spa-0.0.0-py3-none-any.whl/aio_intex_spa/intex_spa_exceptions.py
@@ -0,0 +1,9 @@
+"""Declare IntexSpaExceptions."""
+
+
+class IntexSpaUnreachableException(Exception):
+    """Exception raised when spa is unreachable."""
+
+
+class IntexSpaDnsException(Exception):
+    """Exception raised when DNS address cannot be resolved."""
