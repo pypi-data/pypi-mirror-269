be402165241a72408bf3ad3441324465b9ab452e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:39 +0000

    Add package qstd-core/0.2.4/qstd_core-0.2.4-py3-none-any.whl

---
+++ packages/qstd-core/qstd_core-0.2.4-py3-none-any.whl/qstd_core/marshmallow/schema.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema as DefaultSchema
+
+from . import validate, Integer, List, fields
+
+
+class Schema(DefaultSchema):
+    @classmethod
+    def pagination(cls):
+        class PaginationResponse(Schema):
+            total = Integer(required=True, validate=validate.Range(min=0))
+            page_count = Integer(required=True, validate=validate.Range(min=0))
+            data = List(fields.Nested(cls()), required=True)
+        return PaginationResponse()
+
+
+
