2604e997b9daa526189d4584ea7ac23950286d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:48 +0000

    Add package varys-client/1.0.1/varys_client-1.0.1-py3-none-any.whl

---
+++ packages/varys-client/varys_client-1.0.1-py3-none-any.whl/tests/test_varys.py
@@ -0,0 +1,244 @@
+import unittest
+import time
+import tempfile
+import os
+import json
+import logging
+from varys import Varys
+import pika
+
+DIR = os.path.dirname(__file__)
+LOG_FILENAME = os.path.join(DIR, "test.log")
+TMP_HANDLE, TMP_FILENAME = tempfile.mkstemp()
+TEXT = "Hello, world!"
+
+
+class TestVarys(unittest.TestCase):
+
+    def tearDown(self):
+        # this seems to prevent some hanging
+        # or errors related to closing connections that haven't opened yet
+        # I presume because some operations are so fast
+        # that we try to close the connections before they've opened
+        # 0.01s seems to be sufficient; 0.1s is just a bit conservative
+        time.sleep(0.1)
+
+        self.v.close()
+        os.remove(TMP_FILENAME)
+        time.sleep(0.1)
+
+        credentials = pika.PlainCredentials("guest", "guest")
+
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters("localhost", credentials=credentials)
+        )
+        channel = connection.channel()
+
+        channel.queue_delete(queue="test_varys.q")
+
+        connection.close()
+
+        # check that all file handles were dropped
+        logger = logging.getLogger("test_varys")
+        self.assertEqual(len(logger.handlers), 0)
+
+    def send_and_receive(self):
+        self.v.send(TEXT, "test_varys", queue_suffix="q")
+        message = self.v.receive("test_varys", queue_suffix="q")
+        self.assertEqual(TEXT, json.loads(message.body))
+
+        logger = logging.getLogger("test_varys")
+        self.assertEqual(len(logger.handlers), 1)
+
+    def manual_ack(self):
+
+        self.v.auto_ack = False
+
+        self.v.send(TEXT, "test_varys", queue_suffix="q")
+
+        message = self.v.receive("test_varys", queue_suffix="q")
+
+        self.v.acknowledge_message(message)
+
+    def nack(self):
+        self.v.auto_ack = False
+
+        self.v.send(TEXT, "test_varys", queue_suffix="q")
+
+        message = self.v.receive("test_varys", queue_suffix="q")
+
+        self.v.nack_message(message)
+
+        # check that the message has been requeued
+        message_2 = self.v.receive("test_varys", queue_suffix="q")
+
+        self.v.acknowledge_message(message_2)
+
+        self.assertEqual(message.body, message_2.body)
+
+    def send_and_receive_batch(self):
+        self.v.send(TEXT, "test_varys", queue_suffix="q")
+        self.v.send(TEXT, "test_varys", queue_suffix="q")
+
+        messages = self.v.receive_batch("test_varys", queue_suffix="q", timeout=1)
+        parsed_messages = [json.loads(m.body) for m in messages]
+        self.assertListEqual([TEXT, TEXT], parsed_messages)
+
+    def receive_no_message(self):
+        self.assertIsNone(
+            self.v.receive("test_varys", queue_suffix="q", timeout=0)
+        )
+
+    def send_no_suffix(self):
+        self.assertRaises(Exception, self.v.send, TEXT, "test_varys")
+
+    def receive_no_suffix(self):
+        self.assertRaises(Exception, self.v.receive, "test_varys")
+
+    def receive_batch_no_suffix(self):
+        self.assertRaises(Exception, self.v.receive_batch, "test_varys")
+
+
+class TestVarysTLS(TestVarys):
+
+    def setUp(self):
+        config = {
+            "version": "0.1",
+            "profiles": {
+                "test": {
+                    "username": "guest",
+                    "password": "guest",
+                    "amqp_url": "localhost",
+                    "port": 5671,
+                    "use_tls": True,
+                    "ca_certificate": ".rabbitmq/ca_certificate.pem",
+                }
+            },
+        }
+
+        with open(TMP_FILENAME, "w") as f:
+            json.dump(config, f, ensure_ascii=False)
+
+        self.v = Varys("test", LOG_FILENAME, config_path=TMP_FILENAME)
+
+    def test_send_and_receive(self):
+        self.send_and_receive()
+
+    def test_manual_ack(self):
+        self.manual_ack()
+
+    def test_nack(self):
+        self.nack()
+
+    def test_send_and_receive_batch(self):
+        self.send_and_receive_batch()
+
+    def test_receive_no_message(self):
+        self.receive_no_message()
+
+    def test_send_no_suffix(self):
+        self.send_no_suffix()
+
+    def test_receive_no_suffix(self):
+        self.receive_no_suffix()
+
+    def test_receive_batch_no_suffix(self):
+        self.receive_batch_no_suffix()
+
+
+class TestVarysNoTLS(TestVarys):
+
+    def setUp(self):
+        config = {
+            "version": "0.1",
+            "profiles": {
+                "test": {
+                    "username": "guest",
+                    "password": "guest",
+                    "amqp_url": "127.0.0.1",
+                    "port": 5672,
+                    "use_tls": False,
+                    "ca_certificate": "this-value-shouldn't-matter",
+                }
+            },
+        }
+
+        with open(TMP_FILENAME, "w") as f:
+            json.dump(config, f, ensure_ascii=False)
+
+        self.v = Varys("test", LOG_FILENAME, config_path=TMP_FILENAME)
+
+    def test_send_and_receive(self):
+        self.send_and_receive()
+
+    def test_manual_ack(self):
+        self.manual_ack()
+
+    def test_nack(self):
+        self.nack()
+
+    def test_send_and_receive_batch(self):
+        self.send_and_receive_batch()
+
+    def test_receive_no_message(self):
+        self.receive_no_message()
+
+    def test_send_no_suffix(self):
+        self.send_no_suffix()
+
+    def test_receive_no_suffix(self):
+        self.receive_no_suffix()
+
+    def test_receive_batch_no_suffix(self):
+        self.receive_batch_no_suffix()
+
+
+class TestVarysConfig(unittest.TestCase):
+    def tearDown(self):
+        os.remove(TMP_FILENAME)
+
+    def test_config_not_json(self):
+        with open(TMP_FILENAME, "w") as f:
+            f.write("asdf9υ021ζ3;-ö×=()[]{}∇Δοo")
+
+        # use a context manager so we can check SystemExit code
+        with self.assertRaises(SystemExit) as cm:
+            v = Varys("test", LOG_FILENAME, config_path=TMP_FILENAME)
+
+        self.assertEqual(cm.exception.code, 11)
+
+    def test_config_profile_missing(self):
+        config = {
+            "version": "0.2",  # bad version prints warning but doesn't raise error
+            "profiles": {"asdfadsf": {}},
+        }
+
+        with open(TMP_FILENAME, "w") as f:
+            json.dump(config, f, ensure_ascii=False)
+
+        with self.assertRaises(SystemExit) as cm:
+            v = Varys("test", LOG_FILENAME, config_path=TMP_FILENAME)
+
+        self.assertEqual(cm.exception.code, 2)
+
+    def test_config_profile_incomplete(self):
+        config = {
+            "version": "0.1",
+            "profiles": {
+                "test": {
+                    "username": "username",
+                    "extra": "unnecessary",
+                }
+            },
+        }
+
+        with open(TMP_FILENAME, "w") as f:
+            json.dump(config, f, ensure_ascii=False)
+
+        with self.assertRaises(SystemExit) as cm:
+            v = Varys("test", LOG_FILENAME, config_path=TMP_FILENAME)
+
+        self.assertEqual(cm.exception.code, 11)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/varys-client/varys_client-1.0.1-py3-none-any.whl/varys/producer.py
@@ -0,0 +1,120 @@
+import functools
+import pika
+import time
+import json
+
+from varys.process import Process
+
+
+class Producer(Process):
+    def __init__(
+        self,
+        message_queue,
+        exchange,
+        configuration,
+        log_file,
+        log_level,
+        queue_suffix,
+        exchange_type,
+        routing_key="arbitrary_string",
+        reconnect_wait=10,
+    ):
+        super().__init__(
+            message_queue,
+            exchange,
+            configuration,
+            log_file,
+            log_level,
+            queue_suffix,
+            exchange_type,
+            routing_key=routing_key,
+            reconnect_wait=reconnect_wait,
+        )
+
+        self._message_number = 0
+
+        self._message_properties = pika.BasicProperties(
+            content_type="json", delivery_mode=pika.DeliveryMode.Persistent,
+        )
+
+    def publish_message(self, message, max_attempts=1):
+        try:
+            message_str = json.dumps(message, ensure_ascii=False)
+        except TypeError:
+            self._log.error(f"Unable to serialise message into json: {str(message)}")
+
+        attempt = 0
+        while attempt < max_attempts:
+            try:
+                attempt += 1
+                self._log.info(f"Sending message (attempt {attempt}): {json.dumps(message)}")
+                self._connection.add_callback_threadsafe(
+                    functools.partial(
+                        self._channel.basic_publish,
+                        self._exchange,
+                        self._routing_key,
+                        message_str,
+                        self._message_properties,
+                        mandatory=True,
+                    )
+                )
+            except pika.exceptions.ConnectionWrongStateError:
+                self._log.exception(f"Exception while trying to publish message on attempt {attempt}!")
+
+                if attempt < max_attempts and self._reconnect_wait >= 0:
+                    time.sleep(self._reconnect_wait)
+                    continue
+                else:
+                    raise
+
+            break
+
+        self._message_number += 1
+        self._log.info(f"Published message #{self._message_number}")
+
+    def run(self):
+        while not self._stopping:
+            try:
+                self._connection = pika.BlockingConnection(self._parameters)
+                self._channel = self._connection.channel()
+                self._channel.exchange_declare(
+                    exchange=self._exchange,
+                    exchange_type=self._exchange_type,
+                    durable=True,
+                )
+                self._channel.queue_declare(queue=self._queue, durable=True)
+                self._channel.queue_bind(queue=self._queue, exchange=self._exchange, routing_key=self._routing_key)
+                self._channel.confirm_delivery()
+                # time_limit=None leads to the connection being dropped for inactivity
+                # not sure if this should be while not self._stopping
+                while True:
+                    self._connection.process_data_events(time_limit=1)
+            except:
+                self._log.exception("Producer caught exception:")
+
+            if self._stopping or self._reconnect_wait < 0:
+                break
+            else:
+                time.sleep(self._reconnect_wait)
+                continue
+
+    def stop(self):
+        self._log.info("Stopping producer as instructed...")
+        # probably have to say we're closing so run doesn't try to reopen connection
+        self._stopping = True
+
+        self._connection.add_callback_threadsafe(
+            functools.partial(self._connection.process_data_events, time_limit=1)
+        )
+
+        self._connection.add_callback_threadsafe(
+            self._channel.close
+        )
+        self._connection.add_callback_threadsafe(
+            self._connection.close
+        )
+
+        self._log.debug("Stopping producer logger...")
+        self._stop_logger()
+
+        self._log.info("Stopped producer as instructed.")
