a658a2a83168bf7d0869925b8935e623ba8a8fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:05:15 +0000

    Add package swagger-coverage/3.5.1/swagger_coverage-3.5.1.tar.gz

---
+++ packages/swagger-coverage/swagger_coverage-3.5.1.tar.gz/swagger_coverage-3.5.1/swagger_coverage_py/listener.py
@@ -0,0 +1,34 @@
+import requests
+
+from swagger_coverage_py.configs import IS_DISABLED
+from swagger_coverage_py.request_schema_handler import RequestSchemaHandler
+from swagger_coverage_py.uri import URI
+
+
+class CoverageListener:
+    def __init__(
+        self,
+        method: str,
+        base_url: str,
+        raw_path: str,
+        uri_params: dict,
+        base_path: str = "",
+        **kwargs
+    ):
+        """Records an HTTP request as a file in swagger format
+
+        :param method: the HTTP method name in lowercase.
+        :param base_url: Base URl with a protocol but without a path. (e.g. "https://petstore.swagger.io")
+        :param raw_path: Not formatted URL path.
+            Parameters names in braces will be used for further formatting  (e.g. "/v2/store/order/{orderId}")
+        :param uri_params: URL path parameters. Must match to parameters names specified in "raw_path"
+        :param kwargs: Optional arguments that are applicable
+            for appropriate request of "requests" library. (e.g. "auth", "headers", "cookies", etc.)
+        """
+        self.__uri = URI(base_url, base_path, raw_path, **uri_params)
+        self.response = requests.request(method, self.__uri.full, **kwargs)
+
+        if not IS_DISABLED:
+            RequestSchemaHandler(
+                self.__uri, method, self.response, kwargs
+            ).write_schema()
