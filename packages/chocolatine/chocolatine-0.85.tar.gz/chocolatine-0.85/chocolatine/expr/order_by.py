16ac2fcf1644c1542f69d0089552d6af3f02223a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:05:11 +0000

    Add package chocolatine/0.85/chocolatine-0.85.tar.gz

---
+++ packages/chocolatine/chocolatine-0.85.tar.gz/chocolatine-0.85/chocolatine/expr/order_by.py
@@ -0,0 +1,26 @@
+from typing import Iterable
+from typeguard import typechecked
+
+from .select import Select
+from .col import Col
+from .choc_expr import ChocExpr
+
+
+@typechecked
+class OrderBy(ChocExpr):
+    """ OrderBy expression """
+    def __init__(
+            self,
+            select: Select,
+            compact: bool = True
+    ) -> None:
+        self._select = select
+        super().__init__("ORDER BY {$(cols).ordering_label}", compact=compact)
+
+    @property
+    def cols(self) -> Iterable[Col]:
+        return [col for col in self._select.cols if col.ordering_label]
+
+    @property
+    def buildable(self) -> bool:
+        return len(self.cols) > 0
