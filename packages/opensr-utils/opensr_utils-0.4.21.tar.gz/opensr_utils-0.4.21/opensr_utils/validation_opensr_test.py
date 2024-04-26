790f5ce75e2c3b55fa5089c9f946022b8a59eed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:05:03 +0000

    Add package opensr-utils/0.4.21/opensr_utils-0.4.21.tar.gz

---
+++ packages/opensr-utils/opensr_utils-0.4.21.tar.gz/opensr_utils-0.4.21/opensr_utils/validation_opensr_test.py
@@ -0,0 +1,55 @@
+import torch
+import opensr_test
+
+def compute_metrics(lr,sr):
+    try:
+        lr = lr.detach()
+    except:
+        pass
+    try:
+        sr = sr.detach()
+    except:
+        pass
+
+    print("LR SHAPE:",lr.shape)
+    print("SR SHAPE:",sr.shape)
+
+    if len(sr.shape)>3:
+        sr = sr.squeeze(0)
+    if len(lr.shape)>3:
+        lr = lr.squeeze(0)
+
+    if sr.shape[-1]<sr.shape[0]:
+        sr = sr.permute(1,2,0)
+    if lr.shape[-1]<lr.shape[0]:
+        lr = lr.permute(1,2,0)
+
+    print("LR SHAPE:",lr.shape)
+    print("SR SHAPE:",sr.shape)
+    
+
+    metrics = opensr_test.Metrics()
+    m = metrics.compute(lr=lr, sr=sr, hr=sr)
+    return(m)
+
+def append_dicts(original_dict, new_dict):
+    """
+    Append values from keys of a new dictionary to the original dictionary.
+
+    Args:
+        original_dict (dict): The original dictionary to which values will be appended.
+        new_dict (dict): The new dictionary containing values to be appended.
+
+    Returns:
+        dict: The original dictionary with values appended.
+    """
+    for key, value in new_dict.items():
+        if key in original_dict:
+            if isinstance(original_dict[key], list):
+                original_dict[key].append(value)
+            else:
+                original_dict[key] = [original_dict[key], value]
+        else:
+            original_dict[key] = value if isinstance(value, list) else [value]
+    return original_dict
+
