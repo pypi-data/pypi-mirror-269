5ec512bdc62e58df39a194f9560def012124a827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:05:14 +0000

    Add package beacontower-btpy/0.1.6/beacontower_btpy-0.1.6.tar.gz

---
+++ packages/beacontower-btpy/beacontower_btpy-0.1.6.tar.gz/beacontower_btpy-0.1.6/btpy/suffix.py
@@ -0,0 +1,72 @@
+import os
+import random
+import string
+import json
+
+from btpy import application, environment, misc
+
+logger = misc.setup_logging()
+
+
+def generate(prefix=""):
+    suffix = "".join(
+        random.choice(string.ascii_lowercase + string.digits) for _ in range(4)
+    )
+    return {f"{prefix}suffix": suffix}
+
+
+def write(data, filename="suffix.json"):
+    """
+    Write suffix to file. If file exists, ask user if they want to overwrite.
+    """
+    try:
+        existing_suffix = get_data(filename, in_parent_directories=False)
+        key = os.path.basename(filename).split(".")[0]
+        new_suffix = input(
+            f"Suffix already exists. ({existing_suffix[key]}) Do you want to generate a new one? (N/y): "
+        )
+        if new_suffix == "y":
+            misc.write_json(filename, data)
+    except FileNotFoundError:
+        logger.info("Suffix file not found. Generating a new one.")
+        misc.write_json(filename, data)
+
+
+def get_data(filename="suffix.json", in_parent_directories=True):
+    path = (
+        misc.find_file_in_parent_directories(filename)
+        if in_parent_directories
+        else filename
+    )
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def write_global_suffix(suffix=None):
+    data = (
+        generate(prefix="global_")
+        if not suffix
+        else {"global_suffix": suffix["suffix"]}
+    )
+    app_dir = application.get_directory()
+    write(data, filename=os.path.join(app_dir, "global_suffix.json"))
+
+
+def get_global_suffix():
+    app_dir = application.get_directory()
+    return get_data(filename=os.path.join(app_dir, "global_suffix.json"))
+
+
+def write_environment_suffix(suffix=None):
+    data = (
+        generate(prefix="environment_")
+        if not suffix
+        else {"environment_suffix": suffix["suffix"]}
+    )
+    env_dir = environment.get_directory()
+    write(data, filename=os.path.join(env_dir, "environment_suffix.json"))
+
+
+def get_environment_suffix():
+    env_dir = environment.get_directory()
+    return get_data(filename=os.path.join(env_dir, "environment_suffix.json"))
