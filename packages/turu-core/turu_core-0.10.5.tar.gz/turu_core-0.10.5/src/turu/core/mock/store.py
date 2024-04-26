ccc732e765d8d56f79e208d1ddc368ddf7b1c004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:52 +0000

    Add package turu-core/0.10.5/turu_core-0.10.5.tar.gz

---
+++ packages/turu-core/turu_core-0.10.5.tar.gz/turu_core-0.10.5/src/turu/core/mock/store.py
@@ -0,0 +1,55 @@
+from typing import Any, List, Optional, Sequence, Tuple, Type, Union, overload
+
+from turu.core.cursor import GenericRowType
+from turu.core.mock.exception import (
+    TuruMockResponseTypeMismatchError,
+    TuruMockStoreDataNotFoundError,
+)
+
+
+class TuruMockStore:
+    def __init__(self):
+        self._data: List[Tuple[Optional[Type], Union[Sequence, None, Exception]]] = []
+        self._counter = 0
+
+    @overload
+    def inject_response(
+        self,
+        row_type: None,
+        response: Union[GenericRowType, Sequence[GenericRowType], None, Exception],
+    ): ...
+
+    @overload
+    def inject_response(
+        self,
+        row_type: Type[GenericRowType],
+        response: Union[GenericRowType, Sequence[GenericRowType], Exception],
+    ): ...
+
+    def inject_response(self, row_type, response):
+        if row_type is not None and isinstance(response, row_type):
+            response = (response,)
+
+        self._data.append((row_type, response))
+
+    def provide_response(
+        self,
+        row_type: Optional[Type],
+    ) -> "Optional[Sequence[Any]]":
+        self._counter += 1
+
+        if len(self._data) == 0:
+            raise TuruMockStoreDataNotFoundError(self._counter)
+
+        _row_type, _response = self._data.pop(0)
+
+        if _row_type is not row_type and not (
+            row_type.__module__ == _row_type.__module__
+            and row_type.__name__ == _row_type.__name__  # type: ignore
+        ):
+            raise TuruMockResponseTypeMismatchError(row_type, _row_type, self._counter)
+
+        if isinstance(_response, Exception):
+            raise _response
+
+        return _response
