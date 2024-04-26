209c9912246a3f53863c64fd3be9d126adb8a867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:05:15 +0000

    Add package pybind11-numpy-example/0.0.12/pybind11_numpy_example-0.0.12-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/pybind11-numpy-example/pybind11_numpy_example-0.0.12-cp311-cp311-macosx_10_9_x86_64.whl/lib/cmake/pybind11_numpy_example/pybind11_numpy_example_config-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pybind11_numpy_example::pybind11_numpy_example" for configuration "Release"
+set_property(TARGET pybind11_numpy_example::pybind11_numpy_example APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pybind11_numpy_example::pybind11_numpy_example PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libpybind11_numpy_example.a"
+  )
+
+list(APPEND _cmake_import_check_targets pybind11_numpy_example::pybind11_numpy_example )
+list(APPEND _cmake_import_check_files_for_pybind11_numpy_example::pybind11_numpy_example "${_IMPORT_PREFIX}/lib/libpybind11_numpy_example.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
