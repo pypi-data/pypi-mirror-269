044abfb710ca294d5eefb8dd4f83d4d394e2b17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:05:49 +0000

    Add package sequrekey-pkg/0.0.4/sequrekey_pkg-0.0.4.tar.gz

---
+++ packages/sequrekey-pkg/sequrekey_pkg-0.0.4.tar.gz/sequrekey_pkg-0.0.4/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh: 
+    description = fh.read() 
+  
+setuptools.setup( 
+    name="sequrekey_pkg", 
+    version="0.0.4", 
+    author="7399", 
+    author_email="", 
+    packages=["sequrekey_pkg"], 
+    description="A sample SequreKey package", 
+    long_description=description, 
+    include_package_data=True,
+    long_description_content_type="text/markdown", 
+    url="", 
+    license='MIT', 
+    python_requires='>=3.12', 
+    install_requires=[] 
+) 
