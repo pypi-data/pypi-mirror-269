227f051dceeb65728ed4cadc45874953bf4d2273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:47 +0000

    Add package journeylib/0.0.1/journeylib-0.0.1.tar.gz

---
+++ packages/journeylib/journeylib-0.0.1.tar.gz/journeylib-0.0.1/journeylib/funciones.py
@@ -0,0 +1,19 @@
+#Imports necesarios para las funciones de la libreria
+
+#Metodo de prueba para el despliege de la libreria
+def hola_journey():
+    print('Holitaa, esta es una librería diseñada y publicada por JourneyGen.')
+
+
+
+#Implementacion de las funciones necesarias para la libreria
+
+"""
+METODO INSERTAR HISTORICO
+Descripcion: Insertamos en un fichero predeterminado los mensajes de la IA para poder guardarlo en el historico a posteriori
+"""
+
+"""
+METODO DEVOLVER HISTORICO
+Descripcion: Devolvemos el historico asociado a un usuario
+"""
