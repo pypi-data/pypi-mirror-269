d971f1e1b596e9fb93a0031ac91757e49a6113fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:05:25 +0000

    Add package annif/1.1.0/annif-1.1.0.tar.gz

---
+++ packages/annif/annif-1.1.0.tar.gz/annif-1.1.0/annif/analyzer/simplemma.py
@@ -0,0 +1,18 @@
+"""Simplemma analyzer for Annif, based on simplemma lemmatizer."""
+
+from __future__ import annotations
+
+import simplemma
+
+from . import analyzer
+
+
+class SimplemmaAnalyzer(analyzer.Analyzer):
+    name = "simplemma"
+
+    def __init__(self, param: str, **kwargs) -> None:
+        self.lang = param
+        super().__init__(**kwargs)
+
+    def _normalize_word(self, word: str) -> str:
+        return simplemma.lemmatize(word, lang=self.lang)
