830880cf2340600bac09c4533202f66778b2e404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:05:07 +0000

    Add package algo-fun-bfs-traversal-order/0.2.0/algo_fun_bfs_traversal_order-0.2.0.tar.gz

---
+++ packages/algo-fun-bfs-traversal-order/algo_fun_bfs_traversal_order-0.2.0.tar.gz/algo_fun_bfs_traversal_order-0.2.0/algo_fun/get_bfs_traversal_order.py
@@ -0,0 +1,36 @@
+#!/!usr/bin/env python
+__program__ = "get_bfs_traversal_order.py"
+__description__ = "Get the bfs traversal order of a given graph"
+__date__ = "25/04/2024"
+__author__ = "Christophe Lagaillarde"
+__version__ = "2.0"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2024 (c) Christophe Lagaillarde"
+
+def get_bfs_traversal_order(graph: dict[str, list[str]]) -> list: 
+	queue: list[str] = []
+	traversal_order: list[str] = []
+	node: str = next(iter(graph))
+	queue.append(node)
+
+	while queue:
+		traversal_order.append(queue[0])
+		queue.pop(0)
+		queue.extend(graph.get(node, []))
+		
+		if queue:
+			node = queue[0] 
+
+	return traversal_order
+
+if __name__ == '__main__':
+
+	import sys
+
+	user_input_graph: dict[list] = {}
+
+	for arg in sys.argv[1:]:
+		key, values = arg.split(':')
+		user_input_graph[key] = values.split(',')
+	
+	print(get_bfs_traversal_order(user_input_graph))
