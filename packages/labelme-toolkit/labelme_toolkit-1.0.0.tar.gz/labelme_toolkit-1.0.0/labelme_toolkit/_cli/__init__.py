c6afc866723714d6c27353098a35aecd9e564c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:53 +0000

    Add package labelme-toolkit/1.0.0/labelme_toolkit-1.0.0.tar.gz

---
+++ packages/labelme-toolkit/labelme_toolkit-1.0.0.tar.gz/labelme_toolkit-1.0.0/labelme_toolkit/_cli/__init__.py
@@ -0,0 +1,21 @@
+import sys
+
+import click
+from loguru import logger
+
+from labelme_toolkit import __version__
+
+
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(__version__)
+def cli():
+    logger.remove()
+    logger.add(
+        sys.stderr, level="INFO", colorize=True, format="<level>{message}</level>"
+    )
+
+
+from labelme_toolkit._cli import _extract_image  # noqa
+from labelme_toolkit._cli import _json_to_mask  # noqa
+from labelme_toolkit._cli import _json_to_visualization  # noqa
+from labelme_toolkit._cli import _list_labels  # noqa
---
+++ packages/labelme-toolkit/labelme_toolkit-1.0.0.tar.gz/labelme_toolkit-1.0.0/labelme_toolkit/_cli/_list_labels_test.py
@@ -0,0 +1,19 @@
+import subprocess
+
+from labelme_toolkit._testing import small_dataset  # noqa
+
+
+def test_list_labels(small_dataset):  # noqa
+    output = subprocess.check_output(
+        ["labelmetk", "list-labels", small_dataset]
+    ).decode()
+
+    assert [line.strip() for line in output.splitlines()] == [
+        "__ignore__",
+        "bottle",
+        "bus",
+        "car",
+        "chair",
+        "person",
+        "sofa",
+    ]
